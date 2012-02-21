cf11fb383d2a3e17eb19597ba1a14d818612f692
Andrew Dunstan <andrew@dunslane.net>
2012-02-21 11:03:51 -0500

    Fix typo, noticed by Will Crawford.
    (cherry-picked from 6b044cb810460993ad9e458a0ee8fcc9fde5a350)
    
    Author:             Álvaro Hernández Tortosa <aht@8kdata.com>
    Date:               Tue Jun 14 00:36:00 UTC 2016

--- src/backend/utils/adt/json.c
+++ src/backend/utils/adt/json.c
@@ -89,7 +89,7 @@ TYPCATEGORY TypeCategoryJsonGPDB(Oid type);
 /* fake type category for JSON so we can distinguish it in datum_to_json */
 #define TYPCATEGORY_JSON 'j'
 /* letters appearing in numeric output that aren't valid in a JSON number */
-#define NON_NUMERIC_LETTER "NnAnIiFfTtYy"
+#define NON_NUMERIC_LETTER "NnAaIiFfTtYy"
 
 /*
  * Input.
