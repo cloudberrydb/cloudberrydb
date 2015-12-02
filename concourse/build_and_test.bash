c0f3aa7a07c6d2bd1353f66858ea636e47f95a57
Craig Chasseur <spoonboy42@gmail.com>
2015-12-01 17:01:48 -0800

    Detect number of CPUs to use for parallel build and test

--- concourse/build_and_test.bash
+++ concourse/build_and_test.bash
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-set -u -e
+set -u -e -x
 
 main() {
         local build_type
         build_type=$1
         mkdir build
         cd build
-        cmake -D CMAKE_BUILD_TYPE=${build_type} ../gpos_src
-        make -j4
-        ctest -j4
+#        cmake -D CMAKE_BUILD_TYPE=${build_type} ../gpos_src
+        make -j$(nproc_wrapper)
+        ctest -j$(nproc_wrapper)
+}
+
+nproc_wrapper() {
+        if ! type nproc &> /dev/null; then
+                echo 4
+        else
+                command nproc
+        fi
 }
 
 main "$@"
