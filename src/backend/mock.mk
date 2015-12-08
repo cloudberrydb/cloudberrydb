0e27dff3e0a94ef7177683c09b3adb6c1bcfefae
Heikki Linnakangas <hlinnakangas@pivotal.io>
2015-12-08 19:27:35 +0200

    Simplify mock test Makefiles.
    There are a bunch of backend files that are never mocked, but always linked
    into test program as is. List those in Makefile.mock, so that every Makefile
    doesn't need to list them. Makes the makefiles simpler, and you need less
    boilerplate code when adding a new test program.
    
    Not all of these backend files were listed in the _REAL_OBJS variable of
    every test, but in those tests that they were not, they were also not used,
    so it doesn't matter if we use the real or mock version.

--- src/backend/mock.mk
+++ src/backend/mock.mk
@@ -18,42 +18,111 @@ override CPPFLAGS+= -I$(top_srcdir)/src/backend/libpq \
 # postgres in src/backend/Makefile doesn't need this and -pthread.
 MOCK_LIBS := -ldl $(filter-out -lpgport -ledit, $(LIBS))
 
-# We are unable to mock them up at the moment.  Real objects should contain them.
-EXCL_OBJS=src/backend/bootstrap/bootparse.o \
-			src/backend/catalog/caql/gram.o \
-			src/backend/parser/gram.o \
-			src/backend/nodes/readfast.o \
-			src/backend/nodes/outfast.o \
-			src/backend/regex/regcomp.o \
-			src/backend/regex/regexec.o \
-			src/backend/utils/adt/like.o \
-			src/backend/utils/misc/guc.o \
-			src/backend/utils/misc/guc_gp.o \
-			src/backend/replication/repl_gram.o \
-			src/timezone/%.o \
-			src/backend/main/%.o \
-			src/backend/gpopt/%.o \
-			src/backend/gpopt/config/%.o \
-			src/backend/gpopt/relcache/%.o \
-			src/backend/gpopt/translate/%.o \
-			src/backend/gpopt/utils/%.o
+# This is a basic set of backend object files that every test program is linked
+# with by default.
+BACKEND_REAL_OBJS=\
+	src/backend/access/common/heaptuple.o \
+	src/backend/access/heap/tuptoaster.o \
+	src/backend/access/hash/hashfunc.o \
+	src/backend/access/transam/filerepdefs.o \
+	src/backend/access/transam/transam.o \
+	src/backend/cdb/cdbgang.o \
+	src/backend/lib/stringinfo.o \
+	src/backend/nodes/bitmapset.o \
+	src/backend/nodes/equalfuncs.o \
+	src/backend/nodes/list.o \
+	src/backend/nodes/makefuncs.o \
+	src/backend/nodes/value.o \
+	src/backend/storage/file/compress_nothing.o \
+	src/backend/storage/page/itemptr.o \
+	src/backend/utils/adt/arrayfuncs.o \
+	src/backend/utils/adt/arrayutils.o \
+	src/backend/utils/adt/datetime.o \
+	src/backend/utils/adt/datum.o \
+	src/backend/utils/fmgr/fmgr.o \
+	src/backend/utils/hash/hashfn.o \
+	src/backend/utils/init/globals.o \
+	src/backend/bootstrap/bootparse.o \
+	src/backend/catalog/caql/catquery.o \
+	src/backend/catalog/caql/gram.o \
+	src/backend/catalog/core/catcore.o \
+	src/backend/catalog/core/catcoretable.o \
+	src/backend/parser/gram.o \
+	src/backend/parser/kwlookup.o \
+	src/backend/parser/parse_type.o \
+	src/backend/parser/scansup.o \
+	src/backend/regex/regcomp.o \
+	src/backend/regex/regexec.o \
+	src/backend/utils/adt/like.o \
+	src/backend/utils/adt/varlena.o \
+	src/backend/utils/misc/atomic.o \
+	src/backend/utils/misc/bitstream.o \
+	src/backend/utils/misc/guc.o \
+	src/backend/utils/misc/guc_gp.o \
+	src/backend/utils/misc/size.o \
+	src/backend/utils/time/tqual.o \
+	src/backend/replication/repl_gram.o \
+	src/timezone/pgtz.o \
+	src/timezone/localtime.o \
+	src/timezone/strftime.o \
+	src/port/libpgport_srv.a
 
+# These files are not needed by any test program, so don't mock or link them.
+EXCL_OBJS=\
+	src/timezone/%.o \
+	src/backend/main/%.o \
+	src/backend/gpopt/%.o \
+	src/backend/gpopt/config/%.o \
+	src/backend/gpopt/relcache/%.o \
+	src/backend/gpopt/translate/%.o \
+	src/backend/gpopt/utils/%.o \
+
+# These files cannot be mocked, because they #include header files or other
+# C files that are not in the standard include path.
+EXCL_OBJS+=\
+	src/backend/bootstrap/bootparse.o \
+	src/backend/parser/gram.o \
+	src/backend/catalog/caql/gram.o \
+	src/backend/nodes/outfast.o \
+	src/backend/nodes/readfast.o \
+	src/backend/replication/repl_gram.o \
+	src/backend/regex/regcomp.o \
+	src/backend/regex/regexec.o \
+	src/backend/utils/adt/like.o \
+	src/backend/utils/misc/guc.o \
+	src/backend/utils/misc/guc_gp.o
+
+# Create a _mock.c version of every backend object file that's not listed in
+# EXCL_OBJS
 OBJFILES=$(top_srcdir)/src/backend/objfiles.txt
-ALL_OBJS=$(addprefix $(top_srcdir)/, \
+MOCK_OBJS=$(addprefix $(top_srcdir)/, \
 			$(filter-out $(EXCL_OBJS), \
 				$(shell test -f $(OBJFILES) && cat $(OBJFILES))))
 
 SUT_OBJ=$(top_srcdir)/$(subdir)/$(1).o
+
 # A function that generates TARGET_OBJS = test case, mock objects, real objects,
-# and cmockery.o. $(OBJFILES) contains %/objfiles.txt, because
-# src/backend/Makefile will create it with rule=objfiles.txt, which is not
-# expected in postgres rule.  It actually uses expand_subsys to obtain the .o
-# file list.  But here we don't include common.mk so just clear out
-# objfiles.txt from the list for $(TARGET_OBJS)
-TARGET_OBJS=$(shell echo $(1)_test.o $(CMOCKERY_OBJS) $($(1)_REAL_OBJS) \
-		$(patsubst $(top_srcdir)/src/%.o,$(MOCK_DIR)/%_mock.o,\
-			$(filter-out $(SUT_OBJ) $($(1)_REAL_OBJS) %/objfiles.txt, \
-				$(ALL_OBJS))))
+# and cmockery.o.
+TARGET_OBJS=$(1)_test.o $($(1)_OBJS) $(CMOCKERY_OBJS)
+
+# Add the non-mocked versions of requested files.
+TARGET_OBJS+=$($(1)_REAL_OBJS)
+
+# Add the standard set of backend .o files that have not been explicitly
+# mocked
+TARGET_OBJS+=$(filter-out $(SUT_OBJ) $($(1)_MOCK_OBJS), $(addprefix $(top_srcdir)/, $(BACKEND_REAL_OBJS)))
+
+# Add mock versions of other backend .o files
+#
+# $(OBJFILES) contains %/objfiles.txt, because src/backend/Makefile will
+# create it with rule=objfiles.txt, which is not expected in postgres rule.
+# It actually uses expand_subsys to obtain the .o file list.  But here we
+# don't include common.mk so just clear out objfiles.txt from the list for
+# $(TARGET_OBJS)
+TARGET_OBJS+=$(patsubst $(top_srcdir)/src/%.o,$(MOCK_DIR)/%_mock.o,\
+			$($(1)_MOCK_OBJS) \
+			$(filter-out $(SUT_OBJ) $(addprefix $(top_srcdir)/, $(BACKEND_REAL_OBJS)) $($(1)_REAL_OBJS) %/objfiles.txt, \
+				$(MOCK_OBJS)))
 
 # The test target depends on $(OBJFILES) which would update files including mocks.
 %.t: $(OBJFILES) $(CMOCKERY_OBJS) %_test.o mockup-phony
@@ -63,7 +132,7 @@ TARGET_OBJS=$(shell echo $(1)_test.o $(CMOCKERY_OBJS) $($(1)_REAL_OBJS) \
 # src/timezone before src/backend.  This is not the case when main build has finished,
 # but this makes sure a simple make works fine in this directory any time.
 # With PARTIAL_LINKING it will generate objfiles.txt
-$(OBJFILES): $(ALL_OBJS)
+$(OBJFILES): $(MOCK_OBJS)
 	$(MAKE) -C $(top_srcdir)/src
 	$(MAKE) PARTIAL_LINKING= -C $(top_srcdir)/src/backend objfiles.txt
 
@@ -72,4 +141,4 @@ mockup-phony: $(OBJFILES)
 	$(MAKE) mockup
 
 .PHONY:
-mockup: $(patsubst $(top_srcdir)/src/%.o,$(MOCK_DIR)/%_mock.o,$(ALL_OBJS))
+mockup: $(patsubst $(top_srcdir)/src/%.o,$(MOCK_DIR)/%_mock.o,$(MOCK_OBJS))
