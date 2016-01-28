a719e7514b88b0d2b4e862ae855bbce220d9f764
Dave Cramer <dcramer@pivotal.io>
2016-01-28 13:45:02 -0500

    move vagrant setup into debian directory

---
+++ vagrant/debian/vagrant-build.sh
@@ -0,0 +1,19 @@
+cd /gpdb
+./configure --enable-debug --with-python --with-java --with-libxml --prefix=/usr/local/gpdb
+
+make clean
+make -j4  
+sudo make install
+
+cd /gpdb/gpAux
+cp -rp gpdemo /home/vagrant/
+sudo chown -R vagrant:vagrant /home/vagrant/gpdemo
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+cd gpdemo
+source /usr/local/gpdb/greenplum_path.sh
+make cluster
+
+
+#cd /vagrant/src/pl/plspython/tests
+#make containers
+#sudo -u postgres make
---
+++ vagrant/debian/vagrant-setup.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+sudo apt-get -y install apt-transport-https
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo bash -c 'echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list'
+sudo apt-get update
+sudo apt-get install wget
+sudo apt-get -y install docker-engine build-essential libreadline6 \
+libreadline6-dev zlib1g-dev bison flex git-core libcurl4-openssl-dev \
+python-dev libxml2-dev pkg-config vim libbz2-dev  python-pip
+
+su vagrant -c "ssh-keygen -t rsa -f .ssh/id_rsa -q -N ''"
+
+pip install lockfile
+pip install paramiko
+pip install setuptools
+pip install epydoc
+pip install --pre psi
+
+sudo service docker start
+sudo useradd postgres
+sudo usermod -aG docker postgres
+sudo usermod -aG docker vagrant
