27e5a99e02a52367fe5fcdd2f9316c2b52c6856d
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-02-04 15:41:01 +0100

    Fix misplaced right paren bugs in pgstatfuncs.c
    This is a cherry-pick (with slight massage) of the commit referenced below
    which would come eventually in the merge but was trivial enough to fix now.
    
      commit b2d80147d5a3d0a5815c5b8e63a6fd9cef17f4a7
      Author: Kevin Grittner <kgrittn@postgresql.org>
      Date:   Fri Dec 27 15:41:46 2013 -0600
    
          Fix misplaced right paren bugs in pgstatfuncs.c.
    
          The bug would only show up if the C sockaddr structure contained
          zero in the first byte for a valid address; otherwise it would
          fail to fail, which is probably why it went unnoticed for so long.
    
          Patch submitted by Joel Jacobson after seeing an article by Andrey
          Karpov in which he reports finding this through static code
          analysis using PVS-Studio.  While I was at it I moved a definition
          of a local variable referenced in the buggy code to a more local
          context.
    
          Backpatch to all supported branches.

--- src/backend/utils/adt/pgstatfuncs.c
+++ src/backend/utils/adt/pgstatfuncs.c
@@ -448,7 +448,6 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 		bool		nulls[13];
 		HeapTuple	tuple;
 		PgBackendStatus *beentry;
-		SockAddr	zero_clientaddr;
 
 		MemSet(values, 0, sizeof(values));
 		MemSet(nulls, 0, sizeof(nulls));
@@ -489,6 +488,7 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 		/* Values only available to same user or superuser */
 		if (superuser() || beentry->st_userid == GetUserId())
 		{
+			SockAddr	zero_clientaddr;
 			bool		waiting;
 
 			if (*(beentry->st_activity) == '\0')
@@ -521,7 +521,7 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 			/* A zeroed client addr means we don't know */
 			memset(&zero_clientaddr, 0, sizeof(zero_clientaddr));
 			if (memcmp(&(beentry->st_clientaddr), &zero_clientaddr,
-					   sizeof(zero_clientaddr) == 0))
+					   sizeof(zero_clientaddr)) == 0)
 			{
 				nulls[9] = true;
 				nulls[10] = true;
@@ -804,7 +804,7 @@ pg_stat_get_backend_client_addr(PG_FUNCTION_ARGS)
 	/* A zeroed client addr means we don't know */
 	memset(&zero_clientaddr, 0, sizeof(zero_clientaddr));
 	if (memcmp(&(beentry->st_clientaddr), &zero_clientaddr,
-			   sizeof(zero_clientaddr) == 0))
+			   sizeof(zero_clientaddr)) == 0)
 		PG_RETURN_NULL();
 
 	switch (beentry->st_clientaddr.addr.ss_family)
@@ -850,7 +850,7 @@ pg_stat_get_backend_client_port(PG_FUNCTION_ARGS)
 	/* A zeroed client addr means we don't know */
 	memset(&zero_clientaddr, 0, sizeof(zero_clientaddr));
 	if (memcmp(&(beentry->st_clientaddr), &zero_clientaddr,
-			   sizeof(zero_clientaddr) == 0))
+			   sizeof(zero_clientaddr)) == 0)
 		PG_RETURN_NULL();
 
 	switch (beentry->st_clientaddr.addr.ss_family)
