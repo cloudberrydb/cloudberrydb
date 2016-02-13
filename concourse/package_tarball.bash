4fca7a328a8ca0788d2a753fea74b90214bec642
C.J. Jameson and Tom Meyer <pair+cjameson+tmeyer@pivotal.io>
2016-02-12 17:15:21 -0800

    Refactor packaging tasks for orca job
    - Run bash script instead of /bin/bash
    - Use params to specify tarball path and path to source
    - Simplify bash script

--- concourse/package_tarball.bash
+++ concourse/package_tarball.bash
@@ -3,13 +3,7 @@
 set -u -e -x
 
 main() {
-  local build_task_name
-  local output_name
-  build_task_name="$1"
-  output_name="$2"
-  cd "$build_task_name"
-  pushd install
-  tar czf "../../${output_name}" .
+  tar czf "$dst_tarball" -C "$src_root" .
 }
 
 main "$@"
