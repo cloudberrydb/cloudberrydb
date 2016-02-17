f578b4d5b3364c6464e0918336d324af47704f3b
Ashwin Agrawal <aagrawal@pivotal.io>
2016-02-17 15:50:31 -0800

    Make UAO threshold tests cluster size independent.
    Earlier tests only worked if was run with cluster having 2 segments, if
    number of segemnts changed the output differed and hence will cause
    test failure, which shouldn't be the case.

--- src/test/regress/sql/uao_compaction/threshold.sql
+++ src/test/regress/sql/uao_compaction/threshold.sql
@@ -1,9 +1,6 @@
 -- @Description Tests the basic behavior of (lazy) vacuum w.r.t. to the threshold guc.
--- The output depends on the number of segments as the skip decision and the 
--- notify is done on the segments.
-
-CREATE TABLE uao_threshold (a INT, b INT, c CHAR(128)) WITH (appendonly=true);
-CREATE INDEX uao_threshold_index ON uao_threshold(b);
+CREATE TABLE uao_threshold (a INT, b INT, c CHAR(128)) WITH (appendonly=true) distributed by (b);
+CREATE INDEX uao_threshold_index ON uao_threshold(a);
 INSERT INTO uao_threshold SELECT i as a, 1 as b, 'hello world' as c FROM generate_series(1, 100) AS i;
 
 \set QUIET off
@@ -28,7 +25,14 @@ SELECT segno, tupcount, state FROM gp_toolkit.__gp_aoseg_name('uao_threshold');
 -- 3 invisible tuples, no vacuum
 VACUUM uao_threshold;
 SELECT segno, tupcount, state FROM gp_toolkit.__gp_aoseg_name('uao_threshold');
+-- set guc to 2%, so vacuum should trigger
 SET gp_appendonly_compaction_threshold=2;
--- 3 invisible tuples, no vacuum
+-- 3 invisible tuples, do vacuum
+VACUUM uao_threshold;
+SELECT segno, tupcount, state FROM gp_toolkit.__gp_aoseg_name('uao_threshold');
+
+INSERT INTO uao_threshold SELECT i as a, i as b, 'hello world' as c FROM generate_series(100, 200) AS i;
+DELETE FROM uao_threshold WHERE a > 100 and a < 175;
+SELECT segno, tupcount, state FROM gp_toolkit.__gp_aoseg_name('uao_threshold');
 VACUUM uao_threshold;
 SELECT segno, tupcount, state FROM gp_toolkit.__gp_aoseg_name('uao_threshold');
--- src/test/regress/sql/uaocs_compaction/threshold.sql
+++ src/test/regress/sql/uaocs_compaction/threshold.sql
@@ -1,9 +1,6 @@
 -- @Description Tests the basic behavior of (lazy) vacuum w.r.t. to the threshold guc.
--- The output depends on the number of segments as the skip decision and the 
--- notify is done on the segments.
-
-CREATE TABLE uaocs_threshold (a INT, b INT, c CHAR(128)) WITH (appendonly=true, orientation=column);
-CREATE INDEX uaocs_threshold_index ON uaocs_threshold(b);
+CREATE TABLE uaocs_threshold (a INT, b INT, c CHAR(128)) WITH (appendonly=true, orientation=column) distributed by (b);
+CREATE INDEX uaocs_threshold_index ON uaocs_threshold(a);
 INSERT INTO uaocs_threshold SELECT i as a, 1 as b, 'hello world' as c FROM generate_series(1, 100) AS i;
 
 \set QUIET off
@@ -29,5 +26,11 @@ VACUUM uaocs_threshold;
 SELECT DISTINCT segno, tupcount, state FROM gp_toolkit.__gp_aocsseg_name('uaocs_threshold');
 SET gp_appendonly_compaction_threshold=2;
 -- 3 invisible tuples, no vacuum
+VACUUM uaocs_threshold;
+SELECT DISTINCT segno, tupcount, state FROM gp_toolkit.__gp_aocsseg_name('uaocs_threshold');
+
+INSERT INTO uaocs_threshold SELECT i as a, i as b, 'hello world' as c FROM generate_series(100, 200) AS i;
+DELETE FROM uaocs_threshold WHERE a > 100 and a < 175;
+SELECT DISTINCT segno, tupcount, state FROM gp_toolkit.__gp_aocsseg_name('uaocs_threshold');
 VACUUM uaocs_threshold;
 SELECT DISTINCT segno, tupcount, state FROM gp_toolkit.__gp_aocsseg_name('uaocs_threshold');
