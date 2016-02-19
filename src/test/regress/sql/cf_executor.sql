d35714a9c2d702f232e409bcd3b585b53fd8746b
Foyzur Rahman <foyzur.rahman@gopivotal.com>
2016-02-19 14:30:56 -0800

    Porting executor, olap window and olap window error handling tests from legacy test suites.

---
+++ src/test/regress/sql/cf_executor.sql
@@ -0,0 +1,31 @@
+--start_ignore
+drop table if exists cf_executor_test;
+create table cf_executor_test (a integer);
+insert into cf_executor_test select a from generate_series(1,100) a;
+set log_min_messages=debug5;
+--end_ignore
+
+set debug_print_slice_table=on;
+select count(*) from cf_executor_test;
+
+--FOR UPDATE/FOR SHARE
+select * from cf_executor_test order by a limit 1 for update;
+select * from cf_executor_test order by a limit 1 for share;
+
+--returning clause
+insert into cf_executor_test values (1) returning *;
+
+--start_ignore
+drop table cf_executor_test;
+create table cf_executor_test (a integer);
+insert into cf_executor_test select a from generate_series(1,5) a;
+--end_ignore
+
+select * from cf_executor_test limit null;
+select * from cf_executor_test limit 0;
+
+--start_ignore
+reset log_min_messages;
+reset debug_print_slice_table;
+drop table cf_executor_test;
+--end_ignore
