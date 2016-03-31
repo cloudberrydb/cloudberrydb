9a09b638d9d7eb5f774a52f05a0a3eceaf51b128
Asim R P <apraveen@pivotal.io>
2016-03-31 11:36:44 -0700

    Remove incorrect assumption that reindex cannot happen within a transaction block.
    ReindexRelationList is a GPDB specific function that wraps each
    reindex table operation within a separate transaction.  If the table
    being reindexed is a partitioned table, each partition is reindexed in
    a separate transaction.  Now that we have removed the assertion,
    refactor this function to commit and start top level transcation,
    rather than leaving this burden on callers.
    
    Fixes #567.

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -1639,19 +1639,24 @@ ReindexIndex(ReindexStmt *stmt)
 }
 
 /*
- * Perform REINDEX on each relation of the relids list.  The caller should
- * close the transaction before calling this since it opens and closes a
- * transaction per relation.  This is designed for QD/utility, and is not
- * useful for QE.
+ * Perform REINDEX on each relation of the relids list.  The function
+ * opens and closes a transaction per relation.  This is designed for
+ * QD/utility, and is not useful for QE.
  */
 static void
 ReindexRelationList(List *relids)
 {
 	ListCell   *lc;
 
-	/* The caller should have closed transaction (see function comments). */
-	Assert(!IsTransactionOrTransactionBlock());
 	Assert(Gp_role != GP_ROLE_EXECUTE);
+
+	/*
+	 * Commit ongoing transaction so that we can start a new
+	 * transaction per relation.
+	 */
+	CommitTransactionCommand();
+
+	SIMPLE_FAULT_INJECTOR(ReindexDB);
 
 	foreach (lc, relids)
 	{
@@ -1697,6 +1702,13 @@ ReindexRelationList(List *relids)
 
 		CommitTransactionCommand();
 	}
+
+	/*
+	 * We committed the transaction above, so start a new one before
+	 * returning.
+	 */
+	setupRegularDtxContext();
+	StartTransactionCommand();
 }
 
 /*
@@ -1797,13 +1809,8 @@ ReindexTable(ReindexStmt *stmt)
 		caql_endscan(pcqCtx);
 	}
 
-	/* Now reindex each rel in a separate transaction */
-	CommitTransactionCommand();
-
 	ReindexRelationList(relids);
 
-	setupRegularDtxContext();
-	StartTransactionCommand();
 	MemoryContextDelete(private_context);
 }
 
@@ -1919,15 +1926,7 @@ ReindexDatabase(ReindexStmt *stmt)
 	}
 	caql_endscan(pcqCtx);
 
-	/* Now reindex each rel in a separate transaction */
-	CommitTransactionCommand();
-
-	SIMPLE_FAULT_INJECTOR(ReindexDB);
-
 	ReindexRelationList(relids);
-
-	setupRegularDtxContext();
-	StartTransactionCommand();
 
 	MemoryContextDelete(private_context);
 }
