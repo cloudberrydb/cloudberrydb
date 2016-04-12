b62dd655a3900fd564481b214554231a5dc64620
Nikhil Kak and Stephen Wu <nkak+stwu@pivotal.io>
2016-04-12 14:17:21 -0700

    Improve queries for unique index violation
    - optimize query to find all unique indexes
    - omit rows with null values when searching for violated indexes

--- gpMgmt/bin/gpcheckcat_modules/gpcheckcat_unique_index_violation_check.py
+++ gpMgmt/bin/gpcheckcat_modules/gpcheckcat_unique_index_violation_check.py
@@ -2,23 +2,26 @@
 
 class UniqueIndexViolationCheck:
     unique_indexes_query = """
-        select pg_class.oid as tableoid, indexname, tablename, substring(indexdef from '%#(#"%#"#)%' for '#') as columnnames
-        from pg_indexes
-        join pg_class on relname = tablename
-        where indexdef like 'CREATE UNIQUE INDEX%'
-        and schemaname='pg_catalog'
-        and relkind = 'r';
+        select table_class.oid as tableoid, index_class.relname as indexname, table_class.relname as tablename, substring(pg_get_indexdef(index_class.oid) from '%#(#"%#"#)%' for '#') as columnnames
+        from pg_index
+        join pg_class table_class on table_class.oid = pg_index.indrelid
+        join pg_class index_class on index_class.oid = pg_index.indexrelid
+        left join pg_namespace on pg_namespace.oid = table_class.relnamespace
+        left join pg_tablespace on pg_tablespace.oid = index_class.reltablespace
+        where table_class.relkind = 'r'::"char" and index_class.relkind = 'i'::"char" and pg_namespace.nspname = 'pg_catalog' and pg_index.indisunique='t';
     """
 
     def __init__(self):
         self.violated_index_query = """
             (select gp_segment_id, %s, count(*)
             from gp_dist_random('%s')
+            where (%s) is not null
             group by gp_segment_id, %s
             having count(*) > 1)
             union
             (select gp_segment_id, %s, count(*)
             from %s
+            where (%s) is not null
             group by gp_segment_id, %s
             having count(*) > 1)
         """
@@ -40,5 +43,5 @@ class UniqueIndexViolationCheck:
 
     def get_violated_index_query(self, table_name, column_names):
         return self.violated_index_query % (
-            column_names, table_name, column_names, column_names, table_name, column_names
+            column_names, table_name, column_names, column_names, column_names, table_name, column_names, column_names
         )
