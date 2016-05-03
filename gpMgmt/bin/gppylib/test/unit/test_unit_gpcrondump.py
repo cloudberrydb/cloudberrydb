tangp3/gpdb

Add ddboost storage unit option into gpcrondump, gpdbrestore and gpmfr
When user dumps database to Data Domain Boost server, storage
unit and backup directory must be already created and specified,
previously, we hard coded the storage unit to "GPDB" and user
had no option to use others.

This commit adds --ddboost-storage-unit option, which allows
user to dynamically specify storage unit for dump and restore.

This commits allows user to have storage unit information
statically saved into configure file in their cluster host.

This commit added storage unit option into gpmfr for replicating
and recovering dump copies, in which case it uses identical storage
unit and backup directory between primary and secondary DDBoost server.

--ddboost-storage-unit option takes higher priority than using
statically configured storage unit.

Authors:
Pengcheng Tang, Marbin Tan, Nikhil Kak
Lawrence Hamel, Stephen Wu, Chris Hajas, Chumki Roy

gpMgmt/bin/gppylib/test/unit/test_unit_gpcrondump.py
@@ -66,6 +66,7 @@ class GpCronDumpTestCase(unittest.TestCase):
             ## Enterprise init
             self.incremental = False
             self.ddboost = False
+            self.ddboost_storage_unit = None
             self.ddboost_hosts = None
             self.ddboost_user = None
             self.ddboost_config_remove = False
@@ -659,6 +660,17 @@ class GpCronDumpTestCase(unittest.TestCase):
         options.netbackup_policy = "test_policy"
         options.netbackup_schedule = "test_schedule"
         with self.assertRaisesRegexp(Exception, '--ddboost is not supported with NetBackup'):
+            GpCronDump(options, None)
+
+    @patch('gpcrondump.GpCronDump._get_master_port')
+    def test_options30(self, mock):
+        """
+        --ddboost-storage-unit option must come with --ddboost option
+        """
+        options = GpCronDumpTestCase.Options()
+        options.dump_databases = ['bkdb']
+        options.ddboost_storage_unit = "GPDB"
+        with self.assertRaisesRegexp(Exception, 'Must specify --ddboost option together with the --ddboost-storage-unit'):
             GpCronDump(options, None)
 
     @patch('gpcrondump.GpCronDump._get_master_port')

cloudberrydb/cloudberrydb

gpMgmt/bin/gppylib/test/unit/test_unit_gpcrondump.py
@@ -66,6 +66,7 @@ class GpCronDumpTestCase(unittest.TestCase):
             ## Enterprise init
             self.incremental = False
             self.ddboost = False
+            self.ddboost_storage_unit = None
             self.ddboost_hosts = None
             self.ddboost_user = None
             self.ddboost_config_remove = False
@@ -659,6 +660,17 @@ class GpCronDumpTestCase(unittest.TestCase):
         options.netbackup_policy = "test_policy"
         options.netbackup_schedule = "test_schedule"
         with self.assertRaisesRegexp(Exception, '--ddboost is not supported with NetBackup'):
+            GpCronDump(options, None)
+
+    @patch('gpcrondump.GpCronDump._get_master_port')
+    def test_options_ddboost_storage_unit_should_be_used_with_ddboost(self, mock):
+        """
+        --ddboost-storage-unit option must come with --ddboost option
+        """
+        options = GpCronDumpTestCase.Options()
+        options.dump_databases = ['bkdb']
+        options.ddboost_storage_unit = "GPDB"
+        with self.assertRaisesRegexp(Exception, 'Must specify --ddboost option together with the --ddboost-storage-unit'):
             GpCronDump(options, None)
 
     @patch('gpcrondump.GpCronDump._get_master_port')