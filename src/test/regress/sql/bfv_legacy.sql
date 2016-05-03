397bcb4685f89d82d28064180ce4ea2bc502a6b2
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-05-03 13:02:12 +0300

    Remove redundant test case.
    This exact same test case, with some extra EXPLAINs, is in the
    co_nestloop_idxscan regression test.

--- src/test/regress/sql/bfv_legacy.sql
+++ src/test/regress/sql/bfv_legacy.sql
@@ -1090,32 +1090,6 @@ RESET ROLE;
 ---
 ---
 
-create schema co_nestloop_idxscan;
-create table co_nestloop_idxscan.foo (id bigint, data text) with (appendonly=true, orientation=column);
-create table co_nestloop_idxscan.bar (id bigint);
-	
--- Changing the text to be smaller doesn't repro the issue
-insert into co_nestloop_idxscan.foo select 1, repeat('xxxxxxxxxx', 100000);
-insert into co_nestloop_idxscan.bar values (1);
-create index foo_id_idx on co_nestloop_idxscan.foo(id);
-
--- test with hash join
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
--- test with nested loop join
-set enable_hashjoin=off;
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
--- test with nested loop join and index scan
-set enable_seqscan = off;
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
-drop schema co_nestloop_idxscan cascade;
-
----
----
----
-
 --start_ignore
 drop table if exists mpp;
 create table mpp(a int);
--- src/test/regress/sql/qp_misc_rio.sql
+++ src/test/regress/sql/qp_misc_rio.sql
@@ -1096,37 +1096,6 @@ DROP FUNCTION IF EXISTS func_plpythonu(INT);
 RESET ALL;
 
 -- ----------------------------------------------------------------------
--- Test: 39
--- ----------------------------------------------------------------------
--- start_ignore
-set search_path to qp_misc_rio;
-create schema co_nestloop_idxscan;
-create table co_nestloop_idxscan.foo (id bigint, data text) with (appendonly=true, orientation=column);
-create table co_nestloop_idxscan.bar (id bigint);
-
--- Changing the text to be smaller doesn't repro the issue
-insert into co_nestloop_idxscan.foo select 1, repeat('xxxxxxxxxx', 100000);
-insert into co_nestloop_idxscan.bar values (1);
-create index foo_id_idx on co_nestloop_idxscan.foo(id);
--- end_ignore
-
--- test with hash join
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
--- test with nested loop join
-set enable_hashjoin=off;
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
--- test with nested loop join and index scan
-set enable_seqscan = off;
-select f.id from co_nestloop_idxscan.foo f, co_nestloop_idxscan.bar b where f.id = b.id;
-
--- start_ignore
-drop schema co_nestloop_idxscan cascade;
-RESET ALL;
--- end_ignore
-
--- ----------------------------------------------------------------------
 -- Test: 38
 -- ----------------------------------------------------------------------
 
