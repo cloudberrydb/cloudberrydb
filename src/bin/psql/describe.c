a243b842f60be4951b72471d474bc7804a35b093
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-06-15 16:39:16 +0300

    Re-enable psql queries that depend on 8.3 catalogs.
    These queries were backported from later PostgreSQL releases earlier,
    but had to be disabled when we started the 8.3 merge, because we didn't
    have all the catalog changes from 8.3 yet. Now we do, so re-enable the
    queries.

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -22,8 +22,6 @@
 #include "settings.h"
 #include "variables.h"
 
-#define CATALOG_83_SUPPORTED false
-
 static bool describeOneTableDetails(const char *schemaname,
 						const char *relationname,
 						const char *oid,
@@ -630,8 +628,7 @@ describeTypes(const char *pattern, bool verbose, bool showSystem)
 						  gettext_noop("Internal name"),
 						  gettext_noop("Size"));
 	}
-	/* FIXME when we add enum types */
-	if (verbose && pset.sversion >= 80300 && CATALOG_83_SUPPORTED )
+	if (verbose && pset.sversion >= 80300)
 		appendPQExpBuffer(&buf,
 						  "  pg_catalog.array_to_string(\n"
 						  "      ARRAY(\n"
@@ -667,8 +664,7 @@ describeTypes(const char *pattern, bool verbose, bool showSystem)
 	 * do not include array types (before 8.3 we have to use the assumption
 	 * that their names start with underscore)
 	 */
-	/* FIXME when we pull in array types */
-	if (pset.sversion >= 80300 && CATALOG_83_SUPPORTED)
+	if (pset.sversion >= 80300)
 		appendPQExpBuffer(&buf, "  AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)\n");
 	else
 		appendPQExpBuffer(&buf, "  AND t.typname !~ '^_'\n");
@@ -2245,13 +2241,7 @@ describeOneTableDetails(const char *schemaname,
 		/* print rules */
 		if (tableinfo.hasrules)
 		{
-			/*
-			* FIXME: temporarily disabled, because GPDB hasn't been merged
-			* up to 8.3 completely yet, so the column is not there yet.
-			* Re-enable once we reach that commit where ev_enabled is
-			* added.
-			*/
-			if (pset.sversion >= 80300 && CATALOG_83_SUPPORTED)
+			if (pset.sversion >= 80300)
 			{
 				printfPQExpBuffer(&buf,
 								  "SELECT r.rulename, trim(trailing ';' from pg_catalog.pg_get_ruledef(r.oid, true)), "
@@ -2356,13 +2346,7 @@ describeOneTableDetails(const char *schemaname,
 							  oid);
 			if (pset.sversion >= 90000)
 				appendPQExpBuffer(&buf, "NOT t.tgisinternal");
-			/*
-			* FIXME: temporarily disabled, because GPDB hasn't been merged
-			* up to 8.3 completely yet, so the column is not there yet.
-			* Re-enable once we reach that commit where tgconstraint is
-			* added.
-			*/
-			else if (pset.sversion >= 80300 && CATALOG_83_SUPPORTED)
+			else if (pset.sversion >= 80300)
 				appendPQExpBuffer(&buf, "t.tgconstraint = 0");
 			else
 				appendPQExpBuffer(&buf,
@@ -2489,8 +2473,7 @@ describeOneTableDetails(const char *schemaname,
 		PQclear(result);
 
 		/* print child tables */
-		/* FIXME: this needs to be enabled after we have fully merged with 8.3, so that this works */
-		if (pset.sversion >= 80300 && CATALOG_83_SUPPORTED)
+		if (pset.sversion >= 80300)
 			printfPQExpBuffer(&buf, "SELECT c.oid::pg_catalog.regclass FROM pg_catalog.pg_class c, pg_catalog.pg_inherits i WHERE c.oid=i.inhrelid AND i.inhparent = '%s' ORDER BY c.oid::pg_catalog.regclass::pg_catalog.text;", oid);
 		else
 			printfPQExpBuffer(&buf, "SELECT c.oid::pg_catalog.regclass FROM pg_catalog.pg_class c, pg_catalog.pg_inherits i WHERE c.oid=i.inhrelid AND i.inhparent = '%s' ORDER BY c.relname;", oid);
