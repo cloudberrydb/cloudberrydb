f9adeaf0c1abd1dfa05c1599c239402d90523392
Haisheng Yuan and Omer Arap <hyuan@pivotal.io>
2016-06-22 15:36:19 -0700

    [#121003303] Adding gpos osx debug release

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -7,12 +7,14 @@ set -u -e -x
 main() {
   local INPUT_RLS_DIR=bin_gpos_release
   local INPUT_DBG_DIR=bin_gpos_debug
-  local INPUT_OSX_DIR=bin_gpos_osx_release
+  local INPUT_OSX_RLS_DIR=bin_gpos_osx_release
+  local INPUT_OSX_DBG_DIR=bin_gpos_osx_debug
   local OUTPUT_DIR=gpos_github_release_stage
   env LC_ALL=C tar tf $INPUT_RLS_DIR/*.tar.gz | grep "libgpos.so." | sort -n | head -n 1 | sed 's/\.\/lib\/libgpos\.so\./v/' > $OUTPUT_DIR/tag.txt
   cp -v $INPUT_RLS_DIR/*.tar.gz $OUTPUT_DIR/
   cp -v $INPUT_DBG_DIR/*.tar.gz $OUTPUT_DIR/
-  cp -v $INPUT_OSX_DIR/*.tar.gz $OUTPUT_DIR/
+  cp -v $INPUT_OSX_RLS_DIR/*.tar.gz $OUTPUT_DIR/
+  cp -v $INPUT_OSX_DBG_DIR/*.tar.gz $OUTPUT_DIR/
   env GIT_DIR=gpos_src/.git git rev-parse HEAD > $OUTPUT_DIR/commit.txt
 }
 
