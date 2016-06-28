tvesely/yugabyte-db

Fix bogus handling of control characters in json_lex_string().
The original coding misbehaved if "char" is signed, and also made the
extremely poor decision to print control characters literally when trying
to complain about them.  Report and patch by Shigeru Hanada.

In passing, also fix core dump risk in report_parse_error() should the
parse state be something other than what it expects.

src/postgres/src/backend/utils/adt/json.c
@@ -419,7 +419,7 @@ json_lex_string(JsonLexContext *lex)
 	for (s = lex->token_start + 1; *s != '"'; ++s)
 	{
 		/* Per RFC4627, these characters MUST be escaped. */
-		if (*s < 32)
+		if ((unsigned char) *s < 32)
 		{
 			/* A NUL byte marks the (premature) end of the string. */
 			if (*s == '\0')
@@ -430,8 +430,8 @@ json_lex_string(JsonLexContext *lex)
 			ereport(ERROR,
 					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 					 errmsg("invalid input syntax for type json"),
-					 errdetail_internal("line %d: Character \"%c\" must be escaped.",
-						lex->line_number, *s)));
+					 errdetail_internal("line %d: Character with value \"0x%02x\" must be escaped.",
+						lex->line_number, (unsigned char) *s)));
 		}
 		else if (*s == '\\')
 		{
@@ -637,7 +637,7 @@ report_parse_error(JsonParseStack *stack, JsonLexContext *lex)
 			(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 			 errmsg("invalid input syntax for type json: \"%s\"",
 				lex->input),
- 			 errdetail_internal(detail, lex->line_number, token)));
+			 detail ? errdetail_internal(detail, lex->line_number, token) : 0));
 }
 
 /*

cloudberrydb/cloudberrydb

(cherry-picked from 3dd8e596812e3adb72aecafb23fbb6a30836c475)
Author:             Álvaro Hernández Tortosa <aht@8kdata.com>
Date:               Tue Jun 14 00:59:18 UTC 2016

src/backend/utils/adt/json.c
@@ -422,7 +422,7 @@ json_lex_string(JsonLexContext *lex)
 	for (s = lex->token_start + 1; *s != '"'; ++s)
 	{
 		/* Per RFC4627, these characters MUST be escaped. */
-		if (*s < 32)
+		if ((unsigned char) *s < 32)
 		{
 			/* A NUL byte marks the (premature) end of the string. */
 			if (*s == '\0')
@@ -435,8 +435,8 @@ json_lex_string(JsonLexContext *lex)
 					 errmsg("invalid input syntax for type json"),
 					 /* FIXME: replace by errdetail_internal
 					    (commit 5384a73f98d9829725186a7b65bf4f8adb3cfaf1) */
-					 errdetail("line %d: Character \"%c\" must be escaped.",
-						lex->line_number, *s)));
+					 errdetail("line %d: Character with value \"0x%02x\" must be escaped.",
+						lex->line_number, (unsigned char) *s)));
 		}
 		else if (*s == '\\')
 		{
@@ -650,7 +650,7 @@ report_parse_error(JsonParseStack *stack, JsonLexContext *lex)
 				lex->input),
 			 /* FIXME: replace by errdetail_internal
 			    (commit 5384a73f98d9829725186a7b65bf4f8adb3cfaf1) */
- 			 errdetail(detail, lex->line_number, token)));
+			 detail ? errdetail(detail, lex->line_number, token) : 0));
 }
 
 /*