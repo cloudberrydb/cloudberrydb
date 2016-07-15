712705e1e8f58897274a8a23dcc163cd7fe99364
Adam Lee <adam8157@gmail.com>
2016-07-15 16:55:24 +0800

    s3ext: update regress cases (#950)
    1. speed up running by specifying proper files with sufficient size
    2. add a pair of tests which connect to the same S3 file in parallel
    3. revise sql scripts to make them neat
    
    Signed-off-by: Adam Lee <ali@pivotal.io>
    Signed-off-by: Kuien Liu <kliu@pivotal.io>

--- gpAux/extensions/gps3ext/regress/gpcheckcloud_regress.sh
+++ gpAux/extensions/gps3ext/regress/gpcheckcloud_regress.sh
@@ -3,16 +3,16 @@
 EXIT_CODE=0
 GPCHECKCLOUD=./bin/gpcheckcloud/gpcheckcloud
 
-PREFIX[0]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/small17/data0014"
-PREFIX[1]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/small17/data0016"
-PREFIX[2]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/small17/data0014"
-PREFIX[3]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/small17/data0016"
-PREFIX[4]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/gzipped/data0001.gz"
-PREFIX[5]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/threebytes/"
-PREFIX[6]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/small17/"
-PREFIX[7]="s3://s3-us-east-1.amazonaws.com/useast1.s3test.pivotal.io/small17/"
-PREFIX[8]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/gzipped/"
-PREFIX[9]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset1/gzipped_normal1/"
+PREFIX[0]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014"
+PREFIX[1]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016"
+PREFIX[2]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014"
+PREFIX[3]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016"
+PREFIX[4]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/data0001.gz"
+PREFIX[5]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/threebytes/"
+PREFIX[6]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/"
+PREFIX[7]="s3://s3-us-east-1.amazonaws.com/us-east-1.s3test.pivotal.io/regress/small17/"
+PREFIX[8]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/"
+PREFIX[9]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal1/"
 
 HASHSUM[0]="68c4a63b721e7af0ae945ce109ca87ad"
 HASHSUM[1]="0fd502a303eb8f138f5916ec357721b1"
@@ -26,13 +26,13 @@ HASHSUM[8]="7b2260e9a3a3f26e84aa28dc2124f68f"
 HASHSUM[9]="eacb7b210d3f7703ee06d16f520b103e"
 
 #HUGE FILES
-PREFIX[10]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset2/hugefile/airlinedata1.csv"
-PREFIX[11]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset2/hugefile/"
-PREFIX[12]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/dataset2/gzipped_normal2/"
+PREFIX[10]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/hugefile/airlinedata1.csv"
+PREFIX[11]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal2/"
+PREFIX[12]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/5120files/"
 
 HASHSUM[10]="f5811ad92c994f1d6913d5338575fe38"
-HASHSUM[11]="75baaa39f2b1544ed8af437c2cad86b7"
-HASHSUM[12]="a930794bc885bccf6eed45bd40367a7d"
+HASHSUM[11]="a930794bc885bccf6eed45bd40367a7d"
+HASHSUM[12]="71277a7004842241191cc65146be24f6"
 
 for ((i=0; i<${#PREFIX[@]}; i++))
 do
