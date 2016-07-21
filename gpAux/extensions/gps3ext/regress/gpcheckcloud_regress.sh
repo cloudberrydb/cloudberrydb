19b9134a63ba5d6fbd10e739ec0058cfdac4a4d6
Adam Lee <adam8157@gmail.com>
2016-07-21 10:02:46 +0800

    s3ext: refactor gpcheckcloud_regress.sh
    Use `openssl md5` instead of `md5sum` for compatibility.
    Define the array in another way, which makes it easy to be updated.

--- gpAux/extensions/gps3ext/regress/gpcheckcloud_regress.sh
+++ gpAux/extensions/gps3ext/regress/gpcheckcloud_regress.sh
@@ -1,44 +1,31 @@
 #!/bin/bash
 
 EXIT_CODE=0
-GPCHECKCLOUD=./bin/gpcheckcloud/gpcheckcloud
+GPCHECKCLOUD=../bin/gpcheckcloud/gpcheckcloud
 
-PREFIX[0]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014"
-PREFIX[1]="http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016"
-PREFIX[2]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014"
-PREFIX[3]="https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016"
-PREFIX[4]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/data0001.gz"
-PREFIX[5]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/threebytes/"
-PREFIX[6]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/"
-PREFIX[7]="s3://s3-us-east-1.amazonaws.com/us-east-1.s3test.pivotal.io/regress/small17/"
-PREFIX[8]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/"
-PREFIX[9]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal1/"
-
-HASHSUM[0]="68c4a63b721e7af0ae945ce109ca87ad"
-HASHSUM[1]="0fd502a303eb8f138f5916ec357721b1"
-HASHSUM[2]="68c4a63b721e7af0ae945ce109ca87ad"
-HASHSUM[3]="0fd502a303eb8f138f5916ec357721b1"
-HASHSUM[4]="b958fb80b98605a6095e6ebc4b9b4786"
-HASHSUM[5]="fe7d81814e02eb1296757e75bb3c6be9"
-HASHSUM[6]="138fc555074671912125ba692c678246"
-HASHSUM[7]="138fc555074671912125ba692c678246"
-HASHSUM[8]="7b2260e9a3a3f26e84aa28dc2124f68f"
-HASHSUM[9]="eacb7b210d3f7703ee06d16f520b103e"
-
+CHECK_CASES=(
+"http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014 68c4a63b721e7af0ae945ce109ca87ad"
+"http://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016 0fd502a303eb8f138f5916ec357721b1"
+"https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0014 68c4a63b721e7af0ae945ce109ca87ad"
+"https://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/data0016 0fd502a303eb8f138f5916ec357721b1"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/data0001.gz b958fb80b98605a6095e6ebc4b9b4786"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/threebytes/ fe7d81814e02eb1296757e75bb3c6be9"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/small17/ 138fc555074671912125ba692c678246"
+"s3://s3-us-east-1.amazonaws.com/us-east-1.s3test.pivotal.io/regress/small17/ 138fc555074671912125ba692c678246"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped/ 7b2260e9a3a3f26e84aa28dc2124f68f"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal1/ eacb7b210d3f7703ee06d16f520b103e"
 #HUGE FILES
-PREFIX[10]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/hugefile/airlinedata1.csv"
-PREFIX[11]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal2/"
-PREFIX[12]="s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/5120files/"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/hugefile/airlinedata1.csv f5811ad92c994f1d6913d5338575fe38"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/gzipped_normal2/ a930794bc885bccf6eed45bd40367a7d"
+"s3://s3-us-west-2.amazonaws.com/s3test.pivotal.io/regress/5120files/ 71277a7004842241191cc65146be24f6"
+)
 
-HASHSUM[10]="f5811ad92c994f1d6913d5338575fe38"
-HASHSUM[11]="a930794bc885bccf6eed45bd40367a7d"
-HASHSUM[12]="71277a7004842241191cc65146be24f6"
-
-for ((i=0; i<${#PREFIX[@]}; i++))
+for ((i=0; i<${#CHECK_CASES[@]}; i++))
 do
-	[ `$GPCHECKCLOUD -d "${PREFIX[$i]} config=/home/gpadmin/s3.conf" 2>/dev/null |md5sum |cut -d ' ' -f 1` = "${HASHSUM[$i]}" ] \
-		&& echo test ${PREFIX[$i]} ... ok \
-		|| { EXIT_CODE=1; echo test ${PREFIX[$i]} ... failed; }
+	PREFIX=`echo ${CHECK_CASES[$i]} |cut -d' ' -f 1`
+	DIGEST=`echo ${CHECK_CASES[$i]} |cut -d' ' -f 2`
+	[ `$GPCHECKCLOUD -d "$PREFIX config=/home/gpadmin/s3.conf" 2>/dev/null |openssl md5 |cut -d ' ' -f 2` = "$DIGEST" ] \
+		&& echo test $PREFIX ... ok || { EXIT_CODE=1; echo test $PREFIX ... failed; }
 done
 
 exit $EXIT_CODE
