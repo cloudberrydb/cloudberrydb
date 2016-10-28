4e506b383134f809fa369a70cf048dd51b78096e
Ashwin Agrawal <aagrawal@pivotal.io>
2016-10-28 12:04:27 -0700

    Infra to run C&S TINC tests natively on concourse.
    With this commit adding infrastructure scripts to enable running catalog and
    storage tests in Concourse containers natively. Using this infra interatively
    will be migrating CS tests suites to concourse.

---
+++ concourse/scripts/tinc_gpdb.bash
@@ -0,0 +1,54 @@
+#!/bin/bash -l
+
+set -eox pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${CWDIR}/common.bash"
+
+function gen_env(){
+  cat > /opt/run_test.sh <<-EOF
+		source /usr/local/greenplum-db-devel/greenplum_path.sh
+		cd "\${1}/gpdb_src/gpAux"
+		source gpdemo/gpdemo-env.sh
+		PGDATABASE=${PGDATABASE:=gptest}
+		createdb ${PGDATABASE}
+		export PGDATABASE="${PGDATABASE}"
+		cd ../src/test/tinc
+		source tinc_env.sh
+		make ${MAKE_TEST_COMMAND}
+	EOF
+
+	chmod a+x /opt/run_test.sh
+}
+
+function _main() {
+
+    if [ -z "${MAKE_TEST_COMMAND}" ]; then
+        echo "FATAL: MAKE_TEST_COMMAND is not set"
+        exit 1
+    fi
+
+    if [ -z "$TEST_OS" ]; then
+        echo "FATAL: TEST_OS is not set"
+        exit 1
+    fi
+
+    if [ "$TEST_OS" != "centos" -a "$TEST_OS" != "sles" ]; then
+        echo "FATAL: TEST_OS is set to an invalid value: $TEST_OS"
+        echo "Configure TEST_OS to be centos or sles"
+        exit 1
+    fi
+
+    time install_gpdb
+    time ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash "$TEST_OS"
+
+    #set the connection limit to 250 and segment pairs to 2 for TINC tests
+    export DEFAULT_QD_MAX_CONNECT=250
+    export NUM_PRIMARY_MIRROR_PAIRS=2
+    time make_cluster
+
+    time gen_env
+    time run_test
+}
+
+_main "$@"
