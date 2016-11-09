969d82922a88aa3525cbdf17a6c08540f81da3db
Pengzhou Tang <ptang@pivotal.io>
2016-11-09 15:42:40 +0800

    Clean up some cases from bugbuster
    1. Remove TestPSQL from bugbuster
    The whole test is completely enclosed in a start/end_ignore block, we can not
    verify the correctness except it crash the whole service. Moreover,
    allow_system_table_mods has been tested by other cases in ICG, so this test case
    is useless for now.
    
    2. Remove useless codecoverage case from bugbuster
    
    3. Remove gp_persistent_table from bugbuster
    gp_enable_inheritance is not invalid under current code base, so Remove this
    case
    
    4. merge DMLstmnt from bugbuster to icg

--- src/test/regress/sql/role.sql
+++ src/test/regress/sql/role.sql
@@ -74,3 +74,26 @@ drop view t1_view;
 drop table t1;
 drop role u1;
 drop role superuser;
+
+-- Test creating user who has been renamed before.
+CREATE USER jonathan11 WITH PASSWORD 'abc1';
+CREATE USER jonathan12 WITH PASSWORD 'abc2';
+
+ALTER USER jonathan11 RENAME TO jona11;
+ALTER USER jonathan12 RENAME TO jona12;
+
+DROP USER jona11;
+DROP USER jona12;
+
+CREATE USER jonathan12 WITH PASSWORD 'abc2';
+
+ALTER USER jonathan11 RENAME TO jona11;
+ALTER USER jonathan12 RENAME TO jona12;
+
+CREATE GROUP marketing WITH USER jona11,jona12;
+
+ALTER GROUP marketing RENAME TO market;
+
+DROP GROUP market;
+DROP USER jona11;
+DROP USER jona12;
