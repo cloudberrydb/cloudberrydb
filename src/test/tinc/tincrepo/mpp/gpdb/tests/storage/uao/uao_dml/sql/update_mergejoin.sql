03f04057c093d762ec3ef0a53eac60990e1bbdbe
Amil Khanzada and Jimmy Yih <pair+akhanzada+jyih@pivotal.io>
2016-11-18 17:36:22 -0800

    Speed up cs-uao TINC tests related to merge join
    On gpdb4, the enable_mergejoin=off GUC was ignored, which allowed
    certain tests to use merge join, running very quickly.  On gpdb5, this
    GUC is now read correctly, which causes these tests to NOT use merge
    join, which resulted in test runs taking up to 10 minutes longer. This
    commit fixes the tests to properly set the GUC when attempting to test
    merge join.
    
    https://github.com/greenplum-db/gpdb/issues/1341

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_dml/sql/update_mergejoin.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_dml/sql/update_mergejoin.sql
@@ -5,4 +5,5 @@ set enable_hashjoin=off;
 set enable_nestloop=off;
 set enable_seqscan=off;
 set enable_bitmapscan=off;
+set enable_mergejoin=on;
 UPDATE foo SET b = bar.a FROM bar WHERE foo.a = bar.a AND foo.b = bar.b AND foo.c = bar.c AND foo.d = bar.d;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uaocs_dml/sql/delete_mergejoin.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uaocs_dml/sql/delete_mergejoin.sql
@@ -5,4 +5,5 @@ set enable_hashjoin=off;
 set enable_nestloop=off;
 set enable_seqscan=off;
 set enable_bitmapscan=off;
+set enable_mergejoin=on;
 DELETE FROM foo using BAR WHERE foo.a = bar.a AND foo.b = bar.b AND foo.c = bar.c AND foo.d = bar.d;
