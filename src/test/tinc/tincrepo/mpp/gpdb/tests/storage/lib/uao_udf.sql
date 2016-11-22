7912a0b24bd576ecbd5c11062366a5edc41f5d2e
Amil Khanzada and Jimmy Yih <pair+akhanzada+jyih@pivotal.io>
2016-11-22 11:11:37 -0800

    Fixed cs-uao/uao_isolation TINC tests
    Note that the max_concurrency tests are prone to failure if system
    resources are heavily occupied because they demand a lot of process
    forking. You may encounter the below error message when running this
    test.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/lib/uao_udf.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/lib/uao_udf.sql
@@ -11,8 +11,8 @@ DROP FUNCTION IF EXISTS gp_aovisimap_hidden_info_name(text);
 CREATE FUNCTION gp_aovisimap_hidden_info_name(text) RETURNS TABLE (segno integer, hidden_tupcount bigint, total_tupcount bigint) AS '$GPHOME/lib/postgresql/gp_ao_co_diagnostics.so','gp_aovisimap_hidden_info_name_wrapper' LANGUAGE C STRICT;
 
 DROP FUNCTION IF EXISTS gp_aoseg_name(text);
-CREATE FUNCTION gp_aoseg_name(text) RETURNS TABLE (segno int, eof bigint, tupcount bigint, varblockcount bigint, eofuncompressed bigint, modcount bigint, state smallint) AS '$GPHOME/lib/postgresql/gp_ao_co_diagnostics.so','gp_aoseg_name_wrapper' LANGUAGE C STRICT;
+CREATE FUNCTION gp_aoseg_name(text) RETURNS TABLE (segno int, eof bigint, tupcount bigint, varblockcount bigint, eofuncompressed bigint, modcount bigint, formatversion smallint, state smallint) AS '$GPHOME/lib/postgresql/gp_ao_co_diagnostics.so','gp_aoseg_name_wrapper' LANGUAGE C STRICT;
 
 DROP FUNCTION IF EXISTS gp_aocsseg_name(text);
-CREATE FUNCTION gp_aocsseg_name(text) RETURNS TABLE (gp_tid tid, segno int4, column_num smallint, pysical_segno int, tupcount bigint, eof bigint, eof_uncompressed bigint, modcount bigint, state smallint) AS '$GPHOME/lib/postgresql/gp_ao_co_diagnostics.so','gp_aocsseg_name_wrapper' LANGUAGE C STRICT;
+CREATE FUNCTION gp_aocsseg_name(text) RETURNS TABLE (gp_tid tid, segno int4, column_num smallint, pysical_segno int, tupcount bigint, eof bigint, eof_uncompressed bigint, modcount bigint, formatversion smallint, state smallint) AS '$GPHOME/lib/postgresql/gp_ao_co_diagnostics.so','gp_aocsseg_name_wrapper' LANGUAGE C STRICT;
 
