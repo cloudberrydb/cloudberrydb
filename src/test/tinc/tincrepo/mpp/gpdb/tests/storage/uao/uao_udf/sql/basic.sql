7b1c0f8d20263b2bd2a19eb366914e713661fa82
Amil Khanzada and Jimmy Yih <pair+akhanzada+jyih@pivotal.io>
2016-11-22 14:27:15 -0800

    Fixed cs-uao/uao_udf TINC tests

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_udf/sql/basic.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_udf/sql/basic.sql
@@ -2,13 +2,13 @@
 -- 
 
 2U: SELECT * FROM gp_aovisimap_name('foo');
-2U: SELECT * FROM gp_aovisimap_entry_name('foo');
+2U: SELECT sego, first_row_num, hiddentupcount FROM gp_aovisimap_entry_name('foo');
 2U: SELECT * FROM gp_aovisimap_hidden_info_name('foo');
 DELETE FROM foo WHERE a IN (1, 2, 3, 5, 8, 13, 21);
 2U: SELECT * FROM gp_aovisimap_name('foo');
-2U: SELECT * FROM gp_aovisimap_entry_name('foo');
+2U: SELECT sego, first_row_num, hiddentupcount FROM gp_aovisimap_entry_name('foo');
 2U: SELECT * FROM gp_aovisimap_hidden_info_name('foo');
 UPDATE FOO SET b=0 WHERE a > 21;
 2U: SELECT * FROM gp_aovisimap_name('foo');
-2U: SELECT * FROM gp_aovisimap_entry_name('foo');
+2U: SELECT sego, first_row_num, hiddentupcount FROM gp_aovisimap_entry_name('foo');
 2U: SELECT * FROM gp_aovisimap_hidden_info_name('foo');
