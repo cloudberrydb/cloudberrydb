b60fb1b84fdff69aee3535e845b0114fcc531b21
Jimmy Yih <jyih@pivotal.io>
2016-12-01 18:28:28 -0800

    Move TINC uao/uao_catalog_tabs to installcheck
    The uao/uao_catalog_tabs tests were originally created in TINC because
    the uao/uaocs auxiliary tables needed the uao/uaocs table's oid. Some
    gp_toolkit functions also needed the oid and were thought to require
    to be called on segments using utility mode too. TINC made this easy
    since it's doing simple Python scripting. These requirements have been
    dismissed by using PL/pgSQL functions in order to get these tests
    running in installcheck.
    
    Author: Jimmy Yih

---
+++ src/test/regress/sql/uaocs_catalog_tables.sql
@@ -0,0 +1,64 @@
+-- create functions to query uaocs auxiliary tables through gp_dist_random instead of going through utility mode
+CREATE OR REPLACE FUNCTION gp_aocsseg_dist_random(
+  IN relation_name text) RETURNS setof record AS $$
+DECLARE
+  record_text text;
+  result record;
+BEGIN
+  for record_text in
+      EXECUTE 'select gp_toolkit.__gp_aocsseg(''' || relation_name || '''::regclass)::text from gp_dist_random(''gp_id'');'
+  loop
+      EXECUTE 'select a[3], a[4], a[5], a[6], a[7], a[8] from
+              (select regexp_split_to_array(''' || record_text ||''', '','')) as dt(a);' into result;
+      return next result;
+  end loop;
+  return;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Verify empty visimap for uaocs table
+create table uaocs_table_check_empty_visimap (i int, j varchar(20), k int ) with (appendonly=true, orientation=column) DISTRIBUTED BY (i);
+insert into uaocs_table_check_empty_visimap values(1,'test',2);
+select 1 from pg_appendonly where visimapidxid is not null and visimapidxid is not NULL and relid='uaocs_table_check_empty_visimap'::regclass;
+
+-- Verify the hidden tup_count using UDF gp_aovisimap_hidden_info(oid) for uaocs relation after delete and vacuum
+create table uaocs_table_check_hidden_tup_count_after_delete(i int, j varchar(20), k int ) with (appendonly=true, orientation=column) DISTRIBUTED BY (i);
+insert into uaocs_table_check_hidden_tup_count_after_delete select i,'aa'||i,i+10 from generate_series(1,10) as i;
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_delete'::regclass) from gp_dist_random('gp_id');
+delete from uaocs_table_check_hidden_tup_count_after_delete where i = 1;
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_delete'::regclass) from gp_dist_random('gp_id');
+vacuum full uaocs_table_check_hidden_tup_count_after_delete;
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_delete'::regclass) from gp_dist_random('gp_id');
+
+-- Verify the hidden tup_count using UDF gp_aovisimap_hidden_info(oid) for uaocs relation after update and vacuum
+create table uaocs_table_check_hidden_tup_count_after_update(i int, j varchar(20), k int ) with (appendonly=true, orientation=column) DISTRIBUTED BY (i);
+insert into uaocs_table_check_hidden_tup_count_after_update select i,'aa'||i,i+10 from generate_series(1,10) as i;
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_update'::regclass) from gp_dist_random('gp_id');
+update uaocs_table_check_hidden_tup_count_after_update set j = 'test_update';
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_update'::regclass) from gp_dist_random('gp_id');
+vacuum full uaocs_table_check_hidden_tup_count_after_update;
+select gp_toolkit.__gp_aovisimap_hidden_info('uaocs_table_check_hidden_tup_count_after_update'::regclass) from gp_dist_random('gp_id');
+
+-- Verify the eof in pg_aocsseg and gp_persistent_relation_node are the same for uaocs relation after delete
+create table uaocs_table_check_eof_after_delete(i int, j varchar(20), k int ) with (appendonly=true, orientation=column) DISTRIBUTED BY (i);
+insert into uaocs_table_check_eof_after_delete select i,'aa'||i,i+10 from generate_series(1,20) as i;
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_delete'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_delete') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
+delete from uaocs_table_check_eof_after_delete where i in (1,5,13);
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_delete'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_delete') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
+vacuum full uaocs_table_check_eof_after_delete;
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_delete'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_delete') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
+
+-- Verify the eof in pg_aocsseg and gp_persistent_relation_node are the same for uaocs relation after update
+create table uaocs_table_check_eof_after_update(i int, j varchar(20), k int ) with (appendonly=true, orientation=column) DISTRIBUTED BY (i);
+insert into uaocs_table_check_eof_after_update select i,'aa'||i,i+10 from generate_series(1,20) as i;
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_update'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_update') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
+update uaocs_table_check_eof_after_update set k = k + 100 where i in (1,5,13);
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_update'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_update') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
+vacuum full uaocs_table_check_eof_after_update;
+select segment_file_num, mirror_append_only_new_eof from gp_dist_random('gp_persistent_relation_node') where relfilenode_oid = 'uaocs_table_check_eof_after_update'::regclass and mirror_append_only_new_eof != 0;
+select * from gp_aocsseg_dist_random('uaocs_table_check_eof_after_update') as (segno text, column_num text, physical_segno text, tupcount text, eof text, eof_uncompressed text);
