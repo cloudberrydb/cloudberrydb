e34cf5c13367cac4cc9c138c73e897b2f6cafa30
Jesse Zhang <jzhang@pivotal.io>
2016-12-13 14:21:24 -0800

    Remove dead tests [#135663277]
    We did not port SQL files for the HAWQ and planner tests, now removing
    mentions of those tests for the module can vastly simplify the running
    of ORCA's functional tests.

--- src/test/tinc/tincrepo/dml/functional/test_dml.py
+++ src/test/tinc/tincrepo/dml/functional/test_dml.py
@@ -30,26 +30,3 @@ class test_dml_partition(SQLTestCase):
     sql_dir = 'sql_partition/'
     ans_dir = 'expected_partition/'
     out_dir = 'output/'
-
-class test_dml_hawq_partition(SQLTestCase):
-    """
-    @optimizer_mode on
-    @product_version hawq: [1.2.1.0-]
-    @tags ORCA MPP-21090
-    @db_name dmldb
-    """
-    sql_dir = 'sql_hawq_partition/'
-    ans_dir = 'expected_hawq_partition/'
-    out_dir = 'output/'
-
-class test_dml_planner(SQLTestCase):
-    """
-    Run the hotfix test cases(for 42x)
-    @optimizer_mode off
-    @product_version gpdb: [4.2.6.2-], hawq: [1.1.0.2-]
-    @db_name dmldb
-    """
-    sql_dir = 'sql_planner'
-    ans_dir = 'expected/'
-    out_dir = 'output/'
-
