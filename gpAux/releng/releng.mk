aab8128a2a1fb84e3d4d623553c7fd5c49ce4a54
Dhanashree Kashid <dkashid@pivotal.io>
2016-12-19 14:12:29 -0800

    Bump ORCA version to 1.696
    Signed-off-by: Haisheng Yuan <hyuan@pivotal.io>

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -176,7 +176,7 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	@echo "Resolve finished";
 
 	wget -O - https://github.com/greenplum-db/gpos/releases/download/v1.146/bin_gpos_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
-	wget -O - https://github.com/greenplum-db/gporca/releases/download/v1.695/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+	wget -O - https://github.com/greenplum-db/gporca/releases/download/v1.696/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
 
 clean_tools: opt_write_test
 	@cd releng/make/dependencies; \
