cloudberrydb/cloudberrydb

commit 232eb64ad9f93dce8941f7b124a98f0c21c3350b

Add config option to choose fqdn or hostname and use socket module
The new option spells out fully_qualified_domain_name since fqdn is
such a mouthful of an abbreviation. In order to retrieve the fqdn or
hostname in a portable manner the Python socket module seems the best
bet. It also avoids the overhead of opening a command interpreter and
pipe to get the information.

gpMgmt/bin/gpload.py
@@ -51,6 +51,7 @@ except Exception, e:
 import hashlib
 import datetime,getpass,os,signal,socket,subprocess,threading,time,traceback,re
 import uuid
+import socket
 
 thePlatform = platform.system()
 if thePlatform in ['Windows', 'Microsoft']:
@@ -1628,15 +1629,13 @@ class gpload:
 
             # do default host, the current one
             if not local_hostname:
-                try:
-                    pipe = subprocess.Popen("hostname",
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE)
-                    result  = pipe.communicate();
-                except OSError, e:
-                    self.log(self.ERROR, "command failed: " + str(e))
-                
-                local_hostname = [result[0].strip()]
+                # if fully_qualified_domain_name is defined and set to true we want to
+                # resolve the fqdn rather than just grabbing the hostname.
+                fqdn = self.getconfig(name+':fully_qualified_domain_name', bool, False)
+                if fqdn:
+                    local_hostname = socket.getfqdn()
+                else:
+                    local_hostname = socket.gethostname()
 
             # build gpfdist parameters
             popenList = ['gpfdist']

commit d8aaf459b5c5cbbc44b2c19b2320f01d6fbcc9cf

gpload local_hostname var should be list instead of string
This was fixed in GPDB 4.3 immediately after it was brought up but was
never ported to 5.0. Issue was introduced in
https://github.com/greenplum-db/gpdb/commit/232eb64ad9f93dce8941f7b124a98f0c21c3350b
which has the initial discussion.

gpMgmt/bin/gpload.py
@@ -1635,9 +1635,9 @@ class gpload:
                 # resolve the fqdn rather than just grabbing the hostname.
                 fqdn = self.getconfig('gpload:input:fully_qualified_domain_name', bool, False)
                 if fqdn:
-                    local_hostname = socket.getfqdn()
+                    local_hostname = [socket.getfqdn()]
                 else:
-                    local_hostname = socket.gethostname()
+                    local_hostname = [socket.gethostname()]
 
             # build gpfdist parameters
             popenList = ['gpfdist']