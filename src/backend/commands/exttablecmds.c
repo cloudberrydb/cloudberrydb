cloudberrydb/cloudberrydb

commit 2a3df5d087e6e886db4e5059ffb80e348448fe83

Move duplicate handling of LOCATION to the parsing stage
Creating an external table with duplicate location URIs is a syntax
error so move the duplicate check to the parsing step. This simplifies
the external table command code as well as provides an error message
with context marker to the user.

Also fix up test sources to match the new error messages and remove
the duplicate test from gpfdist since we already test that in ICW.

src/backend/commands/exttablecmds.c
@@ -470,28 +470,6 @@ transformLocationUris(List *locs, bool isweb, bool iswritable)
 	astate = NULL;
 
 	/*
-	 * first, check for duplicate URI entries
-	 */
-	foreach(cell, locs)
-	{
-		Value	   *v1 = lfirst(cell);
-		const char *uri1 = v1->val.str;
-		ListCell   *rest;
-
-		for_each_cell(rest, lnext(cell))
-		{
-			Value	   *v2 = lfirst(rest);
-			const char *uri2 = v2->val.str;
-
-			if (strcmp(uri1, uri2) == 0)
-				ereport(ERROR,
-						(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
-						 errmsg("location uri \"%s\" appears more than once",
-								uri1)));
-		}
-	}
-
-	/*
 	 * iterate through the user supplied URI list from LOCATION clause.
 	 */
 	foreach(cell, locs)

src/backend/parser/gram.y
@@ -8949,7 +8949,15 @@ DeallocateStmt: DEALLOCATE name
 
 cdb_string_list:
 			cdb_string							{ $$ = list_make1($1); }  
-			| cdb_string_list ',' cdb_string	{ $$ = lappend($1, $3); }
+			| cdb_string_list ',' cdb_string
+				{
+					if (list_member($1, $3))
+						ereport(ERROR,
+								(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+								 errmsg("duplicate location uri"),
+								 scanner_errposition(@3)));
+					$$ = lappend($1, $3);
+				}
 		;
 
 

src/bin/gpfdist/regress/input/exttab1.source
@@ -443,7 +443,6 @@ create readable external table ret_region(like wet_region) location('gpfdist://@
 
 -- negative
 create writable external table wet_neg1(a text, b text) location('file://@hostname@@abs_srcdir@/badt1.tbl') format 'text';
-create writable external table wet_neg1(a text, b text) location('gpfdist://@hostname@:7070/wet.out', 'gpfdist://@hostname@:7070/wet.out') format 'text';
 create writable external web table wet_pos5(a text, b text) execute 'some command' on segment 0 format 'text';
 
 --

src/bin/gpfdist/regress/output/exttab1.source
@@ -705,8 +705,6 @@ create readable external table ret_region(like wet_region) location('gpfdist://@
 create writable external table wet_neg1(a text, b text) location('file://@hostname@@abs_srcdir@/badt1.tbl') format 'text';
 ERROR:  unsupported URI protocol 'file' for writable external table
 HINT:  Writable external tables may use 'gpfdist', 'gpfdists' or 'gphdfs' URIs only.
-create writable external table wet_neg1(a text, b text) location('gpfdist://@hostname@:7070/wet.out', 'gpfdist://@hostname@:7070/wet.out') format 'text';
-ERROR:  location uri "gpfdist://@hostname@:7070/wet.out" appears more than once
 create writable external web table wet_pos5(a text, b text) execute 'some command' on segment 0 format 'text';
 ERROR:  ON clause may not be used with a writable external table
 --

src/bin/gpfdist/regress/output/exttab1_optimizer.source
@@ -705,8 +705,6 @@ create readable external table ret_region(like wet_region) location('gpfdist://@
 create writable external table wet_neg1(a text, b text) location('file://@hostname@@abs_srcdir@/badt1.tbl') format 'text';
 ERROR:  unsupported URI protocol 'file' for writable external table
 HINT:  Writable external tables may use 'gpfdist', 'gpfdists' or 'gphdfs' URIs only.
-create writable external table wet_neg1(a text, b text) location('gpfdist://@hostname@:7070/wet.out', 'gpfdist://@hostname@:7070/wet.out') format 'text';
-ERROR:  location uri "gpfdist://@hostname@:7070/wet.out" appears more than once
 create writable external web table wet_pos5(a text, b text) execute 'some command' on segment 0 format 'text';
 ERROR:  ON clause may not be used with a writable external table
 --

src/test/regress/input/external_table.source
@@ -184,7 +184,7 @@ create writable external web table wet_pos4(a text, b text) execute 'some comman
 
 -- negative
 create writable external table wet_neg1(a text, b text) location('file://@hostname@@abs_srcdir@/badt1.tbl') format 'text';
-create writable external table wet_neg1(a text, b text) location('gpfdist://@hostname@:7070/wet.out', 'gpfdist://@hostname@:7070/wet.out') format 'text';
+create writable external table wet_neg1(a text, b text) location('gpfdist://foo:7070/wet.out', 'gpfdist://foo:7070/wet.out') format 'text';
 create writable external web table wet_pos5(a text, b text) execute 'some command' on segment 0 format 'text';
 --
 -- test CREATE EXTERNAL TABLE privileges

src/test/regress/output/external_table.source
@@ -269,8 +269,10 @@ create writable external web table wet_pos4(a text, b text) execute 'some comman
 create writable external table wet_neg1(a text, b text) location('file://@hostname@@abs_srcdir@/badt1.tbl') format 'text';
 ERROR:  unsupported URI protocol 'file' for writable external table
 HINT:  Writable external tables may use 'gpfdist', 'gpfdists' or 'gphdfs' URIs only.
-create writable external table wet_neg1(a text, b text) location('gpfdist://@hostname@:7070/wet.out', 'gpfdist://@hostname@:7070/wet.out') format 'text';
-ERROR:  location uri "gpfdist://@hostname@:7070/wet.out" appears more than once
+create writable external table wet_neg1(a text, b text) location('gpfdist://foo:7070/wet.out', 'gpfdist://foo:7070/wet.out') format 'text';
+ERROR:  duplicate location uri
+LINE 1: ...t, b text) location('gpfdist://foo:7070/wet.out', 'gpfdist:/...
+                                                             ^
 create writable external web table wet_pos5(a text, b text) execute 'some command' on segment 0 format 'text';
 ERROR:  ON clause may not be used with a writable external table
 --

src/test/tinc/tincrepo/mpp/gpdb/tests/storage/basic/exttab/write/sql/functional_multiple_gpfdist.ans
@@ -3,8 +3,10 @@ CREATE TABLE
 insert into test values (generate_series(1,5),'test_1');
 INSERT 0 5
 -- Negative Test of using 2 identical gpfdist URLS. Should Fail
-CREATE WRITABLE EXTERNAL TABLE tbl_wet_2gpfdist_identical ( a int, b text) LOCATION ('gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist.tbl', 'gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist.tbl') FORMAT 'TEXT' (DELIMITER AS '|' NULL AS 'null'  ) ;
-psql:/path/sql_file:1: ERROR:  location uri "gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist.tbl" appears more than once
+CREATE WRITABLE EXTERNAL TABLE tbl_wet_2gpfdist_identical ( a int, b text) LOCATION ('gpfdist://hostname:123/output/wet_2gpfdist.tbl', 'gpfdist://hostname:123/output/wet_2gpfdist.tbl') FORMAT 'TEXT' (DELIMITER AS '|' NULL AS 'null');
+psql:/path/sql_file:1: ERROR:  duplicate location uri
+LINE 1: ... ('gpfdist://hostname:123/output/wet_2gpfdist.tbl', 'gpfdist...
+                                                               ^
 -- Negative Test of using more gpfdist URLS than valid primary segments. Should Fail
 CREATE WRITABLE EXTERNAL TABLE tbl_wet_multiple_gpfdist ( a int, b text) LOCATION ('gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist1.tbl', 'gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist2.tbl','gpfdist://@hostname@:@gp_port@/output/wet_2gpfdist3.tbl') FORMAT 'TEXT' (DELIMITER AS '|' NULL AS 'null'  ) ;
 CREATE EXTERNAL TABLE

commit 8ad3b462c3bbaece3094fdee9ecd0525e9b0ca59

Fix broken ereport() call
Commit 2a3df5d introduced broken changes to the ereport() calls in the
external table command. Fix by including the proper format specifier.
Also concatenate the error message to a single row while at it to
improve readability.

Spotted by Heikki Linnakangas

src/backend/commands/exttablecmds.c
@@ -389,8 +389,7 @@ DefineExternalRelation(CreateExternalStmt *createExtStmt)
 				if (getgpsegmentCount() < list_length(exttypeDesc->location_list))
 					ereport(WARNING,
 							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-							 errmsg("number of locations (%) exceeds the number "
-									"of segments (%d)",
+							 errmsg("number of locations (%d) exceeds the number of segments (%d)",
 									list_length(exttypeDesc->location_list),
 									getgpsegmentCount()),
 							 errhint("The table cannot be queried until cluster "