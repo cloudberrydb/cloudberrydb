11058be899d44b6a13f2b10390efe42c45e23f1d
Daniel Gustafsson <dgustafsson@pivotal.io>
2017-01-15 22:06:45 +0100

    Avoid compiler warning on type cache identifier
    While the type cache used in binary upgrades isn't a dumpable
    object, it has a DO_ identifier associated to be able to use
    the indexing code. Skip it when looking through the DO objects
    to keep compilers and static analyzers happy.
    
    This was already done in pg_dump, do so for cdb_dump_agent too
    and expand the documentation on why.

--- src/bin/pg_dump/cdb/cdb_dump_agent.c
+++ src/bin/pg_dump/cdb/cdb_dump_agent.c
@@ -2672,6 +2672,14 @@ dumpDumpableObject(Archive *fout, DumpableObject *dobj)
 						 NULL, 0,
 						 dumpBlobComments, NULL);
 			break;
+		/*
+		 * The TYPE_CACHE object is only used for the pg_type cache during
+		 * binary_upgrade operation and should not be dumped. To keep the
+		 * compilers and static analyzers happy we still need to handle
+		 * the case though.
+		 */
+		case DO_TYPE_CACHE:
+			break;
 	}
 }
 
