03e97feed2245549c7f07cb33f30232c06632936
Daniel Gustafsson <dgustafsson@pivotal.io>
2017-01-18 13:45:13 +0100

    Remove duplicate copyright header

--- src/bin/pg_dump/dumputils.h
+++ src/bin/pg_dump/dumputils.h
@@ -6,7 +6,6 @@
  *
  *
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
- * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * $PostgreSQL: pgsql/src/bin/pg_dump/dumputils.h,v 1.21 2008/01/01 19:45:55 momjian Exp $
