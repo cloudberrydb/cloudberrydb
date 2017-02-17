2cf8b28a5ec92fea9545195fd2eff0fb255eb18f
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-02-17 22:47:39 +0200

    Rewrite readindex test cases so that they work with ORCA.
    The "CREATE TABLE ... AS SELECT ..." commands that were used to create
    the test tables choose random distribution policy when ORCA is used, while
    without ORCA, it chooses the first column as the distribution key. I'm not
    sure why, and it seems inconsistent, but work around that by using CREATE
    TABLE + INSERT ... SELECT to create and populate the tables instead.
    
    With CTAS, the all the rows in AO tables go to AO segfile 0, while with
    CREATE TABLE + separate INSERT, they go to segfile 1, so update the
    expected output for that.

--- contrib/indexscan/sql/read_co_index.sql
+++ contrib/indexscan/sql/read_co_index.sql
@@ -1,6 +1,7 @@
 drop table if exists co_t1;
 drop index if exists co_i1;
-create table co_t1 with (appendonly=true, orientation=column) as select 1 as a, * from generate_series(1, 1000) as b;
+create table co_t1 (a integer, b integer) with (appendonly=true, orientation=column);
+insert into co_t1 select 1 as a, * from generate_series(1, 1000) as b;
 create index co_i1 on co_t1(b);
 select count(*) from co_t1;
 
--- contrib/indexscan/sql/read_heap_index.sql
+++ contrib/indexscan/sql/read_heap_index.sql
@@ -1,6 +1,8 @@
 drop table if exists heap_t1;
 drop index if exists heap_i1;
-create table heap_t1 as select 1 as a, * from generate_series(1, 1000) as b;
+
+create table heap_t1 (a integer, b integer);
+insert into heap_t1 select 1 as a, * from generate_series(1, 1000) as b;
 create index heap_i1 on heap_t1(b);
 select count(*) from heap_t1;
 
