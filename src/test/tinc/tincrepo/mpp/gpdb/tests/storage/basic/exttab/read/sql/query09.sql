63312d241592a6926ffe6fefc7740c7bbe4f05e9
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-02-27 09:52:31 +0200

    Remove redundant external table TINC tests.
    We have sufficient coverage of external tables in the main regression
    suite and src/bin/gpfdist/regress already.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/basic/exttab/read/sql/query09.sql
+++
@@ -1,5 +0,0 @@
-drop external table if exists ret_table1;
-CREATE EXTERNAL TABLE ret_table1(a integer)
-location ('file://@hostname@@abs_srcdir@/sql/query09.sql')
-FORMAT 'text';
-select * from ret_table1;
