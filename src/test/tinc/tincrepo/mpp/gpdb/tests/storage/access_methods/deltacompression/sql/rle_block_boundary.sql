9cad0a0a29ad3b9d8d2108b086a3ab1bdebd0714
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-02-27 09:21:25 +0200

    Move RLE delta compression tests from TINC.
    I kept the tests using QuickLZ in TINC, as we don't have QuickLZ support in
    the open source version anymore. They ought to be moved to where we keep
    the code for reading legacy quicklz-compressed tables.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/access_methods/deltacompression/sql/rle_block_boundary.sql
+++
@@ -1,8 +0,0 @@
--- @product_version gpdb: [4.3.6.2 - 4.3.99.0)
-set time zone PST8PDT;
-
-DROP TABLE IF EXISTS rle_block_boundary;
-CREATE TABLE rle_block_boundary (a int, b text) with (appendonly=true, orientation=column, compresstype=rle_type);
-
-COPY rle_block_boundary(b) FROM '%PATH%/data/rle_block_boundary';
-SELECT COUNT(*) FROM rle_block_boundary;
