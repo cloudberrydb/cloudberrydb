45507229cb6ff0d32070358accb594750c78333c
Kenan Yao <kyao@pivotal.io>
2017-03-01 02:42:21 +0000

    Implement ALTER ROLE RESOURCE GROUP syntax.
    There is the same restrictions with the CREATE ROLE syntax, only
    superuser can be assigned to the admin_group.
    
        -- assign the role r1 to resource group g1
        ALTER ROLE r1 RESOURCE GROUP g1;
    
    Signed-off-by: Ning Yu <nyu@pivotal.io>

--- src/test/regress/sql/resource_group.sql
+++ src/test/regress/sql/resource_group.sql
@@ -1,5 +1,5 @@
 -- ----------------------------------------------------------------------
--- Test: assign a role to a resource group
+-- Test: assign/alter a role to a resource group
 -- ----------------------------------------------------------------------
 
 --start_ignore
@@ -7,9 +7,19 @@ DROP ROLE IF EXISTS rg_test_role;
 --end_ignore
 CREATE ROLE rg_test_role;
 SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
+ALTER ROLE rg_test_role RESOURCE GROUP non_exist_group;
+SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
+ALTER ROLE rg_test_role RESOURCE GROUP admin_group;
+SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
+ALTER ROLE rg_test_role RESOURCE GROUP none;
+SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
 
 DROP ROLE rg_test_role;
 CREATE ROLE rg_test_role SUPERUSER;
+SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
+ALTER ROLE rg_test_role RESOURCE GROUP default_group;
+SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
+ALTER ROLE rg_test_role RESOURCE GROUP none;
 SELECT rolresgroup FROM pg_authid WHERE rolname = 'rg_test_role';
 
 DROP ROLE rg_test_role;
