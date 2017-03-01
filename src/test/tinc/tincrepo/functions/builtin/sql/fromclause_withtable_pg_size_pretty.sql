c25d162a6da5fd9f133e31296b3a1365cb465450
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-03-01 08:37:39 +0200

    Remove boring tests on pg_size_pretty.
    pg_size_pretty() is completely ordinary and boring function. It doesn't
    do database access or anything like that. We can live without testing it
    in every possible context.

--- src/test/tinc/tincrepo/functions/builtin/sql/fromclause_withtable_pg_size_pretty.sql
+++
@@ -1,8 +0,0 @@
--- @description fromclause_withtable_pg_size_pretty.sql
--- @db_name builtin_functionproperty
--- @author tungs1
--- @modified 2013-04-17 12:00:00
--- @created 2013-04-17 12:00:00
--- @executemode ORCA_PLANNER_DIFF
--- @tags functionPropertiesBuiltin HAWQ
-SELECT * FROM pg_size_pretty('12345'), foo;
