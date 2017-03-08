0f21b57c87a2fbbf778f613f7606feed172d4f1a
Omer Arap <oarap@pivotal.io>
2017-03-08 07:52:30 -0800

    Bump Orca version to 2.10.0

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -120,7 +120,7 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	-Divyrepo.user=$(IVYREPO_USER) -Divyrepo.passwd="$(IVYREPO_PASSWD)" resolve);
 	@echo "Resolve finished";
 
-	wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.8.0/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+	wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.10.0/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
 
 clean_tools: opt_write_test
 	@cd releng/make/dependencies; \
