2415aff4f215daae45543a511f08d5d5392d85f3
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-03-27 10:21:09 -0700

    Rewrite kerberos tests (#2087)
    * Rewrite Kerberos test suite
    
    * Remove obsolete Kerberos test stuff from pipeline and TINC
    
    We now have a rewritten Kerberos test script in installcheck-world.
    
    * Update ICW kerberos test to run on concourse container
    
    This adds a whole new test script in src/test/regress, implemented in plain bash. It sets up temporary a KDC as part of the script, and doesn't therefore rely on a pre-existing Kerberos server, like the old MU_kerberos-smoke test job did. It does require MIT Kerberos server-side utilities to be installed, instead, but no server needs to be running, and no superuser privileges are required.
    
    This supersedes the MU_kerberos-smoke behave tests. The new rewritten bash script tests the same things:
      1. You cannot connect to the server before running 'kinit' (to verify that the server doesn't just let anyone in, which could happen if the pg_hba.conf is misconfigured for the test, for example)
      2. You can connect, after running 'kinit'
      3. You can no longer connect, if the user account is expired
    
    The new test script is hooked up to the top-level installcheck-world target.
    
    There were also some Kerberos-related tests in TINC. Remove all that, too. They didn't seem interesting in the first place, looks like they were just copies of a few random other tests, intended to be run as a smoke test, after a connection had been authenticated with Kerberos, but there was nothing in there to actually set up the Kerberos environment in TINC.
    
    Other minor patches added:
    
    * Remove absolute path when calling kerberos utilities
    -- assume they are on path, so that they can be accessed from various installs
    -- add clarification message if sample kerberos utility is not found with 'which'
    
    * Specify empty load library for kerberos tools
    
    * Move kerberos test to its own script file
    -- this allows a failure to be recorded without exiting Make, and
    therefore the server can be turned off always
    
    * Add trap for stopping kerberos server in all cases
    * Use localhost for kerberos connection
    
    Signed-off-by: Marbin Tan <mtan@pivotal.io>
    Signed-off-by: Chumki Roy <croy@pivotal.io>
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

---
+++ src/test/kerberos/installcheck.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Test Kerberos authentication
+#
+# This test script sets up a KDC for testing purposes, re-configures
+# the GPDB server to use it for authentication, and opens a test
+# connection with psql and Kerberos authentication.
+#
+# Requires a GPDB server to be running, and GPDB environment variables to be
+# set up correctly. MASTER_DATA_DIRECTORY must point to the GPDB master
+# server's data directory. (These tests need to be run on the master node,
+# because this script needs to modify the server configuration files.
+#
+# Requires MIT Kerberos KDC utilities, krb5kdc, kadmin etc. to be installed.
+#
+
+set -e  # exit on error
+
+if [ -z "$MASTER_DATA_DIRECTORY" ]; then
+  echo "FAILED: MASTER_DATA_DIRECTORY not set!"
+  exit 1
+fi
+
+export KRB5_CONFIG=$(pwd)/krb5.conf
+export KRB5_KDC_PROFILE=$(pwd)/test-kdc-db/kdc.conf
+export KRB5CCNAME=FILE:$(pwd)/krb5cc
+
+###
+# Clean up any leftovers from previous run
+make clean
+
+###
+# Save old server config
+cp ${MASTER_DATA_DIRECTORY}/pg_hba.conf ./pg_hba.conf.orig
+cp ${MASTER_DATA_DIRECTORY}/postgresql.conf ./postgresql.conf.orig
+
+###
+# Check that krb5kdc is in $PATH.
+if ! command -v krb5kdc > /dev/null; then
+    echo "Kerberos utility 'krb5kdc' not found. Is /usr/sbin or /usr/local/sbin in your PATH?"
+    exit 1
+fi
+
+# Set up KDC database, with a service principal for the server, and a user
+# principal for "krbtestuser"
+echo "Setting up test KDC..."
+bash setup-kdc.sh
+
+###
+# Launch KDC daemon. (Note that -P requires an absolute path)
+#
+# We use the -n option to prevent it from detaching, but we put  it to
+# background with &. This way, it gets killed if the terminal session is
+# closed.
+LD_LIBRARY_PATH= krb5kdc -p 7500 -r GPDB.EXAMPLE -P "$(pwd)/krb5kdc.pid" -n &
+KDC_PID=$!
+echo "KDC daemon launched, pid ${KDC_PID}"
+
+kill_kdc_pid(){
+kill ${KDC_PID}
+wait %1
+}
+
+trap kill_kdc_pid EXIT
+
+###
+# test KDC without exiting make, to ensure that server is stopped
+./test-gpdb-auth-with-kdc.sh
+SUCCESS=$?
+
+exit $SUCCESS
