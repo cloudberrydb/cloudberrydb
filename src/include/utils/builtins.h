cloudberrydb/cloudberrydb

commit fae97ae742bbfe130363ffe84ff80fb3434ea897

Remove deprecated analytical functions
Most of the builtin analytical functions added in Greenplum have
been deprecated in favour of the corresponding functionality in
MADLib. The deprecation notice was committed in a61bf8b, code as
well as tests are removed here. The sum(array[]) function require
the matrix_add() backend code and thus it remains.

This removes matrix_add(), matrix_multiply(), matrix_transpose(),
pinv(), mregr_coef(), mregr_r2(), mregr_pvalues(), mregr_tstats(),
nb_classify() and nb_probabilities().

src/include/utils/builtins.h
@@ -1102,35 +1102,11 @@ extern Datum pg_resqueue_status_kv(PG_FUNCTION_ARGS);
 /* utils/resgroup/resgroup.c */
 extern Datum pg_resgroup_get_status_kv(PG_FUNCTION_ARGS);
 
-/* utils/adt/matrix.c */
-extern Datum matrix_transpose(PG_FUNCTION_ARGS);
-extern Datum matrix_multiply(PG_FUNCTION_ARGS);
-extern Datum matrix_add(PG_FUNCTION_ARGS);
-extern Datum int8_matrix_smultiply(PG_FUNCTION_ARGS);
-extern Datum float8_matrix_smultiply(PG_FUNCTION_ARGS);
-
 /* utils/adt/pivot.c */
 Datum int4_pivot_accum(PG_FUNCTION_ARGS);
 Datum int8_pivot_accum(PG_FUNCTION_ARGS);
 Datum float8_pivot_accum(PG_FUNCTION_ARGS);
 Datum unnest(PG_FUNCTION_ARGS);
-
-/* utils/stat/bayes.c */
-extern Datum nb_classify_accum(PG_FUNCTION_ARGS);
-extern Datum nb_classify_combine(PG_FUNCTION_ARGS);
-extern Datum nb_classify_final(PG_FUNCTION_ARGS);
-extern Datum nb_classify_probabilities(PG_FUNCTION_ARGS);
-
-/* utils/stat/regress.c */
-extern Datum float8_mregr_accum(PG_FUNCTION_ARGS);
-extern Datum float8_mregr_combine(PG_FUNCTION_ARGS);
-extern Datum float8_mregr_coef(PG_FUNCTION_ARGS);
-extern Datum float8_mregr_r2(PG_FUNCTION_ARGS);
-extern Datum float8_mregr_tstats(PG_FUNCTION_ARGS);
-extern Datum float8_mregr_pvalues(PG_FUNCTION_ARGS);
-
-/* utils/stat/pinv.c */
-Datum pseudoinverse(PG_FUNCTION_ARGS);
 
 /* cdb/cdbpersistentbuild.c */
 Datum gp_persistent_build_db(PG_FUNCTION_ARGS);

src/backend/utils/Makefile
@@ -10,7 +10,7 @@ include $(top_builddir)/src/Makefile.global
 
 OBJS        = fmgrtab.o session_state.o
 SUBDIRS     = adt cache datumstream error fmgr hash init mb misc mmgr resowner \
-			   resgroup resscheduler sort stat time gpmon gp workfile_manager resource_manager
+			   resgroup resscheduler sort time gpmon gp workfile_manager resource_manager
 
 include $(top_srcdir)/src/backend/common.mk
 

src/backend/utils/adt/matrix.c
@@ -8,7 +8,6 @@
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
 
-
 #define SAMESIGN(a,b)	(((a) < 0) == ((b) < 0))
 
 /*
@@ -40,311 +39,28 @@ do {															\
 } while(0)
 
 /*
- * check to see if a int64 val has overflow in addition
-*
-* Overflow check.	We basically check to see if result / arg2 gives arg1
-* again.  There are two cases where this fails: arg2 = 0 (which cannot
-* overflow) and arg1 = INT64_MIN, arg2 = -1 (where the division itself
-* will overflow and thus incorrectly match).
-*
-* Since the division is likely much more expensive than the actual
-* multiplication, we'd like to skip it where possible.  The best bang for
-* the buck seems to be to check whether both inputs are in the int32
-* range; if so, no overflow is possible.  (But that only works if we
-* really have a 64-bit int64 datatype...)
-*
-*/
-#define CHECKINT64MULT(result, arg1, arg2)												\
-do {																					\
-	if (!(arg1 >= (int32) INT_MIN && arg1 <= (int32) INT_MAX &&							\
-		  arg2 >= (int32) INT_MIN && arg2 <= (int32) INT_MAX) &&						\
-		arg2 != 0 &&																	\
-		(result / arg2 != arg1 || (arg2 == -1 && arg1 < 0 && result < 0)))				\
-		ereport(ERROR,																	\
-				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),							\
-		  errmsg("int value out of range: overflow")));			\
-} while(0)
-
-
-/*
- * matrix_transpose - transpose an array[x][y] into an array[y][x].
- */
-Datum 
-matrix_transpose(PG_FUNCTION_ARGS)
-{
-	ArrayType   *m, *result;
-	Oid         eltype;
-	int         elsize, size, ndim, databytes;
-	int         i,j;
-	int16       typlen;
-	bool        typbyval;
-	char        typalign;
-	char       *data_m, *data_r;
-	
-    m = PG_GETARG_ARRAYTYPE_P(0);
-	ndim = ARR_NDIM(m);
-	
-	/* Sanity check, transpose only valid for two dimensional arrays. */
-	if (ndim != 2)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("argument must be a two dimensional array")));
-	if (m->dataoffset || ARR_NULLBITMAP(m))
-		ereport(ERROR,
-				(errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
-				 errmsg("null array element not allowed in this context")));
-	if (ARR_ELEMTYPE(m) == InvalidOid)
-		ereport(ERROR, 
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("could not determine anyarray/anyelement type because "
-						"input has type \"unknown\"")));
-	
-	/* datatype and size of input matrix */
-	eltype = ARR_ELEMTYPE(m);
-	size   = ARR_SIZE(m);
-	databytes = size - ARR_DATA_OFFSET(m);
-	
-	/* Allocate the result matrix */
-	result = (ArrayType *) palloc(size);
-	SET_VARSIZE(result, size);
-	result->ndim = ndim;
-	result->dataoffset = 0;      /* We disallowed arrays with NULLS */
-	result->elemtype = eltype;
-	ARR_DIMS(result)[1] = ARR_DIMS(m)[0];
-	ARR_DIMS(result)[0] = ARR_DIMS(m)[1];
-	ARR_LBOUND(result)[1] = ARR_LBOUND(m)[0];
-	ARR_LBOUND(result)[0] = ARR_LBOUND(m)[1];
-	
-	/* 
-	 * If one of the dimensions was length 1 then it is a simple transpose
-	 * and we can directly copy the data 
-	 */
-	if (ARR_DIMS(m)[0] == 1 || ARR_DIMS(m)[1] == 1)
-	{
-		memcpy(ARR_DATA_PTR(result), ARR_DATA_PTR(m), databytes);
-		PG_RETURN_ARRAYTYPE_P(result);
-	}
-	
-	/* Otherwise we have a more complicated data reshuffle */
-	get_typlenbyvalalign(eltype, &typlen, &typbyval, &typalign);
-	elsize = att_align_nominal(typlen, typalign);
-	data_m = ARR_DATA_PTR(m);
-	data_r = ARR_DATA_PTR(result);
-	for (i = 0; i < ARR_DIMS(m)[1]; i++)
-	{
-		for (j = 0; j < ARR_DIMS(m)[0]; j++)
-		{
-			int index_m = i + j * ARR_DIMS(m)[1];
-			if (typlen > 0)
-			{
-				char *seek = data_m + (elsize * index_m);
-				memcpy(data_r, seek, elsize);
-				data_r = (char *) att_align_nominal(data_r+elsize, typalign);
-			}
-			else 
-			{
-				char *seek = data_m;
-				int k;
-				for (k = 0; k < index_m; k++) 
-				{
-					seek += VARSIZE(seek);
-					seek = (char*) att_align_nominal(seek, typalign);
-				}
-				memcpy(data_r, seek, VARSIZE(seek));
-				data_r = (char *) att_align_nominal(data_r+VARSIZE(seek), typalign);
-			}
-		}
-	}
-	Assert(data_r - ARR_DATA_PTR(result) == databytes);
-	PG_RETURN_ARRAYTYPE_P(result);
-}
-
-/*
- * matrix_multipy - array multiplication of two input arrays
- */
-Datum matrix_multiply(PG_FUNCTION_ARGS)
-{
-	ArrayType  *m, *n, *result;
-	Oid         eltype;
-	int         elsize, size;
-	int         i,j,k;
-	int16       typlen;
-	bool        typbyval;
-	char        typalign;
-	char       *data_m, *data_n, *data_r;
-	
-    m = PG_GETARG_ARRAYTYPE_P(0);
-    n = PG_GETARG_ARRAYTYPE_P(1);
-	eltype = ARR_ELEMTYPE(m);
-	
-	/* Do all error checking */
-	if (ARR_NDIM(m) != 2 || ARR_NDIM(n) != 2)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("argument must be a two dimensional array")));
-	if (ARR_DIMS(m)[1] != ARR_DIMS(n)[0])
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("non-conformable arrays")));
-	if (m->dataoffset || ARR_NULLBITMAP(m) ||
-		n->dataoffset || ARR_NULLBITMAP(n))
-		ereport(ERROR,
-				(errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
-				 errmsg("null array element not allowed in this context")));
-	if (!OidIsValid(ARR_ELEMTYPE(m)) || !OidIsValid(ARR_ELEMTYPE(n)))
-		ereport(ERROR, 
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("could not determine anyarray/anyelement type because "
-						"input has type \"unknown\"")));
-	if (ARR_ELEMTYPE(m) != ARR_ELEMTYPE(n))
-		ereport(ERROR,
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("cannot multiply arrays of different element types")));
-	if (eltype != INT2OID && eltype != INT4OID && eltype != INT8OID &&
-		eltype != FLOAT4OID && eltype != FLOAT8OID /* && eltype != NUMERICOID */)
-		ereport(ERROR,
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("datatype not supported for array multiplication")));
-	
-	/* datatype and size of input matrix */
-	get_typlenbyvalalign(eltype, &typlen, &typbyval, &typalign);
-	elsize = att_align_nominal(typlen, typalign);
-	
-	/* Output array is always either int8[] or float8[] */
-	size = ARR_OVERHEAD_NONULLS(2);  /* overhead for two dimensional array */
-	size += ARR_DIMS(m)[0] * ARR_DIMS(n)[1] * sizeof(int64);
-	result = (ArrayType *) palloc(size);
-	SET_VARSIZE(result, size);
-	result->ndim = 2;
-	result->dataoffset = 0;          /* We dissallowed arrays with NULLS */
-	ARR_DIMS(result)[0] = ARR_DIMS(m)[0];
-	ARR_DIMS(result)[1] = ARR_DIMS(n)[1];
-	ARR_LBOUND(result)[0] = 1;       /* may need better lbound handling? */
-	ARR_LBOUND(result)[1] = 1;
-	switch (eltype) {
-		case INT2OID:
-		case INT4OID:
-		case INT8OID:
-			result->elemtype = INT8OID;
-			break;
-		case FLOAT4OID:
-		case FLOAT8OID:
-			result->elemtype = FLOAT8OID;
-			break;
-		default:
-			Assert(false);
-	}
-	data_r = ARR_DATA_PTR(result);
-	memset(data_r, 0, ARR_DIMS(m)[0] * ARR_DIMS(n)[1] * sizeof(int64));
-
-	/*
-	 * Integer Overflow Detecting: We should check arithmetic overflow
-	 * of int64 and float8; Below local variable is used to restore temporary
-	 * arithmetic result.
-	 *
-	 */
-	int64 int64_mul, int64_add;
-	float8 float8_mul, float8_add;
-
-	/*
-	 * Three dimensional loop over result arrays two dimensional space and
-	 * over the intersection dimension of the two input matrices.
-	 * Locality is preserved for the m matrix.
-	 *
-	 * This will need to be reworked for variable length numeric elements.
-	 */
-	for (i = 0; i < ARR_DIMS(result)[0]; i++)
-	{
-		for (j = 0; j < ARR_DIMS(result)[1]; j++)
-		{
-			data_m = ARR_DATA_PTR(m) + i * ARR_DIMS(m)[1] * elsize;
-			for (k = 0; k < ARR_DIMS(m)[1]; k++)
-			{
-				data_n = ARR_DATA_PTR(n) + k * ARR_DIMS(n)[1] * elsize;
-				switch (eltype) {
-					case INT2OID:
-						/*
-						 * int16 * int16 -> int64 will never overflow
-						 * int64 + int64 -> int64 will overflow, need detection
-						 */
-						int64_mul = ((int16*)data_n)[j] * ((int16*)data_m)[k];
-						int64_add = *(int64*)data_r + int64_mul;
-						CHECKINTADD(int64_add, *(int64*)data_r, int64_mul);
-						*(int64*)data_r = int64_add;
-						break;
-					case INT4OID:
-						/*
-						 * int32 * int32 -> int64 will never overflow
-						 * int64 + int64 -> int64 will overflow, need detection
-						 */
-						int64_mul = ((int32*)data_n)[j] * ((int32*)data_m)[k];
-						int64_add = *(int64*)data_r + int64_mul;
-						CHECKINTADD(int64_add, *(int64*)data_r, int64_mul);
-						*(int64*)data_r = int64_add;
-						break;
-					case INT8OID:
-						/*
-						 * int64 * int64 -> int64 could overflow, need detection
-						 * int64 + int64 -> int64 will overflow, need detection
-						 */
-						int64_mul = ((int64*)data_n)[j] * ((int64*)data_m)[k];
-						int64_add = *(int64*)data_r + int64_mul;
-						CHECKINT64MULT(int64_mul, ((int64*)data_n)[j], ((int64*)data_m)[k]);
-						CHECKINTADD(int64_add, *(int64*)data_r, int64_mul);
-						*(int64*)data_r = int64_add;
-						break;
-					case FLOAT4OID:
-						float8_mul = ((float*)data_n)[j] * ((float*)data_m)[k];
-						float8_add = *(float8*)data_r + float8_mul;
-						/*
-						 * check overflow of multiply
-						 */
-						CHECKFLOATVAL(float8_mul, isinf(((float*)data_n)[j]) || isinf(((float*)data_m)[k]),
-								((float*)data_n)[j] == 0 || ((float*)data_m)[k] == 0);
-						CHECKFLOATVAL(float8_add, isinf(*(float8*)data_r) || isinf(float8_mul), true);
-						*(float8*)data_r = float8_add;
-						break;
-					case FLOAT8OID:
-						float8_mul = ((float8*)data_n)[j] * ((float8*)data_m)[k];
-						float8_add = *(float8*)data_r + float8_mul;
-						/*
-						 * check overflow of multiply
-						 */
-						CHECKFLOATVAL(float8_mul, isinf(((float8*)data_n)[j]) || isinf(((float8*)data_m)[k]),
-								((float8*)data_n)[j] == 0 || ((float8*)data_m)[k] == 0);
-						CHECKFLOATVAL(float8_add, isinf(*(float8*)data_r) || isinf(float8_mul), true);
-						*(float8*)data_r = float8_add;
-						break;
-					default:
-						Assert(false);
-				}
-			}
-			data_r += 8;
-		}
-	}
-	PG_RETURN_ARRAYTYPE_P(result);
-}
-
-/*
  * matrix_add - array summation over two input arrays
  */
-Datum matrix_add(PG_FUNCTION_ARGS)
+Datum
+matrix_add(PG_FUNCTION_ARGS)
 {
-	ArrayType  *m, *n;
-	Oid         mtype, ntype;
-	int         i, ndim, len;
-	bool        transition_function;
+	ArrayType  *m;
+	ArrayType  *n;
+	Oid			mtype;
+	Oid			ntype;
+	int			i;
+	int			ndim;
+	int			len;
+	bool		transition_function;
 	
 	/* If we're in a transition function we can be smarter */
 	transition_function = fcinfo->context && IsA(fcinfo->context, AggState);
 	
 	/* Validate arguments */
 	if (PG_NARGS() != 2) 
-    {
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				 errmsg("matrix_add called with %d arguments", PG_NARGS())));
-	}   
 	
 	/* 
 	 * This function is sometimes strict, and sometimes not in order to deal
@@ -1057,108 +773,3 @@ Datum matrix_add(PG_FUNCTION_ARGS)
 		PG_RETURN_ARRAYTYPE_P(result);
 	} 
 }
-
-
-/*
- * int8_matrix_smultiply - scalar multiple of input array by an int8
- */
-Datum int8_matrix_smultiply(PG_FUNCTION_ARGS)
-{
-	ArrayType  *m, *result;
-	int64      *data_r;
-	int64       scalar;
-	int         size, nelem, ndim;
-	int         i;
-	
-	if (PG_NARGS() != 2)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("matrix_smultiply_int8 called with %d arguments", 
-						PG_NARGS())));
-	if (PG_ARGISNULL(0) || PG_ARGISNULL(1))
-		PG_RETURN_NULL();
-	
-	scalar = PG_GETARG_INT64(1);
-    m = PG_GETARG_ARRAYTYPE_P(0);
-	ndim = ARR_NDIM(m);
-	
-	/* Do all error checking */
-	if (ARR_ELEMTYPE(m) != INT8OID)
-		ereport(ERROR,
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("matrix_smultiply_int8 datatype mismatch")));
-	
-	/* count number of elements */
-	for (nelem = 1, i = 0; i < ndim; i++)
-		nelem *= ARR_DIMS(m)[i];
-	
-	/* Make a copy of the input matrix */
-	size = VARSIZE(m);
-	result = (ArrayType *) palloc(size);
-	SET_VARSIZE(result, size);
-	memcpy(result, m, size);
-	
-	/* And multiply by the scalar in place */
-	data_r = (int64*) ARR_DATA_PTR(result);
-	int64 mult_r;
-	for (i = 0; i < nelem; i++){
-		/* flow checking */
-		mult_r = data_r[i] * scalar;
-		CHECKINT64MULT(mult_r, data_r[i], scalar);
-		data_r[i] = mult_r;
-	}
-	PG_RETURN_ARRAYTYPE_P(result);
-}
-
-/*
- * float8_matrix_smultiply - scalar multiple of input array by a float8
- */
-Datum float8_matrix_smultiply(PG_FUNCTION_ARGS)
-{
-	ArrayType  *m, *result;
-	float8     *data_r;
-	float8      scalar;
-	int         size, nelem, ndim;
-	int         i;
-	
-	if (PG_NARGS() != 2)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("matrix_smultiply_float8 called with %d arguments", 
-						PG_NARGS())));
-	if (PG_ARGISNULL(0) || PG_ARGISNULL(1))
-		PG_RETURN_NULL();
-	
-	scalar = PG_GETARG_FLOAT8(1);
-    m = PG_GETARG_ARRAYTYPE_P(0);
-	ndim = ARR_NDIM(m);
-	
-	/* Do all error checking */
-	if (ARR_ELEMTYPE(m) != FLOAT8OID)
-		ereport(ERROR,
-				(errcode(ERRCODE_DATATYPE_MISMATCH),
-				 errmsg("matrix_smultiply_float8 datatype mismatch")));
-	
-	/* count number of elements */
-	for (nelem = 1, i = 0; i < ndim; i++)
-		nelem *= ARR_DIMS(m)[i];
-	
-	/* Make a copy of the input matrix */
-	size = VARSIZE(m);
-	result = (ArrayType *) palloc(size);
-	SET_VARSIZE(result, size);
-	memcpy(result, m, size);
-	
-	/* And multiply by the scalar in place */
-	data_r = (float8*) ARR_DATA_PTR(result);
-	float8 mult_r;
-	for (i = 0; i < nelem; i++){
-		mult_r = data_r[i] * scalar;
-		CHECKFLOATVAL(mult_r, isinf(data_r[i]) || isinf(scalar),
-				data_r[i] == 0 || scalar == 0);
-		data_r[i] = mult_r;
-	}
-	PG_RETURN_ARRAYTYPE_P(result);
-}
-
-

src/backend/utils/stat/Makefile
@@ -1,14 +1,0 @@
-#
-# Makefile for utils/stat
-#
-# $PostgreSQL: pgsql/src/backend/utils/stat/Makefile,v 1.60 2006/04/05 22:11:55 tgl Exp $
-#
-
-subdir = src/backend/utils/stat
-top_builddir = ../../../..
-include $(top_builddir)/src/Makefile.global
-
-OBJS = bayes.o pinv.o regress.o student.o
-
-include $(top_srcdir)/src/backend/common.mk
-

commit d12174de93d58209c9954d7c60d9a0d2a915aed8

Put back prototype for matrix_add.
Commit fae97ae742 accidentally removed the prototype for matrix_add(), even
though it didn't remove the function itself. Put back the prototype, to
silence compiler warning about missing prototype.

src/include/utils/builtins.h
@@ -1102,6 +1102,9 @@ extern Datum pg_resqueue_status_kv(PG_FUNCTION_ARGS);
 /* utils/resgroup/resgroup.c */
 extern Datum pg_resgroup_get_status_kv(PG_FUNCTION_ARGS);
 
+/* utils/adt/matrix.c */
+extern Datum matrix_add(PG_FUNCTION_ARGS);
+
 /* utils/adt/pivot.c */
 Datum int4_pivot_accum(PG_FUNCTION_ARGS);
 Datum int8_pivot_accum(PG_FUNCTION_ARGS);