6ccaa17bf006bc2bceb76b0e1c858d63d67305bc
Dhanashree Kashid <dkashid@pivotal.io>
2017-03-30 11:25:54 -0700

    Fix publishing osx related artifacts
    Signed-off-by: Omer Arap <oarap@pivotal.io>

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -8,8 +8,6 @@ main() {
   env LC_ALL=C tar tf bin_orca_centos5_release/*.tar.gz | grep "libgpopt.so." | sort -n | head -n 1 | sed 's/\.\/lib\/libgpopt\.so\./v/' > orca_github_release_stage/tag.txt
   cp -v bin_orca_centos5_release/*.tar.gz orca_github_release_stage/
   cp -v bin_orca_centos5_debug/*.tar.gz orca_github_release_stage/
-  cp -v bin_orca_osx32_release/*.tar.gz orca_github_release_stage/
-  cp -v bin_orca_osx32_debug/*.tar.gz orca_github_release_stage/
   env GIT_DIR=orca_src/.git git rev-parse HEAD > orca_github_release_stage/commit.txt
 }
 
