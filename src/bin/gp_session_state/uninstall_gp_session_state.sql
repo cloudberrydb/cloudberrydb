94c762d7aed3cc72880fd268f30b45ca2814b604
foyzur <foyzur@gmail.com>
2017-04-12 11:13:34 -0700

    Renaming session_state_memory_entries to session_level_memory_consumption in uninstall script for the session state view (#2208)
    * Renaming session_state_memory_entries to session_level_memory_consumption in uninstall script for the session state view.
    
    * Adding ICW test to check the installation/uninstallation, shape and basic content of session_level_memory_consumption.
    
    * Changing expected file to a dynamically generated one.

--- src/bin/gp_session_state/uninstall_gp_session_state.sql
+++ src/bin/gp_session_state/uninstall_gp_session_state.sql
@@ -3,7 +3,7 @@ SET search_path = session_state;
 
 BEGIN;
 
-DROP VIEW session_state_memory_entries; 
+DROP VIEW session_level_memory_consumption; 
 DROP FUNCTION session_state_memory_entries_f();
 DROP SCHEMA session_state;
 
