1cfb8183dbff265e6e56e82a6deb62a7f5f0963f
Asim R P <apraveen@pivotal.io>
2017-04-19 14:51:05 -0700

    Fix partition_locking test that fails intermittently
    Objective of the test is only to check if DDL/DML on partition tables lock only
    the root table on QD.  Child partitions are locked on QE as needed.  The
    intermittent failures are due to more locks acquired than expected.  In all
    such cases, the additional locks were AccessShare locks on some catalog table.
    This commit filters out locks on catalog tables from expected output.
    
    The intermittent failures merit investigation but that can happen regardless of
    this test fix.

--- src/test/regress/sql/partition_locking.sql
+++ src/test/regress/sql/partition_locking.sql
@@ -63,57 +63,57 @@ SELECT coalesce(
 begin;
 
 -- creation
-create table g (i int, t text) partition by range(i)
+create table partlockt (i int, t text) partition by range(i)
 (start(1) end(10) every(1));
 
-select * from locktest_master;
-select * from locktest_segments;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- drop
 begin;
-drop table g;
-select * from locktest_master;
-select * from locktest_segments;
+drop table partlockt;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- AO table (ao segments, block directory won't exist after create)
 begin;
 -- creation
-create table g (i int, t text, n numeric)
+create table partlockt (i int, t text, n numeric)
 with (appendonly = true)
 partition by list(i)
 (values(1), values(2), values(3));
-select * from locktest_master;
-select * from locktest_segments;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 begin;
 
 -- add a little data
-insert into g values(1), (2), (3);
-insert into g values(1), (2), (3);
-insert into g values(1), (2), (3);
-insert into g values(1), (2), (3);
-insert into g values(1), (2), (3);
-select * from locktest_master;
-select * from locktest_segments;
+insert into partlockt values(1), (2), (3);
+insert into partlockt values(1), (2), (3);
+insert into partlockt values(1), (2), (3);
+insert into partlockt values(1), (2), (3);
+insert into partlockt values(1), (2), (3);
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- drop
 begin;
-drop table g;
-select * from locktest_master;
-select * from locktest_segments;
+drop table partlockt;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- Indexing
-create table g (i int, t text) partition by range(i)
+create table partlockt (i int, t text) partition by range(i)
 (start(1) end(10) every(1));
 
 begin;
-create index g_idx on g(i);
-select * from locktest_master;
-select * from locktest_segments;
+create index partlockt_idx on partlockt(i);
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- Force use of the index in the select and delete below. We're not interested
@@ -124,30 +124,30 @@ set enable_seqscan=off;
 
 -- test select locking
 begin;
-select * from g where i = 1;
+select * from partlockt where i = 1;
 -- Known_opt_diff: MPP-20936
-select * from locktest_master;
-select * from locktest_segments;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 begin;
 -- insert locking
-insert into g values(3, 'f');
-select * from locktest_master;
-select * from locktest_segments;
+insert into partlockt values(3, 'f');
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- delete locking
 begin;
-delete from g where i = 4;
+delete from partlockt where i = 4;
 -- Known_opt_diff: MPP-20936
-select * from locktest_master;
-select * from locktest_segments;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
 
 -- drop index
 begin;
-drop table g;
-select * from locktest_master;
-select * from locktest_segments;
+drop table partlockt;
+select * from locktest_master where coalesce not like 'gp_%' and coalesce not like 'pg_%';
+select * from locktest_segments where coalesce not like 'gp_%' and coalesce not like 'pg_%';
 commit;
