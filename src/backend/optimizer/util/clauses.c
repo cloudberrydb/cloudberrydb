cloudberrydb/cloudberrydb

commit 9a817d453550776ee02a2a0d73a33b8870842fac

Transform small Array constants to ArrayExprs.
ORCA can do some optimizations - partition pruning at least - if it can
"see" into the elements of an array in a ScalarArrayOpExpr. For example, if
you have a qual like "column IN (1, 2, 3)", and the table is partitioned on
column, it can eliminate partitions that don't hold those values. The
IN-clause is converted into an ScalarArrayOpExpr, so that is really
equivalent to "column = ANY <array>"

However, ORCA doesn't know how to extract elements from an array-typed
Const, so it can only do that if the array in the ScalarArrayOpExpr is
an ArrayExpr. Normally, eval_const_expressions() simplifies any ArrayExprs
into Const, if all the elements are constants, but we had disabled that
when ORCA was used, to keep the ArrayExprs visible to it.

There are a couple of reasons why that was not a very good solution. First,
while we refrain from converting an ArrayExpr to an array Const, it doesn't
help if the argument was an array Const to begin with. The "x IN (1,2,3)"
construct is converted to an ArrayExpr by the parser, but we would miss the
opportunity if it's written as "x = ANY ('{1,2,3}'::int[])" instead.
Secondly, by not simplifying the ArrayExpr, we miss the opportunity to
simplify the expression further. For example, if you have a qual like
"1 IN (1,2)", we can evaluate that completely at plan time to 'true', but
we would not do that with ORCA because the ArrayExpr was not simplified.

To be able to also optimize those cases, and to slighty reduce our diff
vs upstream in clauses.c, always simplify ArrayExprs to Consts, when
possible. To compensate, so that ORCA still sees ArrayExprs rather than
array Consts (in those cases where it matters), when a ScalarArrayOpExpr
is handed over to ORCA, we check if the argument array is a Const, and
convert it (back) to an ArrayExpr if it is.

Signed-off-by: Jemish Patel <jpatel@pivotal.io>

src/backend/optimizer/util/clauses.c
@@ -59,7 +59,7 @@ typedef struct
 	bool		transform_stable_funcs;
 	bool		recurse_queries; /* recurse into query structures */
 	bool		recurse_sublink_testexpr; /* recurse into sublink test expressions */
-	bool		transform_saop; /* transform scalar array ops */
+	bool		transform_functions_returning_composite_values; /* transform functions returning constants of composite values */
 	Size        max_size; /* max constant binary size in bytes, 0: no restrictions */
 } eval_const_expressions_context;
 
@@ -1797,7 +1797,10 @@ fold_constants(PlannerGlobal *glob, Query *q, ParamListInfo boundParams, Size ma
 	context.transform_stable_funcs = true;	/* safe transformations only */
 	context.recurse_queries = true; /* recurse into query structures */
 	context.recurse_sublink_testexpr = false; /* do not recurse into sublink test expressions */
-	context.transform_saop = false; /* do not transform scalar array ops */
+	context.transform_functions_returning_composite_values = true;
+
+	/* when optimizer is on then do not fold functions that return a constant of composite values */
+	context.transform_functions_returning_composite_values = false;
 
 	context.max_size = max_size;
 	
@@ -1810,27 +1813,69 @@ fold_constants(PlannerGlobal *glob, Query *q, ParamListInfo boundParams, Size ma
 						);
 }
 
-/**
- * fold_arrayexpr_constants
+/*
+ * Transform a small array constant to an ArrayExpr.
  *
- * Fold array expression for optimizer
+ * This used by ORCA, to transform the array argument of a ScalarArrayExpr
+ * into an ArrayExpr. If a ScalarArrayExpr has an ArrayExpr argument, ORCA
+ * can perform some optimizations - partition pruning at least - based on
+ * the elements in the ArrayExpr. It doesn't currently know how to extract
+ * elements from an Array const, however, so to enable those optimizations
+ * in ORCA, we convert small Array Consts into corresponding ArrayExprs.
+ *
+ * If the argument is not an array constant, returns the original Const
+ * unmodified.
  */
-Node *
-fold_arrayexpr_constants(ArrayExpr *arrayexpr)
+Expr *
+transform_array_Const_to_ArrayExpr(Const *c)
 {
-	eval_const_expressions_context context;
+	Oid			elemtype;
+	int16		elemlen;
+	bool		elembyval;
+	char		elemalign;
+	int			nelems;
+	Datum	   *elems;
+	bool	   *nulls;
+	ArrayType  *ac;
+	ArrayExpr *aexpr;
+	int			i;
 
-	context.boundParams = NULL;
-	context.active_fns = NIL;	/* nothing being recursively simplified */
-	context.case_val = NULL;	/* no CASE being examined */
-	context.transform_stable_funcs = true;	/* safe transformations only */
-	context.recurse_queries = false; /* do not recurse into query structures */
-	context.recurse_sublink_testexpr = false; /* do not recurse into sublink test expressions */
-	context.transform_saop = true; /* transform scalar array ops */
+	Assert(IsA(c, Const));
 
-	context.max_size = GPOPT_MAX_FOLDED_CONSTANT_SIZE;
+	/* Does it look like the right kind of an array Const? */
+	if (c->constisnull)
+		return (Expr *) c;	/* NULL const */
 
-	return eval_const_expressions_mutator((Node *) arrayexpr, &context);
+	elemtype = get_element_type(c->consttype);
+	if (elemtype == InvalidOid)
+		return (Expr *) c;	/* not an array */
+
+	ac = (ArrayType *) c->constvalue;
+	nelems = ArrayGetNItems(ARR_NDIM(ac), ARR_DIMS(ac));
+
+	/* All set, extract the elements, and an ArrayExpr to hold them. */
+	get_typlenbyvalalign(elemtype, &elemlen, &elembyval, &elemalign);
+	deconstruct_array(ac, elemtype, elemlen, elembyval, elemalign,
+					  &elems, &nulls, &nelems);
+
+	aexpr = makeNode(ArrayExpr);
+	aexpr->array_typeid = c->consttype;
+	aexpr->element_typeid = elemtype;
+	aexpr->multidims = false;
+	aexpr->location = c->location;
+
+	for (i = 0; i < nelems; i++)
+	{
+		aexpr->elements = lappend(aexpr->elements,
+								  makeConst(elemtype,
+											-1,
+											elemlen,
+											elems[i],
+											nulls[i],
+											elembyval));
+	}
+
+	return (Expr *) aexpr;
 }
 
 /*--------------------
@@ -1884,7 +1929,6 @@ eval_const_expressions(PlannerInfo *root, Node *node)
 	context.transform_stable_funcs = true;	/* safe transformations only */
 	context.recurse_queries = false; /* do not recurse into query structures */
 	context.recurse_sublink_testexpr = true;
-	context.transform_saop = true; 	/* transform scalar array ops */
 	context.max_size = 0;
 
 	return eval_const_expressions_mutator(node, &context);
@@ -1919,7 +1963,6 @@ estimate_expression_value(PlannerInfo *root, Node *node)
 	context.transform_stable_funcs = true;	/* unsafe transformations OK */
 	context.recurse_queries = false; /* do not recurse into query structures */
 	context.recurse_sublink_testexpr = true;
-	context.transform_saop = true; 	/* transform scalar array ops */
 	context.max_size = 0;
 
 	return eval_const_expressions_mutator(node, &context);
@@ -2583,7 +2626,7 @@ eval_const_expressions_mutator(Node *node,
 
         return (Node *) saop; /* this has been walked and is a new one */
 	}
-	if (IsA(node, ArrayExpr) && context->transform_saop)
+	if (IsA(node, ArrayExpr))
 	{
 		ArrayExpr  *arrayexpr = (ArrayExpr *) node;
 		ArrayExpr  *newarray;

src/backend/gpopt/gpdbwrappers.cpp
@@ -2851,15 +2851,29 @@ gpdb::SzDefGetString
 	return NULL;
 }
 
-Node *
-gpdb::PnodeFoldArrayexprConstants
+Expr *
+gpdb::PexprTransformArrayConstToArrayExpr
 	(
-	ArrayExpr *arrayexpr
+	Const *c
 	)
 {
 	GP_WRAP_START;
 	{
-		return fold_arrayexpr_constants(arrayexpr);
+		return transform_array_Const_to_ArrayExpr(c);
+	}
+	GP_WRAP_END;
+	return NULL;
+}
+
+Node *
+gpdb::PnodeEvalConstExpressions
+	(
+	Node *node
+	)
+{
+	GP_WRAP_START;
+	{
+		return eval_const_expressions(NULL, node);
 	}
 	GP_WRAP_END;
 	return NULL;

src/backend/gpopt/translate/CTranslatorDXLToScalar.cpp
@@ -1890,7 +1890,15 @@ CTranslatorDXLToScalar::PexprArray
 	pexpr->multidims = pdxlop->FMultiDimensional();
 	pexpr->elements = PlistTranslateScalarChildren(pexpr->elements, pdxlnArray, pmapcidvar);
 
-	return (Expr *) gpdb::PnodeFoldArrayexprConstants(pexpr);
+	/*
+	 * ORCA doesn't know how to construct array constants, so it will
+	 * return any arrays as ArrayExprs. Convert them to array constants,
+	 * for more efficient evaluation at runtime. (This will try to further
+	 * simplify the elements, too, but that is most likely futile, as the
+	 * expressions were already simplified as much as we could before they
+	 * were passed to ORCA. But there's little harm in trying).
+	 */
+	return (Expr *) gpdb::PnodeEvalConstExpressions((Node *) pexpr);
 }
 
 //---------------------------------------------------------------------------

src/backend/gpopt/translate/CTranslatorScalarToDXL.cpp
@@ -52,6 +52,7 @@
 using namespace gpdxl;
 using namespace gpopt;
 
+
 //---------------------------------------------------------------------------
 //	@function:
 //		CTranslatorScalarToDXL::CTranslatorScalarToDXL
@@ -497,6 +498,14 @@ CTranslatorScalarToDXL::PdxlnScArrayCompFromExpr
 	CDXLNode *pdxlnLeft = PdxlnScOpFromExpr(pexprLeft, pmapvarcolid);
 
 	Expr *pexprRight = (Expr*) gpdb::PvListNth(pscarrayopexpr->args, 1);
+
+	// If the argument array is an array Const, try to transform it to an
+	// ArrayExpr, to allow ORCA to optimize it better. (ORCA knows how to
+	// extract elements of an ArrayExpr, but doesn't currently know how
+	// to do it from an array-typed Const.)
+	if (IsA(pexprRight, Const))
+		pexprRight = gpdb::PexprTransformArrayConstToArrayExpr((Const *) pexprRight);
+
 	CDXLNode *pdxlnRight = PdxlnScOpFromExpr(pexprRight, pmapvarcolid);
 
 	GPOS_ASSERT(NULL != pdxlnLeft);

src/include/gpopt/gpdbwrappers.h
@@ -609,8 +609,11 @@ namespace gpdb {
 	// extract string value from defelem's value
 	char *SzDefGetString(DefElem *pdefelem);
 
-	// fold array expression constant values
-	Node *PnodeFoldArrayexprConstants(ArrayExpr *parrayexpr);
+	// transform array Const to an ArrayExpr
+	Expr *PexprTransformArrayConstToArrayExpr(Const *pConst);
+
+	// transform array Const to an ArrayExpr
+	Node *PnodeEvalConstExpressions(Node *node);
 
 	// static partition selection given a PartitionSelector node
 	SelectedParts *SpStaticPartitionSelection(PartitionSelector *ps);

src/include/optimizer/clauses.h
@@ -101,7 +101,7 @@ extern Node *eval_const_expressions(PlannerInfo *root, Node *node);
 
 extern Query *fold_constants(PlannerGlobal *glob, Query *q, ParamListInfo boundParams, Size max_size);
 
-extern Node *fold_arrayexpr_constants(ArrayExpr *arrayexpr);
+extern Expr *transform_array_Const_to_ArrayExpr(Const *c);
 
 extern Node *estimate_expression_value(PlannerInfo *root, Node *node);
 

src/test/regress/expected/partition_pruning.out
@@ -2435,6 +2435,108 @@ select * from partprune_foo where b = a*5;
  5 | 25 | 5
 (1 row)
 
+-- Test with very large IN() lists 
+select get_selected_parts('explain select * from partprune_foo where b in (-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22,-23,-24,-25,-26,-27,-28,-29,-30,-31,-32,-33,-34,-35,-36,-37,-38,-39,-40,-41,-42,-43,-44,-45,-46,-47,-48,-49,-50,-51,-52,-53,-54,-55,-56,-57,-58,-59,-60,-61,-62,-63,-64,-65,-66,-67,-68,-69,-70,-71,-72,-73,-74,-75,-76,-77,-78,-79,-80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200)');
+ get_selected_parts 
+--------------------
+ [0, 0]
+(1 row)
+
+select * from partprune_foo where b in (-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22,-23,-24,-25,-26,-27,-28,-29,-30,-31,-32,-33,-34,-35,-36,-37,-38,-39,-40,-41,-42,-43,-44,-45,-46,-47,-48,-49,-50,-51,-52,-53,-54,-55,-56,-57,-58,-59,-60,-61,-62,-63,-64,-65,-66,-67,-68,-69,-70,-71,-72,-73,-74,-75,-76,-77,-78,-79,-80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200);
+ a |  b  | c  
+---+-----+----
+ 1 |  81 |  1
+ 2 |  82 |  2
+ 1 |  86 |  6
+ 2 |  87 |  7
+ 1 |  91 |  1
+ 2 |  92 |  2
+ 1 |  96 |  6
+ 2 |  97 |  7
+ 3 |  83 |  3
+ 4 |  84 |  4
+ 5 |  85 |  5
+ 3 |  88 |  8
+ 4 |  89 |  9
+ 5 |  90 | 10
+ 3 |  93 |  3
+ 4 |  94 |  4
+ 5 |  95 |  5
+ 3 |  98 |  8
+ 4 |  99 |  9
+ 5 | 100 | 10
+(20 rows)
+
+select get_selected_parts('explain select * from partprune_foo where b in (-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22,-23,-24,-25,-26,-27,-28,-29,-30,-31,-32,-33,-34,-35,-36,-37,-38,-39,-40,-41,-42,-43,-44,-45,-46,-47,-48,-49,-50,-51,-52,-53,-54,-55,-56,-57,-58,-59,-60,-61,-62,-63,-64,-65,-66,-67,-68,-69,-70,-71,-72,-73,-74,-75,-76,-77,-78,-79,-80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,419,420,421,422,423,424,425,426,427,428,429,430,431,432,433,434,435,436,437,438,439,440,441,442,443,444,445,446,447,448,449,450,451,452,453,454,455,456,457,458,459,460,461,462,463,464,465,466,467,468,469,470,471,472,473,474,475,476,477,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,519,520,521,522,523,524,525,526,527,528,529,530,531,532,533,534,535,536,537,538,539,540,541,542,543,544,545,546,547,548,549,550,551,552,553,554,555,556,557,558,559,560,561,562,563,564,565,566,567,568,569,570,571,572,573,574,575,576,577,578,579,580,581,582,583,584,585,586,587,588,589,590,591,592,593,594,595,596,597,598,599,600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615,616,617,618,619,620,621,622,623,624,625,626,627,628,629,630,631,632,633,634,635,636,637,638,639,640,641,642,643,644,645,646,647,648,649,650,651,652,653,654,655,656,657,658,659,660,661,662,663,664,665,666,667,668,669,670,671,672,673,674,675,676,677,678,679,680,681,682,683,684,685,686,687,688,689,690,691,692,693,694,695,696,697,698,699,700,701,702,703,704,705,706,707,708,709,710,711,712,713,714,715,716,717,718,719,720,721,722,723,724,725,726,727,728,729,730,731,732,733,734,735,736,737,738,739,740,741,742,743,744,745,746,747,748,749,750,751,752,753,754,755,756,757,758,759,760,761,762,763,764,765,766,767,768,769,770,771,772,773,774,775,776,777,778,779,780,781,782,783,784,785,786,787,788,789,790,791,792,793,794,795,796,797,798,799,800,801,802,803,804,805,806,807,808,809,810,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900,901,902,903,904,905,906,907,908,909,910,911,912,913,914,915,916,917,918,919,920,921,922,923,924,925,926,927,928,929,930,931,932,933,934,935,936,937,938,939,940,941,942,943,944,945,946,947,948,949,950,951,952,953,954,955,956,957,958,959,960,961,962,963,964,965,966,967,968,969,970,971,972,973,974,975,976,977,978,979,980,981,982,983,984,985,986,987,988,989,990,991,992,993,994,995,996,997,998,999,1000)');
+ get_selected_parts 
+--------------------
+ [0, 0]
+(1 row)
+
+select * from partprune_foo where b in (-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22,-23,-24,-25,-26,-27,-28,-29,-30,-31,-32,-33,-34,-35,-36,-37,-38,-39,-40,-41,-42,-43,-44,-45,-46,-47,-48,-49,-50,-51,-52,-53,-54,-55,-56,-57,-58,-59,-60,-61,-62,-63,-64,-65,-66,-67,-68,-69,-70,-71,-72,-73,-74,-75,-76,-77,-78,-79,-80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,419,420,421,422,423,424,425,426,427,428,429,430,431,432,433,434,435,436,437,438,439,440,441,442,443,444,445,446,447,448,449,450,451,452,453,454,455,456,457,458,459,460,461,462,463,464,465,466,467,468,469,470,471,472,473,474,475,476,477,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,519,520,521,522,523,524,525,526,527,528,529,530,531,532,533,534,535,536,537,538,539,540,541,542,543,544,545,546,547,548,549,550,551,552,553,554,555,556,557,558,559,560,561,562,563,564,565,566,567,568,569,570,571,572,573,574,575,576,577,578,579,580,581,582,583,584,585,586,587,588,589,590,591,592,593,594,595,596,597,598,599,600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615,616,617,618,619,620,621,622,623,624,625,626,627,628,629,630,631,632,633,634,635,636,637,638,639,640,641,642,643,644,645,646,647,648,649,650,651,652,653,654,655,656,657,658,659,660,661,662,663,664,665,666,667,668,669,670,671,672,673,674,675,676,677,678,679,680,681,682,683,684,685,686,687,688,689,690,691,692,693,694,695,696,697,698,699,700,701,702,703,704,705,706,707,708,709,710,711,712,713,714,715,716,717,718,719,720,721,722,723,724,725,726,727,728,729,730,731,732,733,734,735,736,737,738,739,740,741,742,743,744,745,746,747,748,749,750,751,752,753,754,755,756,757,758,759,760,761,762,763,764,765,766,767,768,769,770,771,772,773,774,775,776,777,778,779,780,781,782,783,784,785,786,787,788,789,790,791,792,793,794,795,796,797,798,799,800,801,802,803,804,805,806,807,808,809,810,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900,901,902,903,904,905,906,907,908,909,910,911,912,913,914,915,916,917,918,919,920,921,922,923,924,925,926,927,928,929,930,931,932,933,934,935,936,937,938,939,940,941,942,943,944,945,946,947,948,949,950,951,952,953,954,955,956,957,958,959,960,961,962,963,964,965,966,967,968,969,970,971,972,973,974,975,976,977,978,979,980,981,982,983,984,985,986,987,988,989,990,991,992,993,994,995,996,997,998,999,1000);
+ a |  b  | c  
+---+-----+----
+ 1 |  81 |  1
+ 2 |  82 |  2
+ 1 |  86 |  6
+ 2 |  87 |  7
+ 1 |  91 |  1
+ 2 |  92 |  2
+ 1 |  96 |  6
+ 2 |  97 |  7
+ 3 |  83 |  3
+ 4 |  84 |  4
+ 5 |  85 |  5
+ 3 |  88 |  8
+ 4 |  89 |  9
+ 5 |  90 | 10
+ 3 |  93 |  3
+ 4 |  94 |  4
+ 5 |  95 |  5
+ 3 |  98 |  8
+ 4 |  99 |  9
+ 5 | 100 | 10
+(20 rows)
+
+-- Test "ANY (<array>)" syntax.
+select get_selected_parts($$ explain select * from partprune_foo where b = ANY ('{5, 6, 14}') $$);
+ get_selected_parts 
+--------------------
+ [0, 0]
+(1 row)
+
+select * from partprune_foo where b = ANY ('{5, 6, 14}');
+ a | b  | c 
+---+----+---
+ 1 |  6 | 6
+ 5 |  5 | 5
+ 4 | 14 | 4
+(3 rows)
+
+select get_selected_parts($$ explain select * from partprune_foo where b < ANY ('{12, 14, 11}') $$);
+ get_selected_parts 
+--------------------
+ [0, 0]
+(1 row)
+
+select * from partprune_foo where b < ANY ('{12, 14, 11}');
+ a | b  | c  
+---+----+----
+ 1 |  1 |  1
+ 2 |  2 |  2
+ 1 |  6 |  6
+ 2 |  7 |  7
+ 1 | 11 |  1
+ 2 | 12 |  2
+ 3 |  3 |  3
+ 4 |  4 |  4
+ 5 |  5 |  5
+ 3 |  8 |  8
+ 4 |  9 |  9
+ 5 | 10 | 10
+ 3 | 13 |  3
+(13 rows)
+
 -- Check for all the different number of partition selections
 DROP TABLE IF EXISTS DATE_PARTS;
 NOTICE:  table "date_parts" does not exist, skipping

commit 175f1f3b9a0dc54c61b670e2feaf60bf9d6c1787

Remove unused field.
Commit fb93e7e73f removed this field, but commit 9a817d4535 accidentally
resurrected it. Remove it again.

src/backend/optimizer/util/clauses.c
@@ -59,7 +59,6 @@ typedef struct
 	bool		transform_stable_funcs;
 	bool		recurse_queries; /* recurse into query structures */
 	bool		recurse_sublink_testexpr; /* recurse into sublink test expressions */
-	bool		transform_functions_returning_composite_values; /* transform functions returning constants of composite values */
 	Size        max_size; /* max constant binary size in bytes, 0: no restrictions */
 } eval_const_expressions_context;
 
@@ -1797,10 +1796,6 @@ fold_constants(PlannerGlobal *glob, Query *q, ParamListInfo boundParams, Size ma
 	context.transform_stable_funcs = true;	/* safe transformations only */
 	context.recurse_queries = true; /* recurse into query structures */
 	context.recurse_sublink_testexpr = false; /* do not recurse into sublink test expressions */
-	context.transform_functions_returning_composite_values = true;
-
-	/* when optimizer is on then do not fold functions that return a constant of composite values */
-	context.transform_functions_returning_composite_values = false;
 
 	context.max_size = max_size;
 	