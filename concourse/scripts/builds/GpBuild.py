c25008b3ecad1d032171850b521b6b6c5dd0a56e
Jesse Zhang <jzhang@pivotal.io>
2017-04-27 21:03:21 -0700

    Up MASTER_MAX_CONNECT for ICG
    Commit 35e0dfdb14ce8175012c65ee722e7b00211a79cb first bumped this up for
    an isolation test.
    Higher and higher concurrency has since crept into installcheck,
    resulting in a flaky group that contains `mapred`. This hopefully
    addresses that flake.

--- concourse/scripts/builds/GpBuild.py
+++ concourse/scripts/builds/GpBuild.py
@@ -24,7 +24,7 @@ class GpBuild(GpdbBuildBase):
             return status
         status = subprocess.call([
             "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
-            && make cluster\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
+            && make cluster DEFAULT_QD_MAX_CONNECT=150\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
         if status:
             return status
         return subprocess.call([
