oarap/gpdb

Set optimizer GUC to on by default
When the GPDB is build with GPORCA, `optimizer` GUC is set to on
by default. If GPDB is not build with GPORCA this guc is off by default.

Signed-off-by: Omer Arap <oarap@pivotal.io>

src/backend/utils/misc/guc_gp.c
@@ -2444,7 +2444,12 @@ struct config_bool ConfigureNamesBool_gp[] =
 			NULL
 		},
 		&optimizer,
-		false, assign_optimizer, NULL
+#ifndef USE_ORCA
+		false,
+#else
+		true,
+#endif
+		assign_optimizer, NULL
 	},
 
 	{

cloudberrydb/cloudberrydb

src/backend/utils/misc/guc_gp.c
@@ -2432,7 +2432,12 @@ struct config_bool ConfigureNamesBool_gp[] =
 			NULL
 		},
 		&optimizer,
-		false, assign_optimizer, NULL
+#ifdef USE_ORCA
+		true,
+#else
+		false,
+#endif
+		assign_optimizer, NULL
 	},
 
 	{