c00333c265b5d80bd634e885e10deefc028afd31
Ashwin Agrawal <aagrawal@pivotal.io>
2017-05-18 11:26:30 -0700

    Remove ORCA specific answer files for pg_twophase test suite.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/pg_twophase/abort_create_tests/post_sql/sql/abort_co_ctas_post.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/pg_twophase/abort_create_tests/post_sql/sql/abort_co_ctas_post.sql
@@ -2,7 +2,6 @@
 SET gp_create_table_random_default_distribution=off;
 -- end_ignore
 CREATE TABLE cr_co_ctas WITH (appendonly=true,orientation=column) AS SELECT * FROM cr_seed_table_for_co;
-\d cr_co_ctas
 select count(*) from cr_co_ctas;
 drop table cr_co_ctas;
 drop table cr_seed_table_for_co;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/pg_twophase/abort_create_tests/post_sql/sql/abort_heap_ctas_post.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/pg_twophase/abort_create_tests/post_sql/sql/abort_heap_ctas_post.sql
@@ -2,7 +2,6 @@
 SET gp_create_table_random_default_distribution=off;
 -- end_ignore
 CREATE TABLE cr_heap_ctas AS SELECT * FROM cr_seed_table;
-\d cr_heap_ctas
 select count(*) from cr_heap_ctas;
 drop table cr_heap_ctas;
 drop table cr_seed_table;
