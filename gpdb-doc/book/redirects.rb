78104232c318f90225c75b028df309d8e20c2361
David Yozie <dyozie@pivotal.io>
2017-06-01 09:25:43 -0700

    Changing local doc build directory to /doc
    This makes it easier to generate html docs for greenplum.org/doc

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,2 +1,2 @@
-r301 '/', '/docs-gpdb/common/pivotal-gpdb-features.html'
-r301 '/index.html', '/docs-gpdb/common/pivotal-gpdb-features.html'
+r301 '/', '/docs/common/pivotal-gpdb-features.html'
+r301 '/index.html', '/docs/common/pivotal-gpdb-features.html'
