39345dd4853f1877c944a92f7b223a27b17a3d1e
David Yozie <dyozie@pivotal.io>
2017-06-14 08:25:24 -0700

    Removing pivotal references (#2619)
    * rewording/conditionalizing more Pivotal-specific info
    
    * conditionalizing/editing/or getting rid of more Pivotal-specific references and info

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,2 +1,2 @@
-r301 '/', '/docs/common/pivotal-gpdb-features.html'
-r301 '/index.html', '/docs/common/pivotal-gpdb-features.html'
+r301 '/', '/docs/common/gpdb-features.html'
+r301 '/index.html', '/docs/common/gpdb-features.html'
