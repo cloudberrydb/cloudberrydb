da3178dd28605751c7a6ac72cb1074264ad9b035
Abhijit Subramanya <asubramanya@pivotal.io>
2017-06-27 18:03:07 -0700

    Add test for heap checksums
    This test is based on ao_checksum_corruption.sql.
    
    We added new UDF `invalidate_buffers()` to invalidate buffers for given
    relation, so that we can read the content from the corrupted file again.
    
    We tested corruptions on heap table, toast table, btree and bitmap indexes.
    
    Signed-off-by: Xin Zhang <xzhang@pivotal.io>

---
+++ src/test/heap_checksum/heap_checksum_helper.c
@@ -0,0 +1,27 @@
+#include "postgres.h"
+#include "funcapi.h"
+#include "nodes/pg_list.h"
+#include "storage/buf_internals.h"
+#include "storage/bufmgr.h"
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+Datum invalidate_buffers(PG_FUNCTION_ARGS);
+
+PG_FUNCTION_INFO_V1(invalidate_buffers);
+Datum
+invalidate_buffers(PG_FUNCTION_ARGS)
+{
+	RelFileNode			rnode;
+
+	rnode.spcNode = PG_GETARG_OID(0);
+	rnode.dbNode  = PG_GETARG_OID(1);
+	rnode.relNode = PG_GETARG_OID(2);
+
+	DropRelFileNodeBuffers(rnode, false, 0);
+
+	PG_RETURN_BOOL(true);
+}
+
---
+++ src/test/heap_checksum/sql/heap_checksum_corruption.sql
@@ -0,0 +1,154 @@
+-- The following test simulates corruption of a heap relation file and
+-- verifies the select behavior on this corrupted table, the related indexes
+-- and toast tables.
+
+-- Mask out the expected and actual values of the checksums when comparing the
+-- result. All we care about is that they don't match.
+--
+-- start_matchsubs
+-- m/^ERROR:.*invalid page in block.*/
+-- s/invalid page in block.*/invalid page in block/
+-- m/^ERROR:  missing chunk number.*/
+-- s/missing chunk number.*/missing chunk number/
+-- end_matchsubs
+
+-- Ignore the status messages from the helper function. They're useful for
+-- debugging, but the output is different on every invocation.
+-- start_matchignore
+-- m/^INFO:  corrupting file/
+-- m/^INFO:  skipping non-existent file/
+-- m/^WARNING:  page verification failed, calculated checksum.*/
+-- end_matchignore
+
+-- start_ignore
+CREATE LANGUAGE plpythonu;
+-- end_ignore
+
+-- Create our test tables (and functions) in a bespoken schema that we can drop
+-- at the end. We don't want to leave any corrupt files lying around!
+CREATE SCHEMA corrupt_heap_checksum;
+set search_path='corrupt_heap_checksum';
+
+-- to ignore the CONTEXT from messages from the plpython helpers, and to ignore
+-- DETAILs from the checksum errors.
+\set VERBOSITY terse
+
+-- Return path to the file holding data for the given table (relative to
+-- $PGDATA).
+--
+CREATE FUNCTION get_relation_path(tbl regclass) returns text as $$
+  (select 'base/' || db.oid || '/' || c.relfilenode from pg_class c, pg_database db where c.oid = $1 AND db.datname = current_database())
+$$ language sql VOLATILE;
+
+-- Return path to the file holding data for the given table's TOAST table (relative to
+-- $PGDATA).
+--
+CREATE FUNCTION get_toast_path(tbl regclass) returns text as $$
+  (select 'base/' || db.oid || '/' || c.relfilenode from pg_class c, pg_database db where c.oid = ( SELECT reltoastrelid FROM pg_class  WHERE oid = $1) AND db.datname = current_database())
+$$ language sql VOLATILE;
+
+-- Return name of the given table's TOAST table (relative to
+-- $PGDATA).
+--
+CREATE FUNCTION get_toast_name(tbl regclass) returns text as $$
+  (select relname::text from pg_class where oid = ( SELECT reltoastrelid FROM pg_class  WHERE oid = $1))
+$$ language sql VOLATILE;
+
+-- Return path to the file holding data for the given table's index (relative to
+-- $PGDATA).
+--
+CREATE FUNCTION get_index_path(tbl regclass) returns text as $$
+  (select 'base/' || db.oid || '/' || c.relfilenode from pg_class c, pg_database db where c.oid = ( SELECT indexrelid FROM pg_index  WHERE indrelid = $1) AND db.datname = current_database())
+$$ language sql VOLATILE;
+
+-- Corrupt data file at given path (if it exists on this segment)
+--
+-- If corruption_offset is negative, it's an offset from the end of file.
+-- Otherwise it's from the beginning of file.
+--
+-- Returns 0. (That's handy in the way this function is called, because we can
+-- do a SUM() over the return values, and it's always 0, regardless of the
+-- number of segments in the cluster.)
+CREATE FUNCTION corrupt_file(data_file text, corruption_offset int4)
+RETURNS integer as $$
+  import os;
+
+  if not os.path.isfile(data_file):
+    plpy.info('skipping non-existent file %s' % (data_file))
+  else:
+    plpy.info('corrupting file %s at %s' % (data_file, corruption_offset))
+
+    with open(data_file , "rb+") as f:
+      char_location=0
+      write_char='*'  # CONST.CORRUPTION
+
+      if corruption_offset >= 0:
+        f.seek(corruption_offset, 0)
+      else:
+        f.seek(corruption_offset, 2)
+
+      f.write(write_char)
+      f.close()
+
+  return 0
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION invalidate_buffers_for_rel(tablename text) RETURNS BOOL AS
+$$
+DECLARE
+tablespace Oid;
+database Oid;
+relfile Oid;
+result bool;
+BEGIN
+    SELECT dattablespace, oid INTO tablespace, database FROM pg_database WHERE datname = current_database();
+    SELECT relfilenode INTO relfile FROM pg_class WHERE relname = tablename;
+    SELECT public.invalidate_buffers(tablespace, database, relfile) INTO result;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Make sure that checksum is enabled
+SHOW data_checksums;
+
+--  Corrupt a heap table
+create table corrupt_table(a int);
+insert into corrupt_table select i from generate_series(1, 10) i;
+checkpoint;
+select invalidate_buffers_for_rel('corrupt_table') from gp_dist_random('gp_id');
+-- Verify corruption on heap table
+select SUM(corrupt_file(get_relation_path('corrupt_table'), -50)) from gp_dist_random('gp_id');
+SELECT COUNT(*) FROM corrupt_table;
+
+-- Corrupt a heap table, with toast table 
+create table corrupt_toast_table(a int, comment bytea);
+insert into corrupt_toast_table select i, ("decode"(repeat('a',3000000),'escape')) from generate_series(1,10) i;
+checkpoint;
+select invalidate_buffers_for_rel(get_toast_name('corrupt_toast_table')) from gp_dist_random('gp_id');
+-- Verify corruption on toast table
+select SUM(corrupt_file(get_toast_path('corrupt_toast_table'), -50)) from gp_dist_random('gp_id');
+SELECT md5(comment::text) FROM corrupt_toast_table;
+
+-- Corrupt a Btree Index
+create table corrupt_btree_index(a int, b char(50)); 
+create index btree_index on corrupt_btree_index(a);
+insert into corrupt_btree_index select i, 'a' from generate_series(1, 10) i;
+checkpoint;
+select invalidate_buffers_for_rel('btree_index') from gp_dist_random('gp_id');
+-- Verify corruption on Btree index
+select SUM(corrupt_file(get_index_path('corrupt_btree_index'), -50)) from gp_dist_random('gp_id');
+insert into corrupt_btree_index select i, 'a' from generate_series(1, 10) i; -- insert will trigger scan of the index
+
+-- Corrupt a Bitmap Index 
+create table corrupt_bitmap_index(a int, b char(50)); 
+create index bitmap_index on corrupt_bitmap_index(a);
+insert into corrupt_bitmap_index select i, 'a' from generate_series(1, 10) i;
+checkpoint;
+select invalidate_buffers_for_rel('bitmap_index') from gp_dist_random('gp_id');
+-- Verify corruption on Bitmap index 
+select SUM(corrupt_file(get_index_path('corrupt_bitmap_index'), -50)) from gp_dist_random('gp_id');
+insert into corrupt_bitmap_index select i, 'a' from generate_series(1, 10) i; -- insert will trigger scan of the index
+
+-- Clean up. We don't want to leave the corrupt tables lying around!
+reset search_path;
+DROP SCHEMA corrupt_heap_checksum CASCADE;
