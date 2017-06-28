d34d4f8933d7a511ed462e99bc08b5c009fe9fe5
Nadeem Ghani <nghani@pivotal.io>
2017-06-28 10:22:23 -0700

    Whitespace reformat only
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

--- gpMgmt/bin/gppylib/util/ssh_utils.py
+++ gpMgmt/bin/gppylib/util/ssh_utils.py
@@ -15,23 +15,31 @@ from gppylib.commands.unix import Hostname, Echo
 sys.path.insert(1, sys.path[0] + '/lib')
 from pexpect import pxssh
 
+
 class HostNameError(Exception):
-    def __init__(self, msg, lineno = 0):
-        if lineno: self.msg = ('%s at line %d' % (msg, lineno))
-        else: self.msg = msg
+    def __init__(self, msg, lineno=0):
+        if lineno:
+            self.msg = ('%s at line %d' % (msg, lineno))
+        else:
+            self.msg = msg
+
     def __str__(self):
         return self.msg
+
 
 class SSHError(Exception):
     def __init__(self, msg):
         self.msg = msg
+
     def __str__(self):
         return self.msg
+
 
 # Utility Functions
 def ssh_prefix(host):
     ssh = 'ssh -o "BatchMode yes" -o "StrictHostKeyChecking no" ' + host
     return ssh
+
 
 def get_hosts(hostsfile):
     hostlist = HostList()
@@ -40,11 +48,10 @@ def get_hosts(hostsfile):
 
 
 class HostList():
-    
-    def __init__(self): 
+    def __init__(self):
         self.list = []
 
-    def get(self): 
+    def get(self):
         return self.list
 
     def add(self, host, lineno=0):
@@ -78,9 +85,9 @@ class HostList():
         with open(path) as fp:
             for i, line in enumerate(fp):
                 line = line.strip()
-                if not line or line[0] == '#': 
+                if not line or line[0] == '#':
                     continue
-                self.add(line, i+1)
+                self.add(line, i + 1)
         return self.list
 
     def checkSSH(self):
@@ -91,10 +98,9 @@ class HostList():
         for h in self.list:
             cmd = Echo('ssh test', '', ctxt=REMOTE, remoteHost=h)
             pool.addCommand(cmd)
-            
-        pool.join()
-        pool.haltWork()  
 
+        pool.join()
+        pool.haltWork()
 
         for cmd in pool.getCompletedItems():
             if not cmd.get_results().wasSuccessful():
@@ -110,10 +116,10 @@ class HostList():
         for h in self.list:
             cmd = Hostname('hostname', ctxt=REMOTE, remoteHost=h)
             pool.addCommand(cmd)
-            
+
         pool.join()
         pool.haltWork()
-        
+
         for finished_cmd in pool.getCompletedItems():
             hostname = finished_cmd.get_hostname()
             if (not hostname):
@@ -124,18 +130,23 @@ class HostList():
                 unique[hostname] = finished_cmd.remoteHost
 
         self.list = unique.values()
-        
+
         return self.list
+
 
 # Session is a command session, derived from a base class cmd.Cmd
 class Session(cmd.Cmd):
     '''Implements a list of open ssh sessions ready to execute commands'''
-    verbose=False
-    hostList=[]
-    userName=None
-    echoCommand=False
-    class SessionError(StandardError): pass
-    class SessionCmdExit(StandardError): pass
+    verbose = False
+    hostList = []
+    userName = None
+    echoCommand = False
+
+    class SessionError(StandardError):
+        pass
+
+    class SessionCmdExit(StandardError):
+        pass
 
     def __init__(self, hostList=None, userName=None):
         cmd.Cmd.__init__(self)
@@ -145,7 +156,7 @@ class Session(cmd.Cmd):
         if hostList:
             for host in hostList:
                 self.hostList.append(host)
-        if userName: self.userName=userName
+        if userName: self.userName = userName
 
     def peerStringFormat(self):
         if self.peerStringFormatRaw: return self.peerStringFormatRaw
@@ -162,15 +173,15 @@ class Session(cmd.Cmd):
             raise self.SessionError('No host list available to Login method')
         if not (self.userName or userName):
             raise self.SessionError('No user name available to Login method')
-        
-        #Cleanup    
+
+        # Cleanup
         self.clean()
-    
-        if hostList: #We have a new hostlist to use, initialize it
-            self.hostList=[]
+
+        if hostList:  # We have a new hostlist to use, initialize it
+            self.hostList = []
             for host in hostList:
                 self.hostList.append(host)
-        if userName: self.userName=userName  #We have a new userName to use
+        if userName: self.userName = userName  # We have a new userName to use
 
         # MPP-6583.  Save off term type and set to nothing before creating ssh process
         origTERM = os.getenv('TERM', None)
@@ -221,12 +232,12 @@ class Session(cmd.Cmd):
 
     def close(self):
         return self.clean()
-    
+
     def reset(self):
         '''reads from all the ssh connections to make sure we dont have any pending cruft'''
         for s in self.pxssh_list:
             s.readlines()
-                
+
     def clean(self):
         net_return_code = self.closePxsshList(self.pxssh_list)
         self.pxssh_list = []
@@ -235,7 +246,7 @@ class Session(cmd.Cmd):
     def emptyline(self):
         pass
 
-    def escapeLine(self,line):
+    def escapeLine(self, line):
         '''Escape occurrences of \ and $ as needed and package the line as an "eval" shell command'''
         line = line.strip()
         if line == 'EOF' or line == 'exit' or line == 'quit':
@@ -247,75 +258,77 @@ class Session(cmd.Cmd):
         line = line.split('$')
         line = '\\$'.join(line)
         line = 'eval "' + line + '" < /dev/null'
-        
+
         return line
 
-    def executeCommand(self,command):
-        commandoutput=[]
-        
+    def executeCommand(self, command):
+        commandoutput = []
+
         if self.echoCommand:
             escapedCommand = command.replace('"', '\\"')
             command = 'echo "%s"; %s' % (escapedCommand, command)
-            
-        #Execute the command in all of the ssh sessions
+
+        # Execute the command in all of the ssh sessions
         for s in self.pxssh_list:
             s.sendline(command)
-            
-        #Wait for each command and retrieve the output
+
+        # Wait for each command and retrieve the output
         for s in self.pxssh_list:
-            #Wait for each command to finish
-            #!! TODO verify that this is a tight wait loop and find another way to do this
+            # Wait for each command to finish
+            # !! TODO verify that this is a tight wait loop and find another way to do this
             while not s.prompt(120) and s.isalive() and not s.eof(): pass
-            
+
         for s in self.pxssh_list:
-            #Split the output into an array of lines so that we can add text to the beginning of
+            # Split the output into an array of lines so that we can add text to the beginning of
             #    each line
             output = s.before.split('\n')
             output = output[1:-1]
-                
+
             commandoutput.append(output)
-            
+
         return commandoutput.__iter__()
 
-# Interactive command line handler
-#    Override of base class, handles commands that aren't recognized as part of a predefined set
-#    The "command" argument is a command line to be executed on all available command sessions
-#    The output of the command execution is printed to the standard output, prepended with
-#        the hostname of each machine from which the output came
+    # Interactive command line handler
+    #    Override of base class, handles commands that aren't recognized as part of a predefined set
+    #    The "command" argument is a command line to be executed on all available command sessions
+    #    The output of the command execution is printed to the standard output, prepended with
+    #        the hostname of each machine from which the output came
     def default(self, command):
-    
+
         line = self.escapeLine(command)
-    
+
         if self.verbose: print command
-        
-    #Execute the command on our ssh sessions
-        commandoutput=self.executeCommand(command)
+
+        # Execute the command on our ssh sessions
+        commandoutput = self.executeCommand(command)
         self.writeCommandOutput(commandoutput)
 
-    def writeCommandOutput(self,commandoutput):
+    def writeCommandOutput(self, commandoutput):
         '''Takes a list of output lists as an iterator and writes them to standard output,
         formatted with the hostname from which each output array was obtained'''
         for s in self.pxssh_list:
             output = commandoutput.next()
-            #Write the output
+            # Write the output
             if len(output) == 0:
                 print (self.peerStringFormat() % s.x_peer)
             else:
                 for line in output:
                     print (self.peerStringFormat() % s.x_peer), line
-    
-    def closePxsshList(self,list):
+
+    def closePxsshList(self, list):
         lock = threading.Lock()
         return_codes = [0]
-        def closePxsshOne(p, return_codes): 
+
+        def closePxsshOne(p, return_codes):
             p.logout()
             with lock:
                 return_codes.append(p.exitstatus)
+
         th = []
         for p in list:
             t = threading.Thread(target=closePxsshOne, args=(p, return_codes))
             t.start()
             th.append(t)
         for t in th:
-            t.join() 
+            t.join()
         return max(return_codes)
