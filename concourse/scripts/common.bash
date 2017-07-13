4fecddd19b49bbeb0ff7088cd521847a7bc936ba
Xin Zhang <xzhang@pivotal.io>
2017-07-12 18:22:22 -0700

    Fix bug in common.bash for make_cluster
    Signed-off-by: Abhijit Subramanya <asubramanya@pivotal.io>

--- concourse/scripts/common.bash
+++ concourse/scripts/common.bash
@@ -42,9 +42,9 @@ function make_cluster() {
   workaround_before_concourse_stops_stripping_suid_bits
   pushd gpdb_src/gpAux/gpdemo
     if [[ $CONFIGURE_FLAGS == *"--enable-segwalrep"* ]]; then
-      su gpadmin -c make create-segwalrep-cluster
+      su gpadmin -c "make create-segwalrep-cluster"
     else
-      su gpadmin -c make create-demo-cluster
+      su gpadmin -c "make create-demo-cluster"
     fi
   popd
 }
