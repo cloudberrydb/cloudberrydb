bd26a2689e917620e37642901ad3e4a60fdaf549
Asim R P <apraveen@pivotal.io>
2017-07-21 13:55:41 -0700

    Move gp_inject_fault function to pg_regress/regress.c
    This fixes ICW breakage due to "postgres" binary cannot be loaded as a shared
    library.  To run gp_fault_inject() function manually, generate regress.so by
    running make in src/test/regress.  Thereafter, create function command can be
    used to create the function, as in create_fault_function.source.

--- src/backend/utils/misc/faultinjector.c
+++ src/backend/utils/misc/faultinjector.c
@@ -17,33 +17,23 @@
  */
 
 #include "postgres.h"
-#include "funcapi.h"
 
 #include <signal.h>
 
-#include "access/heapam.h"
-#include "access/genam.h"
 #include "access/xact.h"
 #include "cdb/cdbfilerep.h"
 #include "cdb/cdbfilerepservice.h"
 #include "cdb/cdbresynchronizechangetracking.h"
 #include "cdb/cdbutil.h"
-#include "cdb/cdbvars.h"
-#include "libpq/ip.h"
 #include "postmaster/bgwriter.h"
 #include "postmaster/fts.h"
-#include "postmaster/postmaster.h"
-#include "postmaster/primary_mirror_transition_client.h"
 #include "storage/spin.h"
 #include "storage/shmem.h"
-#include "utils/builtins.h"
 #include "utils/faultinjector.h"
-#include "utils/fmgroids.h"
 #include "utils/hsearch.h"
 #include "miscadmin.h"
 
 #ifdef FAULT_INJECTOR
-extern Datum gp_inject_fault(PG_FUNCTION_ARGS);
 
 /*
  * gettext() can't be used in a static initializer... This breaks nls builds.
@@ -1542,138 +1532,5 @@ exit:
 						FaultInjectorTypeEnumToString[entry->faultInjectorType])));
 	}
 	return isCompleted;
-}
-
-static StringInfo transitionMsgErrors;
-
-static void
-transitionErrorLogFn(char *response)
-{
-	appendStringInfo(transitionMsgErrors, "%s\n", response);
-}
-
-static void
-transitionReceivedDataFn(char *response)
-{
-	elog(NOTICE, "%s", response);
-}
-
-static bool
-checkForNeedToExitFn(void)
-{
-	CHECK_FOR_INTERRUPTS();
-	return false;
-}
-
-PG_FUNCTION_INFO_V1(gp_inject_fault);
-Datum
-gp_inject_fault(PG_FUNCTION_ARGS)
-{
-	char	   *faultName = TextDatumGetCString(PG_GETARG_DATUM(0));
-	char	   *type = TextDatumGetCString(PG_GETARG_DATUM(1));
-	char	   *ddlStatement = TextDatumGetCString(PG_GETARG_DATUM(2));
-	char	   *databaseName = TextDatumGetCString(PG_GETARG_DATUM(3));
-	char	   *tableName = TextDatumGetCString(PG_GETARG_DATUM(4));
-	int			numOccurrences = PG_GETARG_INT32(5);
-	int			sleepTimeSeconds = PG_GETARG_INT32(6);
-	int         dbid = PG_GETARG_INT16(7);
-	StringInfo  faultmsg = makeStringInfo();
-
-	/* Fast path if injecting fault in our postmaster. */
-	if (GpIdentity.dbid == dbid)
-	{
-		appendStringInfo(faultmsg, "%s\n%s\n%s\n%s\n%s\n%d\n%d\n",
-						 faultName, type, ddlStatement, databaseName,
-						 tableName, numOccurrences, sleepTimeSeconds);
-		int offset = 0;
-		char *response =
-			processTransitionRequest_faultInject(
-				faultmsg->data, &offset, faultmsg->len);
-		if (!response)
-			elog(ERROR, "failed to inject fault locally (dbid %d)", dbid);
-		if (strncmp(response, "Success:",  strlen("Success:")) != 0)
-			elog(ERROR, "%s", response);
-
-		elog(NOTICE, "%s", response);
-		PG_RETURN_DATUM(true);
-	}
-
-	/* Obtain host and port of the requested dbid */
-	HeapTuple tuple;
-	Relation rel = heap_open(GpSegmentConfigRelationId, AccessShareLock);
-	ScanKeyData scankey;
-	SysScanDesc sscan;
-	ScanKeyInit(&scankey,
-				Anum_gp_segment_configuration_dbid,
-				BTEqualStrategyNumber, F_INT2EQ,
-				Int16GetDatum(dbid));
-	sscan = systable_beginscan(rel, GpSegmentConfigDbidIndexId, true,
-							   GetTransactionSnapshot(), 1, &scankey);
-	tuple = systable_getnext(sscan);
-
-	if (!HeapTupleIsValid(tuple))
-		elog(ERROR, "cannot find dbid %i", dbid);
-
-	bool isnull;
-	Datum datum = heap_getattr(tuple, Anum_gp_segment_configuration_hostname,
-							   RelationGetDescr(rel), &isnull);
-	char *hostname;
-	if (!isnull)
-		hostname =
-				DatumGetCString(DirectFunctionCall1(textout, datum));
-	else
-		elog(ERROR, "hostname is null for dbid %d", dbid);
-	int port = DatumGetInt32(heap_getattr(tuple,
-										  Anum_gp_segment_configuration_port,
-										  RelationGetDescr(rel), &isnull));
-	systable_endscan(sscan);
-	heap_close(rel, NoLock);
-
-	struct addrinfo *addrList = NULL;
-	struct addrinfo hint;
-	int			ret;
-
-	/* Initialize hint structure */
-	MemSet(&hint, 0, sizeof(hint));
-	hint.ai_socktype = SOCK_STREAM;
-	hint.ai_family = AF_UNSPEC;
-
-	char portStr[100];
-	if (snprintf(portStr, sizeof(portStr), "%d", port) >= sizeof(portStr))
-		elog(ERROR, "port number too long for dbid %d", dbid);
-
-	/* Use pg_getaddrinfo_all() to resolve the address */
-	ret = pg_getaddrinfo_all(hostname, portStr, &hint, &addrList);
-	if (ret || !addrList)
-	{
-		if (addrList)
-			pg_freeaddrinfo_all(hint.ai_family, addrList);
-		elog(ERROR, "could not translate host name \"%s\" to address: %s\n",
-			 hostname, gai_strerror(ret));
-	}
-
-	PrimaryMirrorTransitionClientInfo client;
-	client.receivedDataCallbackFn = transitionReceivedDataFn;
-	client.errorLogFn = transitionErrorLogFn;
-	client.checkForNeedToExitFn = checkForNeedToExitFn;
-	transitionMsgErrors = makeStringInfo();
-
-	appendStringInfo(faultmsg, "%s\n%s\n%s\n%s\n%s\n%s\n%d\n%d\n",
-					 "faultInject",	faultName, type, ddlStatement,
-					 databaseName, tableName, numOccurrences,
-					 sleepTimeSeconds);
-
-	if (sendTransitionMessage(&client, addrList, faultmsg->data, faultmsg->len,
-							  1 /* retries */, 60 /* timeout */) !=
-		TRANS_ERRCODE_SUCCESS)
-	{
-		pg_freeaddrinfo_all(hint.ai_family, addrList);
-		ereport(ERROR, (errmsg("failed to inject %s fault in dbid %d",
-							   faultName, dbid),
-						errdetail("%s", transitionMsgErrors->data)));
-	}
-
-	pg_freeaddrinfo_all(hint.ai_family, addrList);
-	PG_RETURN_DATUM(BoolGetDatum(true));
 }
 #endif
