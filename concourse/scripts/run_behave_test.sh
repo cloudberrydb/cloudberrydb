ac44e4b1e7150a3849dd44cc5e25d53a36aef011
Chris Hajas <chajas@pivotal.io>
2017-07-27 14:04:08 -0700

    Create test database after exporting PGDATABASE in test driver script

--- concourse/scripts/run_behave_test.sh
+++ concourse/scripts/run_behave_test.sh
@@ -6,5 +6,6 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh
 export PGPORT=5432
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
 export PGDATABASE=gptest
+createdb gptest
 cd /home/gpadmin/gpdb_src/gpMgmt
 make -f Makefile.behave behave flags="$BEHAVE_FLAGS"
