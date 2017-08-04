99b6da9f8d0cebe1171699981adbd29c2373f760
xiong-gang <gxiong@pivotal.io>
2017-08-04 14:44:17 +0800

    Add pipeline job for resource group test
    1)add pipeline job for resource group test
    2)make test cases independent of segment count and hosts count

--- concourse/scripts/ic_gpdb_resgroup.bash
+++ concourse/scripts/ic_gpdb_resgroup.bash
@@ -2,27 +2,52 @@
 
 set -eox pipefail
 
-CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+./ccp_src/aws/setup_ssh_to_cluster.sh
 
-basedir=/sys/fs/cgroup
-options=rw,nosuid,nodev,noexec,relatime
-groups="hugetlb freezer pids devices cpuset blkio net_prio net_cls cpuacct cpu memory perf_event"
+CLUSTER_NAME=$(cat ./cluster_env_files/terraform/name)
 
-# mount cgroup base dir
-mkdir -p $basedir
-mount -t tmpfs tmpfs $basedir
+prepare_cgroups() {
+    local gpdb_host_alias=$1
+    local basedir=/cgroup
+    local options=rw,nosuid,nodev,noexec,relatime
+    local groups="hugetlb freezer pids devices cpuset blkio net_prio net_cls cpuacct cpu memory perf_event"
 
-# mount cgroup controllers
-for group in $groups; do
-	mkdir -p $basedir/$group
-	mount -t cgroup -o $options,$group cgroup $basedir/$group
-done
+    ssh -t $gpdb_host_alias "sudo bash -c '(\
+        mkdir -p $basedir; \
+        mount -t tmpfs tmpfs $basedir; \
+        for group in $groups; do \
+                mkdir -p $basedir/\$group; \
+                mount -t cgroup -o $options,\$group cgroup $basedir/\$group; \
+        done; \
+        chmod -R 777 $basedir/cpu; \
+        chmod -R 777 $basedir/cpuacct; \
+        mkdir -p $basedir/cpu/gpdb; \
+        chown -R gpadmin:gpadmin $basedir/cpu/gpdb; \
+        chmod -R 777 $basedir/cpu/gpdb; \
+        mkdir -p $basedir/cpuacct/gpdb; \
+        chown -R gpadmin:gpadmin $basedir/cpuacct/gpdb; \
+        chmod -R 777 $basedir/cpuacct/gpdb; \
+    )'"
+}
 
-mkdir -p $basedir/cpu/gpdb
+run_resgroup_test() {
+    local gpdb_master_alias=$1
 
-# set all dirs' permission to 777 to allow test cases to control
-# when and how cgroup is enabled
-find $basedir -type d | xargs chmod 777
+    ssh $gpdb_master_alias "bash -c '(\
+        source /usr/local/greenplum-db-devel/greenplum_path.sh; \
+        export PGPORT=5432; \
+        export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 
-# do the actual job
-${CWDIR}/ic_gpdb.bash
+        cd /home/gpadmin/gpdb_src; \
+        ./configure --prefix=/usr/local/greenplum-db-devel --without-zlib --without-rt --without-libcurl --without-libedit-preferred --without-docdir --without-PACKAGE --without-readline --disable-gpcloud --disable-gpfdist --disable-orca ${CONFIGURE_FLAGS}; \
+        cd /home/gpadmin/gpdb_src/src/test/regress && make;\
+        ssh sdw1 \"mkdir -p /home/gpadmin/gpdb_src/src/test/regress\";\
+        ssh sdw1 \"mkdir -p /home/gpadmin/gpdb_src/src/test/isolation2\";\
+        scp /home/gpadmin/gpdb_src/src/test/regress/regress.so gpadmin@sdw1:/home/gpadmin/gpdb_src/src/test/regress/ ; \
+        cd /home/gpadmin/gpdb_src; \
+        make installcheck-resgroup; \
+		)'"
+}
+prepare_cgroups ccp-${CLUSTER_NAME}-0
+prepare_cgroups ccp-${CLUSTER_NAME}-1
+run_resgroup_test mdw
