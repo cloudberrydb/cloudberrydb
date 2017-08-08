bhuvnesh2703/gpdb

[#149699023] Handle interrupts in ORCA to avoid crashes
In ORCA, we donot process interrupts during planning stage, however
if there are elog/ereport (which further calls errfinish) statements to
print additional messages we prematurely exit out the planning stage
without cleaning up the memory pools leading to inconsistent memory pool
state. This results in crashes for the subsequent queries.

This commit fixes the issue by handling interrupts while
printing messages using elog/ereport in ORCA.

Signed-off-by: Ekta Khanna <ekhanna@pivotal.io>

src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -837,7 +837,10 @@ CTranslatorQueryToDXL::PdxlnCTAS()
 	}
 	else
 	{
-		elog(NOTICE, "Table doesn't have 'DISTRIBUTED BY' clause. Creating a NULL policy entry.");
+		RaiseGpdbError(ERRCODE_SUCCESSFUL_COMPLETION,
+					   NOTICE,
+					   "Table doesn't have 'DISTRIBUTED BY' clause. Creating a NULL policy entry.",
+					   NULL);
 	}
 	
 	GPOS_ASSERT(IMDRelation::EreldistrMasterOnly != ereldistrpolicy);

src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
@@ -2314,8 +2314,13 @@ CTranslatorRelcacheToDXL::PimdobjColStats
 		pdrgdatumMCVValues = NULL;
 		pdrgfMCVFrequencies = NULL;
 
-		elog(LOG, "The number of most common values and frequencies do not match on column %ls of table %ls.",
+		char msgbuf[NAMEDATALEN * 2 + 100];
+		snprintf(msgbuf, sizeof(msgbuf), "The number of most common values and frequencies do not match on column %ls of table %ls.",
 				pmdcol->Mdname().Pstr()->Wsz(), pmdrel->Mdname().Pstr()->Wsz());
+		RaiseGpdbError(ERRCODE_SUCCESSFUL_COMPLETION,
+					   LOG,
+					   msgbuf,
+					   NULL);
 	}
 
 	Form_pg_statistic fpsStats = (Form_pg_statistic) GETSTRUCT(heaptupleStats);

src/backend/gpopt/utils/COptTasks.cpp
@@ -1210,18 +1210,27 @@ COptTasks::PrintMissingStatsWarning
 			}
 
 			CMDName mdname = pmdrel->Pmdcol(ulPos)->Mdname();
-			elog(LOG, "Missing statistics for column: %s.%s", SzFromWsz(pmdrel->Mdname().Pstr()->Wsz()), SzFromWsz(mdname.Pstr()->Wsz()));
+
+			char msgbuf[NAMEDATALEN * 2 + 100];
+			snprintf(msgbuf, sizeof(msgbuf), "Missing statistics for column: %s.%s", SzFromWsz(pmdrel->Mdname().Pstr()->Wsz()), SzFromWsz(mdname.Pstr()->Wsz()));
+			RaiseGpdbError(ERRCODE_SUCCESSFUL_COMPLETION,
+						   LOG,
+						   msgbuf,
+						   NULL);
 		}
 	}
 
 	if (0 < phsmdidRel->UlEntries())
 	{
-		ereport(NOTICE,
-				(errcode(ERRCODE_SUCCESSFUL_COMPLETION),
-				errmsg("One or more columns in the following table(s) do not have statistics: %s", SzFromWsz(str.Wsz())),
-				errhint("For non-partitioned tables, run analyze <table_name>(<column_list>)."
-					" For partitioned tables, run analyze rootpartition <table_name>(<column_list>)."
-					" See log for columns missing statistics.")));
+		int length = NAMEDATALEN * phsmdidRel->UlEntries() + 200;
+		char msgbuf[length];
+		snprintf(msgbuf, sizeof(msgbuf), "One or more columns in the following table(s) do not have statistics: %s", SzFromWsz(str.Wsz()));
+		RaiseGpdbError(ERRCODE_SUCCESSFUL_COMPLETION,
+					   NOTICE,
+					   msgbuf,
+					   "For non-partitioned tables, run analyze <table_name>(<column_list>)."
+					   " For partitioned tables, run analyze rootpartition <table_name>(<column_list>)."
+					   " See log for columns missing statistics.");
 	}
 
 }

cloudberrydb/cloudberrydb

src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -837,7 +837,10 @@ CTranslatorQueryToDXL::PdxlnCTAS()
 	}
 	else
 	{
-		elog(NOTICE, "Table doesn't have 'DISTRIBUTED BY' clause. Creating a NULL policy entry.");
+		GpdbEreport(ERRCODE_SUCCESSFUL_COMPLETION,
+					   NOTICE,
+					   "Table doesn't have 'DISTRIBUTED BY' clause. Creating a NULL policy entry.",
+					   NULL);
 	}
 	
 	GPOS_ASSERT(IMDRelation::EreldistrMasterOnly != ereldistrpolicy);

src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
@@ -2314,8 +2314,13 @@ CTranslatorRelcacheToDXL::PimdobjColStats
 		pdrgdatumMCVValues = NULL;
 		pdrgfMCVFrequencies = NULL;
 
-		elog(LOG, "The number of most common values and frequencies do not match on column %ls of table %ls.",
+		char msgbuf[NAMEDATALEN * 2 + 100];
+		snprintf(msgbuf, sizeof(msgbuf), "The number of most common values and frequencies do not match on column %ls of table %ls.",
 				pmdcol->Mdname().Pstr()->Wsz(), pmdrel->Mdname().Pstr()->Wsz());
+		GpdbEreport(ERRCODE_SUCCESSFUL_COMPLETION,
+					   LOG,
+					   msgbuf,
+					   NULL);
 	}
 
 	Form_pg_statistic fpsStats = (Form_pg_statistic) GETSTRUCT(heaptupleStats);

src/backend/gpopt/utils/COptTasks.cpp
@@ -1210,18 +1210,27 @@ COptTasks::PrintMissingStatsWarning
 			}
 
 			CMDName mdname = pmdrel->Pmdcol(ulPos)->Mdname();
-			elog(LOG, "Missing statistics for column: %s.%s", SzFromWsz(pmdrel->Mdname().Pstr()->Wsz()), SzFromWsz(mdname.Pstr()->Wsz()));
+
+			char msgbuf[NAMEDATALEN * 2 + 100];
+			snprintf(msgbuf, sizeof(msgbuf), "Missing statistics for column: %s.%s", SzFromWsz(pmdrel->Mdname().Pstr()->Wsz()), SzFromWsz(mdname.Pstr()->Wsz()));
+			GpdbEreport(ERRCODE_SUCCESSFUL_COMPLETION,
+						   LOG,
+						   msgbuf,
+						   NULL);
 		}
 	}
 
 	if (0 < phsmdidRel->UlEntries())
 	{
-		ereport(NOTICE,
-				(errcode(ERRCODE_SUCCESSFUL_COMPLETION),
-				errmsg("One or more columns in the following table(s) do not have statistics: %s", SzFromWsz(str.Wsz())),
-				errhint("For non-partitioned tables, run analyze <table_name>(<column_list>)."
-					" For partitioned tables, run analyze rootpartition <table_name>(<column_list>)."
-					" See log for columns missing statistics.")));
+		int length = NAMEDATALEN * phsmdidRel->UlEntries() + 200;
+		char msgbuf[length];
+		snprintf(msgbuf, sizeof(msgbuf), "One or more columns in the following table(s) do not have statistics: %s", SzFromWsz(str.Wsz()));
+		GpdbEreport(ERRCODE_SUCCESSFUL_COMPLETION,
+					   NOTICE,
+					   msgbuf,
+					   "For non-partitioned tables, run analyze <table_name>(<column_list>)."
+					   " For partitioned tables, run analyze rootpartition <table_name>(<column_list>)."
+					   " See log for columns missing statistics.");
 	}
 
 }