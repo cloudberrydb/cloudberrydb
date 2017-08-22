6810843fe1c2565dd4f6225ca2838c182b03ca72
Jimmy Yih <jyih@pivotal.io>
2017-08-21 18:46:41 -0700

    Auxiliary and some backend processes should be set to GP_ROLE_UTILITY
    Currently, auxiliary and backend processes are defaulted to
    GP_ROLE_DISPATCH.  This was kind of harmless until we started enabling
    segment WAL replication.  The WAL sender would be initialized as a
    backend process that would have a gp_session_id set and mppIsWriter be
    True.  This would intermittently cause issues with cursors.  The writer
    would open a temporary file with its session id and process id but the
    reader would construct the temporary filename incorrectly with the
    right session id but the WAL sender's process id.  The reader would
    then fail to open the correct temporary file.
    
    To fix this, we set Gp_role to GP_ROLE_UTILITY for all auxiliary
    processes and make exceptions for certain backend processes
    (Autovacuum and WAL sender).

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -43,6 +43,7 @@
 #include "miscadmin.h"
 #include "postmaster/autovacuum.h"
 #include "replication/syncrep.h"
+#include "replication/walsender.h"
 #include "storage/ipc.h"
 #include "storage/spin.h"
 #include "storage/sinval.h"
@@ -258,6 +259,14 @@ InitProcess(void)
 	volatile PROC_HDR *procglobal = ProcGlobal;
 	SHMEM_OFFSET myOffset;
 	int			i;
+
+	/*
+	 * Autovacuum and WAL sender processes are marked as GP_ROLE_UTILITY to
+	 * prevent unwanted GP_ROLE_DISPATCH MyProc settings such as mppSessionId
+	 * being valid and mppIsWriter set to true.
+	 */
+	if (IsAutoVacuumWorkerProcess() || am_walsender)
+		Gp_role = GP_ROLE_UTILITY;
 
 	/*
 	 * ProcGlobal should be set up already (if we are a backend, we inherit
@@ -485,6 +494,12 @@ InitAuxiliaryProcess(void)
 	PGPROC	   *auxproc;
 	int			proctype;
 	int			i;
+
+	/*
+	 * Set Gp_role to GP_ROLE_UTILITY to avoid auxiliary process from going
+	 * into any GP_ROLE_DISPATCH code paths if any exists.
+	 */
+	Gp_role = GP_ROLE_UTILITY;
 
 	/*
 	 * ProcGlobal should be set up already (if we are a backend, we inherit
