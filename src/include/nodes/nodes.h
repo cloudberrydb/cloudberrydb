4f71d679450d3d4f575a681a2c54f55fde6f36d4
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-09-02 08:33:21 +0300

    Also remove WindowSpecParse.
    I meant to remove this in previous commit already, but forgot to "git add".

--- src/include/nodes/nodes.h
+++ src/include/nodes/nodes.h
@@ -436,7 +436,6 @@ typedef enum NodeTag
 	T_AlterTSDictionaryStmt,
 	T_AlterTSConfigurationStmt,
 	T_WindowSpec,
-	T_WindowSpecParse,		/* GPDB_84_MERGE_FIXME: Unused, remove once we're open for catalog changes again */
 	T_PartitionBy,
 	T_PartitionElem,
 	T_PartitionRangeItem,
