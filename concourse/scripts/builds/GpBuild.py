b1a21354735e3e4b58cf63c3fc81b6e8e2ee5ed7
Jesse Zhang <sbjesse@gmail.com>
2017-09-05 08:44:08 -0700

    Disable PXF in ORCA CI

--- concourse/scripts/builds/GpBuild.py
+++ concourse/scripts/builds/GpBuild.py
@@ -14,6 +14,7 @@ class GpBuild(GpdbBuildBase):
                                 "--with-libxml",
                                 "--with-python",
                                 "--disable-gpcloud",
+                                "--disable-pxf",
                                 "--prefix=/usr/local/gpdb"], cwd="gpdb_src")
 
     def icg(self):
