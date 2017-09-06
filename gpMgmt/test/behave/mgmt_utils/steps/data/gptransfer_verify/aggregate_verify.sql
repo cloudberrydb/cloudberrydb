8539adcdb5bc2a5035cea24e7e9626856fce0599
Chris Hajas <chajas@pivotal.io>
2017-09-06 10:56:22 -0700

    Remove proiswindow column from gptransfer verification query
    This column has a different name between master and previous GPDB
    versions.
    
    Signed-off-by: Karen Huddleston <khuddleston@pivotal.io>

--- gpMgmt/test/behave/mgmt_utils/steps/data/gptransfer_verify/aggregate_verify.sql
+++ gpMgmt/test/behave/mgmt_utils/steps/data/gptransfer_verify/aggregate_verify.sql
@@ -1,5 +1,5 @@
 \df
 
-select proname,pronamespace,proowner,prolang,proisagg,prosecdef,proisstrict,proretset,provolatile,pronargs,prorettype,proiswindow,proargtypes,prosrc from pg_proc where proname='scube';
-select proname,pronamespace,proowner,prolang,proisagg,prosecdef,proisstrict,proretset,provolatile,pronargs,prorettype,proiswindow,proargtypes,prosrc from pg_proc where proname='old_scube';
+select proname,pronamespace,proowner,prolang,proisagg,prosecdef,proisstrict,proretset,provolatile,pronargs,prorettype,proargtypes,prosrc from pg_proc where proname='scube';
+select proname,pronamespace,proowner,prolang,proisagg,prosecdef,proisstrict,proretset,provolatile,pronargs,prorettype,proargtypes,prosrc from pg_proc where proname='old_scube';
 
