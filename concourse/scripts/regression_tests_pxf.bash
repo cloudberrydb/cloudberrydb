facc4df1a9aa748f8998b42078a29cca3fbb4b05
Alexander Denissov <denalex@users.noreply.github.com>
2017-09-22 10:29:29 -0700

    PXF CI: run pxf_automation suite as part of pxf regression test job (#3347)
    * run pxf_automation as part of pxf regression test
    
    Signed-off-by: Alexander Denissov <adenissov@pivotal.io>
    
    * added missing input
    
    Signed-off-by: Lav Jain <ljain@pivotal.io>
    
    * Fix symbolic links
    
    * create extension pxf before running automation tests
    
    * Hack python psi module by copying it from system to gpdb python
    
    * remove if exists for extension
    
    * Run pxf_automation before regression tests
    
    * Change owner to gpadmin before running tests
    
    * Generalize copying of PSI package
    
    * Generalize install path using GPHOME

--- concourse/scripts/regression_tests_pxf.bash
+++ concourse/scripts/regression_tests_pxf.bash
@@ -4,22 +4,18 @@ set -exo pipefail
 
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${CWDIR}/common.bash"
+GPHOME="/usr/local/greenplum-db-devel"
+PXF_HOME="${GPHOME}/pxf"
 
-function gen_env(){
+function run_regression_test() {
 	cat > /home/gpadmin/run_regression_test.sh <<-EOF
 	set -exo pipefail
 
 	source /opt/gcc_env.sh
-	source /usr/local/greenplum-db-devel/greenplum_path.sh
+	source ${GPHOME}/greenplum_path.sh
 
 	cd "\${1}/gpdb_src/gpAux"
 	source gpdemo/gpdemo-env.sh
-
-	if [ "$overwrite_pxf" = "true" ]
-	then
-		cd "\${1}/gpdb_src/gpAux/extensions/pxf"
-		make install
-	fi
 
 	cd "\${1}/gpdb_src/gpAux/extensions/pxf"
 	make installcheck USE_PGXS=1
@@ -36,13 +32,44 @@ function gen_env(){
 	exit 0
 	EOF
 
-	chown -R gpadmin:gpadmin $(pwd)
 	chown gpadmin:gpadmin /home/gpadmin/run_regression_test.sh
 	chmod a+x /home/gpadmin/run_regression_test.sh
+
+	su gpadmin -c "bash /home/gpadmin/run_regression_test.sh $(pwd)"
 }
 
-function run_regression_test() {
-	su gpadmin -c "bash /home/gpadmin/run_regression_test.sh $(pwd)"
+function run_pxf_automation() {
+	export GPHD_ROOT=$1
+	pushd ${GPHD_ROOT} > /dev/null
+		mkdir -p pxf && cd pxf
+		ln -s ${PXF_HOME}/conf conf
+		for X in ${PXF_HOME}/lib/pxf-*-[0-9]*.jar; do \
+			ln -s ${X} $(echo $(basename ${X}) | sed -e 's/-[a-zA-Z0-9.]*.jar/.jar/'); \
+		done
+	popd > /dev/null
+
+	cat > /home/gpadmin/run_pxf_automation_test.sh <<-EOF
+	set -exo pipefail
+
+	source ${GPHOME}/greenplum_path.sh
+	source \${1}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+
+	export PG_MODE=GPDB
+	export GPHD_ROOT=\${1}/singlecluster
+
+	# Copy PSI package from system python to GPDB as automation test requires it
+	psi_dir=\$(find /usr/lib64 -name psi | sort -r | head -1)
+	cp -r \${psi_dir} ${GPHOME}/lib/python
+	psql -d template1 -c "CREATE EXTENSION PXF"
+	cd \${1}/pxf_automation_src
+	make GROUP=gpdb
+
+	exit 0
+	EOF
+
+	chown gpadmin:gpadmin /home/gpadmin/run_pxf_automation_test.sh
+	chmod a+x /home/gpadmin/run_pxf_automation_test.sh
+	su gpadmin -c "bash /home/gpadmin/run_pxf_automation_test.sh $(pwd)"
 }
 
 function setup_gpadmin_user() {
@@ -69,8 +96,7 @@ function setup_singlecluster() {
 
 function start_pxf() {
 	local hdfsrepo=$1
-	local pxfhome="/usr/local/greenplum-db-devel/pxf"
-	pushd ${pxfhome} > /dev/null
+	pushd ${PXF_HOME} > /dev/null
 	./bin/pxf init --hadoop-home ${hdfsrepo}/hadoop
 	./bin/pxf start
 	popd > /dev/null
@@ -92,10 +118,11 @@ function _main() {
 	time install_gpdb
 	time setup_gpadmin_user
 	time make_cluster
-	time gen_env
 
 	time setup_singlecluster
 	time start_pxf $(pwd)/singlecluster
+	chown -R gpadmin:gpadmin $(pwd)
+	time run_pxf_automation $(pwd)/singlecluster
 	time run_regression_test
 }
 
