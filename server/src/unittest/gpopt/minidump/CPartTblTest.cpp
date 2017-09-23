6ddaecece0a8d1336ac826a8c2ac43ce15f27919
Shreedhar Hardikar <shardikar@pivotal.io>
2017-09-22 17:48:50 -0700

    Ctest changes for IDF in partition selection
    Signed-off-by: Ekta Khanna <ekhanna@pivotal.io>

--- server/src/unittest/gpopt/minidump/CPartTblTest.cpp
+++ server/src/unittest/gpopt/minidump/CPartTblTest.cpp
@@ -98,7 +98,8 @@ const CHAR *rgszPartTblFileNames[] =
 	"../data/dxl/minidump/PartTbl-LeftOuterNLJoin-DPE-IsNull.mdp",
 	"../data/dxl/minidump/PartTbl-List-DPE-Varchar-Predicates.mdp",
 	"../data/dxl/minidump/PartTbl-List-DPE-Int-Predicates.mdp",
-
+	"../data/dxl/minidump/PartTbl-IDFList.mdp",
+	"../data/dxl/minidump/PartTbl-IDFNull.mdp"
 	};
 
 //---------------------------------------------------------------------------
