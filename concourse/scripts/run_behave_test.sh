d1d852163de27af7624709eb9e0d6edb4a434d32
Chris Hajas <chrhajas@gmail.com>
2017-09-27 09:57:35 -0700

    Turn fsync off for behave terraform tests (#3314)
    This should speed up the tests a bit, since they are currently spending a significant amount of time in createdb statements.

--- concourse/scripts/run_behave_test.sh
+++ concourse/scripts/run_behave_test.sh
@@ -7,5 +7,7 @@ export PGPORT=5432
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
 export PGDATABASE=gptest
 createdb gptest
+gpconfig --skipvalidation -c fsync -v off
+gpstop -u
 cd /home/gpadmin/gpdb_src/gpMgmt
 make -f Makefile.behave behave flags="$BEHAVE_FLAGS"
