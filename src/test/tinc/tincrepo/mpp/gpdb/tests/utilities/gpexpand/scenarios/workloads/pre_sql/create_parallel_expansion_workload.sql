2a77e0f47a7844d45830029c7f43e9285978a157
Nadeem Ghani <nghani@pivotal.io>
2017-10-02 12:25:12 -0700

    Change query to reduce memory consumption
    Terraform deployed machines uses less memory compared to pulse, so the
    query that uses a lot of memoery was getting killed by the kernel OOM
    manager.
    
    Signed-off-by: Marbin Tan <mtan@pivotal.io>

--- src/test/tinc/tincrepo/mpp/gpdb/tests/utilities/gpexpand/scenarios/workloads/pre_sql/create_parallel_expansion_workload.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/utilities/gpexpand/scenarios/workloads/pre_sql/create_parallel_expansion_workload.sql
@@ -7,7 +7,7 @@ CREATE TABLE users_dur_pkey (
     age   INT4
 );
 ALTER TABLE users_dur_pkey set TABLESPACE test_ts;
-INSERT INTO users_dur_pkey select i, 'name'||i, i%50+1 from generate_series(1,10000000) i;
+INSERT INTO users_dur_pkey select i, 'name'||i, i%50+1 from generate_series(1,100000) i;
 
 DROP TABLE IF EXISTS users_duration_4;
 CREATE TABLE users_duration_4(
@@ -19,7 +19,7 @@ CREATE TABLE users_duration_4(
 distributed by (a);
 INSERT INTO users_duration_4  
 SELECT i, (i * 10) % 100, i::text, '2013-09-01 03:04:05'
-FROM generate_series(1, 10000000)i;
+FROM generate_series(1, 100000)i;
 
 DROP TABLE IF EXISTS users_duration_5;
 CREATE TABLE users_duration_5(
@@ -31,7 +31,7 @@ CREATE TABLE users_duration_5(
 distributed by (a);
 INSERT INTO users_duration_5  
 SELECT i, (i * 10) % 100, i::text, '2013-09-01 03:04:05'
-FROM generate_series(1, 10000000)i;
+FROM generate_series(1, 100000)i;
 
 DROP TABLE IF EXISTS users_duration_6;
 CREATE TABLE users_duration_6(
@@ -43,5 +43,5 @@ CREATE TABLE users_duration_6(
 distributed by (a);
 INSERT INTO users_duration_6  
 SELECT i, (i * 10) % 100, i::text, '2013-09-01 03:04:05'
-FROM generate_series(1, 10000000)i;
+FROM generate_series(1, 100000)i;
 
