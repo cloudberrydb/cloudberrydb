f5e6e79a666d2619947a0ccb745da7c5e1faf5ac
Xiaoran Wang <xiwang@pivotal.io>
2017-10-11 09:43:01 +0800

    copy from on segment should not check random distributing policy
    copy from on segment will not check distributing policy
    when the table is distributed randomly.
    
    Signed-off-by: Xiaoran Wang <xiwang@pivotal.io>

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -4846,7 +4846,7 @@ PROCESS_SEGMENT_DATA:
 					nulls = baseNulls;
 				}
 
-				if (is_check_distkey)
+				if (is_check_distkey && distData->p_nattrs > 0)
 				{
 					target_seg = GetTargetSeg(distData, values, nulls);
 					/*check distribution key if COPY FROM ON SEGMENT*/
