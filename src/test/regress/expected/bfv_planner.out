cloudberrydb/cloudberrydb

commit 7e268107f31c305d74b532633ab146944ba40d52

Remove row order information from Flow.
A Motion node often needs to "merge" the incoming streams, to preserve the
overall sort order. Instead of carrying sort order information throughout
the later stages of planning, in the Flow struct, pass it as argument
directly to make_motion() and other functions, where a Motion node is
created. This simplifies things.

To make that work, we can no longer rely on apply_motion() to add the final
Motion on top of the plan, when the (sub-)query contains an ORDER BY. That's
because we no longer have that information available at apply_motion(). Add
the Motion node in grouping_planner() instead, where we still have that
information, as a path key.

When I started to work on this, this also fixed a bug, where the sortColIdx
of plan flow node may refer to wrong resno. A test case for that is
included. However, that case was since fixed by other coincidental changes
to partition elimination, so now this is just refactoring.

src/backend/optimizer/plan/planner.c
@@ -786,37 +786,6 @@ subquery_planner(PlannerGlobal *glob, Query *parse,
 		plan = grouping_planner(root, tuple_fraction);
 
 	/*
-	 * Deal with explicit redistribution requirements for TableValueExpr
-	 * subplans with explicit distribitution
-	 */
-	if (parse->scatterClause)
-	{
-		bool		r;
-		List	   *exprList;
-
-
-		/* Deal with the special case of SCATTER RANDOMLY */
-		if (list_length(parse->scatterClause) == 1 && linitial(parse->scatterClause) == NULL)
-			exprList = NIL;
-		else
-			exprList = parse->scatterClause;
-
-		/*
-		 * Repartition the subquery plan based on our distribution
-		 * requirements
-		 */
-		r = repartitionPlan(plan, false, false, exprList);
-		if (!r)
-		{
-			/*
-			 * This should not be possible, repartitionPlan should never fail
-			 * when both stable and rescannable are false.
-			 */
-			elog(ERROR, "failure repartitioning plan");
-		}
-	}
-
-	/*
 	 * If any subplans were generated, or if we're inside a subplan, build
 	 * initPlan list and extParam/allParam sets for plan nodes, and attach the
 	 * initPlans to the top plan node.
@@ -1655,15 +1624,6 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 				}
 			}
 
-			if (result_plan != NULL && result_plan->flow->numSortCols == 0)
-			{
-				/*
-				 * cdb_grouping_planner generated the full plan, with the the
-				 * right tlist.  And it has no sort order
-				 */
-				current_pathkeys = NIL;
-			}
-
 			if (result_plan != NULL && querynode_changed)
 			{
 				/*
@@ -1807,9 +1767,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 					canonical_grpsets->grpset_counts != NULL &&
 					canonical_grpsets->grpset_counts[0] > 1)
 				{
-					result_plan->flow = pull_up_Flow(result_plan,
-													 result_plan->lefttree,
-												  (current_pathkeys != NIL));
+					result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 					result_plan = add_repeat_node(result_plan,
 										 canonical_grpsets->grpset_counts[0],
 												  0);
@@ -1876,9 +1834,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 					canonical_grpsets->grpset_counts != NULL &&
 					canonical_grpsets->grpset_counts[0] > 1)
 				{
-					result_plan->flow = pull_up_Flow(result_plan,
-													 result_plan->lefttree,
-												  (current_pathkeys != NIL));
+					result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 					result_plan = add_repeat_node(result_plan,
 										 canonical_grpsets->grpset_counts[0],
 												  0);
@@ -1974,9 +1930,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 	 * which we can obtain the distribution info.)
 	 */
 	if (!result_plan->flow)
-		result_plan->flow = pull_up_Flow(result_plan,
-										 getAnySubplan(result_plan),
-										 (current_pathkeys != NIL));
+		result_plan->flow = pull_up_Flow(result_plan, getAnySubplan(result_plan));
 
 	/*
 	 * MPP: If there's a DISTINCT clause and we're not collocated on the
@@ -2043,9 +1997,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 
 					result_plan = (Plan *) make_unique(result_plan, parse->distinctClause);
 
-					result_plan->flow = pull_up_Flow(result_plan,
-													 result_plan->lefttree,
-													 true);
+					result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 
 					result_plan->plan_rows = numDistinct;
 
@@ -2105,10 +2057,32 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 		}
 
 		/*
-		 * Update numOrderbyCols to length of sort_pathkeys. For cdb: decide
-		 * which sort attribute should be preserved by merge gather motion
+		 * An ORDER BY doesn't make much sense, unless we bring all the data
+		 * to a single node. Otherwise it's just a partial order. (If there's
+		 * a LIMIT or OFFSET clause, we'll take care of this below, after
+		 * inserting the Limit node).
+		 *
+		 * In a subquery, though, a partial order is OK. In fact, we could
+		 * probably not bother with the sort at all, unless there's a LIMIT
+		 * or OFFSET, because it's not going to make any difference to the
+		 * overall query's result. For example, in "WHERE x IN (SELECT ...
+		 * ORDER BY foo)", the ORDER BY in the subquery will make no
+		 * difference. PostgreSQL honors the sort, though, and historically,
+		 * GPDB has also done a partial sort, separately on each node. So
+		 * keep that behavior for now.
+		 *
+		 * In a TABLE function's input subquery, a partial order is the
+		 * documented behavior, so in that case that's definitely what we
+		 * want.
 		 */
-		result_plan->flow->numOrderbyCols = list_length(sort_pathkeys);
+		if (result_plan->flow->flotype != FLOW_SINGLETON &&
+			(root->config->honor_order_by || !root->parent_root) &&
+			!parse->isTableValueSelect &&
+			!parse->limitCount && !parse->limitOffset)
+		{
+			result_plan = (Plan *) make_motion_gather(root, result_plan, -1,
+													  sort_pathkeys);
+		}
 	}
 
 	/*
@@ -2119,9 +2093,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 		if (IsA(result_plan, Sort) &&gp_enable_sort_distinct)
 			((Sort *) result_plan)->noduplicates = true;
 		result_plan = (Plan *) make_unique(result_plan, parse->distinctClause);
-		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 true);
+		result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 
 		/*
 		 * If there was grouping or aggregation, leave plan_rows as-is (ie,
@@ -2143,7 +2115,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 			result_plan = pushdown_preliminary_limit(result_plan, parse->limitCount, count_est, parse->limitOffset, offset_est);
 			
 			/* Focus on QE [merge to preserve order], prior to final LIMIT. */
-			result_plan = (Plan *) make_motion_gather_to_QE(result_plan, current_pathkeys != NIL);
+			result_plan = (Plan *) make_motion_gather_to_QE(root, result_plan, current_pathkeys);
 			result_plan->total_cost += motion_cost_per_row * result_plan->plan_rows;
 		}
 			
@@ -2162,9 +2134,37 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 										  parse->limitCount,
 										  offset_est,
 										  count_est);
-		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 true);
+		result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
+	}
+
+	/*
+	 * Deal with explicit redistribution requirements for TableValueExpr
+	 * subplans with explicit distribitution
+	 */
+	if (parse->scatterClause)
+	{
+		bool		r;
+		List	   *exprList;
+
+		/* Deal with the special case of SCATTER RANDOMLY */
+		if (list_length(parse->scatterClause) == 1 && linitial(parse->scatterClause) == NULL)
+			exprList = NIL;
+		else
+			exprList = parse->scatterClause;
+
+		/*
+		 * Repartition the subquery plan based on our distribution
+		 * requirements
+		 */
+		r = repartitionPlan(result_plan, false, false, exprList);
+		if (!r)
+		{
+			/*
+			 * This should not be possible, repartitionPlan should never fail
+			 * when both stable and rescannable are false.
+			 */
+			elog(ERROR, "failure repartitioning plan");
+		}
 	}
 
 	Insist(result_plan->flow);
@@ -3709,9 +3709,7 @@ pushdown_preliminary_limit(Plan *plan, Node *limitCount, int64 count_est, Node *
 										  0,
 										  precount_est);
 
-		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 true);
+		result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 	}
 
 	return result_plan;

src/test/regress/expected/bfv_planner.out
@@ -299,9 +299,31 @@ NOTICE:  stabletestfunc executed
   1
 (1 row)
 
+-- Test agg on top of join subquery on partition table with ORDER-BY clause
+CREATE TABLE bfv_planner_t1 (a int, b int, c int) distributed by (c);
+CREATE TABLE bfv_planner_t2 (f int,g int) DISTRIBUTED BY (f) PARTITION BY RANGE(g)
+(
+PARTITION "201612" START (1) END (10)
+);
+NOTICE:  CREATE TABLE will create partition "bfv_planner_t2_1_prt_201612" for table "bfv_planner_t2"
+insert into bfv_planner_t1 values(1,2,3), (2,3,4), (3,4,5);
+insert into bfv_planner_t2 values(3,1), (4,2), (5,2);
+select count(*) from
+(select a,b,c from bfv_planner_t1 order by c) T1
+join
+(select f,g from bfv_planner_t2) T2
+on
+T1.a=T2.g and T1.c=T2.f;
+ count 
+-------
+     2
+(1 row)
+
 -- start_ignore
 drop table if exists bfv_planner_x;
 drop table if exists testbadsql;
 drop table if exists bfv_planner_foo;
 drop table if exists testmedian;
+drop table if exists bfv_planner_t1;
+drop table if exists bfv_planner_t2;
 -- end_ignore

src/test/regress/sql/bfv_planner.sql
@@ -196,9 +196,28 @@ execute myprep;
 execute myprep;
 
 
+-- Test agg on top of join subquery on partition table with ORDER-BY clause
+CREATE TABLE bfv_planner_t1 (a int, b int, c int) distributed by (c);
+CREATE TABLE bfv_planner_t2 (f int,g int) DISTRIBUTED BY (f) PARTITION BY RANGE(g)
+(
+PARTITION "201612" START (1) END (10)
+);
+insert into bfv_planner_t1 values(1,2,3), (2,3,4), (3,4,5);
+insert into bfv_planner_t2 values(3,1), (4,2), (5,2);
+
+select count(*) from
+(select a,b,c from bfv_planner_t1 order by c) T1
+join
+(select f,g from bfv_planner_t2) T2
+on
+T1.a=T2.g and T1.c=T2.f;
+
+
 -- start_ignore
 drop table if exists bfv_planner_x;
 drop table if exists testbadsql;
 drop table if exists bfv_planner_foo;
 drop table if exists testmedian;
+drop table if exists bfv_planner_t1;
+drop table if exists bfv_planner_t2;
 -- end_ignore

src/backend/cdb/cdbgroup.c
@@ -1050,14 +1050,14 @@ make_one_stage_agg_plan(PlannerInfo *root,
 			break;
 			
 	case MPP_GRP_PREP_FOCUS_QE:
-			result_plan = (Plan*)make_motion_gather_to_QE(result_plan, (current_pathkeys != NIL));
+			result_plan = (Plan*)make_motion_gather_to_QE(root, result_plan, current_pathkeys);
 			result_plan->total_cost += 
 				incremental_motion_cost(result_plan->plan_rows, 
 										result_plan->plan_rows * root->config->cdbpath_segments);
 			break;
 			
 	case MPP_GRP_PREP_FOCUS_QD:
-			result_plan = (Plan*)make_motion_gather_to_QD(result_plan, (current_pathkeys != NIL));	
+			result_plan = (Plan*)make_motion_gather_to_QD(root, result_plan, current_pathkeys);
 			result_plan->total_cost += 
 				incremental_motion_cost(result_plan->plan_rows, 
 										result_plan->plan_rows * root->config->cdbpath_segments);
@@ -1189,8 +1189,7 @@ make_one_stage_agg_plan(PlannerInfo *root,
 	{
 		Assert(!IsA(result_plan, Motion));
 		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 (current_pathkeys != NIL));
+										 result_plan->lefttree);
 	}
 
 	/* Marshal implicit results. Return explicit result. */
@@ -1476,7 +1475,7 @@ make_two_stage_agg_plan(PlannerInfo *root,
 		break;
 			
 	case MPP_GRP_TYPE_PLAIN_2STAGE:
-		result_plan = (Plan*)make_motion_gather_to_QE(result_plan, false);
+		result_plan = (Plan*)make_motion_gather_to_QE(root, result_plan, false);
 		result_plan->total_cost += 
 			incremental_motion_cost(result_plan->plan_rows,
 									result_plan->plan_rows * root->config->cdbpath_segments);
@@ -1764,9 +1763,7 @@ make_three_stage_agg_plan(PlannerInfo *root, MppGroupContext *ctx)
 		 * Reconstruct the flow since the targetlist for the result_plan may have
 		 * changed.
 		 */
-		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 true);
+		result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 		
 		/* Need to adjust root.  Is this enuf?  I think so. */
 		root->parse->rtable = rtable;
@@ -2066,7 +2063,7 @@ make_plan_for_one_dqa(PlannerInfo *root, MppGroupContext *ctx, int dqa_index,
 	
 	dqaduphazard = (aggstrategy == AGG_HASHED && stream_bottom_agg);
 
-	result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree, (aggstrategy == AGG_SORTED));
+	result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 	
 	current_pathkeys = NIL;
 	
@@ -2245,7 +2242,7 @@ make_plan_for_one_dqa(PlannerInfo *root, MppGroupContext *ctx, int dqa_index,
 		Assert(ctx->numGroupCols == 0); /* No grouping columns */
 		Assert(n == 1);
 
-		result_plan = (Plan*)make_motion_gather_to_QE(result_plan, false);
+		result_plan = (Plan*)make_motion_gather_to_QE(root, result_plan, false);
 		result_plan->total_cost += 
 				incremental_motion_cost(result_plan->plan_rows,
 						result_plan->plan_rows * root->config->cdbpath_segments);
@@ -2490,7 +2487,7 @@ join_dqa_coplan(PlannerInfo *root, MppGroupContext *ctx, Plan *outer, int dqa_in
 	join_plan->total_cost = outer->total_cost + inner->total_cost;
 	join_plan->total_cost += cpu_tuple_cost * join_plan->plan_rows;
 	
-	join_plan->flow = pull_up_Flow(join_plan, join_plan->lefttree, true);
+	join_plan->flow = pull_up_Flow(join_plan, join_plan->lefttree);
 	
 	return join_plan;
 }
@@ -4073,10 +4070,7 @@ add_second_stage_agg(PlannerInfo *root,
 	/*
 	 * Agg will not change the sort order unless it is hashed.
 	 */
-	agg_node->flow = pull_up_Flow(agg_node, 
-								  agg_node->lefttree, 
-								  (*p_current_pathkeys != NIL)
-								   && aggstrategy != AGG_HASHED );
+	agg_node->flow = pull_up_Flow(agg_node, agg_node->lefttree);
 
 	/* 
 	 * Since the rtable has changed, we had better recreate a RelOptInfo entry
@@ -5865,7 +5859,7 @@ within_agg_add_outer_sort(PlannerInfo *root,
 		}
 		if (outer_plan->flow->flotype != FLOW_SINGLETON)
 		{
-			outer_plan = (Plan *) make_motion_gather_to_QE(outer_plan, true);
+			outer_plan = (Plan *) make_motion_gather_to_QE(root, outer_plan, wag_context->current_pathkeys);
 			outer_plan->total_cost +=
 				incremental_motion_cost(outer_plan->plan_rows,
 							outer_plan->plan_rows * root->config->cdbpath_segments);
@@ -6969,9 +6963,7 @@ within_agg_planner(PlannerInfo *root,
 		 * Reconstruct the flow since the targetlist for the result_plan may have
 		 * changed.
 		 */
-		result_plan->flow = pull_up_Flow(result_plan,
-										 result_plan->lefttree,
-										 true);
+		result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 		/* Need to adjust root->parse for upper plan. */
 		root->parse->rtable = rtable;
 		root->parse->targetList = copyObject(result_plan->targetlist);

src/backend/cdb/cdbllize.c
@@ -253,20 +253,21 @@ cdbparallelize(PlannerInfo *root,
 	 * information in the profile stucture.
 	 */
 	prescan(plan, context);
-	
+
 	if ( context->dispatchParallel )
 	{
-		
-		/* From this point on, since part of plan is parallel, we have to
-		 * regard the whole plan as parallel. */
-		plan->dispatch = DISPATCH_PARALLEL;
-
 		/*
 		 * Implement the parallelizing directions in the Flow nodes attached
 		 * to the root plan node of each root slice of the plan.
 		 */
 		Assert(root->parse == query);
 		plan = apply_motion(root, plan, query);
+
+		/*
+		 * From this point on, since part of plan is parallel, we have to
+		 * regard the whole plan as parallel.
+		 */
+		plan->dispatch = DISPATCH_PARALLEL;
 	}
 
 	if (gp_enable_motion_deadlock_sanity)
@@ -411,7 +412,7 @@ ParallelizeCorrelatedSubPlanUpdateFlowMutator(Node *node)
 		case T_Result:
 		{
 			if(((Plan *)node)->lefttree && ((Plan *)node)->lefttree->flow)
-				((Plan *)node)->flow = pull_up_Flow((Plan *)node, ((Plan *)node)->lefttree, true);
+				((Plan *)node)->flow = pull_up_Flow((Plan *)node, ((Plan *)node)->lefttree);
 			break;
 		}
 		case T_Append:
@@ -421,8 +422,7 @@ ParallelizeCorrelatedSubPlanUpdateFlowMutator(Node *node)
 				break;
 			Plan *first_append = (Plan *) (lfirst(list_head(append_list)));
 			Assert(first_append && first_append->flow);
-			bool with_sort = list_length(append_list) == 1 ? true : false;
-			((Plan *)node)->flow = pull_up_Flow((Plan *)node, first_append, with_sort);
+			((Plan *)node)->flow = pull_up_Flow((Plan *)node, first_append);
 			break;
 		}
 		default:
@@ -607,7 +607,7 @@ ParallelizeCorrelatedSubPlanMutator(Node *node, ParallelizeCorrelatedPlanWalkerC
 		((Plan *) res)->extParam = saveExtParam;
 
 		Assert(((Plan *) res)->lefttree && ((Plan *) res)->lefttree->flow);
-		((Plan *) res)->flow = pull_up_Flow((Plan *) res, ((Plan *)res)->lefttree, true);
+		((Plan *) res)->flow = pull_up_Flow((Plan *) res, ((Plan *)res)->lefttree);
 
 		/**
 		 * It is possible that there is an additional level of correlation in the result node.
@@ -901,7 +901,7 @@ makeFlow(FlowType flotype)
  * want to use mark_passthru_locus (after make_subqueryscan) instead.
  */
 Flow *
-pull_up_Flow(Plan *plan, Plan *subplan, bool withSort)
+pull_up_Flow(Plan *plan, Plan *subplan)
 {
 	Flow	   *model_flow = NULL;
 	Flow	   *new_flow = NULL;
@@ -936,72 +936,6 @@ pull_up_Flow(Plan *plan, Plan *subplan, bool withSort)
                                                 plan->targetlist))
             new_flow->hashExpr = copyObject(model_flow->hashExpr);
 	}
-
-	/* Pull up sort key info if requested. */
-    if (withSort)
-	{
-        if (IsA(plan, Sort))
-        {
-	        Sort   *sort = (Sort *)plan;
-       	
-	        new_flow->numSortCols = sort->numCols;
-	        ARRAYCOPY(new_flow->sortColIdx, sort->sortColIdx, sort->numCols);
-	        ARRAYCOPY(new_flow->sortOperators, sort->sortOperators, sort->numCols);
-	        ARRAYCOPY(new_flow->nullsFirst, sort->nullsFirst, sort->numCols);
-        }
-        else if (model_flow->numSortCols == 0)
-            new_flow->numSortCols = 0;
-        else if (is_projection_capable_plan(plan))
-	    {
-		    /*
-             * Try to derive a sortColIdx to align with projected targetlist.
-             * It might be shorter than the subplan's sortColIdx, maybe even
-             * empty, if not all of the subplan's sort cols are projected.
-             */
-		    new_flow->numSortCols = cdbpullup_colIdx(plan,
-                                                     subplan,
-                                                     model_flow->numSortCols,
-                                                     model_flow->sortColIdx,
-                                                     &new_flow->sortColIdx);
-
-			/* preserve subplan sort attributes*/
-			if (new_flow->numSortCols < model_flow->numOrderbyCols)
-			{
-				new_flow->numOrderbyCols = new_flow->numSortCols;
-			}
-			else
-			{
-				new_flow->numOrderbyCols = model_flow->numOrderbyCols;
-			}
-
-		    if (new_flow->numSortCols > 0)
-			{
-                ARRAYCOPY(new_flow->sortOperators,
-                          model_flow->sortOperators,
-                          new_flow->numSortCols);
-                ARRAYCOPY(new_flow->nullsFirst,
-                          model_flow->nullsFirst,
-                          new_flow->numSortCols);
-			}
-	    }
-	    else
-	    {
-		    /* Non-projecting so sort attributes are the same. */
-            new_flow->numSortCols = model_flow->numSortCols;
-		    ARRAYCOPY(new_flow->sortColIdx,
-                      model_flow->sortColIdx,
-                      new_flow->numSortCols);
-            ARRAYCOPY(new_flow->sortOperators,
-                      model_flow->sortOperators,
-                      new_flow->numSortCols);
-            ARRAYCOPY(new_flow->nullsFirst,
-                      model_flow->nullsFirst,
-                      new_flow->numSortCols);
-
-			/* preserve subplan sort attributes*/
-            new_flow->numOrderbyCols = model_flow->numOrderbyCols;
-	    }
-	}   /* withSort */
 
 	new_flow->locustype = model_flow->locustype;
 	
@@ -1251,15 +1185,6 @@ adjustPlanFlow(Plan        *plan,
         flow->hashExpr = copyObject(kidflow->hashExpr);
 		plan->dispatch = plan->lefttree->dispatch;
 
-        /* Zap sort cols if motion has destroyed the ordering. */
-        if (disorder && !reorder)
-        {
-            flow->numSortCols = 0;
-            flow->sortColIdx = NULL;
-            flow->sortOperators = NULL;
-			flow->nullsFirst = NULL;
-        }
-
 		return true;  /* success */
 	}
 
@@ -1328,15 +1253,6 @@ adjustPlanFlow(Plan        *plan,
             Assert(0);
     }
 
-    /* Discard ordering info if the motion will disturb the order. */
-    if (disorder)
-    {
-        flow->numSortCols = 0;
-        flow->sortColIdx = NULL;
-        flow->sortOperators = NULL;
-		flow->nullsFirst = NULL;
-    }
-	
 	/* Since we added Motion, dispatch must be parallel. */
 	plan->dispatch = DISPATCH_PARALLEL;
 

commit d325233c2d0160383b1439a044cec6bbd0429bdd

Fix planner bug in handling LIMIT without an ORDER BY.
This bug was introduced in commit 7e268107f3, which changed the way we
track the "current" ordering in the planner.

src/backend/optimizer/plan/planner.c
@@ -2098,8 +2098,24 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 			/* pushdown the first phase of multi-phase limit (which takes offset into account) */
 			result_plan = pushdown_preliminary_limit(result_plan, parse->limitCount, count_est, parse->limitOffset, offset_est);
 			
-			/* Focus on QE [merge to preserve order], prior to final LIMIT. */
-			result_plan = (Plan *) make_motion_gather_to_QE(root, result_plan, current_pathkeys);
+			/*
+			 * Focus on QE [merge to preserve order], prior to final LIMIT.
+			 *
+			 * If there is an ORDER BY, the input should be in the required
+			 * order now, and we must preserve the order in the merge. But if
+			 * there is no ORDER BY, don't try to maintain the current input
+			 * order. In that case, current_pathkeys might contain unneeded
+			 * columns that have been eliminated from the final target list,
+			 * and we cannot maintain such an order in the Motion anymore.
+			 */
+			if (parse->sortClause)
+			{
+				if (!pathkeys_contained_in(sort_pathkeys, current_pathkeys))
+					elog(ERROR, "invalid result order generated for ORDER BY + LIMIT");
+				result_plan = (Plan *) make_motion_gather_to_QE(root, result_plan, sort_pathkeys);
+			}
+			else
+				result_plan = (Plan *) make_motion_gather_to_QE(root, result_plan, NIL);
 			result_plan->total_cost += motion_cost_per_row * result_plan->plan_rows;
 		}
 			

src/test/regress/expected/bfv_planner.out
@@ -319,6 +319,19 @@ T1.a=T2.g and T1.c=T2.f;
      2
 (1 row)
 
+-- This produced a "could not find pathkey item to sort" error at one point.
+-- The problem was that we stripped out column b from the SubqueryScan's
+-- target list, as it's not needed in the final result, but we tried to
+-- maintain the ordering (a,b) in the Gather Motion node, which would have
+-- required column b to be present, at least as a junk column.
+create table bfv_planner_t3 (a int4, b int4);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+select a from (select * from bfv_planner_t3 order by a, b) as x limit 1;
+ a 
+---
+(0 rows)
+
 -- start_ignore
 drop table if exists bfv_planner_x;
 drop table if exists testbadsql;

src/test/regress/sql/bfv_planner.sql
@@ -213,6 +213,15 @@ on
 T1.a=T2.g and T1.c=T2.f;
 
 
+-- This produced a "could not find pathkey item to sort" error at one point.
+-- The problem was that we stripped out column b from the SubqueryScan's
+-- target list, as it's not needed in the final result, but we tried to
+-- maintain the ordering (a,b) in the Gather Motion node, which would have
+-- required column b to be present, at least as a junk column.
+create table bfv_planner_t3 (a int4, b int4);
+select a from (select * from bfv_planner_t3 order by a, b) as x limit 1;
+
+
 -- start_ignore
 drop table if exists bfv_planner_x;
 drop table if exists testbadsql;