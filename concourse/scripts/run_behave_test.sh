440aec33851d203d71d0a28062388def35daeb34
Chris Hajas <chajas@pivotal.io>
2017-10-19 09:48:17 -0700

    Improve terraform run_*_test.sh scripts for easier debugging (#3467)
    We add a gpdb-env.sh file that sets up the test environment.
    This will be backported to 5X_STABLE.

--- concourse/scripts/run_behave_test.sh
+++ concourse/scripts/run_behave_test.sh
@@ -2,10 +2,16 @@
 
 BEHAVE_FLAGS=$@
 
-source /usr/local/greenplum-db-devel/greenplum_path.sh
-export PGPORT=5432
-export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
-export PGDATABASE=gptest
+cat > ~/gpdb-env.sh << EOF
+  source /usr/local/greenplum-db-devel/greenplum_path.sh
+  export PGPORT=5432
+  export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
+  export PGDATABASE=gptest
+
+  alias mdd='cd \$MASTER_DATA_DIRECTORY'
+EOF
+source ~/gpdb-env.sh
+
 createdb gptest
 gpconfig --skipvalidation -c fsync -v off
 gpstop -u
--- concourse/scripts/run_tinc_test.sh
+++ concourse/scripts/run_tinc_test.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-TINC_TARGET=$@
+TINC_TARGET="$@"
+TINC_DIR=/home/gpadmin/gpdb_src/src/test/tinc
 
-source /usr/local/greenplum-db-devel/greenplum_path.sh
-export PGPORT=5432
-export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
-export PGDATABASE=gptest
+cat > ~/gpdb-env.sh << EOF
+  source /usr/local/greenplum-db-devel/greenplum_path.sh
+  export PGPORT=5432
+  export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
+  export PGDATABASE=gptest
+
+  alias mdd='cd \$MASTER_DATA_DIRECTORY'
+  alias tinc='cd ${TINC_DIR}'
+EOF
+source ~/gpdb-env.sh
+
 createdb gptest
 createdb gpadmin
-cd /home/gpadmin/gpdb_src/src/test/tinc
+cd ${TINC_DIR}
 source tinc_env.sh
-make $@
+make ${TINC_TARGET}
