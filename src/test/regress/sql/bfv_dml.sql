fd05415259a85fcca9ccd3784321ab5b4ebd24ae
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-10-19 12:19:54 +0300

    Move tests for MPP-21536 out of TINC, to a new 'bfv_dml' test.
    I'm not entirely sure what the bug was that these test, or whether we have
    coverage for it already, but the tests are cheap enough that I don't mind
    just adding them.

---
+++ src/test/regress/sql/bfv_dml.sql
@@ -0,0 +1,102 @@
+--  MPP-21536: Duplicated rows inserted when ORCA is turned on
+
+-- create test table
+create table m();
+alter table m add column a int;
+alter table m add column b int;
+
+-- generate data for m
+insert into m select i, i%5 from generate_series(1,10)i;
+
+-- INSERT and UPDATE
+create table yyy(a int, b int) distributed randomly;
+
+insert into yyy select a,b from m;
+select * from yyy order by 1, 2;
+
+update yyy set a=m.b from m where m.a=yyy.b;
+select * from yyy order by 1, 2;
+
+drop table yyy;
+
+
+-- UPDATE with different values
+create table yyy(a int, b int) distributed randomly;
+
+insert into yyy select a,b from m;
+update yyy set b=m.b from m where m.a=yyy.a;
+select * from yyy order by 1, 2;
+
+drop table yyy;
+
+
+-- DELETE
+create table yyy(a int, b int) distributed randomly;
+
+insert into yyy select a,b from m;
+delete from yyy where a in (select a from m);
+select * from yyy order by 1, 2;
+
+drop table yyy;
+
+create table yyy(a int, b int) distributed randomly;
+insert into yyy select a,b from m;
+delete from yyy where b in (select a from m);
+select * from yyy order by 1, 2;
+
+drop table yyy;
+
+
+-- Now repeat all the above tests, but using a hacked master-only 'm' table
+drop table m;
+
+set optimizer_enable_master_only_queries=on;
+
+
+-- create master-only table
+create table m();
+set allow_system_table_mods='DML';
+delete from gp_distribution_policy where localoid='m'::regclass;
+reset allow_system_table_mods;
+alter table m add column a int;
+alter table m add column b int;
+
+-- generate data for m
+insert into m select i, i%5 from generate_series(1,10)i;
+
+create table zzz(a int primary key, b int) distributed by (a);
+insert into zzz select a,b from m;
+select * from zzz order by 1, 2;
+
+delete from zzz where a in (select a from m);
+select * from zzz order by 1, 2;
+
+drop table zzz;
+
+create table zzz(a int primary key, b int) distributed by (a);
+
+insert into zzz select a,b from m;
+delete from zzz where b in (select a from m);
+select * from zzz order by 1, 2;
+
+drop table zzz;
+
+create table zzz(a int primary key, b int) distributed by (a);
+insert into zzz select a,b from m;
+
+-- This update fails with duplicate key error, but it varies which segment
+-- reports it first, i.e. it varies which row it complaints first. Silence
+-- that difference in the error DETAIL line
+\set VERBOSITY terse
+update zzz set a=m.b from m where m.a=zzz.b;
+select * from zzz order by 1, 2;
+
+drop table zzz;
+
+create table zzz(a int primary key, b int) distributed by (a);
+insert into zzz select a,b from m;
+update zzz set b=m.b from m where m.a=zzz.a;
+select * from zzz order by 1, 2;
+
+drop table zzz;
+drop table m;
