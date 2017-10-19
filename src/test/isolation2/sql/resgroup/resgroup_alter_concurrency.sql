09211864f3bc5a8436952d90bc7aae5fc0dabc02
Ning Yu <nyu@pivotal.io>
2017-10-19 00:37:30 -0500

    resgroup: filter out '<IDLE>' sessions in two cases.
    In resgroup_alter_concurrency and resgroup_alter_memory cases we query
    pg_stat_activity to verify pending queries get executed after quitting
    of old sessions. Old sessions are quitted with the 'q' command of the
    isolation2 test framework, however this only disconnect the client but
    the QD may still be alive and if we then query pg_stat_activity quick
    enough then we might still see this old session occasionly.
    
    To get rid of this kind of __quitted__ sessions we added a filter to not
    list `<IDLE>` sessions. So we can always get a stable output in these
    two test cases.

--- src/test/isolation2/sql/resgroup/resgroup_alter_concurrency.sql
+++ src/test/isolation2/sql/resgroup/resgroup_alter_concurrency.sql
@@ -7,10 +7,15 @@ CREATE RESOURCE GROUP rg_concurrency_test WITH
 (concurrency=1, cpu_rate_limit=20, memory_limit=60, memory_shared_quota=0, memory_spill_ratio=10);
 CREATE ROLE role_concurrency_test RESOURCE GROUP rg_concurrency_test;
 
+-- After a 'q' command the client connection is disconnected but the
+-- QD may still be alive, if we then query pg_stat_activity quick enough
+-- we might still see this session with current_query '<IDLE>'.
+-- A filter is put to filter out this kind of quitted sessions.
 CREATE OR REPLACE VIEW rg_activity_status AS
 	SELECT rsgname, waiting_reason, current_query
 	FROM pg_stat_activity
-	WHERE rsgname='rg_concurrency_test';
+	WHERE rsgname='rg_concurrency_test'
+	  AND current_query <> '<IDLE>';
 
 --
 -- 1. increase concurrency after pending queries
