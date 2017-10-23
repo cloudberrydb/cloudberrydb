dd276d7296e999f94f48880c74a6c09e705a96b7
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-10-23 11:42:49 +0300

    Change one more test query to make the output stable.
    Previous commit missed one instance of this.

--- src/test/regress/sql/qp_dml_joins.sql
+++ src/test/regress/sql/qp_dml_joins.sql
@@ -323,7 +323,7 @@ SELECT COUNT(*) FROM dml_ao_check_s;
 -- test4: Negative test - Insert with joins where the result tuples violate the user defined check constraint
 SELECT COUNT(*) FROM dml_ao_check_r;
 SELECT COUNT(*) FROM (SELECT dml_ao_check_r.a + 110 , dml_ao_check_r.b, dml_ao_check_r.c, dml_ao_check_r.d FROM dml_ao_check_r, dml_ao_check_s WHERE dml_ao_check_r.a = dml_ao_check_s.a)foo;
-INSERT INTO dml_ao_check_r SELECT dml_ao_check_r.a + 110 , dml_ao_check_r.b, dml_ao_check_r.c, dml_ao_check_r.d FROM dml_ao_check_r, dml_ao_check_s WHERE dml_ao_check_r.a = dml_ao_check_s.a;
+INSERT INTO dml_ao_check_r SELECT dml_ao_check_r.a + 110 , dml_ao_check_r.b, dml_ao_check_r.c, dml_ao_check_r.d FROM dml_ao_check_r, dml_ao_check_s WHERE dml_ao_check_r.a = dml_ao_check_s.a AND dml_ao_check_r.b > 10;
 SELECT COUNT(*) FROM dml_ao_check_r;
 
 -- test5: Insert with joins where the result tuples violate violates multiple check constraints
