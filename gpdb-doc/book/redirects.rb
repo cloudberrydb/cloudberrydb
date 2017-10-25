e0e017ea31b68d3418a25cea85828546920d786d
dyozie <dyozie@pivotal.io>
2017-10-25 09:40:48 -0700

    Docs: Updating build file to include PXF section, and pull individual books into separate sections

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,2 +1,4 @@
-r301 '/', '/docs/common/gpdb-features.html'
-r301 '/index.html', '/docs/common/gpdb-features.html'
+r301 '/', '/docs/600/common/gpdb-features.html'
+r301 '/index.html', '/docs/600/common/gpdb-features.html'
+r301 '/docs/600/index.html', '/docs/600/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/docs/600/common/gpdb-features.html'
