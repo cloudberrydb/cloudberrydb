9c38d8e65eb5cfb1ba1e39af1b025e54d5a4f7ee
Richard Guo <guofenglinux@gmail.com>
2017-10-26 01:47:33 -0500

    Fix initializing SPIMemReserved when resource group is enabled.
    When resource group is enabled, initialization of
    SPIMemReserved should not depend on statement_mem.

--- src/backend/executor/spi.c
+++ src/backend/executor/spi.c
@@ -22,6 +22,7 @@
 #include "utils/memutils.h"
 #include "utils/snapmgr.h"
 #include "utils/typcache.h"
+#include "utils/resource_manager.h"
 #include "utils/resscheduler.h"
 
 #include "cdb/cdbvars.h"
@@ -2550,7 +2551,15 @@ static uint64 SPIMemReserved = 0;
 void SPI_InitMemoryReservation(void)
 {
 	Assert(!IsResManagerMemoryPolicyNone());
-	SPIMemReserved = (uint64) statement_mem * 1024L;;
+
+	if (IsResGroupEnabled())
+	{
+		SPIMemReserved = 0;
+	}
+	else
+	{
+		SPIMemReserved = (uint64) statement_mem * 1024L;;
+	}
 }
 
 /**
