4e4482d89f54fb3379e789cc5e7cafbcc709f396
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-10-31 08:24:52 +0200

    Remove tests on triggers and falling back to planner.
    I don't see the value of these tests. We have tests on triggers in the
    'triggers' pg_regress test already. Cases that just fall back from ORCA to
    the Postgres planner don't seem very interesting, and in particular, I
    don't see the point of testing specifically that they do fall back.

--- src/test/tinc/tincrepo/dml/triggers/sql/child_part_fallback.sql
+++
@@ -1,9 +0,0 @@
---start_ignore
-SET client_min_messages='log';
-INSERT INTO dml_trigger_table VALUES('TEST',10);
-SET client_min_messages='notice';
---end_ignore
-
-SELECT * FROM dml_trigger_table order by 2;
-
-\!sed -n '/Planner/p' %MYD%/output/child_part_fallback_orca.out
