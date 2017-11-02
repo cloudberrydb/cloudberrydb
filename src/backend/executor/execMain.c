07a88acca360ffd6f3f06a2f02c3873e1e1b0420
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-11-02 19:56:26 +0200

    Don't fire INSERT or DELETE triggers on UPDATE.
    With ORCA, an UPDATE is actually implemented as an DELETE + INSERT. Don't
    fire INSERT or DELETE triggers in that case.
    
    This was broken by commit 740f304eea. I wonder if we should somehow fire
    UPDATE triggers in that case, but I don't see any existing code to do that
    either.

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -3159,18 +3159,13 @@ ExecInsert(TupleTableSlot *slot,
 
 	/* BEFORE ROW INSERT Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
-		resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0)
+		resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0 &&
+		!isUpdate)
 	{
 		HeapTuple	newtuple;
 		HeapTuple	tuple;
 
 		tuple = ExecFetchSlotHeapTuple(slot);
-
-		/*
-		 * Not implemented for ORCA yet. It should fall back to the Postgres planner
-		 * if there are any before-row triggers.
-		 */
-		Assert(planGen == PLANGEN_PLANNER);
 
 		newtuple = ExecBRInsertTriggers(estate, resultRelInfo, tuple);
 
@@ -3284,7 +3279,8 @@ ExecInsert(TupleTableSlot *slot,
 
 	/* AFTER ROW INSERT Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
-		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0)
+		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0 &&
+		!isUpdate)
 	{
 		HeapTuple tuple = ExecFetchSlotHeapTuple(slot);
 
