88aa3411f02bcc6cc6f8388f4555bfdc67e8b2a4
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-11-06 13:24:20 +0200

    Rename test table, to avoid clashing with other tests.

--- src/test/regress/sql/partition.sql
+++ src/test/regress/sql/partition.sql
@@ -633,7 +633,7 @@ copy partsupp from stdin with delimiter '|';
 \.
 drop table partsupp;
 --MPP-3285
-CREATE TABLE LINEITEM (
+CREATE TABLE PARTLINEITEM (
                 L_ORDERKEY INT8,
                 L_PARTKEY INTEGER,
                 L_SUPPKEY INTEGER,
@@ -656,7 +656,7 @@ partition by range (l_commitdate)
 partition p1 start('1992-01-31') end('1998-11-01') every(interval '20 months')
 
 );
-copy lineitem from stdin with delimiter '|';
+copy partlineitem from stdin with delimiter '|';
 18182|5794|3295|4|9|15298.11|0.04|0.01|N|O|1995-07-04|1995-05-30|1995-08-03|DELIVER IN PERSON|RAIL|y special platelets
 \.
 
@@ -667,9 +667,9 @@ select parname, parruleord, pg_get_expr(parrangestart, parchildrelid, false) as
   pg_get_expr(parlistvalues, parchildrelid, false) as list from
    pg_partition_rule
     r, pg_partition p where r.paroid = p.oid and p.parlevel = 0 and
-	 p.parrelid = 'lineitem'::regclass order by 1;
+	 p.parrelid = 'partlineitem'::regclass order by 1;
 
-drop table lineitem;
+drop table partlineitem;
 
 -- Make sure ADD creates dependencies
 create table i (i int) partition by range(i) (start (1) end(3) every(1));
