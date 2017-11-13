85b743e39ec0ba960b6de8b036e2dac969bf1e87
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-11-13 10:35:19 +0200

    Make 'gporca' test pass regardless of log_statement setting.
    This test would only produce the LOG lines memorized in the expected output
    if log_statement='all' was set. Remove the assumption, by temporarily
    setting log_statement (and log_min_duration_statement), like in some
    earlier tests in the same file.

--- src/test/regress/sql/gporca.sql
+++ src/test/regress/sql/gporca.sql
@@ -1399,9 +1399,13 @@ create table foo (a int, b character varying(10));
 explain select * from foo where b in ('1', '2');
 
 set optimizer_enable_ctas = off;
+set log_statement='none';
+set log_min_duration_statement=-1;
 set client_min_messages='log';
 create table foo_ctas(a) as (select generate_series(1,10)) distributed by (a);
 reset client_min_messages;
+reset log_min_duration_statement;
+reset log_statement;
 reset optimizer_enable_ctas;
 
 -- start_ignore
