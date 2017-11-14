3e29b346fb378a86dd0f1f744e0dd9bd3ba4a25a
Larry Hamel <lhamel@pivotal.io>
2017-11-13 16:07:02 -0800

    adapt gpcloud installcheck for mulitple platforms
    - add regression test for gpcloud on ubuntu
    - create pattern for reusing task across platforms
    - Remove configure step since it is not necessary
    - Remove unused files
    
    Signed-off-by: Goutam Tadi <gtadi@pivotal.io>

--- concourse/scripts/build_gpdb.py
+++ concourse/scripts/build_gpdb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 import optparse
+import os
 import subprocess
 import sys
 
@@ -10,6 +11,7 @@ CODEGEN_MODE = 'codegen'
 ORCA_CODEGEN_DEFAULT_MODE = "orca_codegen"
 ORCA_MODE = 'orca'
 PLANNER_MODE = 'planner'
+INSTALL_DIR = "/usr/local/gpdb"
 
 
 def make(num_cpus):
@@ -20,10 +22,17 @@ def make(num_cpus):
                            cwd="gpdb_src")
 
 
-def install(output_dir):
-    subprocess.call("make install", cwd="gpdb_src", shell=True)
-    subprocess.call("mkdir -p " + output_dir, shell=True)
-    return subprocess.call("cp -r /usr/local/gpdb/* " + output_dir, shell=True)
+def install():
+    return subprocess.call("make install", cwd="gpdb_src", shell=True)
+
+
+def copy_installed(output_dir):
+    if os.path.normpath(INSTALL_DIR) != os.path.normpath(output_dir):
+        status = subprocess.call("mkdir -p " + output_dir, shell=True)
+        if status:
+            return status
+        return subprocess.call("cp -r %s/*  %s" % (INSTALL_DIR, output_dir), shell=True)
+    return
 
 
 def unittest():
@@ -31,8 +40,10 @@ def unittest():
 
 
 def print_compiler_version():
-    subprocess.call(["g++", "--version"])
-    subprocess.call(["gcc", "--version"])
+    status = subprocess.call(["g++", "--version"])
+    if status:
+        return status
+    return subprocess.call(["gcc", "--version"])
 
 
 def main():
@@ -41,7 +52,7 @@ def main():
     parser.add_option("--mode", choices=[ORCA_MODE, CODEGEN_MODE, ORCA_CODEGEN_DEFAULT_MODE, PLANNER_MODE], default=ORCA_CODEGEN_DEFAULT_MODE)
     parser.add_option("--compiler", dest="compiler")
     parser.add_option("--cxxflags", dest="cxxflags")
-    parser.add_option("--output_dir", dest="output_dir", default="install")
+    parser.add_option("--output_dir", dest="output_dir", default=INSTALL_DIR)
     (options, args) = parser.parse_args()
     ci_common = GpBuild(ORCA_CODEGEN_DEFAULT_MODE)
     if options.mode == ORCA_MODE:
@@ -53,7 +64,9 @@ def main():
         status = ci_common.install_dependency(dependency)
         if status:
             return status
-    print_compiler_version()
+    status = print_compiler_version()
+    if status:
+        return status
     status = ci_common.configure()
     if status:
         return status
@@ -63,7 +76,10 @@ def main():
     status = unittest()
     if status:
         return status
-    status = install(options.output_dir)
+    status = install()
+    if status:
+        return status
+    status = copy_installed(options.output_dir)
     if status:
         return status
     return 0
--- concourse/scripts/builds/GpBuild.py
+++ concourse/scripts/builds/GpBuild.py
@@ -1,27 +1,29 @@
 import subprocess
 from GpdbBuildBase import GpdbBuildBase
 
+INSTALL_DIR="/usr/local/gpdb"
+
 
 class GpBuild(GpdbBuildBase):
-    def __init__(self, mode):
+    def __init__(self, mode="orca"):
         GpdbBuildBase.__init__(self)
         self.mode = 'on' if mode == 'orca' else 'off'
 
     def configure(self):
-        return subprocess.call(["./configure",
-                                "--enable-mapreduce",
-                                "--with-gssapi",
-                                "--with-perl",
-                                "--with-libxml",
-                                "--with-python",
-                                "--disable-gpcloud",
-                                "--prefix=/usr/local/gpdb"], cwd="gpdb_src")
+        config_options = ["--enable-mapreduce", "--with-gssapi", "--with-perl", "--with-libxml", "--with-python",
+                          "--prefix=%s" % INSTALL_DIR]
+        if self.mode == 'off':
+            config_options.append("--disable-orca")
+        cmd_with_options = ["./configure"]
+        cmd_with_options.extend(config_options)
+        return subprocess.call(cmd_with_options, cwd="gpdb_src")
 
     @staticmethod
     def create_demo_cluster():
         return subprocess.call([
-            "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
-            && make create-demo-cluster DEFAULT_QD_MAX_CONNECT=150\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
+            "runuser gpadmin -c \"source %s/greenplum_path.sh \
+            && make create-demo-cluster DEFAULT_QD_MAX_CONNECT=150\"" % INSTALL_DIR],
+            cwd="gpdb_src/gpAux/gpdemo", shell=True)
 
     def install_check(self, option='good'):
         status = self.create_demo_cluster()
@@ -34,6 +36,6 @@ class GpBuild(GpdbBuildBase):
 
     def run_install_check_with_command(self, make_command):
         return subprocess.call([
-            "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
-            && source gpAux/gpdemo/gpdemo-env.sh && PGOPTIONS='-c optimizer={0}' \
-            {1} \"".format(self.mode, make_command)], cwd="gpdb_src", shell=True)
+            "runuser gpadmin -c \"source {0}/greenplum_path.sh \
+            && source gpAux/gpdemo/gpdemo-env.sh && PGOPTIONS='-c optimizer={1}' \
+            {2} \"".format(INSTALL_DIR, self.mode, make_command)], cwd="gpdb_src", shell=True)
