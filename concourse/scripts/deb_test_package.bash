3de9e17567eae1cb34bebc24d10f27d3fe4dedaf
Goutam Tadi <goutam.tadi1@gmail.com>
2017-12-08 08:30:15 -0800

    Add tests for the effect of --extra-version-suffix
    - should append to both psql 'select version()' and also to 'gpssh --version'

--- concourse/scripts/deb_test_package.bash
+++ concourse/scripts/deb_test_package.bash
@@ -9,3 +9,6 @@ ${CURRENT_DIR}/gpdb_src/concourse/scripts/setup_gpadmin_user.bash
 su - gpadmin ${CURRENT_DIR}/gpdb_src/concourse/scripts/deb_init_cluster.bash
 su - gpadmin ${CURRENT_DIR}/gpdb_src/concourse/scripts/deb_test_cluster.bash
 
+su - gpadmin -c "source /opt/gpdb/greenplum_path.sh && psql -t -U gpadmin template1 -c 'select version()' | grep '\-oss'"
+# gpssh replaces "-" with " " in version string
+su - gpadmin -c "source /opt/gpdb/greenplum_path.sh && gpssh --version | grep ' oss'"
