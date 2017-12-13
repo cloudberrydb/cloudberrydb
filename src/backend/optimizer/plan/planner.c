cloudberrydb/cloudberrydb

commit 916f460f27945df4e0472206435e3cd1c551de7f

Ensure that ORCA is not called on any process other than the master QD
We don't want to use the optimizer for planning queries in SQL, pl/pgSQL
etc. functions when that is done on the segments.

ORCA excels in complex queries, most of which will access distributed
tables. We can't run such queries from the segments slices anyway
because they require dispatching a query within another - which is not
allowed in GPDB. Note that this restriction also applies to non-QD
master slices.  Furthermore, ORCA doesn't currently support pl/*
statements (relevant when they are planned on the segments).

For these reasons, restrict to using ORCA on the master QD processes
only.

Also revert commit d79a2c7 ("Fix pipeline failures caused by 0dfd0eb.")
and separate out gporca fault injector tests in newly added
gporca_faults.sql so that the rest can run in a parallel group.

Signed-off-by: Jesse Zhang <sbjesse@gmail.com>

src/backend/optimizer/plan/planner.c
@@ -184,11 +184,17 @@ standard_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	instr_time		endtime;
 
 	/*
-	 * If ORCA has been enabled, and we are in a state in which ORCA planning
-	 * is supported, then go ahead.
+	 * Use ORCA only if it is enabled and we are in a master QD process.
+	 *
+	 * ORCA excels in complex queries, most of which will access distributed
+	 * tables. We can't run such queries from the segments slices anyway because
+	 * they require dispatching a query within another - which is not allowed in
+	 * GPDB (see querytree_safe_for_segment()). Note that this restriction also
+	 * applies to non-QD master slices.  Furthermore, ORCA doesn't currently
+	 * support pl/* statements (relevant when they are planned on the segments).
+	 * For these reasons, restrict to using ORCA on the master QD processes only.
 	 */
-	if (optimizer &&
-		GP_ROLE_UTILITY != Gp_role && MASTER_CONTENT_ID == GpIdentity.segindex)
+	if (optimizer && GP_ROLE_DISPATCH == Gp_role)
 	{
 		if (gp_log_optimization_time)
 			INSTR_TIME_SET_CURRENT(starttime);

src/test/regress/expected/gporca.out
@@ -1944,35 +1944,6 @@ select sum(a) from orca.r group by b having count(*) > 2 order by b+1;
   57
 (6 rows)
 
--- test interruption requests to optimization
-CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
-select count(*) from orca.s;
- count 
--------
-    30
-(1 row)
-
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
 -- constants
 select 0.001::numeric from orca.r;
  numeric 

src/test/regress/expected/gporca_faults.out
@@ -1,0 +1,80 @@
+--
+-- ORCA tests which require gp_fault_injector
+--
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+CREATE SCHEMA gporca_faults;
+SET search_path = gporca_faults, public;
+CREATE TABLE foo (a int, b int);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+INSERT INTO foo VALUES (1,1);
+-- test interruption requests to optimization
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault
+-----------------
+ t
+(1 row)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+NOTICE:  Success:
+ gp_inject_fault
+-----------------
+ t
+(1 row)
+
+select count(*) from foo;
+ count
+-------
+     1
+(1 row)
+
+-- Ensure that ORCA is not called on any process other than the master QD
+CREATE FUNCTION func1_nosql_vol(x int) RETURNS int AS $$
+BEGIN
+  RETURN $1 +1;
+END
+$$ LANGUAGE plpgsql VOLATILE;
+-- Query that runs the function on an additional non-QD master slice
+-- Include the EXPLAIN to ensure that this happens in the plan.
+EXPLAIN SELECT * FROM func1_nosql_vol(5), foo;
+                                         QUERY PLAN
+--------------------------------------------------------------------------------------------
+ Gather Motion 3:1  (slice2; segments: 3)  (cost=0.30..1.37 rows=4 width=12)
+   ->  Nested Loop  (cost=0.30..1.37 rows=2 width=12)
+         ->  Seq Scan on foo  (cost=0.00..1.01 rows=1 width=8)
+         ->  Materialize  (cost=0.30..0.33 rows=1 width=4)
+               ->  Broadcast Motion 1:3  (slice1)  (cost=0.00..0.30 rows=3 width=4)
+                     ->  Function Scan on func1_nosql_vol  (cost=0.00..0.26 rows=1 width=4)
+ Settings:  optimizer=off
+ Optimizer status: legacy query optimizer
+(8 rows)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault
+-----------------
+ t
+(1 row)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+NOTICE:  Success:
+ gp_inject_fault
+-----------------
+ t
+(1 row)
+
+SELECT * FROM func1_nosql_vol(5), foo;
+ func1_nosql_vol | a | b
+-----------------+---+---
+               6 | 1 | 1
+(1 row)
+
+-- The fault should *not* be hit above when optimizer = off, to reset it now.
+SELECT gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault
+-----------------
+ t
+(1 row)
+

src/test/regress/expected/gporca_faults_optimizer.out
@@ -1,0 +1,72 @@
+--
+-- ORCA tests which require gp_fault_injector
+--
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+CREATE SCHEMA gporca_faults;
+SET search_path = gporca_faults, public;
+CREATE TABLE foo (a int, b int);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+INSERT INTO foo VALUES (1,1);
+-- test interruption requests to optimization
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault 
+-----------------
+ t
+(1 row)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+NOTICE:  Success:
+ gp_inject_fault 
+-----------------
+ t
+(1 row)
+
+select count(*) from foo;
+ERROR:  canceling statement due to user request
+-- Ensure that ORCA is not called on any process other than the master QD
+CREATE FUNCTION func1_nosql_vol(x int) RETURNS int AS $$
+BEGIN
+  RETURN $1 +1;
+END
+$$ LANGUAGE plpgsql VOLATILE;
+-- Query that runs the function on an additional non-QD master slice
+-- Include the EXPLAIN to ensure that this happens in the plan.
+EXPLAIN SELECT * FROM func1_nosql_vol(5), foo;
+                                         QUERY PLAN                                         
+--------------------------------------------------------------------------------------------
+ Gather Motion 3:1  (slice2; segments: 3)  (cost=0.30..1.37 rows=4 width=12)
+   ->  Nested Loop  (cost=0.30..1.37 rows=2 width=12)
+         ->  Seq Scan on foo  (cost=0.00..1.01 rows=1 width=8)
+         ->  Materialize  (cost=0.30..0.33 rows=1 width=4)
+               ->  Broadcast Motion 1:3  (slice1)  (cost=0.00..0.30 rows=3 width=4)
+                     ->  Function Scan on func1_nosql_vol  (cost=0.00..0.26 rows=1 width=4)
+ Settings:  optimizer=on
+ Optimizer status: legacy query optimizer
+(8 rows)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault 
+-----------------
+ t
+(1 row)
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+NOTICE:  Success:
+ gp_inject_fault 
+-----------------
+ t
+(1 row)
+
+SELECT * FROM func1_nosql_vol(5), foo;
+ERROR:  canceling statement due to user request
+-- The fault should *not* be hit above when optimizer = off, to reset it now.
+SELECT gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+NOTICE:  Success:
+ gp_inject_fault 
+-----------------
+ t
+(1 row)
+

src/test/regress/expected/gporca_optimizer.out
@@ -1945,34 +1945,6 @@ select sum(a) from orca.r group by b having count(*) > 2 order by b+1;
   57
 (6 rows)
 
--- test interruption requests to optimization
-CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-INFO:  GPORCA failed to produce a plan, falling back to planner
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
-INFO:  GPORCA failed to produce a plan, falling back to planner
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
-select count(*) from orca.s;
-ERROR:  canceling statement due to user request
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-INFO:  GPORCA failed to produce a plan, falling back to planner
-NOTICE:  Success:
- gp_inject_fault 
------------------
- t
-(1 row)
-
 -- constants
 select 0.001::numeric from orca.r;
  numeric 

src/test/regress/greenplum_schedule
@@ -99,9 +99,9 @@ test: direct_dispatch bfv_dd bfv_dd_multicolumn bfv_dd_types
 # (https://git.postgresql.org/gitweb/?p=postgresql.git;a=commitdiff;h=e5550d5fec66aa74caad1f79b79826ec64898688)
 test: catalog
 
-test: gporca
-
-test: bfv_catalog bfv_index bfv_olap bfv_aggregate bfv_partition bfv_partition_plans DML_over_joins bfv_statistic
+test: bfv_catalog bfv_index bfv_olap bfv_aggregate bfv_partition bfv_partition_plans DML_over_joins gporca bfv_statistic
+# NOTE: gporca_faults uses gp_fault_injector - so do not add to a parallel group
+test: gporca_faults
  
 test: aggregate_with_groupingsets 
 

src/test/regress/sql/gporca.sql
@@ -64,13 +64,6 @@ select b from orca.r group by b having  count(*) <= avg(a) + (select count(*) fr
 select sum(a) from orca.r group by b having count(*) > 2 order by b+1;
 select sum(a) from orca.r group by b having count(*) > 2 order by b+1;
 
--- test interruption requests to optimization
-CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
-select count(*) from orca.s;
-select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
-
 -- constants
 
 select 0.001::numeric from orca.r;

src/test/regress/sql/gporca_faults.sql
@@ -1,0 +1,34 @@
+--
+-- ORCA tests which require gp_fault_injector
+--
+
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+
+CREATE SCHEMA gporca_faults;
+SET search_path = gporca_faults, public;
+
+CREATE TABLE foo (a int, b int);
+INSERT INTO foo VALUES (1,1);
+
+-- test interruption requests to optimization
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+select count(*) from foo;
+
+-- Ensure that ORCA is not called on any process other than the master QD
+CREATE FUNCTION func1_nosql_vol(x int) RETURNS int AS $$
+BEGIN
+  RETURN $1 +1;
+END
+$$ LANGUAGE plpgsql VOLATILE;
+
+-- Query that runs the function on an additional non-QD master slice
+-- Include the EXPLAIN to ensure that this happens in the plan.
+EXPLAIN SELECT * FROM func1_nosql_vol(5), foo;
+
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);
+select gp_inject_fault('opt_relcache_translator_catalog_access', 'interrupt', 1);
+SELECT * FROM func1_nosql_vol(5), foo;
+
+-- The fault should *not* be hit above when optimizer = off, to reset it now.
+SELECT gp_inject_fault('opt_relcache_translator_catalog_access', 'reset', 1);

commit 0d3ae2a02115f7fc70a6c2fc27344e4c379232a9

Fix storage test failures caused by 916f460
The default value of Gp_role is set to GP_ROLE_DISPATCH. Which means
auxiliary processes inherit this value. FileRep does the same, but also
executes queries using SPI on the segment. Which means Gp_role ==
GP_ROLE_DISPATCH is not a sufficient check for master QD.

So, bring back the check on GpIdentity.

Author: Asim R P <apraveen@pivotal.io>
Author: Shreedhar Hardikar <shardikar@pivotal.io>

src/backend/optimizer/plan/planner.c
@@ -194,7 +194,9 @@ standard_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	 * support pl/* statements (relevant when they are planned on the segments).
 	 * For these reasons, restrict to using ORCA on the master QD processes only.
 	 */
-	if (optimizer && GP_ROLE_DISPATCH == Gp_role)
+	if (optimizer &&
+		GP_ROLE_DISPATCH == Gp_role &&
+		MASTER_CONTENT_ID == GpIdentity.segindex)
 	{
 		if (gp_log_optimization_time)
 			INSTR_TIME_SET_CURRENT(starttime);