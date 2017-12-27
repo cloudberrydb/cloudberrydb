629b7b3fab064e4390329a820ae54404835f3fdc
Ashwin Agrawal <aagrawal@pivotal.io>
2017-12-27 14:16:04 -0800

    Delete duplicate_entries tests as its specific to filerep.
    The test is specific to filerep behavior whene truncate was not properly
    resynced, causing the problem.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/access_methods/duplicate_entries/check_duplicate_entry.sql
+++
@@ -1,4 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-select gp_segment_id, oid, count(*) from gp_dist_random('pg_class') group by 1, 2 having count(*) > 1;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/access_methods/duplicate_entries/reindex_vacuum.sql
+++
@@ -1,6 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-\c testdb
-reindex database testdb;
-vacuum full;
