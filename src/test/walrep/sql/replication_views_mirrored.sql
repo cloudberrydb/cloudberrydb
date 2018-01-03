b0966a5e36be6cdedd1ef449047b6d8297175966
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-01-03 14:57:24 +0200

    Update tests, now that QD node has a standby too, by default.
    I'm actually not sure why the QD now suddenly has a standby in the default
    demo cluster. That seems to have happened as a side-effect of the filespace
    removal commit. That change was supposedly made already in commit 67c9ab91b9
    already, on Dec 20th, but seems that it didn't actually happen until now,
    for some reason. In any case, that is what we want, so adjust the tests
    to cope.

--- src/test/walrep/sql/replication_views_mirrored.sql
+++ src/test/walrep/sql/replication_views_mirrored.sql
@@ -4,8 +4,5 @@ SELECT count(*) FROM gp_segment_configuration where preferred_role='m' and statu
 -- Check how many WAL replication mirrors are up and in-sync
 SELECT count(*) FROM gp_segment_configuration where preferred_role='m' and mode='s';
 
--- Check pg_stat_replication view (this assumes standby master has not been created)
-SELECT * FROM pg_stat_replication;
-
 -- Check pg_stat_replication view of master and primary segments
 SELECT gp_segment_id, application_name, state, sync_state FROM gp_stat_replication;
