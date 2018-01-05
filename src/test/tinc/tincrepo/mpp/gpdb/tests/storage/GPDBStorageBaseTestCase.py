a545bd50979673a3bbb9d9265fbd2431db75621d
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-01-05 22:08:32 +0200

    Remove reference to obsolete gpfilespace tinc code.
    There are more in other tests, but I suspect those other tests are obsolete
    in whole, so I'm just patching this one that caused a failure in the
    concourse pipeline.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/GPDBStorageBaseTestCase.py
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/GPDBStorageBaseTestCase.py
@@ -29,7 +29,6 @@ from mpp.lib.gprecoverseg import GpRecover
 from mpp.lib.gpstart import GpStart
 from mpp.lib.gpstop import GpStop
 from mpp.lib.config import GPDBConfig
-from mpp.lib.gpfilespace import Gpfilespace
 from mpp.lib.gpdbverify import GpdbVerify
 from mpp.models import MPPTestCase
 from mpp.gpdb.tests.storage.lib.dbstate import DbStateClass
@@ -52,7 +51,6 @@ class GPDBStorageBaseTestCase():
         self.gprecover = GpRecover(self.config)
         self.gpstop = GpStop()
         self.gpstart = GpStart()
-        self.gpfile = Gpfilespace(self.config)
         self.gpverify = GpdbVerify(config=self.config)
         self.dbstate = DbStateClass('run_validation', self.config)
         self.port = os.getenv('PGPORT')
