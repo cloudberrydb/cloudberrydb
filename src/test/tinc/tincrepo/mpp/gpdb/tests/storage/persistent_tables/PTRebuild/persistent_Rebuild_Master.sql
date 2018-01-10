a2c772c5ffa897889f0c7faef10caa186bebbdca
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-01-10 13:14:08 +0200

    Remove obsolete PT rebuild TINC tests.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/persistent_tables/PTRebuild/persistent_Rebuild_Master.sql
+++
@@ -1,3 +0,0 @@
--- Rebuild Persistent Object on Master --
-select * from gp_persistent_reset_all();
-select * from gp_persistent_build_all(false);
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/persistent_tables/PTRebuild/persistent_Rebuild_Segment.sql
+++
@@ -1,3 +0,0 @@
--- Rebuild Persistent Object on Segment --
-select * from gp_persistent_reset_all();
-select * from gp_persistent_build_all(true);
