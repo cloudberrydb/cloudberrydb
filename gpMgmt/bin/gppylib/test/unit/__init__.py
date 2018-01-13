zhaoshiling1017/gpdb

Fix more calls to GpDB.initFromString()
Follow-up to the previous commit.

gpMgmt/bin/gppylib/test/unit/__init__.py
@@ -5,9 +5,9 @@ __path__[0] = _abspath(__path__[0])
 from gppylib.gparray import GpArray, GpDB
 
 def setup_fake_gparray():
-    master = GpDB.initFromString("1|-1|p|p|s|u|mdw|mdw|5432|None|/data/master||/data/master/base/10899,/data/master/base/1,/data/master/base/10898,/data/master/base/25780,/data/master/base/34782")
-    primary0 = GpDB.initFromString("2|0|p|p|s|u|sdw1|sdw1|40000|41000|/data/primary0||/data/primary0/base/10899,/data/primary0/base/1,/data/primary0/base/10898,/data/primary0/base/25780,/data/primary0/base/34782")
-    primary1 = GpDB.initFromString("3|1|p|p|s|u|sdw2|sdw2|40001|41001|/data/primary1||/data/primary1/base/10899,/data/primary1/base/1,/data/primary1/base/10898,/data/primary1/base/25780,/data/primary1/base/34782")
-    mirror0 = GpDB.initFromString("4|0|m|m|s|u|sdw2|sdw2|50000|51000|/data/mirror0||/data/mirror0/base/10899,/data/mirror0/base/1,/data/mirror0/base/10898,/data/mirror0/base/25780,/data/mirror0/base/34782")
-    mirror1 = GpDB.initFromString("5|1|m|m|s|u|sdw1|sdw1|50001|51001|/data/mirror1||/data/mirror1/base/10899,/data/mirror1/base/1,/data/mirror1/base/10898,/data/mirror1/base/25780,/data/mirror1/base/34782")
+    master = GpDB.initFromString("1|-1|p|p|s|u|mdw|mdw|5432|/data/master")
+    primary0 = GpDB.initFromString("2|0|p|p|s|u|sdw1|sdw1|40000|/data/primary0")
+    primary1 = GpDB.initFromString("3|1|p|p|s|u|sdw2|sdw2|40001|/data/primary1")
+    mirror0 = GpDB.initFromString("4|0|m|m|s|u|sdw2|sdw2|50000|/data/mirror0")
+    mirror1 = GpDB.initFromString("5|1|m|m|s|u|sdw1|sdw1|50001|/data/mirror1")
     return GpArray([master,primary0,primary1,mirror0,mirror1])

cloudberrydb/cloudberrydb

gpMgmt/bin/gppylib/test/unit/__init__.py
@@ -5,9 +5,9 @@ __path__[0] = _abspath(__path__[0])
 from gppylib.gparray import GpArray, Segment
 
 def setup_fake_gparray():
-    master = Segment.initFromString("1|-1|p|p|s|u|mdw|mdw|5432|None|/data/master||/data/master/base/10899,/data/master/base/1,/data/master/base/10898,/data/master/base/25780,/data/master/base/34782")
-    primary0 = Segment.initFromString("2|0|p|p|s|u|sdw1|sdw1|40000|41000|/data/primary0||/data/primary0/base/10899,/data/primary0/base/1,/data/primary0/base/10898,/data/primary0/base/25780,/data/primary0/base/34782")
-    primary1 = Segment.initFromString("3|1|p|p|s|u|sdw2|sdw2|40001|41001|/data/primary1||/data/primary1/base/10899,/data/primary1/base/1,/data/primary1/base/10898,/data/primary1/base/25780,/data/primary1/base/34782")
-    mirror0 = Segment.initFromString("4|0|m|m|s|u|sdw2|sdw2|50000|51000|/data/mirror0||/data/mirror0/base/10899,/data/mirror0/base/1,/data/mirror0/base/10898,/data/mirror0/base/25780,/data/mirror0/base/34782")
-    mirror1 = Segment.initFromString("5|1|m|m|s|u|sdw1|sdw1|50001|51001|/data/mirror1||/data/mirror1/base/10899,/data/mirror1/base/1,/data/mirror1/base/10898,/data/mirror1/base/25780,/data/mirror1/base/34782")
+    master = Segment.initFromString("1|-1|p|p|s|u|mdw|mdw|5432|/data/master")
+    primary0 = Segment.initFromString("2|0|p|p|s|u|sdw1|sdw1|40000|/data/primary0")
+    primary1 = Segment.initFromString("3|1|p|p|s|u|sdw2|sdw2|40001|/data/primary1")
+    mirror0 = Segment.initFromString("4|0|m|m|s|u|sdw2|sdw2|50000|/data/mirror0")
+    mirror1 = Segment.initFromString("5|1|m|m|s|u|sdw1|sdw1|50001|/data/mirror1")
     return GpArray([master,primary0,primary1,mirror0,mirror1])