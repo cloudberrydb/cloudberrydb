a8756078b5d4b4b5838f781558c4d838ceccfb54
Ashwin Agrawal <aagrawal@pivotal.io>
2018-01-16 12:06:44 -0800

    TINC: Remove filerep_end_to_end test files.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/filerep_end_to_end/misc/ctlog_xlog_cons_post_reset_ct/sql/ctlog_xlog_cons_post_reset_ct.sql
+++
@@ -1,8 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-insert into r1 values (generate_series(1,1600));
-insert into r1 values (generate_series(1,1600));
-insert into r1 values (generate_series(1,1600));
-insert into r1 values (generate_series(1,1600));
-insert into r1 values (generate_series(1,1600));
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/filerep_end_to_end/misc/ctlog_xlog_cons_st/sql/ctlog_xlog_cons.sql
+++
@@ -1,6 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-create table r1 (a int);
-create table ao (a int) with (appendonly=true);
-insert into ao values (generate_series(1,100));
