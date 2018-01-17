addc1c441fb8a19c17bb38c20f8697eddf5633e0
Ekta Khanna <ekhanna@pivotal.io>
2018-01-17 15:28:29 -0800

    Updating Memory accounting test query
    The existing memory accounting test for OOM was not able to exceed the
    OOM bound due to the plan generated by ORCA v2.53.11. Prior to that the
    it would hit the OOM limit.  This commit updates the
    test query to exceed OOM with the latest version of ORCA and also bump
    the version to 2.53.11.
    
    Signed-off-by: Abhijit Subramanya <asubramanya@pivotal.io>

--- src/test/tinc/tincrepo/resource_management/memory_accounting/scenario/oom_test/sql/oom.sql
+++ src/test/tinc/tincrepo/resource_management/memory_accounting/scenario/oom_test/sql/oom.sql
@@ -5,23 +5,27 @@
 
 show gp_vmem_protect_limit;
 
+--start_ignore
+set optimizer_enable_motion_redistribute=off;
+--end_ignore
+
 select 2 as oom_test;
 select count(*) from
-  (select l0.l_orderkey from
+  (select l0.l_partkey from
     (lineitem l0
-     left outer join lineitem l1 on l0.l_orderkey = l1.l_orderkey
-     left outer join lineitem l2 on l1.l_orderkey = l2.l_orderkey
-     left outer join lineitem l3 on l2.l_orderkey = l3.l_orderkey
-     left outer join lineitem l4 on l3.l_orderkey = l4.l_orderkey
-     left outer join lineitem l5 on l4.l_orderkey = l5.l_orderkey
-     left outer join lineitem l6 on l5.l_orderkey = l6.l_orderkey
-     left outer join lineitem l7 on l6.l_orderkey = l7.l_orderkey
-     left outer join lineitem l8 on l7.l_orderkey = l8.l_orderkey
-     left outer join lineitem l9 on l8.l_orderkey = l9.l_orderkey
-     left outer join lineitem l10 on l9.l_orderkey = l10.l_orderkey
-     left outer join lineitem l11 on l10.l_orderkey = l11.l_orderkey
-     left outer join lineitem l12 on l11.l_orderkey = l12.l_orderkey
-     left outer join lineitem l13 on l12.l_orderkey = l13.l_orderkey
-     left outer join lineitem l14 on l13.l_orderkey = l14.l_orderkey
-     left outer join lineitem l15 on l14.l_orderkey = l15.l_orderkey)
-    order by l0.l_orderkey) as foo;
+     left outer join lineitem l1 on l0.l_partkey = l1.l_partkey
+     left outer join lineitem l2 on l1.l_partkey = l2.l_partkey
+     left outer join lineitem l3 on l2.l_partkey = l3.l_partkey
+     left outer join lineitem l4 on l3.l_partkey = l4.l_partkey
+     left outer join lineitem l5 on l4.l_partkey = l5.l_partkey
+     left outer join lineitem l6 on l5.l_partkey = l6.l_partkey
+     left outer join lineitem l7 on l6.l_partkey = l7.l_partkey
+     left outer join lineitem l8 on l7.l_partkey = l8.l_partkey
+     left outer join lineitem l9 on l8.l_partkey = l9.l_partkey
+     left outer join lineitem l10 on l9.l_partkey = l10.l_partkey
+     left outer join lineitem l11 on l10.l_partkey = l11.l_partkey
+     left outer join lineitem l12 on l11.l_partkey = l12.l_partkey
+     left outer join lineitem l13 on l12.l_partkey = l13.l_partkey
+     left outer join lineitem l14 on l13.l_partkey = l14.l_partkey
+     left outer join lineitem l15 on l14.l_partkey = l15.l_partkey)
+    order by l0.l_partkey) as foo;
