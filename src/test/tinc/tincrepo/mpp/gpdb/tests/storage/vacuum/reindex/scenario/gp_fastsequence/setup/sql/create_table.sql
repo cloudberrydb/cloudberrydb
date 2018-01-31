09ed20e6fcd4dcb1471067070b351027c6ddb06e
Taylor Vesely <tvesely@pivotal.io>
2018-01-31 11:59:09 -0800

    Moves gp_fastsequence tests to isolation2
    Co-authored-by: Taylor Vesely <tvesely@pivotal.io>
    Co-authored-by: Asim Praveen <apraveen@pivotal.io>

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/vacuum/reindex/scenario/gp_fastsequence/setup/sql/create_table.sql
+++
@@ -1,7 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-drop table if exists test_fastseqence;
-create table test_fastseqence ( a int, b char(20)) with (appendonly = true, orientation=column);
-create index test_fastseqence_idx on test_fastseqence(b);
-insert into test_fastseqence select i , 'aa'||i from generate_series(1,100) i;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/vacuum/reindex/test_reindex.py
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/vacuum/reindex/test_reindex.py
@@ -56,35 +56,6 @@ class ReindexTestCase(ScenarioTestCase, MPPTestCase):
         list_verify.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.reindex_verify")
         self.test_case_scenario.append(list_verify, serial=True)
 
-    def test_reindex_gpFastSequence(self):
-        '''
-        This is to test gp_fastsequence reindex while insert is happening in other relations
-        STEPS:
-        1. Create aoco table with index (setup_gpfastseq)
-        2. In TXN 1 Inject fault to suspend reindex_relation and issue reindex gp_fastsequence (reindex_gpfastseq)
-        3. In TXN 2 Check that the fault injected in TXN 1 is set and then insert 100 rows in table created in step 1.
-           (insert_tup_gpfastseq). This step will wait as it needs to update gp_fastsequence last_sequence for the aoco
-            on the Master relation and TXN 1 has placed a lock on gp_fastsequence table.
-        4. In TXN 3 Reset the reindex_relation fault so that TXN 1 and TXN 2 can finish (reset_fault)
-        5. Verify and validate that relfilenodes (for gp_fastsequence and the aoco tables) are synced on all segments
-           and gp_fastsequence last_sequence is calculated correctly after each insert for aoco relation on all
-           segments (verify_gpfastseq)
-        '''
-
-        list_setup = []
-        list_setup.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.setup_gpfastseq")
-        self.test_case_scenario.append(list_setup, serial=True)
-
-        list_insert_tup_reindex_gpfastsequence = []
-        list_insert_tup_reindex_gpfastsequence.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.reindex_gpfastseq")
-        list_insert_tup_reindex_gpfastsequence.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.insert_tup_gpfastseq")
-        list_insert_tup_reindex_gpfastsequence.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.reset_fault")
-        self.test_case_scenario.append(list_insert_tup_reindex_gpfastsequence, serial=False)
-
-        list_verify = []
-        list_verify.append("mpp.gpdb.tests.storage.vacuum.reindex.scenario.test_scenario.verify_gpfastseq")
-        self.test_case_scenario.append(list_verify, serial=True)
-
 class ReindexConcurrencyTestCase(SQLIsolationTestCase):
     '''
     Test for REINDEX (index/table/system/database) with various concurrent transactions.
