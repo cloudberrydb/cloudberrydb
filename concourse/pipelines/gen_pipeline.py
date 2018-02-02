507b784c403a9b9db6469ce963065379276c87e1
C.J. Jameson <cjameson@pivotal.io>
2018-02-01 17:08:57 -0800

    concourse gen-pipeline: only update file on disk if validated
    Previously, the validation was run on the pipeline after it was written to disk
    
    Now, run validation on the in-memory representation of the pipeline. To make
    this pattern more natural, bring the validate pipeline logic into the
    gen_pipeline.py script. Previously, the validate pipeline script was run
    separately, but I don't think it's used in isolation any more.
    
    Authored-by: C.J. Jameson <cjameson@pivotal.io>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -25,10 +25,12 @@ Python module requirements:
   - jinja2 (install through pip or easy_install)
 """
 
-import os
-import datetime
 import argparse
+import datetime
+import os
+import re
 import subprocess
+import yaml
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -43,9 +45,45 @@ TEMPLATE_ENVIRONMENT = Environment(
     variable_end_string=']]',
     extensions=['jinja2.ext.loopcontrols'])
 
+# Variables that govern pipeline validation
+RELEASE_VALIDATOR_JOB = ['Release_Candidate']
+JOBS_THAT_ARE_GATES = ['gate_compile_start', 'gate_compile_end', 'gate_icw_start',
+                    'gate_icw_end', 'gate_cs_start', 'gate_cs_end', 'gate_mpp_start',
+                    'gate_mpp_end', 'gate_mm_start', 'gate_mm_end', 'gate_dpm_start',
+                    'gate_dpm_end', 'gate_ud_start', 'gate_ud_end']
+JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE = ['compile_gpdb_binary_swap_centos6', 'icw_gporca_centos6_gpos_memory'] + RELEASE_VALIDATOR_JOB + JOBS_THAT_ARE_GATES
+
+
 def render_template(template_filename, context):
     """Render template"""
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+def validate_pipeline_release_jobs(raw_pipeline_yml):
+    print "======================================================================"
+    print "Validate Pipeline Release Jobs"
+    print "----------------------------------------------------------------------"
+
+    pipeline_yml_cleaned = re.sub('{{', '', re.sub('}}', '', raw_pipeline_yml)) # ignore concourse v2.x variable interpolation
+    pipeline = yaml.load(pipeline_yml_cleaned)
+
+    jobs_raw = pipeline['jobs']
+    all_job_names = [job['name'] for job in jobs_raw]
+
+    release_candidate_job = [ job for job in jobs_raw if job['name'] == 'Release_Candidate' ][0]
+    release_qualifying_job_names = release_candidate_job['plan'][0]['passed']
+
+    jobs_that_are_not_blocking_release = [job for job in all_job_names if job not in release_qualifying_job_names]
+
+    unaccounted_for_jobs = [job for job in jobs_that_are_not_blocking_release if job not in JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE]
+
+    if unaccounted_for_jobs:
+        print "Please add the following jobs as a Release_Candidate dependency or ignore them"
+        print "by adding them to JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE in "+ __file__
+        print unaccounted_for_jobs
+        return False
+
+    print "Pipeline validated: all jobs accounted for"
+    return True
 
 def create_pipeline():
     """Generate OS specific pipeline sections
@@ -70,13 +108,58 @@ def create_pipeline():
         'test_trigger': test_trigger
     }
 
-    with open(ARGS.output_filepath, 'w') as output:
-        yml = render_template('pipeline_header.yml', context)
-        output.write(yml)
+    pipeline_yml = render_template(ARGS.template_filename, context)
+    if ARGS.pipeline_type == 'prod':
+        validated = validate_pipeline_release_jobs(pipeline_yml)
+        if not validated:
+            print "Refusing to update the pipeline file"
+            return False
 
-    with open(ARGS.output_filepath, 'a') as output:
-        yml = render_template(ARGS.template_filename, context)
-        output.write(yml)
+    with open(ARGS.output_filepath, 'w') as output:
+        header = render_template('pipeline_header.yml', context)
+        output.write(header)
+        output.write(pipeline_yml)
+
+    return True
+
+def how_to_use_generated_pipeline_message():
+    msg = '\n'
+    msg += '======================================================================\n'
+    msg += '  Generate Pipeline type: .. : %s\n' % ARGS.pipeline_type
+    msg += '  Pipeline file ............ : %s\n' % ARGS.output_filepath
+    msg += '  Template file ............ : %s\n' % ARGS.template_filename
+    msg += '  OS Types ................. : %s\n' % ARGS.os_types
+    msg += '  Test sections ............ : %s\n' % ARGS.test_sections
+    msg += '  test_trigger ............. : %s\n' % ARGS.test_trigger_false
+    msg += '======================================================================\n\n'
+    if ARGS.pipeline_type == 'prod':
+        msg += 'NOTE: You can set the production pipelines with the following:\n\n'
+        msg += 'fly -t gpdb-prod \\\n'
+        msg += '    set-pipeline \\\n'
+        msg += '    -p gpdb_master \\\n'
+        msg += '    -c %s \\\n' % ARGS.output_filepath
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_master-ci-secrets.yml\n\n'
+        msg += 'fly -t gpdb-prod \\\n'
+        msg += '    set-pipeline \\\n'
+        msg += '    -p gpdb_master_without_asserts \\\n'
+        msg += '    -c %s \\\n' % ARGS.output_filepath
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_master_without_asserts-ci-secrets.yml\n' # pylint: disable=line-too-long
+    else:
+        msg += 'NOTE: You can set the developer pipeline with the following:\n\n'
+        msg += 'fly -t gpdb-dev \\\n'
+        msg += '    set-pipeline \\\n'
+        msg += '    -p %s \\\n' % os.path.basename(ARGS.output_filepath).rsplit('.', 1)[0]
+        msg += '    -c %s \\\n' % ARGS.output_filepath
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
+        msg += '    -l ~/workspace/continuous-integration/secrets/gpdb_master-ci-secrets.yml \\\n'
+        msg += '    -v tf-bucket-path=dev/' + ARGS.pipeline_type + '/ \\\n'
+        MSG += '    -v bucket-name=gpdb5-concourse-builds-dev \\\n'
+        MSG += '    -v gpdb-git-remote=<https://github.com/<github-user>/gpdb> \\\n'
+        MSG += '    -v gpdb-git-branch=<branch-name>\n'
+
+    return msg
 
 
 if __name__ == "__main__":
@@ -141,55 +224,10 @@ if __name__ == "__main__":
         default_dev_output_filename = 'gpdb-' + ARGS.pipeline_type + '-' + ARGS.user + '.yml'
         ARGS.output_filepath = os.path.join(PIPELINES_DIR, default_dev_output_filename)
 
-    MSG = '\n'
-    MSG += '======================================================================\n'
-    MSG += '  Generate Pipeline type: .. : %s\n' % ARGS.pipeline_type
-    MSG += '  Pipeline file ............ : %s\n' % ARGS.output_filepath
-    MSG += '  Template file ............ : %s\n' % ARGS.template_filename
-    MSG += '  OS Types ................. : %s\n' % ARGS.os_types
-    MSG += '  Test sections ............ : %s\n' % ARGS.test_sections
-    MSG += '  test_trigger ............. : %s\n' % ARGS.test_trigger_false
-    MSG += '======================================================================\n\n'
-    if ARGS.pipeline_type == 'prod':
-        MSG += 'NOTE: You can set the production pipelines with the following:\n\n'
-        MSG += 'fly -t gpdb-prod \\\n'
-        MSG += '    set-pipeline \\\n'
-        MSG += '    -p gpdb_master \\\n'
-        MSG += '    -c %s \\\n' % ARGS.output_filepath
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_master-ci-secrets.yml\n\n'
-        MSG += 'fly -t gpdb-prod \\\n'
-        MSG += '    set-pipeline \\\n'
-        MSG += '    -p gpdb_master_without_asserts \\\n'
-        MSG += '    -c %s \\\n' % ARGS.output_filepath
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_master_without_asserts-ci-secrets.yml\n' # pylint: disable=line-too-long
+    pipeline_created = create_pipeline()
+
+    if pipeline_created:
+        print how_to_use_generated_pipeline_message()
     else:
-        MSG += 'NOTE: You can set the developer pipeline with the following:\n\n'
-        MSG += 'fly -t gpdb-dev \\\n'
-        MSG += '    set-pipeline \\\n'
-        MSG += '    -p %s \\\n' % os.path.basename(ARGS.output_filepath).rsplit('.', 1)[0]
-        MSG += '    -c %s \\\n' % ARGS.output_filepath
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \\\n'
-        MSG += '    -l ~/workspace/continuous-integration/secrets/gpdb_master-ci-secrets.yml \\\n'
-        MSG += '    -v tf-bucket-path=dev/' + ARGS.pipeline_type + '/ \\\n'
-        MSG += '    -v bucket-name=gpdb5-concourse-builds-dev \\\n'
-        MSG += '    -v gpdb-git-remote=<https://github.com/<github-user>/gpdb> \\\n'
-        MSG += '    -v gpdb-git-branch=<branch-name>\n'
-
-    create_pipeline()
-
-    if ARGS.pipeline_type == 'prod':
-        print "======================================================================"
-        print "Validate Pipeline Release Jobs"
-        print "----------------------------------------------------------------------"
-        try:
-            env = os.environ.copy()
-            env['PIPELINE_FILE'] = ARGS.output_filepath
-            validator_script = os.path.join(PIPELINES_DIR, "../scripts/validate_pipeline_release_jobs.py")
-            subprocess.check_call(["python", validator_script], env=env)
-        except subprocess.CalledProcessError:
-            exit(1)
-
-    print MSG
+        exit(1)
 
