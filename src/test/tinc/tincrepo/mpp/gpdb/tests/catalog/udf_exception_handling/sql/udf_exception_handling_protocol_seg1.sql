2738ccb3f697665ff465f9069bc926a376a689a8
Ashwin Agrawal <aagrawal@pivotal.io>
2018-02-06 13:33:44 -0800

    TINC: Delete make target aocoalter_catalog_loaders.
    Delete udf_exception_handling tinc tests.
    
    Also remove tinc stage from CI `aocoalter_catalog_loaders`. Majority
    of tests from this group has been moved to ICW and remaining ones are
    now part of generic `make storage` target.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/catalog/udf_exception_handling/sql/udf_exception_handling_protocol_seg1.sql
+++
@@ -1,7 +0,0 @@
--- @Description: The Test executes Procedure  to exercise UDF Exception Handling in various subtransaction condition for debug_dtm_action_target=protocol and debug_dtm_action_segment = 1
--- 
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS shops;
-select test_protocol_allseg(1, 2,'f','abc',100,0);
-select * from employees;
-select * from shops order by id;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/catalog/udf_exception_handling/sql/udf_exception_handling_sql_seg0.sql
+++
@@ -1,4 +0,0 @@
--- @Description: Setting up the tables for Exception Handling test for SQL (Exception happening while update is done on table)
--- 
-
-update worker set salary = salary + 10;
