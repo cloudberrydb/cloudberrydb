07893cee7a73841928b1823b1c3e835890e33924
Daniel Gustafsson <dgustafsson@pivotal.io>
2018-02-12 09:24:10 +0100

    Remove workarounds for bug in ancient macOS version
    The referenced bug does not affect modern versions of macOS (tested
    back to El Capitan), and Snow Leopard is EOL since long.  Further,
    this workaround is not present/needed in PostgreSQL, with the code
    running on Leopard as well as Snow Leopard in the buildfarm without
    issues, so it seems that this problem has been fixed even in Snow
    Leopard in the later patchlevels.  Remove the workarounds and align
    the code with upstream.

--- src/backend/commands/tablespace.c
+++ src/backend/commands/tablespace.c
@@ -735,10 +735,6 @@ destroy_tablespace_directories(Oid tablespaceoid, bool redo)
 			strcmp(de->d_name, "..") == 0)
 			continue;
 
-		/* Odd... On snow leopard, we get back "/" as a subdir, which is wrong. Ingore it */
-		if (de->d_name[0] == '/' && de->d_name[1] == '\0')
-			continue;
-
 		subfile = palloc(strlen(linkloc_with_version_dir) + 1 + strlen(de->d_name) + 1);
 		sprintf(subfile, "%s/%s", linkloc_with_version_dir, de->d_name);
 
@@ -819,9 +815,6 @@ directory_is_empty(const char *path)
 	{
 		if (strcmp(de->d_name, ".") == 0 ||
 			strcmp(de->d_name, "..") == 0)
-			continue;
-		/* Odd... On snow leopard, we get back "/" as a subdir, which is wrong. Ingore it */
-		if (de->d_name[0] == '/' && de->d_name[1] == '\0')
 			continue;
 		FreeDir(dirdesc);
 		return false;
