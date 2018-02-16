24d3a75b1fbd4c534d3c0f0c291b5707f781cb36
Ashwin Agrawal <aagrawal@pivotal.io>
2018-02-16 14:56:22 -0800

    TINC: Cleanup storage directory.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/access_methods/appendonly_eof/sql_concurrency/ao_conc_load1_teardown.sql
+++
@@ -1,4 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-SELECT COUNT(*) FROM TEST_AO_LOAD1;
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/access_methods/appendonly_eof/sql_concurrency/aoco_conc_load1_teardown.sql
+++
@@ -1,4 +0,0 @@
--- start_ignore
-SET gp_create_table_random_default_distribution=off;
--- end_ignore
-SELECT COUNT(*) FROM TEST_AOCO_LOAD1;
