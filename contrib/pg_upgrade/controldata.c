c687f53f76ac9d772a41ff4dfde5956351f80b90
Jacob Champion <pchampion@pivotal.io>
2018-02-22 13:33:09 -0800

    get_control_data: fix checksum version check
    Follow-up to 71916050. Checksums were backported to Postgres 8.3, so we
    ignore them for <= 8.2.
    
    While we're at it, switch the data_checksum_version from a false
    assignment to a zero assignment, to match upstream. The actual
    implementation is an unsigned int; Postgres master incorrectly uses a
    bool type in the declaration.

--- contrib/pg_upgrade/controldata.c
+++ contrib/pg_upgrade/controldata.c
@@ -127,11 +127,11 @@ get_control_data(migratorContext *ctx, ClusterInfo *cluster, bool live_check)
 	/*
 	 * In PostgreSQL, checksums were introduced in 9.3 so the test for checksum
 	 * version applies to <= 9.2. Greenplum backported checksums into 5.x which
-	 * is based on PostgreSQL 8.3 so this test need to go on <= 8.3 instead.
+	 * is based on PostgreSQL 8.3 so this test need to go on <= 8.2 instead.
 	 */
-	if (GET_MAJOR_VERSION(cluster->major_version) <= 803)
+	if (GET_MAJOR_VERSION(cluster->major_version) <= 802)
 	{
-		cluster->controldata.data_checksum_version = false;
+		cluster->controldata.data_checksum_version = 0;
 		got_data_checksums = true;
 	}
 
