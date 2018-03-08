cloudberrydb/cloudberrydb

commit ff97c70b82bf5f93e65d04bb961b9f01851f0415

Reduce the contention on tmLock
1. move TMGXACT to PGPROC.
2. create distributed snapshot and create checkpoint will
traverse procArray and acquire ProcArrayLock. shmControlLock
is only used for serialize recoverTM().
3. get rid of shmGxactArray and maintain an array of TMGXACT_LOG
for recovery.

Author: Gang Xiong <gxiong@pivotal.io>
Author: Asim R P <apraveen@pivotal.io>
Author: Ashwin Agrawal <aagrawal@pivotal.io>

src/backend/storage/ipc/procarray.c
@@ -37,6 +37,7 @@
 #include "access/xact.h"
 #include "access/twophase.h"
 #include "miscadmin.h"
+#include "port/atomics.h"
 #include "storage/procarray.h"
 #include "utils/combocid.h"
 #include "utils/snapmgr.h"
@@ -227,6 +228,13 @@ ProcArrayRemove(PGPROC *proc, TransactionId latestXid)
 	elog(LOG, "failed to find proc %p in ProcArray", proc);
 }
 
+
+void
+ProcArrayEndGxact(void)
+{
+	Assert(LWLockHeldByMe(ProcArrayLock));
+	initGxact(&MyProc->gxact);
+}
 
 /*
  * ProcArrayEndTransaction -- mark a transaction as no longer running
@@ -1042,6 +1050,309 @@ QEwriterSnapshotUpToDate(void)
 	LWLockRelease(SharedLocalSnapshotSlot->slotLock);
 
 	return result;
+}
+
+void
+getAllDistributedXactStatus(TMGALLXACTSTATUS **allDistributedXactStatus)
+{
+	TMGALLXACTSTATUS *all;
+	int			count;
+	ProcArrayStruct *arrayP = procArray;
+
+	all = palloc(sizeof(TMGALLXACTSTATUS));
+	all->next = 0;
+	all->count = 0;
+	all->statusArray = NULL;
+
+	LWLockAcquire(ProcArrayLock, LW_EXCLUSIVE);
+	count = arrayP->numProcs;
+	if (count > 0)
+	{
+		int			i;
+
+		all->statusArray =
+			palloc(MAXALIGN(count * sizeof(TMGXACTSTATUS)));
+		for (i = 0; i < count; i++)
+		{
+			PGPROC *proc = arrayP->procs[i];
+			TMGXACT *gxact = &proc->gxact;
+
+			all->statusArray[i].gxid = gxact->gxid;
+			if (strlen(gxact->gid) >= TMGIDSIZE)
+				elog(PANIC, "Distribute transaction identifier too long (%d)",
+						(int) strlen(gxact->gid));
+			memcpy(all->statusArray[i].gid, gxact->gid, TMGIDSIZE);
+			all->statusArray[i].state = gxact->state;
+			all->statusArray[i].sessionId = gxact->sessionId;
+			all->statusArray[i].xminDistributedSnapshot = gxact->xminDistributedSnapshot;
+		}
+
+		all->count = count;
+	}
+
+	LWLockRelease(ProcArrayLock);
+
+	*allDistributedXactStatus = all;
+}
+
+/*
+ * Get check point information
+ *
+ * Whether DTM started or not, we must always store DTM information in
+ * this checkpoint record.  A possible case to consider is we might have
+ * in-progress global transactions in shared memory after postmaster reset,
+ * and shutting down without performing DTM recovery.  The subsequent
+ * recovery after this shutdown will read this checkpoint, so we would
+ * lose the in-progress global transaction information if we didn't write it
+ * here.  Note we will certainly read this global transaction information
+ * even if this is a clean shutdown (i.e. not performing multi-pass recovery.)
+ */
+void
+getDtxCheckPointInfo(char **result, int *result_size)
+{
+	TMGXACT_CHECKPOINT *gxact_checkpoint;
+	TMGXACT_LOG *gxact_log_array;
+	int			i;
+	int			actual;
+	ProcArrayStruct *arrayP = procArray;
+
+	if (GpIdentity.segindex != MASTER_CONTENT_ID)
+	{
+		gxact_checkpoint = palloc(TMGXACT_CHECKPOINT_BYTES(0));
+		gxact_checkpoint->committedCount = 0;
+		*result = (char*) gxact_checkpoint;
+		*result_size = TMGXACT_CHECKPOINT_BYTES(0);
+		return;
+	}
+
+	gxact_checkpoint = palloc(TMGXACT_CHECKPOINT_BYTES(arrayP->numProcs + *shmNumCommittedGxacts));
+	gxact_log_array = &gxact_checkpoint->committedGxactArray[0];
+
+	actual = 0;
+	for (i = 0; i < *shmNumCommittedGxacts; i++)
+		gxact_log_array[actual++] = shmCommittedGxactArray[i++];
+
+	SIMPLE_FAULT_INJECTOR(CheckPointDtxInfo);
+
+	/*
+	 * If a transaction inserted 'commit' record logically before the checkpoint
+	 * REDO pointer, and it hasn't inserted the 'forget' record. we will see its
+	 * 'TMGXACT->state' is between 'DTX_STATE_INSERTED_COMMITTED' and
+	 * 'DTX_STATE_INSERTING_FORGET_COMMITTED'. such transactions should be included
+	 * in the checkpoint record so that the second phase of 2PC can be executed
+	 * during crash recovery.
+	 *
+	 * NOTE: the REDO pointer is obtained much earlier in CreateCheckpoint().
+	 * It is possible to include transactions having their commit records
+	 * *after* the REDO pointer in checkpoint record.  Second phase of 2PC for
+	 * such transactions will be executed twice during crash recovery.
+	 * Although redundant, this is not a problem.
+	 */
+	LWLockAcquire(ProcArrayLock, LW_SHARED);
+
+	for (i = 0; i < arrayP->numProcs; i++)
+	{
+		TMGXACT_LOG *gxact_log;
+		PGPROC  *proc = arrayP->procs[i];
+		TMGXACT *gxact = &proc->gxact;
+
+		if (!includeInCheckpointIsNeeded(gxact))
+			continue;
+
+		gxact_log = &gxact_log_array[actual];
+		if (strlen(gxact->gid) >= TMGIDSIZE)
+			elog(PANIC, "Distribute transaction identifier too long (%d)",
+				 (int) strlen(gxact->gid));
+		memcpy(gxact_log->gid, gxact->gid, TMGIDSIZE);
+		gxact_log->gxid = gxact->gxid;
+
+		elog((Debug_print_full_dtm ? LOG : DEBUG5),
+			 "Add DTM checkpoint entry gid = %s.", gxact->gid);
+
+		actual++;
+	}
+
+	LWLockRelease(ProcArrayLock);
+
+	gxact_checkpoint->committedCount = actual;
+
+	*result = (char *) gxact_checkpoint;
+	*result_size = TMGXACT_CHECKPOINT_BYTES(actual);
+
+	elog((Debug_print_full_dtm ? LOG : DEBUG5),
+		 "Filled in DTM checkpoint information (count = %d).", actual);
+}
+
+/*
+ * DistributedSnapshotMappedEntry_Compare: A compare function for
+ * DistributedTransactionId for use with qsort.
+ */
+static int
+DistributedSnapshotMappedEntry_Compare(const void *p1, const void *p2)
+{
+	const DistributedTransactionId distribXid1 = *(DistributedTransactionId *) p1;
+	const DistributedTransactionId distribXid2 = *(DistributedTransactionId *) p2;
+
+	if (distribXid1 == distribXid2)
+		return 0;
+	else if (distribXid1 > distribXid2)
+		return 1;
+	else
+		return -1;
+}
+
+/*
+ * create distributed snapshot based on current visible distributed transaction
+ */
+static bool
+CreateDistributedSnapshot(DistributedSnapshotWithLocalMapping *distribSnapshotWithLocalMapping)
+{
+	int			i;
+	int			count;
+	DistributedTransactionId xmin;
+	DistributedTransactionId xmax;
+	DistributedSnapshotId distribSnapshotId;
+	DistributedTransactionId globalXminDistributedSnapshots;
+	DistributedSnapshot *ds;
+	ProcArrayStruct *arrayP = procArray;
+
+	Assert(LWLockHeldByMe(ProcArrayLock));
+	if (*shmNumCommittedGxacts != 0)
+		elog(ERROR, "Create distributed snapshot before DTM recovery finish");
+
+	xmin = LastDistributedTransactionId;
+
+	/*
+	 * This is analogous to the code in GetSnapshotData() (which calls
+	 * ReadNewTransactionId(), the distributed-xmax of a transaction is the
+	 * last distributed-xmax available
+	 */
+	xmax = getMaxDistributedXid();
+
+	/*
+	 * initialize for calculation with xmax, the calculation for this is on
+	 * same lines as globalxmin for local snapshot.
+	 */
+	globalXminDistributedSnapshots = xmax;
+	count = 0;
+	ds = &distribSnapshotWithLocalMapping->ds;
+
+	/*
+	 * Gather up current in-progress global transactions for the distributed
+	 * snapshot.
+	 */
+	for (i = 0; i < arrayP->numProcs; i++)
+	{
+		PGPROC	*proc = arrayP->procs[i];
+		TMGXACT	*gxact_candidate = &proc->gxact;
+		volatile DistributedTransactionId gxid;
+		DistributedTransactionId dxid;
+
+		/* just fetch once */
+		gxid = gxact_candidate->gxid;
+		if (gxid == InvalidDistributedTransactionId)
+			continue;
+
+		if (gxact_candidate->state == DTX_STATE_ACTIVE_NOT_DISTRIBUTED)
+			continue;
+
+		Assert(gxact_candidate->state != DTX_STATE_ACTIVE_NOT_DISTRIBUTED &&
+			   gxact_candidate->state != DTX_STATE_NONE);
+
+		/* Update globalXminDistributedSnapshots to be the smallest valid dxid */
+		dxid = gxact_candidate->xminDistributedSnapshot;
+		if ((dxid != InvalidDistributedTransactionId) &&
+			dxid < globalXminDistributedSnapshots)
+		{
+			globalXminDistributedSnapshots = dxid;
+		}
+
+		/*
+		 * Include the current distributed transaction in the min/max
+		 * calculation.
+		 */
+		if (gxid < xmin)
+		{
+			xmin = gxid;
+		}
+		if (gxid > xmax)
+		{
+			xmax = gxid;
+		}
+
+		if (proc == MyProc)
+			continue;
+
+		if (count >= ds->maxCount)
+			elog(ERROR, "Too many distributed transactions for snapshot");
+
+		ds->inProgressXidArray[count++] = gxid;
+
+		elog((Debug_print_full_dtm ? LOG : DEBUG5),
+			 "CreateDistributedSnapshot added inProgressDistributedXid = %u to snapshot",
+			 gxid);
+	}
+
+	distribSnapshotId = pg_atomic_add_fetch_u32((pg_atomic_uint32 *)shmNextSnapshotId, 1);
+
+	/*
+	 * Above globalXminDistributedSnapshots was calculated based on lowest
+	 * dxid in all snapshots but update it to also include actual process
+	 * dxids.
+	 */
+	if (xmin < globalXminDistributedSnapshots)
+		globalXminDistributedSnapshots = xmin;
+
+	/*
+	 * Sort the entry {distribXid} to support the QEs doing culls on their
+	 * DisribToLocalXact sorted lists.
+	 */
+	qsort(
+		  ds->inProgressXidArray,
+		  count,
+		  sizeof(DistributedTransactionId),
+		  DistributedSnapshotMappedEntry_Compare);
+
+	/*
+	 * Copy the information we just captured under lock and then sorted into
+	 * the distributed snapshot.
+	 */
+	ds->distribTransactionTimeStamp = *shmDistribTimeStamp;
+	ds->xminAllDistributedSnapshots = globalXminDistributedSnapshots;
+	ds->distribSnapshotId = distribSnapshotId;
+	ds->xmin = xmin;
+	ds->xmax = xmax;
+	ds->count = count;
+
+	if (xmin < MyProc->gxact.xminDistributedSnapshot)
+		MyProc->gxact.xminDistributedSnapshot = xmin;
+
+	elog((Debug_print_full_dtm ? LOG : DEBUG5),
+		 "CreateDistributedSnapshot distributed snapshot has xmin = %u, count = %u, xmax = %u.",
+		 xmin, count, xmax);
+	elog((Debug_print_snapshot_dtm ? LOG : DEBUG5),
+		 "[Distributed Snapshot #%u] *Create* (gxid = %u, '%s')",
+		 distribSnapshotId,
+		 MyProc->gxact.gxid,
+		 DtxContextToString(DistributedTransactionContext));
+
+	/*
+	 * At snapshot creation time, local xid cache is empty. Gets populated as
+	 * reverse mapping takes place during visibility checks using this
+	 * snapshot.
+	 */
+	distribSnapshotWithLocalMapping->currentLocalXidsCount = 0;
+	distribSnapshotWithLocalMapping->minCachedLocalXid = InvalidTransactionId;
+	distribSnapshotWithLocalMapping->maxCachedLocalXid = InvalidTransactionId;
+
+	Assert(distribSnapshotWithLocalMapping->maxLocalXidsCount != 0);
+	Assert(distribSnapshotWithLocalMapping->inProgressMappedLocalXids != NULL);
+
+	memset(distribSnapshotWithLocalMapping->inProgressMappedLocalXids,
+		   InvalidTransactionId,
+		   sizeof(TransactionId) * distribSnapshotWithLocalMapping->maxLocalXidsCount);
+
+	return true;
 }
 
 /*----------

src/backend/access/transam/xact.c
@@ -1161,9 +1161,28 @@ RecordTransactionCommit(void)
 
 			rdata[1].next = NULL;
 
+			/*
+			 * Checkpoint process should hold off obtaining the REDO
+			 * pointer while a backend is writing distributed commit
+			 * xlog record and changing state of the distributed
+			 * trasaction.  Otherwise, it is possible that a commit
+			 * record is written by a transaction and the checkpointer
+			 * determines REDO pointer to be after this commit record.
+			 * But the transaction is yet to chance its state to
+			 * INSERTED_DISRIBUTED_COMMITTED and the checkpoint process
+			 * fails to record this transaction in the checkpoint.
+			 * Crash recovery will never see the commit record for this
+			 * transaction and the second phase of 2PC will never
+			 * happen.  The inCommit flag avoids the situation by
+			 * blocking checkpointer untill a backend has finished
+			 * updating the state.
+			 */
+			save_inCommit = MyProc->inCommit;
+			MyProc->inCommit = true;
 			insertingDistributedCommitted();
 			recptr = XLogInsert(RM_XACT_ID, XLOG_XACT_DISTRIBUTED_COMMIT, rdata);
 			insertedDistributedCommitted();
+			MyProc->inCommit = save_inCommit;
 		}
 
 		/*
@@ -1253,6 +1272,11 @@ RecordTransactionCommit(void)
 		{
 			insertingDistributedCommitted();
 
+			/*
+			 * MyProc->inCommit flag is already set, checkpointer will
+			 * be able to see this transaction only after distributed
+			 * commit xlog is written and the state is changed.
+			 */
 			recptr = XLogInsert(RM_XACT_ID, XLOG_XACT_DISTRIBUTED_COMMIT, rdata);
 
 			insertedDistributedCommitted();
@@ -2027,7 +2051,8 @@ StartTransaction(void)
 			 * distributed transaction to a local transaction id for the
 			 * master database.
 			 */
-			createDtx(&currentDistribXid);
+			setCurrentGxact();
+			currentDistribXid = MyProc->gxact.gxid;
 
 			if (SharedLocalSnapshotSlot != NULL)
 			{

src/backend/access/transam/xlog.c
@@ -8546,6 +8546,32 @@ CreateCheckPoint(int flags)
 	pfree(vxids);
 
 	/*
+	 * When the crash happens, we need to handle the transactions that have
+	 * already inserted 'commit' record and haven't inserted 'forget' record.
+	 *
+	 * If the 'commit' record is logically before the checkpoint REDO pointer,
+	 * we save the transactions in checkpoint record, and these transactions
+	 * will be load into shared memory and mark as 'crash committed' during
+	 * redo checkpoint.
+	 * If the 'commit' record is logically after the checkpoint REDO pointer,
+	 * the transactions will be added to shared memory and mark as 'crash
+	 * committed' during redo xact.
+	 * All these transactions will be stored in the shutdown checkpoint record
+	 * after recovery, and they will be finally recovered in recoverTM().
+	 *
+	 * So if it's a shutdown checkpoint here, we should include all 'crash
+	 * committed' transactions, and if it's a normal checkpoint should include
+	 * all transactions whose 'commit' record is logically before checkpoint
+	 * REDO pointer.
+	 *
+	 * We don't hold the WALInsertLock, so there's a time window that allows
+	 * transactions insert 'commit' record and/or 'forget' record after
+	 * checkpoint REDO pointer. That's fine, resend 'commit prepared' to already
+	 * finished transactions is handled.
+	 */
+	getDtxCheckPointInfo(&dtxCheckPointInfo, &dtxCheckPointInfoSize);
+
+	/*
 	 * Get the other info we need for the checkpoint record.
 	 */
 	LWLockAcquire(XidGenLock, LW_SHARED);
@@ -8592,12 +8618,10 @@ CreateCheckPoint(int flags)
 	 *
 	 * Here is the locking order and scope:
 	 *
-	 * getDtxCheckPointInfoAndLock (i.e. shmControlLock)
 	 * 	READ_PERSISTENT_STATE_ORDERED_LOCK (i.e. PersistentObjLock)
 	 * 		mmxlog_append_checkpoint_data
 	 * 		XLogInsert
 	 * 	READ_PERSISTENT_STATE_ORDERED_UNLOCK
-	 * freeDtxCheckPointInfoAndUnlock
 	 * XLogFlush
 	 *
 	 * We get the PersistentObjLock to prevent Persistent Object writers as
@@ -8616,8 +8640,6 @@ CreateCheckPoint(int flags)
 	 * then recreated based on the checkpoint record. That will ends-up left behind the directories already
 	 * dropped on the master, break the consistency between the master and the standby.
 	 */
-
-	getDtxCheckPointInfoAndLock(&dtxCheckPointInfo, &dtxCheckPointInfoSize);
 
 	rdata[0].data = (char *) (&checkPoint);
 	rdata[0].len = sizeof(checkPoint);
@@ -8656,8 +8678,6 @@ CreateCheckPoint(int flags)
 	recptr = XLogInsert(RM_XLOG_ID,
 			            shutdown ? XLOG_CHECKPOINT_SHUTDOWN : XLOG_CHECKPOINT_ONLINE,
 			            rdata);
-
-	freeDtxCheckPointInfoAndUnlock(dtxCheckPointInfo, dtxCheckPointInfoSize, &recptr);
 
 	XLogFlush(recptr);
 

src/backend/cdb/cdbtmutils.c
@@ -76,8 +76,6 @@ DtxStateToString(DtxState state)
 			return "Retry Commit Prepared";
 		case DTX_STATE_RETRY_ABORT_PREPARED:
 			return "Retry Abort Prepared";
-		case DTX_STATE_CRASH_COMMITTED:
-			return "Crash Committed";
 		default:
 			return "Unknown";
 	}

src/backend/cdb/test/Makefile
@@ -2,15 +2,13 @@ subdir=src/backend/cdb
 top_builddir=../../../..
 include $(top_builddir)/src/Makefile.global
 
-TARGETS=cdbtm \
-	cdbbufferedread \
+TARGETS=cdbbufferedread \
 	cdbsrlz \
 	cdbdistributedsnapshot
 
 TARGETS += cdbappendonlystorage cdbappendonlyxlog
 
 include $(top_builddir)/src/backend/mock.mk
-cdbtm.t: $(MOCK_DIR)/backend/storage/lmgr/lwlock_mock.o
 
 cdbdistributedsnapshot.t: $(MOCK_DIR)/backend/access/transam/distributedlog_mock.o
 

src/backend/storage/ipc/test/Makefile
@@ -1,0 +1,10 @@
+subdir=src/backend/storage/ipc
+top_builddir=../../../../..
+include $(top_builddir)/src/Makefile.global
+
+TARGETS=procarray
+
+include $(top_builddir)/src/backend/mock.mk
+procarray.t: $(MOCK_DIR)/backend/storage/lmgr/lwlock_mock.o \
+			 $(MOCK_DIR)/backend/cdb/cdbtm_mock.o
+

src/backend/storage/ipc/test/procarray_test.c
@@ -4,37 +4,37 @@
 #include "cmockery.h"
 #include "postgres.h"
 
-#include "../cdbtm.c"
+#include "../procarray.c"
 
 #define SIZE_OF_IN_PROGRESS_ARRAY (10 * sizeof(DistributedTransactionId))
+#define MAX_PROCS 100
 
-void setup(TmControlBlock *controlBlock, TMGXACT *gxact_array)
+void setup(TmControlBlock *controlBlock)
 {
-	shmNumGxacts = &controlBlock->num_active_xacts;
-	shmGxactArray = gxact_array;
-	shmGIDSeq = &controlBlock->seqno;
+	PGPROC *tmp_proc;
+
 	shmNextSnapshotId = &controlBlock->NextSnapshotId;
 	shmDistribTimeStamp = &controlBlock->distribTimeStamp;
+	shmNumCommittedGxacts = &controlBlock->num_committed_xacts;
 
 	/* Some imaginary LWLockId number */
-	controlBlock->ControlLock = 1000;
-	shmControlLock = controlBlock->ControlLock;
 	*shmDistribTimeStamp = time(NULL);
-	*shmGIDSeq = 25;
-	*shmNumGxacts = 0;
+	*shmNumCommittedGxacts = 0;
 
-	TMGXACT *tmp_gxact = (TMGXACT*)malloc(5 * sizeof(TMGXACT));
-	shmGxactArray[0] = tmp_gxact++;
-	shmGxactArray[1] = tmp_gxact++;
-	shmGxactArray[2] = tmp_gxact++;
-	shmGxactArray[3] = tmp_gxact++;
-	shmGxactArray[4] = tmp_gxact++;
+	procArray = malloc(sizeof(ProcArrayStruct) + sizeof(PGPROC *) * (MAX_PROCS - 1));
+	tmp_proc = (PGPROC *)malloc(5 * sizeof(PGPROC));
+	procArray->procs[0] = tmp_proc++;
+	procArray->procs[1] = tmp_proc++;
+	procArray->procs[2] = tmp_proc++;
+	procArray->procs[3] = tmp_proc++;
+	procArray->procs[4] = tmp_proc++;
+
+	procArray->maxProcs = MAX_PROCS;
 }
 
 void
 test__CreateDistributedSnapshot(void **state)
 {
-	TMGXACT gxact_array[5];
 	TmControlBlock controlBlock;
 	DistributedSnapshotWithLocalMapping distribSnapshotWithLocalMapping;
 	DistributedSnapshot *ds = &distribSnapshotWithLocalMapping.ds;
@@ -47,20 +47,22 @@ test__CreateDistributedSnapshot(void **state)
 		(TransactionId*) malloc(1 * sizeof(TransactionId));
 	distribSnapshotWithLocalMapping.maxLocalXidsCount = 1;
 
-	setup(&controlBlock, &gxact_array);
+	setup(&controlBlock);
 
-	expect_value_count(LWLockAcquire, lockid, shmControlLock, -1);
-	expect_value_count(LWLockAcquire, mode, LW_EXCLUSIVE, -1);
-	will_be_called_count(LWLockAcquire, -1);
-	expect_value_count(LWLockRelease, lockid, shmControlLock, -1);
-	will_be_called_count(LWLockRelease, -1);
+#ifdef USE_ASSERT_CHECKING
+	expect_value_count(LWLockHeldByMe, lockid, ProcArrayLock, -1);
+	will_return_count(LWLockHeldByMe, true, -1);
+#endif
+
+	will_return_count(getMaxDistributedXid, 25, -1);
 
 	/* This is going to act as our gxact */
-	shmGxactArray[0]->gxid = 20;
-	shmGxactArray[0]->state = DTX_STATE_ACTIVE_DISTRIBUTED;
-	shmGxactArray[0]->xminDistributedSnapshot = 20;
-	(*shmNumGxacts)++;
-	currentGxact = shmGxactArray[0];
+	procArray->procs[0]->gxact.gxid = 20;
+	procArray->procs[0]->gxact.state = DTX_STATE_ACTIVE_DISTRIBUTED;
+	procArray->procs[0]->gxact.xminDistributedSnapshot = 20;
+	procArray->numProcs = 1;
+
+	MyProc = procArray->procs[0];
 
 	/********************************************************
 	 * Basic case, no other in progress transaction in system
@@ -73,7 +75,7 @@ test__CreateDistributedSnapshot(void **state)
 	assert_true(ds->xmin == 20);
 	assert_true(ds->xmax == 25);
 	assert_true(ds->count == 0);
-	assert_true(currentGxact->xminDistributedSnapshot == 20);
+	assert_true(MyProc->gxact.xminDistributedSnapshot == 20);
 
 	/*************************************************************************
 	 * Case where there exist in-progress having taken snpashot with lower
@@ -81,15 +83,14 @@ test__CreateDistributedSnapshot(void **state)
 	 * differ from xminAllDistributedSnapshots. Also, validates xmin and xmax
 	 * get adjusted correctly based on in-progress.
 	 */
-	shmGxactArray[1]->gxid = 10;
-	shmGxactArray[1]->state = DTX_STATE_ACTIVE_DISTRIBUTED;
-	shmGxactArray[1]->xminDistributedSnapshot = 5;
-	(*shmNumGxacts)++;
+	procArray->procs[1]->gxact.gxid = 10;
+	procArray->procs[1]->gxact.state = DTX_STATE_ACTIVE_DISTRIBUTED;
+	procArray->procs[1]->gxact.xminDistributedSnapshot = 5;
 
-	shmGxactArray[2]->gxid = 30;
-	shmGxactArray[2]->state = DTX_STATE_ACTIVE_DISTRIBUTED;
-	shmGxactArray[2]->xminDistributedSnapshot = 20;
-	(*shmNumGxacts)++;
+	procArray->procs[2]->gxact.gxid = 30;
+	procArray->procs[2]->gxact.state = DTX_STATE_ACTIVE_DISTRIBUTED;
+	procArray->procs[2]->gxact.xminDistributedSnapshot = 20;
+	procArray->numProcs = 3;
 
 	memset(ds->inProgressXidArray, 0, SIZE_OF_IN_PROGRESS_ARRAY);
 	CreateDistributedSnapshot(&distribSnapshotWithLocalMapping);
@@ -99,21 +100,20 @@ test__CreateDistributedSnapshot(void **state)
 	assert_true(ds->xmin == 10);
 	assert_true(ds->xmax == 30);
 	assert_true(ds->count == 2);
-	assert_true(currentGxact->xminDistributedSnapshot == 10);
+	assert_true(MyProc->gxact.xminDistributedSnapshot == 10);
 
 	/*************************************************************************
 	 * Add more elemnets, just to have validation that in-progress array is in
 	 * ascending sorted order with distributed transactions.
 	 */
-	shmGxactArray[3]->gxid = 15;
-	shmGxactArray[3]->state = DTX_STATE_ACTIVE_DISTRIBUTED;
-	shmGxactArray[3]->xminDistributedSnapshot = 12;
-	(*shmNumGxacts)++;
+	procArray->procs[3]->gxact.gxid = 15;
+	procArray->procs[3]->gxact.state = DTX_STATE_ACTIVE_DISTRIBUTED;
+	procArray->procs[3]->gxact.xminDistributedSnapshot = 12;
 
-	shmGxactArray[4]->gxid = 7;
-	shmGxactArray[4]->state = DTX_STATE_ACTIVE_DISTRIBUTED;
-	shmGxactArray[4]->xminDistributedSnapshot = 7;
-	(*shmNumGxacts)++;
+	procArray->procs[4]->gxact.gxid = 7;
+	procArray->procs[4]->gxact.state = DTX_STATE_ACTIVE_DISTRIBUTED;
+	procArray->procs[4]->gxact.xminDistributedSnapshot = 7;
+	procArray->numProcs = 5;
 
 	memset(ds->inProgressXidArray, 0, SIZE_OF_IN_PROGRESS_ARRAY);
 	CreateDistributedSnapshot(&distribSnapshotWithLocalMapping);
@@ -123,7 +123,7 @@ test__CreateDistributedSnapshot(void **state)
 	assert_true(ds->xmin == 7);
 	assert_true(ds->xmax == 30);
 	assert_true(ds->count == 4);
-	assert_true(currentGxact->xminDistributedSnapshot == 7);
+	assert_true(MyProc->gxact.xminDistributedSnapshot == 7);
 	assert_true(ds->inProgressXidArray[0] == 7);
 	assert_true(ds->inProgressXidArray[1] == 10);
 	assert_true(ds->inProgressXidArray[2] == 15);
@@ -131,7 +131,8 @@ test__CreateDistributedSnapshot(void **state)
 
 	free(distribSnapshotWithLocalMapping.inProgressMappedLocalXids);
 	free(ds->inProgressXidArray);
-	free(shmGxactArray[0]);
+	free(procArray->procs[0]);
+	free(procArray);
 }
 
 int

commit 1d70a4505bedb288e105a364e8e5fde00157163d

Fix distributed snapshots to include just-started transactions.
All in-progress transactions, even those in DTX_STATE_ACTIVE_NOT_DISTRIBUTED
state, must be included in a distributed snapshot. All transactions begin
in DTX_STATE_ACTIVE_NOT_DISTRIBUTED state, and can become distributed later
on.

This bug was introduced in commit ff97c70b82b, which added the ill-advised
optimization to skip DTX_STATE_ACTIVE_NOT_DISTRIBUTED transactions.

This showed up occasionally in the regression tests as a failure in the
'oidjoins' test, as a failure like this:

@@ -230,9 +230,17 @@
 SELECT	ctid, attrelid
 FROM	pg_catalog.pg_attribute fk
 WHERE    attrelid != 0 AND
     NOT EXISTS(SELECT 1 FROM pg_catalog.pg_class pk WHERE pk.oid = fk.attrelid);
- ctid | attrelid
-------+----------
-(0 rows)
+  ctid   | attrelid
+---------+----------
+ (20,10) |    17107
+ (20,11) |    17107
+ (20,12) |    17107
+ (20,13) |    17107
+ (20,14) |    17107
+ (20,15) |    17107
+ (20,8)  |    17107
+ (20,9)  |    17107
+(8 rows)

The plan for that is a hash anti-join, with 'pg_class' in the inner side,
and 'pg_attribute' on the outer side. If a table was created concurrently
with that query (with oid 17107 in the above case), you could get the
failure. What happens is that the concurrent CREATE TABLE transaction was
assigned a distributed XID that was incorrectly not included in the
distributed snapshot that the query took. Hence, the transaction became
visible to the query immediately, as soon as it committed. If the CREATE
TABLE transaction committed between the full scan of pg_class, and the
scan on pg_attribute, the query would not see the just-inserted pg_class
row, but would see the pg_attribute rows.

src/backend/storage/ipc/procarray.c
@@ -1253,11 +1253,13 @@ CreateDistributedSnapshot(DistributedSnapshotWithLocalMapping *distribSnapshotWi
 		if (gxid == InvalidDistributedTransactionId)
 			continue;
 
-		if (gxact_candidate->state == DTX_STATE_ACTIVE_NOT_DISTRIBUTED)
-			continue;
+		/*
+		 * NB: We must include transactions in DTX_STATE_ACTIVE_NOT_DISTRIBUTED
+		 * state. All transactions start in that state, even if they become
+		 * distribute later on.
+		 */
 
-		Assert(gxact_candidate->state != DTX_STATE_ACTIVE_NOT_DISTRIBUTED &&
-			   gxact_candidate->state != DTX_STATE_NONE);
+		Assert(gxact_candidate->state != DTX_STATE_NONE);
 
 		/* Update globalXminDistributedSnapshots to be the smallest valid dxid */
 		dxid = gxact_candidate->xminDistributedSnapshot;