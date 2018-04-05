dbc52807b75434f2288aed4a71ef3c434c51f44e
Kris Macoskey <kmacoskey@pivotal.io>
2018-04-05 17:53:41 +0000

    ci: reorganize job groups and drop gate_compile_end
    With dropping the gate_compile_end the groups needed sensible
    reorganization based on the necessary platform builds for each
    particular group.
    
    Additionally we modified each group slightly to be more explicit on
    every job associated with the group, not just the tests.
    
    A new CL group splits out Clients and Loaders because they have a
    logical separation that is similar to the other groups.
    
    Co-authored-by: Kris Macoskey <kmacoskey@pivotal.io>
    Co-authored-by: Jason Vigil <jvigil@pivotal.io>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -47,9 +47,9 @@ TEMPLATE_ENVIRONMENT = Environment(
 
 # Variables that govern pipeline validation
 RELEASE_VALIDATOR_JOB = ['Release_Candidate']
-JOBS_THAT_ARE_GATES = ['gate_compile_start', 'gate_compile_end', 'gate_icw_start',
-                    'gate_icw_end', 'gate_cs_start', 'gate_mpp_start', 'gate_mm_start', 
-                    'gate_dpm_start', 'gate_ud_start']
+JOBS_THAT_ARE_GATES = ['gate_compile_start', 'gate_icw_start', 'gate_icw_end', 
+                       'gate_cs_start', 'gate_mpp_start', 'gate_mm_start', 
+                       'gate_dpm_start', 'gate_ud_start']
 
 JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE = ['compile_gpdb_binary_swap_centos6', 'icw_gporca_centos6_gpos_memory'] + RELEASE_VALIDATOR_JOB + JOBS_THAT_ARE_GATES
 
