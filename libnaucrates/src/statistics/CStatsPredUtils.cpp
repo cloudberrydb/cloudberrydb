bhuvnesh2703/gporca

Add cardinality estimation based on NDV for preds of form f(a) = b
Previously, predicates of the form f(a) = b were considered unsupported
and the cardinality for a join having such a predicate was
estimated using a constant scale factor. This resulted in gross
overestimation, and, in some cases, choosing an inferior plan.

With this commit, predicates of this form on an equijoin with exactly
one scalar ident on either side will be considered supported. This
commit introduces a new comparison type which will be used for
predicates like this and for which we estimate cardinality according to
the following formula: min(NDV(a), NDV(b)).

libnaucrates/src/statistics/CStatsPredUtils.cpp
@@ -371,6 +371,24 @@ CStatsPredUtils::FCmpColsIgnoreCast
 
 	if (NULL == *ppcrLeft || NULL == *ppcrRight)
 	{
+		// if the scalar cmp is of equality type, we may not have been able to extract
+		// the column referenes of scalar ident if they had any other expression than cast
+		// on top of them.
+		// in such cases, check if there is still a possibility to extract scalar ident,
+		// if there is more than one column reference on either side, this is unsupported
+		// If supported, mark the comparision as NDV-based
+		if (*pescmpt == CStatsPred::EstatscmptEq)
+		{
+			(*ppcrLeft) = CCastUtils::PcrExtractFromScExpression(pexprLeft);
+			(*ppcrRight) = CCastUtils::PcrExtractFromScExpression(pexprRight);
+			
+			if (NULL == *ppcrLeft || NULL == *ppcrRight)
+			{
+				return false;
+			}
+			(*pescmpt) = CStatsPred::EstatscmptEqNDV;
+			return true;
+		}
 		// failed to extract a scalar ident
 		return false;
 	}

cloudberrydb/cloudberrydb

libnaucrates/src/statistics/CStatsPredUtils.cpp
@@ -371,6 +371,24 @@ CStatsPredUtils::FCmpColsIgnoreCast
 
 	if (NULL == *ppcrLeft || NULL == *ppcrRight)
 	{
+		// if the scalar cmp is of equality type, we may not have been able to extract
+		// the column referenes of scalar ident if they had any other expression than cast
+		// on top of them.
+		// in such cases, check if there is still a possibility to extract scalar ident,
+		// if there is more than one column reference on either side, this is unsupported
+		// If supported, mark the comparison as NDV-based
+		if (*pescmpt == CStatsPred::EstatscmptEq)
+		{
+			(*ppcrLeft) = CUtils::PcrExtractFromScExpression(pexprLeft);
+			(*ppcrRight) = CUtils::PcrExtractFromScExpression(pexprRight);
+			
+			if (NULL == *ppcrLeft || NULL == *ppcrRight)
+			{
+				return false;
+			}
+			(*pescmpt) = CStatsPred::EstatscmptEqNDV;
+			return true;
+		}
 		// failed to extract a scalar ident
 		return false;
 	}