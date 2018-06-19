dd77c59c0024e074b32c31494a1753326a31903d
Dhanashree Kashid <dkashid@pivotal.io>
2018-06-19 15:28:10 -0700

    Add tests for subqueries nested inside a scalar expression
    Add tests to ensure sane behavior when a subquery appears nested inside
    a scalar expression. The intent is to check for correct results.
    
    Bump ORCA version to 2.63.0
    
    Signed-off-by: Shreedhar Hardikar <shardikar@pivotal.io>

--- src/test/regress/sql/qp_subquery.sql
+++ src/test/regress/sql/qp_subquery.sql
@@ -608,6 +608,43 @@ update TblUp1 set a=100 where a not in (select a from TblUp3);
 select * from TblUp1;
 update TblUp2 set a=100 where a not in (select a from TblUp4);
 select * from TblUp2;
+
+--
+-- Check for correct results for subqueries nested inside a scalar expression
+--
+-- start_ignore
+create table subselect_tab1 (a int, b text, c int);
+create table subselect_tab2 (a int, b int, c int);
+create table subselect_tab3 (a int, b text, c int);
+
+insert into subselect_tab1 VALUES (100, 'false', 1);
+insert into subselect_tab1 VALUES (200, 'true', 2);
+insert into subselect_tab2 VALUES (2,2,2);
+insert into subselect_tab3 VALUES (200, 'falseg', 1);
+-- end_ignore
+
+-- scalar subquery in a null test expression
+select * from subselect_tab1 where (select b from subselect_tab2) is null;
+
+-- ANY subquery nested in a scalar comparison expression
+select * from subselect_tab1 where b::bool = ( c = any(select c from subselect_tab2));
+
+-- ALL subquery deeply nested in a scalar expression
+select * from subselect_tab3 where b = ( a < all(select c from subselect_tab2) || 'g');
+
+-- EXISTS subquery nested in a boolean expression
+select * from subselect_tab1 where b::bool = (exists(select c from subselect_tab2) and not exists (select c from subselect_tab3));
+
+-- ALL and EXISTS nested in a CASE-WHEN-THEN expression
+select * from subselect_tab1 where case when b is not null then (subselect_tab1.c < all(select c from subselect_tab2 where exists (select * from subselect_tab3))) else false end;
+
+-- EXISTS subquery nested in a scalar comparison expression
+select * from subselect_tab1 where b::bool = exists(select c from subselect_tab2);
+
+-- a few more complex combinations..
+SELECT * FROM subselect_tab3 WHERE (EXISTS(SELECT c FROM subselect_tab2) AND NOT EXISTS (SELECT c from subselect_tab3)) IN (SELECT b::BOOL from subselect_tab1);
+SELECT * FROM subselect_tab3 WHERE (NOT EXISTS(SELECT c FROM subselect_tab2)) IN (SELECT b::boolean  from subselect_tab1);
+
 -- start_ignore
 drop schema qp_subquery cascade;
 -- end_ignore
