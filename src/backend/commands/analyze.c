ef39e0d0a75cf9c2bc3e9f7871e48cd99e53b1ad
Omer Arap <oarap@pivotal.io>
2018-06-28 15:13:00 -0700

    Fix incremental analyze for non-matching attnums
    To merge stats in incremental analyze for root partition, we use leaf
    tables' statistics. In commit b28d02971fd85977f7d0812cc73db935288febaf,
    we fixed an issue where child attnum do not match with a root table's
    attnum for the same column. After we fixed that issue with a test, that
    test also exposed the bug in analyze code.
    
    This commit fixes the issue in analyze using the similar fix in
    b28d02971fd85977f7d0812cc73db935288febaf.

--- src/backend/commands/analyze.c
+++ src/backend/commands/analyze.c
@@ -3752,7 +3752,10 @@ merge_leaf_stats(VacAttrStatsP stats,
 		nullCount = nullCount +
 					get_attnullfrac(relid, stats->attr->attnum) * relTuples[i];
 
-		heaptupleStats[i] = get_att_stats(relid, stats->attr->attnum);
+		const char *attname = get_relid_attribute_name(stats->attr->attrelid, stats->attr->attnum);
+		AttrNumber child_attno = get_attnum(relid, attname);
+
+		heaptupleStats[i] = get_att_stats(relid, child_attno);
 
 		// if there is no colstats, we can skip this partition's stats
 		if (!HeapTupleIsValid(heaptupleStats[i]))
