cloudberrydb/cloudberrydb

commit 2ea6aec39cffca1d9028ebacaf50e7eea5664854

Optimize partition query in pg_dump (#4620)
Commit 708a042 added logic to dump unnamed exchange partitions for range
partition tables. However, the join on pg_partitions (the view, not the pg_partition
catalog) is very expensive and slowed pg_dump significantly. Now, we
don't query pg_partitions but instead include only the necessary logic in the
query itself. This will be backported to 5X_STABLE.

Authored-by: Chris Hajas <chajas@pivotal.io>

src/bin/pg_dump/pg_dump.c
@@ -11838,14 +11838,19 @@ dumpTableSchema(Archive *fout, TableInfo *tbinfo)
 			int i_parname = 0;
 			int i_partitionrank = 0;
 			resetPQExpBuffer(query);
-
-			appendPQExpBuffer(query, "SELECT DISTINCT cc.relname, ps.partitionrank, pp.parname "
+			appendPQExpBuffer(query, "SELECT c.relname, pr.parname,"
+					"CASE WHEN p.parkind <> '%c'::char OR pr.parisdefault THEN NULL::bigint "
+					"ELSE pg_catalog.rank() OVER ( "
+					"PARTITION BY p.oid, cc.relname, p.parlevel, cl3.relname "
+					"ORDER BY pr.parisdefault, pr.parruleord) "
+					"END AS partitionrank "
 					"FROM pg_partition p "
-					"JOIN pg_class c on (p.parrelid = c.oid) "
-					"JOIN pg_partitions ps on (c.relname = ps.tablename) "
-					"JOIN pg_class cc on (ps.partitiontablename = cc.relname) "
-					"JOIN pg_partition_rule pp on (cc.oid = pp.parchildrelid) "
-					"WHERE p.parrelid = %u AND cc.relstorage = '%c';", tbinfo->dobj.catId.oid, RELSTORAGE_EXTERNAL);
+					"JOIN pg_class cc ON (p.parrelid = cc.oid), pg_class c "
+					"JOIN pg_partition_rule pr ON (c.oid = pr.parchildrelid) "
+					"LEFT JOIN pg_partition_rule pr2 ON pr.parparentrule = pr2.oid "
+					"LEFT JOIN pg_class cl3 ON pr2.parchildrelid = cl3.oid "
+					"WHERE pr.paroid = p.oid "
+					"AND p.parrelid = %u AND c.relstorage = '%c';", RELKIND_RELATION, tbinfo->dobj.catId.oid, RELSTORAGE_EXTERNAL);
 
 			res = PQexec(g_conn, query->data);
 			check_sql_result(res, g_conn, query->data, PGRES_TUPLES_OK);

commit c4a7b25143d727d01a7545e6c4a29c5e6edbd90c

pg_dump: work around hang during external partition exchange
The new optimized query introduced in commit 2ea6aec3 isn't as optimized
as we'd like if enable_nestloop is on -- released versions of GPDB craft
a plan that runs in polynomial time, and the dump grinds to a halt.

Disable enable_nestloop temporarily as a workaround. This code may need
to be somewhat rewritten to deal with external subpartition leaves
anyway, in which case we may be able to get rid of some of the
problematic JOINs.

Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

src/bin/pg_dump/pg_dump.c
@@ -13109,6 +13109,18 @@ dumpTableSchema(Archive *fout, TableInfo *tbinfo)
 			int i_relname = 0;
 			int i_parname = 0;
 			int i_partitionrank = 0;
+
+			/*
+			 * The multiple JOINs below trigger an apparent planner bug which
+			 * may effectively hang the backend. This bug is present in both
+			 * released versions of GPDB and the current development tip at time
+			 * of writing. Disable nestloops temporarily as a workaround.
+			 *
+			 * TODO: when this bug is fixed, version-gate this code so that we
+			 * don't run it on well-behaved backends.
+			 */
+			do_sql_command(g_conn, "SET enable_nestloop TO off");
+
 			resetPQExpBuffer(query);
 			appendPQExpBuffer(query, "SELECT c.relname, pr.parname,"
 					"CASE WHEN p.parkind <> '%c'::char OR pr.parisdefault THEN NULL::bigint "
@@ -13132,6 +13144,7 @@ dumpTableSchema(Archive *fout, TableInfo *tbinfo)
 			i_parname = PQfnumber(res, "parname");
 			i_partitionrank = PQfnumber(res, "partitionrank");
 
+			/* FIXME: does this code handle external SUBPARTITIONs correctly? */
 			for (i = 0; i < ntups; i++)
 			{
 				char tmpExtTable[500] = {0};
@@ -13163,6 +13176,9 @@ dumpTableSchema(Archive *fout, TableInfo *tbinfo)
 			}
 
 			PQclear(res);
+
+			/* TODO: version-gate this when the planner bug is fixed; see above. */
+			do_sql_command(g_conn, "SET enable_nestloop TO on");
 		}
 
 		/*