1309cc2709748482aaf7b59cbe64439a731a316b
Jacob Champion <pchampion@pivotal.io>
2018-08-22 16:24:33 -0700

    pg_upgrade: drop all AO tables before upgrade tests
    Instead of dropping the entire regression database, we've found we can
    scope the drop to "all AO tables" and "all partition indexes" and get
    most of the way through the smoke test. This gives us back a little bit
    of a safety belt in the master CI.
    
    Co-authored-by: Jim Doty <jdoty@pivotal.io>

--- contrib/pg_upgrade/test_gpdb_pre.sql
+++ contrib/pg_upgrade/test_gpdb_pre.sql
@@ -3,13 +3,68 @@
 -- exist at the time of running upgrades. If objects are to be manipulated
 -- in other databases, make sure to change to the correct database first.
 
-DROP DATABASE IF EXISTS regression;
-DROP DATABASE IF EXISTS dsp3;
+-- drop all AO tables
+\ir pre_drop_ao.sql
+
 DROP DATABASE IF EXISTS isolation2test;
 
-DROP DATABASE IF EXISTS pgutest;
-CREATE DATABASE pgutest;
-\c pgutest;
+\c regression;
 
-CREATE TABLE t (a integer, b numeric);
-CREATE TABLE tt (a integer, b numeric) WITH (appendonly = true);
+-- drop all AO tables
+\ir pre_drop_ao.sql
+
+-- Greenplum pg_upgrade doesn't support indexes on partitions since they can't
+-- be reliably dump/restored in all situations. Drop all such indexes before
+-- attempting the upgrade.
+CREATE OR REPLACE FUNCTION drop_indexes() RETURNS void AS $$
+DECLARE
+       part_indexes RECORD;
+BEGIN
+       FOR part_indexes IN
+       WITH partitions AS (
+           SELECT DISTINCT n.nspname,
+                  c.relname
+           FROM pg_catalog.pg_partition p
+                JOIN pg_catalog.pg_class c ON (p.parrelid = c.oid)
+                JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
+           UNION
+           SELECT n.nspname,
+                  partitiontablename AS relname
+           FROM pg_catalog.pg_partitions p
+                JOIN pg_catalog.pg_class c ON (p.partitiontablename = c.relname)
+                JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
+       )
+       SELECT nspname,
+              relname,
+              indexname
+       FROM partitions
+            JOIN pg_catalog.pg_indexes ON (relname = tablename
+                                                                               AND nspname = schemaname)
+       LOOP
+               EXECUTE 'DROP INDEX IF EXISTS ' || quote_ident(part_indexes.nspname) || '.' || quote_ident(part_indexes.indexname);
+       END LOOP;
+       RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT drop_indexes();
+DROP FUNCTION drop_indexes();
+
+\c dsp1;
+
+-- drop all AO tables
+\ir pre_drop_ao.sql
+
+\c dsp2;
+
+-- drop all AO tables
+\ir pre_drop_ao.sql
+
+\c dsp3;
+
+-- drop all AO tables
+\ir pre_drop_ao.sql
+
+-- toast table and index aren't correctly migrated for this relation, for some
+-- reason
+DROP TABLE IF EXISTS public.alter_table_reorg_heap CASCADE;
