226159cf4fe68cd1ea46c8504efe594ef3adc453
Ning Yu <nyu@pivotal.io>
2018-08-24 12:19:56 +0800

    Fix flaky fault injection in test pg_terminate_backend.
    In 00da6bbf0c we added a test case in pg_terminate_backend, however as
    it uses fault injection it will not work stably in a parallel test
    greoup.
    
    Moved this test case to a separate test group.

---
+++ src/test/isolation2/sql/terminate_in_gang_creation.sql
@@ -0,0 +1,43 @@
+-- SIGSEGV issue when freeing gangs
+--
+-- When SIGTERM is handled during gang creation we used to trigger
+-- a wild pointer access like below backtrace:
+--
+-- #0  raise
+-- #1  StandardHandlerForSigillSigsegvSigbus_OnMainThread
+-- #2  <signal handler called>
+-- #3  MemoryContextFreeImpl
+-- #4  cdbconn_termSegmentDescriptor
+-- #5  DisconnectAndDestroyGang
+-- #6  freeGangsForPortal
+-- #7  AbortTransaction
+-- ...
+-- #14 ProcessInterrupts
+-- #15 createGang_async
+-- #16 createGang
+-- #17 AllocateWriterGang
+
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+
+DROP TABLE IF EXISTS foo;
+CREATE TABLE foo AS SELECT i a, i b FROM generate_series(1, 10) i;
+
+DROP TABLE IF EXISTS foo;
+CREATE TABLE foo (c1 int, c2 int) DISTRIBUTED BY (c1);
+
+10: BEGIN;
+
+SELECT gp_inject_fault('create_gang_in_progress', 'reset', 1);
+SELECT gp_inject_fault('create_gang_in_progress', 'suspend', 1);
+
+10&: SELECT * FROM foo a JOIN foo b USING (c2);
+
+SELECT gp_wait_until_triggered_fault('create_gang_in_progress', 1, 1);
+
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity
+ WHERE query = 'SELECT * FROM foo a JOIN foo b USING (c2);';
+
+SELECT gp_inject_fault('create_gang_in_progress', 'resume', 1);
+
+10<:
+10q
