d4892f817b931e33df2a5ce25aeafd68b868b78e
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-08-27 22:05:04 +0300

    Clean up max, flags, and docs of 'track_activity_query_size' setting.
    In GPDB 5, this setting was called 'pgstat_track_activity_query_size',
    but in the 8.4 merge, it was renamed to just 'track_activity_query_size',
    to match upstream. Clean it up to match upstream:
    
    * Change the maximum from INT_MAX to 100 kB. 100 kB should be enough for
      everybody.
    
    * It is in the sample configuration file now , so remove GUC_NOT_IN_SAMPLE
      flag
    
    * Fix documentation. I just replaced 'pgstat_track_activity_query_size'
      with 'track_activity_query_size', and moved the paragraphs to keep
      alphabetical order. Other than the name, the old text still seems valid.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -2480,18 +2480,13 @@ static struct config_int ConfigureNamesInt[] =
 		NULL, NULL, NULL
 	},
 
-	/* GPDB_84_MERGE_FIXME: we have merged the track_activity_query_size GUC
-	 * from 8.4 into GPDB's pgstat_track_activity_query_size. Besides the name
-	 * change, the two differ in min/max settings, GUC group, flags, and
-	 * description. Make sure we did this right. */
 	{
 		{"track_activity_query_size", PGC_POSTMASTER, RESOURCES_MEM,
 			gettext_noop("Sets the size reserved for pg_stat_activity.query, in bytes."),
-			NULL,
-			GUC_NOT_IN_SAMPLE
+			NULL
 		},
 		&pgstat_track_activity_query_size,
-		1024, 100, INT_MAX,
+		1024, 100, 102400,
 		NULL, NULL, NULL
 	},
 
