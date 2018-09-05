f5130c20b2419c55d8b26f86c052952d0b90adf7
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-09-05 22:44:42 +0300

    Fix command tag in "COPY (SELECT ...) TO <file>".
    It used to always say "COPY 0", instead of the number of rows copied. This
    source line was added in PostgreSQL 9.0 (commit 8ddc05fb01), but it was
    missed in the merge. Add a test case to check the command tags of
    different variants of COPY, including this one.

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -6698,6 +6698,7 @@ copy_dest_receive(TupleTableSlot *slot, DestReceiver *self)
 
 	/* And send the data */
 	CopyOneRowTo(cstate, InvalidOid, slot_get_values(slot), slot_get_isnull(slot));
+	myState->processed++;
 }
 
 /*
