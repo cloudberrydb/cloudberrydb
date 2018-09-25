1f61fde1bcb33266952ed5f252754a41f956dc29
Joao Pereira <joaopapereira@gmail.com>
2018-09-25 14:36:33 -0400

    Add more descriptive text to assert_true and assert_false
    Add a better message for the cmockery functions assert_true and
    assert_false. (2cba56eef0dce1feecc13cb9 on google/cmockery)
    
    Co-authored-by: Jesse Zhang <jzhang@pivotal.io>

--- src/test/unit/cmockery/cmockery.c
+++ src/test/unit/cmockery/cmockery.c
@@ -1402,7 +1402,16 @@ void _assert_true(const LargestIntegralType result,
 		const char * const expression,
 		const char * const file, const int line) {
 	if (!result) {
-		print_error(OUTPUT_PADDING "%s\n", expression);
+		print_error(OUTPUT_PADDING "expected '%s' to be true\n", expression);
+		_fail(file, line);
+	}
+}
+
+void _assert_false(const LargestIntegralType result,
+		const char * const expression,
+		const char * const file, const int line) {
+	if (!result) {
+		print_error(OUTPUT_PADDING "expected '%s' to be false\n", expression);
 		_fail(file, line);
 	}
 }
