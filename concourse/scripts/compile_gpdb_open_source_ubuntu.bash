408ed96d6edea9a1ade52e714573bce022d341b8
Larry Hamel <larhamel+github@gmail.com>
2018-10-17 13:38:05 -0700

    Ensure ccache config file exists before conan compilation (#6012)
    * compile_gpdb_ubuntu: Ensure `.ccache` dir exists
    
    [#161257132]
    
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>
    Co-authored-by: Goutam Tadi <gtadi@pivotal.io>

--- concourse/scripts/compile_gpdb_open_source_ubuntu.bash
+++ concourse/scripts/compile_gpdb_open_source_ubuntu.bash
@@ -6,35 +6,37 @@ TRANSFER_DIR_ABSOLUTE_PATH=$(pwd)/${TRANSFER_DIR}
 COMPILED_BITS_FILENAME=${COMPILED_BITS_FILENAME:="compiled_bits_ubuntu16.tar.gz"}
 
 function build_external_depends() {
+    # fix the /root/.ccache missing issue during build
+    mkdir -p /root/.ccache && touch  /root/.ccache/ccache.conf
     pushd gpdb_src/depends
-    ./configure
-    if ! make > build_external_depends.log 2>&1 ; then
-        cat build_external_depends.log
-        return 1
-    fi
-    grep -v "Installing:" build_external_depends.log
-    make
+        ./configure
+        if ! make > build_external_depends.log 2>&1 ; then
+            cat build_external_depends.log
+            return 1
+        fi
+        grep -v "Installing:" build_external_depends.log
+        make
     popd
 }
 
 function install_external_depends() {
     pushd gpdb_src/depends
-    make install
+        make install
     popd
 }
 
 function build_gpdb() {
     build_external_depends
     pushd gpdb_src
-    CWD=$(pwd)
-    LD_LIBRARY_PATH=${CWD}/depends/build/lib CC=$(which gcc) CXX=$(which g++) ./configure --enable-mapreduce --with-gssapi --with-perl --with-libxml \
-      --with-python \
-      --with-libraries=${CWD}/depends/build/lib \
-      --with-includes=${CWD}/depends/build/include \
-      --prefix=${GREENPLUM_INSTALL_DIR} \
-      ${CONFIGURE_FLAGS}
-    make -j4 -s
-    LD_LIBRARY_PATH=${CWD}/depends/build/lib make install
+        CWD=$(pwd)
+        LD_LIBRARY_PATH=${CWD}/depends/build/lib CC=$(which gcc) CXX=$(which g++) ./configure --enable-mapreduce --with-gssapi --with-perl --with-libxml \
+          --with-python \
+          --with-libraries=${CWD}/depends/build/lib \
+          --with-includes=${CWD}/depends/build/include \
+          --prefix=${GREENPLUM_INSTALL_DIR} \
+          ${CONFIGURE_FLAGS}
+        make -j4 -s
+        LD_LIBRARY_PATH=${CWD}/depends/build/lib make install
     popd
     install_external_depends
 }
