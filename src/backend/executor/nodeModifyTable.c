cloudberrydb/cloudberrydb

commit f4f4bdcc8b7327faccbda8e12721b2c6b943aed7

Table data should be reshuffled to new segments
Each table has a `numsegments` attribute in the
GP_DISTRIBUTION_POLICY table,  it indicates that the table's
data is distributed on the first N segments, In the common case,
the `numsegments` equal the total segment count of this
cluster.

When we add new segments into the cluster, `numsegments` no
longer equal the actual segment count in the cluster, we
need to reshuffle the table data to all segments in 2 steps:

	* Reshuffle the table data to all segments
	* Update `numsegments`

It is easy to update `numsegments`, so we focus on how to
reshuffle the table data, There are 3 type tables in the
Greenplum database, they are reshuffled in different ways.
For the hash distributed table, we reshuffle data based on
Update statement. Updating the hash keys of the	table
ill generate a Plan like:

	Update
		->Redistributed Motion
			->SplitUpdate
				->SeqScan

We can not use this Plan to reshuffle table data directly.
The problem is that we need to know the segment count
when Motion node computes the destination segment. When
we compute the destination segment of deleting tuple, it
need the old segment count which is equal `numsegments`;
n the other hand, we need to use the new segment count to
compute the destination segment for	inserting tuple.
So we have to add a new operator Reshuffle to compute the
destination segment, it records the O and N (O is the count
of old segments and N is the count of new segments), then
the Plan would be adjusted like:

	Update
		->Explicit Motion
			->Reshuffle
				->SplitUpdate
					->SeqScan

It can compute the destination segments directly with O and
N, at the same time we change the Motion type to Explicit,
it can send a tuple to the destination segment which we
computed in the Reshuffle node.

With changing the hash method to the `jump hash`, not all
the table data need to reshuffle, so we add an new

src/backend/executor/nodeReshuffle.c
@@ -1,0 +1,480 @@
+/*-------------------------------------------------------------------------
+ *
+ * nodeReshuffle.c
+ *	  Support for reshuffling data in different segments size.
+ *
+ * DESCRIPTION
+ *
+ * 		Each table has a `numsegments` attribute in the
+ * 		GP_DISTRIBUTION_POLICY table,  it indicates that the table's
+ * 		data is distributed on the first N segments, In common case,
+ * 		the `numsegments` equal the total segment count of this
+ * 		cluster.
+ *
+ * 		When we add new segments into the cluster, `numsegments` no
+ * 		longer equal the actual segment count in the cluster, we
+ * 		need to reshuffle the table data to all segments in 2 steps:
+ *
+ * 			* Reshuffle the table data to all segments
+ * 			* Update `numsegments`
+ *
+ * 		It is easy to update `numsegments`, so we focus on how to
+ * 		reshuffle the table data, There are 3 type tables in the
+ * 		Greenplum database, they are reshuffled in different ways.
+ *
+ * 		For hash distributed table, we reshuffle data based on
+ * 		Update statement. Updating the hash keys of the	table
+ *      will generate an Plan like:
+ *
+ * 			Update
+ * 				->Redistributed Motion
+ * 					->SplitUpdate
+ * 						->SeqScan
+ *
+ * 		We can not use this Plan to reshuffle table data directly.
+ * 		The problem is that we need to know the segment count
+ * 		when Motion node computes the destination segment. When
+ * 		we compute the destination segment of deleting tuple, it
+ * 		need the old segment count which is equal `numsegments`;
+ *      on the other hand, we need use the new segment count to
+ *      compute the destination segment for	inserting tuple.
+ *
+ * 		So we have to add an new operator Reshuffle to compute the
+ * 		destination segment, it record the O and N (O is the count
+ * 		of old segments and N is the count of new segments), then
+ * 		the Plan would be adjusted like:
+ *
+ * 			Update
+ * 				->Explicit Motion
+ * 					->Reshuffle
+ * 						->SplitUpdate
+ * 							->SeqScan
+ *
+ * 		It can compute the destination segments directly with O and
+ * 		N, at the same time we change the Motion type to Explicit,
+ * 		it can send tuple to the destination segment which we
+ * 		computed in the Reshuffle node.
+ *
+ * 		With changing the hash method to the `jump hash`, not all
+ * 		the table data need to reshuffle, so we add an new
+ * 		ReshuffleExpr to filter the tuples which are need to
+ * 		reshuffle, this expression will compute the destination
+ * 		segment ahead of schedule, if the destination segment is
+ * 		current segment, the tuple do not need to reshuffle, with
+ * 		the ReshuffleExpr the plan would adjust like that:
+ *
+ * 			Update
+ * 				->Explicit Motion
+ * 					->Reshuffle
+ * 						->SplitUpdate
+ * 							->SeqScan
+ * 								|-ReshuffleExpr
+ *
+ * 		When we want to reshuffle one table, we use the SQL `ALTER
+ * 		TABLE xxx SET WITH (RESHUFFLE)`, Actually it will generate
+ * 		an new UpdateStmt parse tree, the parse tree is similar to
+ * 		the parse tree which is generated by SQL `UPDATE xxx SET
+ * 		xxx.aaa = COALESCE(xxx.aaa...) WHERE ReshuffleExpr`. We set
+ * 		an reshuffle flag in the UpdateStmt, so it can distinguish
+ * 		the common update and the reshuffling.
+ *
+ * 		In conclusion, we reshuffle hash distributed table by
+ * 		Reshuffle node and ReshuffleExpr, the ReshuffleExpr filter
+ * 		the tuple need to reshuffle and the Reshuffle node do the
+ * 		real reshuffling work, we can use that framework to
+ * 		implement reshuffle random distributed table and replicated
+ * 		table.
+ *
+ * 		For random distributed table, it have no hash keys,  each
+ * 		old segment need reshuffle (O - N) / N data to the new
+ * 		segments, In the ReshuffleExpr, we can generate a random
+ * 		value between [0, N), if the random values is greater than
+ * 		O, it means that the tuple need to reshuffle, so SeqScan
+ * 		node can return this tuple to ReshuffleNode.  Reshuffle node
+ * 		will generate an random value between [O, N), it means which
+ * 		new segment the tuple need to insert.
+ *
+ * 		For replicated table, the table data is same in the all old
+ * 		segments, so there do not need to delete any tuples, it only
+ * 		need copy the tuple which is in the old segments to the new
+ * 		segments, so the ReshuffleExpr do not filte any tuples, In
+ * 		the Reshuffle node, we neglect the tuple which is generated
+ * 		for deleting, only return the inserting tuple to motion. Let
+ * 		me illustrate this with an example:
+ *
+ * 		If there are 3 old segments in the cluster and we add 4 new
+ * 		segments, the segment ID of old segments is (0,1,2) and the
+ * 		segment ID of new segments is (3,4,5,6), when reshuffle the
+ * 		replicated table, the seg#0 is responsible to copy data to
+ * 		seg#3 and seg#6, the seg#1 is responsible to copy data to
+ * 		seg#4, the seg#2 is responsible to copy data to seg#5.
+ *
+ *
+ * Portions Copyright (c) 2012, EMC Corp.
+ * Portions Copyright (c) 2012-Present Pivotal Software, Inc.
+ *
+ * IDENTIFICATION
+ *	  src/backend/executor/nodeReshuffle.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "executor/executor.h"
+#include "executor/nodeReshuffle.h"
+#include "utils/memutils.h"
+
+#include "cdb/cdbhash.h"
+#include "cdb/cdbvars.h"
+#include "cdb/memquota.h"
+#include "cdb/cdbutil.h"
+
+#define INIT_IDX 0
+
+/*
+ *  EvalHashSegID
+ *
+ * 	compute the Hash keys
+ */
+static int
+EvalHashSegID(Datum *values, bool *nulls, List *policyAttrs,
+			  List *targetlist, int nsegs)
+{
+	CdbHash *hnew = makeCdbHash(nsegs);
+	uint32 newSeg;
+	ListCell *lc;
+
+	Assert(policyAttrs);
+	Assert(targetlist);
+
+	cdbhashinit(hnew);
+
+	foreach(lc, policyAttrs)
+	{
+		AttrNumber attidx = lfirst_int(lc);
+
+		if (nulls[attidx - 1])
+		{
+			cdbhashnull(hnew);
+		}
+		else
+		{
+			TargetEntry *entry = list_nth(targetlist, attidx - 1);
+			cdbhash(hnew, values[attidx - 1], exprType((Node *) entry->expr));
+		}
+	}
+
+	newSeg = cdbhashreduce(hnew);
+
+	return newSeg;
+
+}
+
+/* ----------------------------------------------------------------
+ *		ExecReshuffle(node)
+ *
+ *  For hash distributed tables:
+ *  	we compute the destination segment with Hash methods and
+ *  	new segments count.
+ *
+ *  For random distributed tables:
+ *  	we get an random value [0, newSeg# - oldSeg#), then the
+ *  	destination segment is (random value + oldSeg#)
+ *
+ *  For replicated tables:
+ *  	if there are 3 old segments in the cluster and we add 4
+ *  	new segments:
+ *  	old segments: 0,1,2
+ *  	new segments: 3,4,5,6
+ *  	the seg#0 is responsible to copy data to seg#3 and seg#6
+ *  	the seg#1 is responsible to copy data to seg#4
+ *  	the seg#2 is responsible to copy data to seg#5
+ *
+ * ----------------------------------------------------------------
+ */
+TupleTableSlot *
+ExecReshuffle(ReshuffleState *node)
+{
+	PlanState *outerNode = outerPlanState(node);
+	Reshuffle *reshuffle = (Reshuffle *) node->ps.plan;
+	SplitUpdate *splitUpdate;
+
+	TupleTableSlot *slot = NULL;
+
+	Datum *values;
+	bool *nulls;
+
+	int dmlAction;
+
+	Assert(outerNode != NULL);
+	Assert(IsA(outerNode->plan, SplitUpdate));
+
+	splitUpdate = (SplitUpdate *) outerNode->plan;
+
+	Assert(splitUpdate->actionColIdx > 0);
+
+	/* New added segments have no data */
+	if (GpIdentity.segindex >= reshuffle->oldSegs)
+		return NULL;
+
+	if (reshuffle->ptype == POLICYTYPE_PARTITIONED)
+	{
+		slot = ExecProcNode(outerNode);
+
+		if (TupIsNull(slot))
+		{
+			return NULL;
+		}
+
+		slot_getallattrs(slot);
+		values = slot_get_values(slot);
+		nulls = slot_get_isnull(slot);
+
+		dmlAction = DatumGetInt32(values[splitUpdate->actionColIdx - 1]);
+
+		Assert(dmlAction == DML_INSERT || dmlAction == DML_DELETE);
+
+		if (DML_INSERT == dmlAction)
+		{
+			/* For hash distributed tables*/
+			if (NULL != reshuffle->policyAttrs)
+			{
+				values[reshuffle->tupleSegIdx - 1] =
+						Int32GetDatum(EvalHashSegID(values,
+													nulls,
+													reshuffle->policyAttrs,
+													reshuffle->plan.targetlist,
+													getgpsegmentCount()));
+			}
+			else
+			{
+				/* For random distributed tables*/
+				int newSegs = getgpsegmentCount();
+				int oldSegs = reshuffle->oldSegs;
+
+				/*
+				 * Tuple with inserting action must be sent to other segments.
+				 * Since the table is distributed randomly, we randomly pick one
+				 * of the new segments[seg_Old, Seg_New) as target with uniform
+				 * probability.
+				 */
+				values[reshuffle->tupleSegIdx - 1] =
+						Int32GetDatum(cdb_randint((newSegs - oldSegs - 1), 0) + oldSegs);
+			}
+		}
+#ifdef USE_ASSERT_CHECKING
+		else
+		{
+			if (NULL != reshuffle->policyAttrs)
+			{
+				Datum oldSegID = values[reshuffle->tupleSegIdx - 1];
+				Datum newSegID = Int32GetDatum(
+						EvalHashSegID(values,
+									  nulls,
+									  reshuffle->policyAttrs,
+									  reshuffle->plan.targetlist,
+									  reshuffle->oldSegs));
+
+				Assert(oldSegID == newSegID);
+			}
+		}
+
+		/* check */
+		if (DatumGetInt32(values[reshuffle->tupleSegIdx - 1]) >=
+			getgpsegmentCount())
+			elog(ERROR, "ERROR SEGMENT ID : %d",
+				 DatumGetInt32(values[reshuffle->tupleSegIdx - 1]));
+#endif
+	}
+	else if (reshuffle->ptype == POLICYTYPE_REPLICATED)
+	{
+		int segIdx;
+
+		/* For replicated tables*/
+		if (GpIdentity.segindex >= reshuffle->oldSegs >=
+			getgpsegmentCount())
+			return NULL;
+
+		/*
+		 * Each old semgent cound be responsible to copy data to
+		 * more than one new segments
+		 */
+		while(1)
+		{
+			if (node->newTargetIdx == INIT_IDX)
+			{
+				slot = ExecProcNode(outerNode);
+				if (TupIsNull(slot))
+					return NULL;
+
+				/* It seems OK without deep copying the slot*/
+				node->savedSlot = slot;
+			}
+			else
+			{
+				slot = node->savedSlot;
+				Assert(!TupIsNull(slot));
+			}
+
+			slot_getallattrs(slot);
+			values = slot_get_values(slot);
+
+			dmlAction = DatumGetInt32(values[splitUpdate->actionColIdx - 1]);
+			Assert(dmlAction == DML_INSERT || dmlAction == DML_DELETE);
+
+			/* Reshuffling replicate table does not need to delete tuple */
+			if (dmlAction == DML_DELETE)
+				continue;
+
+			/*
+			 * Now we are handling inserting tuples and self_segid < N.
+			 * N old segments(0, 1, 2, ... N-1)
+			 * M new segments(N, N+1, ... N+M-1)
+			 * The algorithm is that self_segid sends to the newsegments with id is
+			 * self_segid + kN(k >= 1)
+			 */
+			segIdx = list_nth_int(node->destList, node->newTargetIdx);
+			node->newTargetIdx ++;
+			if (node->newTargetIdx >= list_length(node->destList))
+			{
+				node->newTargetIdx = INIT_IDX;
+			}
+
+			values[reshuffle->tupleSegIdx - 1] = Int32GetDatum(segIdx);
+			break;
+		}
+	}
+	else
+	{
+		/* Impossible case*/
+		Assert(false);
+	}
+
+	return slot;
+}
+
+/* ----------------------------------------------------------------
+ *		ExecInitReshuffle
+ *
+ * ----------------------------------------------------------------
+ */
+ReshuffleState *
+ExecInitReshuffle(Reshuffle *node, EState *estate, int eflags) {
+	ReshuffleState *reshufflestate;
+	bool has_oids;
+	TupleDesc tupDesc;
+
+	/* check for unsupported flags */
+	Assert(!(eflags & (EXEC_FLAG_REWIND | EXEC_FLAG_MARK | EXEC_FLAG_BACKWARD)) ||
+		   outerPlan(node) != NULL);
+
+	/*
+	 * create state structure
+	 */
+	reshufflestate = makeNode(ReshuffleState);
+	reshufflestate->ps.plan = (Plan *) node;
+	reshufflestate->ps.state = estate;
+
+	/*
+	 * initialize child expressions
+	 */
+	reshufflestate->ps.qual = (List *)
+			ExecInitExpr((Expr *) node->plan.qual,
+						 (PlanState *) reshufflestate);
+
+	/*
+	 * initialize child nodes
+	 */
+	outerPlanState(reshufflestate) = ExecInitNode(outerPlan(node), estate, eflags);
+
+	/*
+	 * we don't use inner plan
+	 */
+	Assert(innerPlan(node) == NULL);
+
+	/*
+	 * tuple table initialization
+	 */
+	ExecInitResultTupleSlot(estate, &reshufflestate->ps);
+
+	/*
+	 * initialize tuple type and projection info
+	 */
+	ExecAssignResultTypeFromTL(&reshufflestate->ps);
+	ExecAssignProjectionInfo(&reshufflestate->ps, NULL);
+
+#if 0
+	if (!IsResManagerMemoryPolicyNone()
+		&& IsResultMemoryIntensive(node))
+	{
+		SPI_ReserveMemory(((Plan *)node)->operatorMemKB * 1024L);
+	}
+#endif
+
+	/* Setup the destination segment ID list */
+	if (!IS_QUERY_DISPATCHER())
+	{
+		if (GpIdentity.segindex < node->oldSegs &&
+			GpIdentity.segindex + node->oldSegs < getgpsegmentCount())
+		{
+			int segIdx = GpIdentity.segindex + node->oldSegs;
+			while (segIdx < getgpsegmentCount())
+			{
+				reshufflestate->destList =
+						lappend_int(reshufflestate->destList, segIdx);
+				segIdx = segIdx + node->oldSegs;
+			}
+		}
+	}
+
+	reshufflestate->newTargetIdx = INIT_IDX;
+	reshufflestate->savedSlot = NULL;
+
+	return reshufflestate;
+}
+
+/* ----------------------------------------------------------------
+ *		ExecEndReshuffle
+ * ----------------------------------------------------------------
+ */
+void
+ExecEndReshuffle(ReshuffleState *node)
+{
+	/*
+	 * Free the exprcontext
+	 */
+	ExecFreeExprContext(&node->ps);
+
+	/*
+	 * clean out the tuple table
+	 */
+	ExecClearTuple(node->ps.ps_ResultTupleSlot);
+
+	/*
+	 * shut down subplans
+	 */
+	ExecEndNode(outerPlanState(node));
+
+	EndPlanStateGpmonPkt(&node->ps);
+
+	return;
+}
+
+/* ----------------------------------------------------------------
+ *		ExecReScanReshuffle
+ * ----------------------------------------------------------------
+ */
+void
+ExecReScanReshuffle(ReshuffleState *node)
+{
+	/*
+	 * If chgParam of subnode is not null then plan will be re-scanned by
+	 * first ExecProcNode.
+	 */
+	if (node->ps.lefttree &&
+		node->ps.lefttree->chgParam == NULL)
+		ExecReScan(node->ps.lefttree);
+
+	return;
+}

src/backend/nodes/copyfuncs.c
@@ -1366,6 +1366,26 @@ _copySplitUpdate(const SplitUpdate *from)
 }
 
 /*
+ * _copyReshuffle
+ */
+static Reshuffle *
+_copyReshuffle(const Reshuffle *from)
+{
+	Reshuffle *newnode = makeNode(Reshuffle);
+
+	/*
+	 * copy node superclass fields
+	 */
+	CopyPlanFields((Plan *) from, (Plan *) newnode);
+
+	COPY_SCALAR_FIELD(tupleSegIdx);
+	COPY_NODE_FIELD(policyAttrs);
+	COPY_SCALAR_FIELD(oldSegs);
+	COPY_SCALAR_FIELD(ptype);
+	return newnode;
+}
+
+/*
  * _copyRowTrigger
  */
 static RowTrigger *
@@ -3149,6 +3169,7 @@ _copyQuery(const Query *from)
 	COPY_NODE_FIELD(constraintDeps);
 	COPY_NODE_FIELD(intoPolicy);
 	COPY_SCALAR_FIELD(isCTAS);
+	COPY_SCALAR_FIELD(needReshuffle);
 
 	return newnode;
 }
@@ -3192,6 +3213,7 @@ _copyUpdateStmt(const UpdateStmt *from)
 	COPY_NODE_FIELD(fromClause);
 	COPY_NODE_FIELD(returningList);
 	COPY_NODE_FIELD(withClause);
+	COPY_SCALAR_FIELD(needReshuffle);
 
 	return newnode;
 }
@@ -4980,6 +5002,19 @@ _copyDistributedBy(const DistributedBy *from)
 	return newnode;
 }
 
+
+static ReshuffleExpr *
+_copyReshuffleExpr(const ReshuffleExpr *from)
+{
+	ReshuffleExpr *newnode = makeNode(ReshuffleExpr);
+	newnode->newSegs = from->newSegs;
+	newnode->oldSegs = from->oldSegs;
+	COPY_NODE_FIELD(hashKeys);
+	COPY_NODE_FIELD(hashTypes);
+	newnode->ptype = from->ptype;
+	return newnode;
+}
+
 /* ****************************************************************
  *					pg_list.h copy functions
  * ****************************************************************
@@ -5235,6 +5270,9 @@ copyObject(const void *from)
 			break;
 		case T_SplitUpdate:
 			retval = _copySplitUpdate(from);
+			break;
+		case T_Reshuffle:
+			retval = _copyReshuffle(from);
 			break;
 		case T_RowTrigger:
 			retval = _copyRowTrigger(from);
@@ -5995,6 +6033,9 @@ copyObject(const void *from)
 
 		case T_DistributedBy:
 			retval = _copyDistributedBy(from);
+			break;
+		case T_ReshuffleExpr:
+			retval = _copyReshuffleExpr(from);
 			break;
 
 		default:

src/backend/nodes/outfuncs.c
@@ -1215,6 +1215,21 @@ _outSplitUpdate(StringInfo str, const SplitUpdate *node)
 }
 
 /*
+ * _outReshuffle
+ */
+static void
+_outReshuffle(StringInfo str, const Reshuffle *node)
+{
+	WRITE_NODE_TYPE("Reshuffle");
+
+	WRITE_INT_FIELD(tupleSegIdx);
+	WRITE_NODE_FIELD(policyAttrs);
+	WRITE_INT_FIELD(oldSegs);
+	WRITE_INT_FIELD(ptype);
+	_outPlanInfo(str, (Plan *) node);
+}
+
+/*
  * _outRowTrigger
  */
 static void
@@ -3433,6 +3448,7 @@ _outUpdateStmt(StringInfo str, const UpdateStmt *node)
 	WRITE_NODE_FIELD(fromClause);
 	WRITE_NODE_FIELD(returningList);
 	WRITE_NODE_FIELD(withClause);
+	WRITE_BOOL_FIELD(needReshuffle);
 }
 
 static void
@@ -3762,6 +3778,7 @@ _outQuery(StringInfo str, const Query *node)
 	WRITE_NODE_FIELD(setOperations);
 	WRITE_NODE_FIELD(constraintDeps);
 	WRITE_BOOL_FIELD(isCTAS);
+	WRITE_BOOL_FIELD(needReshuffle);
 
 	/* Don't serialize policy */
 }
@@ -4584,6 +4601,19 @@ _outAlterTSDictionaryStmt(StringInfo str, const AlterTSDictionaryStmt *node)
 
 #ifndef COMPILING_BINARY_FUNCS
 static void
+_outReshuffleExpr(StringInfo str, const ReshuffleExpr *node)
+{
+	WRITE_NODE_TYPE("RESHUFFLEEXPR");
+
+	WRITE_INT_FIELD(newSegs);
+	WRITE_INT_FIELD(oldSegs);
+	WRITE_NODE_FIELD(hashKeys);
+	WRITE_NODE_FIELD(hashTypes);
+	WRITE_INT_FIELD(ptype);
+}
+
+
+static void
 _outTupleDescNode(StringInfo str, const TupleDescNode *node)
 {
 	int			i;
@@ -4789,6 +4819,9 @@ _outNode(StringInfo str, const void *obj)
 				break;
 			case T_SplitUpdate:
 				_outSplitUpdate(str, obj);
+				break;
+			case T_Reshuffle:
+				_outReshuffle(str, obj);
 				break;
 			case T_RowTrigger:
 				_outRowTrigger(str, obj);
@@ -5522,7 +5555,9 @@ _outNode(StringInfo str, const void *obj)
 			case T_AlterTSDictionaryStmt:
 				_outAlterTSDictionaryStmt(str, obj);
 				break;
-
+			case T_ReshuffleExpr:
+                _outReshuffleExpr(str, obj);
+				break;
 			default:
 
 				/*

src/backend/nodes/readfast.c
@@ -255,6 +255,7 @@ _readQuery(void)
 	READ_NODE_FIELD(setOperations);
 	READ_NODE_FIELD(constraintDeps);
 	READ_BOOL_FIELD(isCTAS);
+	READ_BOOL_FIELD(needReshuffle);
 
 	/* policy not serialized */
 
@@ -701,6 +702,7 @@ _readUpdateStmt(void)
 	READ_NODE_FIELD(fromClause);
 	READ_NODE_FIELD(returningList);
 	READ_NODE_FIELD(withClause);
+	READ_BOOL_FIELD(needReshuffle);
 	READ_DONE();
 }
 
@@ -2287,6 +2289,23 @@ _readSplitUpdate(void)
 }
 
 /*
+ * _readReshuffle
+ */
+static Reshuffle *
+_readReshuffle(void)
+{
+	READ_LOCALS(Reshuffle);
+
+	READ_INT_FIELD(tupleSegIdx);
+	READ_NODE_FIELD(policyAttrs);
+	READ_INT_FIELD(oldSegs);
+	READ_INT_FIELD(ptype);
+	readPlanInfo((Plan *)local_node);
+
+	READ_DONE();
+}
+
+/*
  * _readRowTrigger
  */
 static RowTrigger *
@@ -2901,6 +2920,19 @@ _readLockRows(void)
 	READ_DONE();
 }
 
+static ReshuffleExpr *
+_readReshuffleExprfFast(void)
+{
+	READ_LOCALS(ReshuffleExpr);
+
+	READ_INT_FIELD(newSegs);
+	READ_INT_FIELD(oldSegs);
+	READ_NODE_FIELD(hashKeys);
+	READ_NODE_FIELD(hashTypes);
+	READ_INT_FIELD(ptype);
+	READ_DONE();
+}
+
 
 static Node *
 _readValue(NodeTag nt)
@@ -3187,6 +3219,9 @@ readNodeBinary(void)
 				break;
 			case T_SplitUpdate:
 				return_value = _readSplitUpdate();
+				break;
+			case T_Reshuffle:
+				return_value = _readReshuffle();
 				break;
 			case T_RowTrigger:
 				return_value = _readRowTrigger();
@@ -3837,8 +3872,9 @@ readNodeBinary(void)
 			case T_AlterTableSpaceMoveStmt:
 				return_value = _readAlterTableSpaceMoveStmt();
 				break;
-
-
+			case T_ReshuffleExpr:
+				return_value = _readReshuffleExprfFast();
+				break;
 			default:
 				return_value = NULL; /* keep the compiler silent */
 				elog(ERROR, "could not deserialize unrecognized node type: %d",

src/backend/optimizer/plan/createplan.c
@@ -6720,6 +6720,7 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 			RangeTblEntry *rte = rt_fetch(rti, root->parse->rtable);
 			GpPolicy   *targetPolicy;
 			GpPolicyType targetPolicyType;
+			Query *qry = root->parse;
 
 			Assert(rti > 0);
 			Assert(rte->rtekind == RTE_RELATION);
@@ -6752,7 +6753,9 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 				 * e.g. because the input was eliminated by constraint
 				 * exclusion, we can skip it.
 				 */
-				if (is_split_update && !is_dummy_plan(subplan))
+				if ((is_split_update ||
+					 (targetPolicy->nattrs == 0 && qry->needReshuffle)) &&
+					 !is_dummy_plan(subplan))
 				{
 					List	   *hashExpr;
 					Plan	*new_subplan;
@@ -6767,15 +6770,28 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 					}
 
 					new_subplan = (Plan *) make_splitupdate(root, (ModifyTable *) node, subplan, rte);
-					hashExpr = getExprListFromTargetList(new_subplan->targetlist,
-														 targetPolicy->nattrs,
-														 targetPolicy->attrs,
-														 false);
-					if (!repartitionPlan(new_subplan, false, false, hashExpr,
-										 targetPolicy->numsegments))
-						ereport(ERROR, (errcode(ERRCODE_GP_FEATURE_NOT_YET),
-										errmsg("Cannot parallelize that UPDATE yet")));
 
+					/*
+					 * if need reshuffle, add the Reshuffle node onto the
+					 * SplitUpdate node and Specify the explicit motion
+					 */
+                    if(qry->needReshuffle)
+					{
+						new_subplan = (Plan *) make_reshuffle(root, new_subplan, rte, rti);
+						request_explicit_motion(new_subplan, rti, root->glob->finalrtable);
+					}
+					else
+					{
+						/* Only update hash keys and do not need reshuffle */
+						hashExpr = getExprListFromTargetList(new_subplan->targetlist,
+															 targetPolicy->nattrs,
+															 targetPolicy->attrs,
+															 false);
+						if (!repartitionPlan(new_subplan, false, false, hashExpr,
+											 targetPolicy->numsegments))
+							ereport(ERROR, (errcode(ERRCODE_GP_FEATURE_NOT_YET),
+											errmsg("Cannot parallelize that UPDATE yet")));
+					}
 					lcp->data.ptr_value = new_subplan;
 				}
 				else
@@ -6827,6 +6843,19 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 			}
 			else if (targetPolicyType == POLICYTYPE_REPLICATED)
 			{
+				if (node->operation == CMD_UPDATE &&
+					qry->needReshuffle)
+				{
+					Plan	*new_subplan;
+
+					new_subplan = (Plan *) make_splitupdate(root, (ModifyTable *) node, subplan, rte);
+					new_subplan = (Plan *) make_reshuffle(root, new_subplan, rte, rti);
+					request_explicit_motion(new_subplan, rti, root->glob->finalrtable);
+
+					lcp->data.ptr_value = new_subplan;
+
+					continue;
+				}
 				node->action_col_idxes = lappend_int(node->action_col_idxes, -1);
 				node->ctid_col_idxes = lappend_int(node->ctid_col_idxes, -1);
 				node->oid_col_idxes = lappend_int(node->oid_col_idxes, 0);

src/include/nodes/plannodes.h
@@ -1309,8 +1309,20 @@ typedef struct SplitUpdate
 	AttrNumber	tupleoidColIdx;		/* index of tuple oid column into the target list */
 	List		*insertColIdx;		/* list of columns to INSERT into the target list */
 	List		*deleteColIdx;		/* list of columns to DELETE into the target list */
-
 } SplitUpdate;
+
+/*
+ * Reshuffle Node
+ * More details please read the description in the nodeReshuffle.c
+ */
+typedef struct Reshuffle
+{
+	Plan plan;
+	AttrNumber tupleSegIdx;
+	List *policyAttrs;
+	int oldSegs;
+	GpPolicyType ptype;
+} Reshuffle;
 
 /*
  * AssertOp Node

commit 6a3a31b92e6b79dc8abd8b1b6c1581061a11143b

Fix the errors when reshuffle replicated table
This commit is used to solve some errors which are introduced by the
f4f4bdc :

* Fix compiler warning for three-way comparison Fix an error, when
* reshuffle the replicated table, the tuple cannot insert correctly.

We assume that there are 3 segments(seg#0, seg#1, seg#2) in the cluster
and the numsegments of a replicated table is 2. In the common UPDATE,
when we insert the tuple to this replicated table, It should not
insert into the seg#2, Because the replicated table only store data
in the first 2 segments(seg#0, seg#1).

In the UPDATE with reshuffle flag (This UPDATE cannot use directly, it
only can be composed by ALTER TABLE XXX SET WITH (RESHUFFLE 
= true)), we assume that there are 2 old segments(seg#0, seg#1) and 
1 new segment(seg#2), the replicated table is in the first 2 segment. 
When we reshuffle the replicated table, it will insert new tuple to the seg#2,
then ModifyTable operator prohibits the tuple insert into seg#2
because it thinks seg#2 is out of the range.

We add a new flag to the ModifyTable struct, it can distinguish between
common UPDATE and RESHUFFLE.

src/backend/executor/nodeModifyTable.c
@@ -1549,6 +1549,7 @@ ExecModifyTable(ModifyTableState *node)
 	ItemPointerData tuple_ctid;
 	HeapTupleData oldtupdata;
 	HeapTuple	oldtuple;
+	ModifyTable *mt = (ModifyTable *) node->ps.plan;
 
 	/*
 	 * This should NOT get called during EvalPlanQual; we should have passed a
@@ -1594,9 +1595,10 @@ ExecModifyTable(ModifyTableState *node)
 	 * Prevent replicated tables being updated on segments outside
 	 * the [0, numsegments-1] range.
 	 *
-	 * FIXME: remove this once the flexible gang size feature is ready.
+	 * FIXME: This piece of code maybe remove once we adjusted the gang size
 	 */
 	if (Gp_role == GP_ROLE_EXECUTE &&
+		!mt->isReshuffle &&
 		resultRelInfo->ri_RelationDesc->rd_cdbpolicy->ptype == POLICYTYPE_REPLICATED &&
 		(GpIdentity.segindex >=
 		 resultRelInfo->ri_RelationDesc->rd_cdbpolicy->numsegments))

src/backend/executor/nodeReshuffle.c
@@ -292,7 +292,7 @@ ExecReshuffle(ReshuffleState *node)
 		int segIdx;
 
 		/* For replicated tables*/
-		if (GpIdentity.segindex >= reshuffle->oldSegs >=
+		if (GpIdentity.segindex + reshuffle->oldSegs >=
 			getgpsegmentCount())
 			return NULL;
 

src/backend/nodes/copyfuncs.c
@@ -316,6 +316,7 @@ _copyModifyTable(const ModifyTable *from)
 	COPY_NODE_FIELD(action_col_idxes);
 	COPY_NODE_FIELD(ctid_col_idxes);
 	COPY_NODE_FIELD(oid_col_idxes);
+	COPY_SCALAR_FIELD(isReshuffle);
 
 	return newnode;
 }

src/backend/nodes/outfuncs.c
@@ -464,6 +464,7 @@ _outModifyTable(StringInfo str, const ModifyTable *node)
 	WRITE_NODE_FIELD(action_col_idxes);
 	WRITE_NODE_FIELD(ctid_col_idxes);
 	WRITE_NODE_FIELD(oid_col_idxes);
+	WRITE_BOOL_FIELD(isReshuffle);
 }
 
 static void

src/backend/nodes/readfast.c
@@ -2903,6 +2903,7 @@ _readModifyTable(void)
 	READ_NODE_FIELD(action_col_idxes);
 	READ_NODE_FIELD(ctid_col_idxes);
 	READ_NODE_FIELD(oid_col_idxes);
+	READ_BOOL_FIELD(isReshuffle);
 
 	READ_DONE();
 }

src/backend/optimizer/plan/createplan.c
@@ -6461,6 +6461,7 @@ make_modifytable(PlannerInfo *root,
 	node->action_col_idxes = NIL;
 	node->ctid_col_idxes = NIL;
 	node->oid_col_idxes = NIL;
+	node->isReshuffle = false;
 
 	adjust_modifytable_flow(root, node, is_split_updates);
 
@@ -6775,10 +6776,11 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 					 * if need reshuffle, add the Reshuffle node onto the
 					 * SplitUpdate node and Specify the explicit motion
 					 */
-                    if(qry->needReshuffle)
+					if(qry->needReshuffle)
 					{
 						new_subplan = (Plan *) make_reshuffle(root, new_subplan, rte, rti);
 						request_explicit_motion(new_subplan, rti, root->glob->finalrtable);
+						((ModifyTable *)node)->isReshuffle = true;
 					}
 					else
 					{
@@ -6851,6 +6853,7 @@ adjust_modifytable_flow(PlannerInfo *root, ModifyTable *node, List *is_split_upd
 					new_subplan = (Plan *) make_splitupdate(root, (ModifyTable *) node, subplan, rte);
 					new_subplan = (Plan *) make_reshuffle(root, new_subplan, rte, rti);
 					request_explicit_motion(new_subplan, rti, root->glob->finalrtable);
+					((ModifyTable *)node)->isReshuffle = true;
 
 					lcp->data.ptr_value = new_subplan;
 

src/include/nodes/plannodes.h
@@ -362,6 +362,7 @@ typedef struct ModifyTable
 	List	   *action_col_idxes;
 	List	   *ctid_col_idxes;
 	List	   *oid_col_idxes;
+	bool		isReshuffle;
 } ModifyTable;
 
 /* ----------------