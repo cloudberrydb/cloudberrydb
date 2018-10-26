tvesely/gpdb

Restrict folding of stable functions during planning
Stable functions need not be folded in case the query is simple enough
(e.g. pl/pgsql simple expression, or "select foo()").

Without this patch, every time a simple expression is evaluated, a new
plan and a new ExprState are generated. Because we only clean up the
expression states created for simple expressions at the end of
transaction, this will result in millions of expression states, one per
tuple, being generated that are not reused after they are initially
created.

This patch is taken from Heikki's review comment:
https://github.com/greenplum-db/gpdb/pull/5947#issuecomment-428103480

This patch also removes GPDB specific update for the test `guc.sql`.
Prior to this commit, since we were aggressively folding stable
functions, `current_setting()` was evaluated at plan time causing a diff
from upstream. This patch realigns the test with upstream.

Thanks to Jesse for valuable guidance and carving out the test case.

Co-authored-by: Ekta Khanna <ekhanna@pivotal.io>
Co-authored-by: Heikki Linnakangas <hlinnakangas@pivotal.io>
Co-authored-by: Taylor Vesely <tvesely@pivotal.io>

src/test/modules/test_planner/integration_tests/planner_integration_tests.c
@@ -57,6 +57,32 @@ test_vanilla_subquery_has_correct_extparams()
 		is_equal_to(0));
 }
 
+static void
+test_stable_function_in_subquery_is_evaluated_to_const()
+{
+	const char *query_string = "select * from (select now()) a;";
+
+	Query *query = make_query(query_string);
+	PlannedStmt *plannedstmt = planner(query, 0, NULL);
+
+	TargetEntry *tle = get_first_plan_node_target_entry(plannedstmt);
+
+	assert_that_bool(IsA(tle->expr, Const), is_equal_to(true));
+}
+
+static void
+test_stable_function_in_simple_query_is_not_evaluated_in_planner()
+{
+	const char *query_string = "select now();";
+
+	Query *query = make_query(query_string);
+	PlannedStmt *plannedstmt = planner(query, 0, NULL);
+
+	TargetEntry *tle = get_first_plan_node_target_entry(plannedstmt);
+
+	assert_that_bool(IsA(tle->expr, FuncExpr), is_equal_to(true));
+}
+
 void
 run_planner_integration_test_suite(void)
 {
@@ -65,4 +91,7 @@ run_planner_integration_test_suite(void)
 	 */
 	test_vanilla_subquery_has_correct_extparams();
 	test_window_function_with_subquery_has_correct_extparams();
+	test_stable_function_in_subquery_is_evaluated_to_const();
+	test_stable_function_in_simple_query_is_not_evaluated_in_planner();
 }
+

src/test/modules/test_planner/src/planner_test_helpers.c
@@ -32,3 +32,11 @@ get_first_subplan(PlannedStmt *plannedStmt)
 {
 	return (Plan *)list_nth(plannedStmt->subplans, 0);
 }
+
+TargetEntry *
+get_first_plan_node_target_entry(PlannedStmt *plannedstmt)
+{
+	Result *result_node = (Result*) plannedstmt->planTree;
+	List *tlist = result_node->plan.targetlist;
+	return list_nth(tlist, 0);
+}

src/test/modules/test_planner/src/planner_test_helpers.h
@@ -8,5 +8,6 @@
 
 extern Query *make_query(const char *query_string);
 extern Plan *get_first_subplan(PlannedStmt *plannedStmt);
+extern TargetEntry *get_first_plan_node_target_entry(PlannedStmt *plannedstmt);
 
 #endif //PLANNER_TEST_HELPERS_H

cloudberrydb/cloudberrydb

Co-authored-by: Melanie Plageman <mplageman@pivotal.io>
Co-authored-by: Adam Berlin <aberlin@pivotal.io>

src/test/modules/test_planner/integration_tests/planner_integration_tests.c
@@ -7,7 +7,38 @@
 #include "src/planner_test_helpers.h"
 
 
+static void
+test_stable_function_in_subquery_is_evaluated_to_const()
+{
+	const char *query_string = "select * from (select now()) a;";
+
+	Query *query = make_query(query_string);
+	PlannedStmt *plannedstmt = planner(query, 0, NULL);
+
+	TargetEntry *tle = get_target_entry_from_root_plan_node(plannedstmt);
+
+	assert_that_bool(IsA(tle->expr, Const), is_equal_to(true));
+}
+
+static void
+test_stable_function_in_simple_query_is_not_evaluated_in_planner()
+{
+	const char *query_string = "select now();";
+
+	Query *query = make_query(query_string);
+	PlannedStmt *plannedstmt = planner(query, 0, NULL);
+
+	TargetEntry *tle = get_target_entry_from_root_plan_node(plannedstmt);
+
+	assert_that_bool(IsA(tle->expr, FuncExpr), is_equal_to(true));
+}
+
 void
 run_planner_integration_test_suite(void)
 {
+	/*
+	 * Tests that are generic between planner and optimizer
+	 */
+	test_stable_function_in_subquery_is_evaluated_to_const();
+	test_stable_function_in_simple_query_is_not_evaluated_in_planner();
 }

src/test/modules/test_planner/src/planner_test_helpers.c
@@ -32,3 +32,11 @@ get_first_subplan(PlannedStmt *plannedStmt)
 {
 	return (Plan *)list_nth(plannedStmt->subplans, 0);
 }
+
+TargetEntry *
+get_target_entry_from_root_plan_node(PlannedStmt *plannedstmt)
+{
+	Result *result_node = (Result*) plannedstmt->planTree;
+	List *tlist = result_node->plan.targetlist;
+	return list_nth(tlist, 0);
+}

src/test/modules/test_planner/src/planner_test_helpers.h
@@ -8,5 +8,6 @@
 
 extern Query *make_query(const char *query_string);
 extern Plan *get_first_subplan(PlannedStmt *plannedStmt);
+extern TargetEntry *get_target_entry_from_root_plan_node(PlannedStmt *plannedstmt);
 
 #endif //PLANNER_TEST_HELPERS_H