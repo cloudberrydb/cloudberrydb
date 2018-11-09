f8afec06c8d1b9904b9c7b4b5592130ba84965fe
Ivan Leskin <leskin.in@arenadata.io>
2018-11-09 14:22:13 +0300

    Fix ZSTD compression error "dst buffer too small"
    When ZSTD compression is used for AO CO tables, insertion of data may cause an
    error "Destination buffer is too small". This happens when compressed data is
    larger than uncompressed input data.
    
    This commit adds handling of this situation: do not change output buffer and
    return size used equal to source size. The caller (e.g.,
    'cdbappendonlystoragewrite.c') is able to handle such output; in this case, it
    copies data from input to output itself.

--- gpcontrib/zstd/zstd_compression.c
+++ gpcontrib/zstd/zstd_compression.c
@@ -16,6 +16,7 @@
 #include "utils/builtins.h"
 
 #include <zstd.h>
+#include <zstd_errors.h>
 
 Datum zstd_constructor(PG_FUNCTION_ARGS);
 Datum zstd_destructor(PG_FUNCTION_ARGS);
@@ -83,6 +84,12 @@ zstd_destructor(PG_FUNCTION_ARGS)
 	PG_RETURN_VOID();
 }
 
+/*
+ * zstd compression implementation
+ *
+ * Note that when compression fails due to algorithm inefficiency,
+ * dst_used is set so src_sz, but the output buffer contents are left unchanged
+ */
 Datum
 zstd_compress(PG_FUNCTION_ARGS)
 {
@@ -104,7 +111,16 @@ zstd_compress(PG_FUNCTION_ARGS)
 
 	if (ZSTD_isError(dst_length_used))
 	{
-		elog(ERROR, "%s", ZSTD_getErrorName(dst_length_used));
+		if (ZSTD_getErrorCode(dst_length_used) == ZSTD_error_dstSize_tooSmall) {
+			/*
+			 * This error is returned when "compressed" output is bigger than
+			 * uncompressed input.
+			 * The caller can detect this by checking dst_used >= src_size
+			 */
+			dst_length_used = src_sz;
+		}
+		else
+			elog(ERROR, "%s", ZSTD_getErrorName(dst_length_used));
 	}
 
 	*dst_used = (int32) dst_length_used;
