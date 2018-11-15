99cccf76b4aab5f0614e077de320ae07b880931f
Adam Berlin <aberlin@pivotal.io>
2018-11-15 15:49:54 -0500

    Improve usage text for test_gpdb.sh - including options.

--- contrib/pg_upgrade/test_gpdb.sh
+++ contrib/pg_upgrade/test_gpdb.sh
@@ -198,7 +198,7 @@ upgrade_segment()
 usage()
 {
 	appname=`basename $0`
-	echo "usage: $appname -o <dir> -b <dir>"
+	echo "usage: $appname -o <dir> -b <dir> [options]"
 	echo " -o <dir>     Directory containing old datadir"
 	echo " -b <dir>     Directory containing binaries"
 	echo " -s           Run smoketest only"
