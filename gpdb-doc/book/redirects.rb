03118947f6efde9dff25350f156d32201dd207f4
dyozie <dyozie@pivotal.io>
2018-11-19 11:27:58 -0800

    Docs: Updating book build path; Standardizing home navigation link product name

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/docs/600/common/gpdb-features.html'
-r301 '/index.html', '/docs/600/common/gpdb-features.html'
-r301 '/docs/600/index.html', '/docs/600/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/docs/600/common/gpdb-features.html'
+r301 '/', '/600/common/gpdb-features.html'
+r301 '/index.html', '/600/common/gpdb-features.html'
+r301 '/600/index.html', '/600/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/600/common/gpdb-features.html'
