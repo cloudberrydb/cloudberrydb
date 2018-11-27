9d80c8cd21ef00f37a8f24df87a9cd3467131d07
Jacob Champion <pchampion@pivotal.io>
2018-11-27 15:57:17 -0800

    pg_ctl: fix initdb version check
    We should check for the GPDB version string, not the one from upstream
    Postgres.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -916,7 +916,7 @@ do_init(void)
 	char		cmd[MAXPGPATH];
 
 	if (exec_path == NULL)
-		exec_path = find_other_exec_or_die(argv0, "initdb", "initdb (PostgreSQL) " PG_VERSION "\n");
+		exec_path = find_other_exec_or_die(argv0, "initdb", "initdb (Greenplum Database) " PG_VERSION "\n");
 
 	if (pgdata_opt == NULL)
 		pgdata_opt = "";
