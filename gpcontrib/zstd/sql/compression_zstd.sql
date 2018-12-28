00dd1ead6650f1d56b13e043bc648aa287e2d388
Jimmy Yih <jyih@pivotal.io>
2018-12-28 11:12:29 -0800

    Make zstd tests more deterministic
    The current tests would diff on the SELECT queries containing ORDER BY
    LIMIT on a gpdemo cluster (seen on my MacOS Mojave laptop and on a
    CentOS 6 & 7 VM). Most likely the test answer file was created from a
    cluster that was not the test standard 3 primary segment configuration
    so the SELECT may output differently.
    
    Co-authored-by: Karen Huddleston <khuddleston@pivotal.io>

--- gpcontrib/zstd/sql/compression_zstd.sql
+++ gpcontrib/zstd/sql/compression_zstd.sql
@@ -6,8 +6,8 @@ INSERT INTO zstdtest SELECT g, 'foo' || g FROM generate_series(1, 100000) g;
 INSERT INTO zstdtest SELECT g, 'bar' || g FROM generate_series(1, 100000) g;
 
 -- Check contents, at the beginning of the table and at the end.
-SELECT * FROM zstdtest ORDER BY id LIMIT 5;
-SELECT * FROM zstdtest ORDER BY id DESC LIMIT 5;
+SELECT * FROM zstdtest ORDER BY (id, t) LIMIT 5;
+SELECT * FROM zstdtest ORDER BY (id, t) DESC LIMIT 5;
 
 
 -- Test different compression levels:
@@ -17,13 +17,13 @@ CREATE TABLE zstdtest_19 (id int4, t text) WITH (appendonly=true, compresstype=z
 
 INSERT INTO zstdtest_1 SELECT g, 'foo' || g FROM generate_series(1, 10000) g;
 INSERT INTO zstdtest_1 SELECT g, 'bar' || g FROM generate_series(1, 10000) g;
-SELECT * FROM zstdtest_1 ORDER BY id LIMIT 5;
-SELECT * FROM zstdtest_1 ORDER BY id DESC LIMIT 5;
+SELECT * FROM zstdtest_1 ORDER BY (id, t) LIMIT 5;
+SELECT * FROM zstdtest_1 ORDER BY (id, t) DESC LIMIT 5;
 
 INSERT INTO zstdtest_19 SELECT g, 'foo' || g FROM generate_series(1, 10000) g;
 INSERT INTO zstdtest_19 SELECT g, 'bar' || g FROM generate_series(1, 10000) g;
-SELECT * FROM zstdtest_19 ORDER BY id LIMIT 5;
-SELECT * FROM zstdtest_19 ORDER BY id DESC LIMIT 5;
+SELECT * FROM zstdtest_19 ORDER BY (id, t) LIMIT 5;
+SELECT * FROM zstdtest_19 ORDER BY (id, t) DESC LIMIT 5;
 
 
 -- Test the bounds of compresslevel. None of these are allowed.
