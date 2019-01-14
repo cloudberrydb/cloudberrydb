4007c4b86cc79c3e3fb44eb5a81c173ef92393ff
Adam Berlin <aberlin@pivotal.io>
2019-01-14 17:29:42 -0500

    Adds test for gpexpand.

--- gpMgmt/test/behave/mgmt_utils/steps/replication_slots_utils.py
+++ gpMgmt/test/behave/mgmt_utils/steps/replication_slots_utils.py
@@ -15,14 +15,20 @@ from test.behave_utils.utils import (
 from addmirrors_mgmt_utils import (add_three_mirrors)
 
 
+def assert_successful_command(context):
+    if context.ret_code != 0:
+        raise Exception('%s : %s' % (context.error_message, context.stdout_message))
+
+
 def run_recovery_for_segments(context):
     run_command(context, "gprecoverseg -aFv")
-
-    if context.ret_code != 0:
-        raise Exception('%s' % context.error_message)
+    assert_successful_command(context)
 
 
 def create_cluster(context, with_mirrors=True):
+    context.initial_cluster_size = 3
+    context.current_cluster_size = context.initial_cluster_size
+
     cmd = """
     cd ../gpAux/gpdemo; \
         export MASTER_DEMO_PORT={master_port} && \
@@ -33,13 +39,37 @@ def create_cluster(context, with_mirrors=True):
         ./demo_cluster.sh
     """.format(master_port=os.getenv('MASTER_PORT', 15432),
                port_base=os.getenv('PORT_BASE', 25432),
-               num_primary_mirror_pairs=os.getenv('NUM_PRIMARY_MIRROR_PAIRS', 3),
+               num_primary_mirror_pairs=os.getenv(
+                   'NUM_PRIMARY_MIRROR_PAIRS', context.initial_cluster_size),
                with_mirrors=('true' if with_mirrors else 'false'))
 
     run_command(context, cmd)
+    assert_successful_command(context)
 
-    if context.ret_code != 0:
-        raise Exception('%s' % context.error_message)
+
+def ensure_temp_directory_is_empty(context, temp_directory):
+    run_command(context, "rm -rf /tmp/{temp_directory}".format(
+        temp_directory=temp_directory))
+
+
+def expand(context):
+    ensure_temp_directory_is_empty(context, "behave_test_expansion_primary")
+    ensure_temp_directory_is_empty(context, "behave_test_expansion_mirror")
+
+    run_command(context, "createdb expansion_database")
+
+    expansion_command = """gpexpand -D expansion_database --input <(echo '
+    localhost:localhost:55432:/tmp/behave_test_expansion_primary:8:3:p
+    localhost:localhost:55433:/tmp/behave_test_expansion_mirror:9:3:m
+')
+"""
+    # Initialize
+    run_command(context, expansion_command)
+    assert_successful_command(context)
+
+    # Redistribute tables
+    run_command(context, expansion_command)
+    assert_successful_command(context)
 
 
 def ensure_primary_mirror_switched_roles():
@@ -57,7 +87,7 @@ def step_impl(context):
     stop_database(context)
 
 
-@given(u'a mirror has crashed')
+@when(u'a mirror has crashed')
 def step_impl(context):
     run_command(context, "ps aux | grep dbfast_mirror1 | awk '{print $2}' | xargs kill -9")
 
@@ -74,8 +104,8 @@ def step_impl(context):
         "select pg_get_replication_slots() from gp_dist_random('gp_id') order by gp_segment_id"
     )
 
-    if result_cursor.rowcount != 3:
-        raise Exception("expected all three primaries to have replication slots")
+    if result_cursor.rowcount != context.current_cluster_size:
+        raise Exception("expected all %d primaries to have replication slots, only %d have slots" % (context.current_cluster_size, results.rowcount))
 
     for content_id, result in enumerate(result_cursor.fetchall()):
         if not result[0].startswith('(internal_wal_replication_slot,,physical,,t,'):
@@ -117,3 +147,9 @@ def step_impl(context):
 @when("I fully recover a mirror")
 def step_impl(context):
     run_recovery_for_segments(context)
+
+
+@when("I add a segment to the cluster")
+def step_imp(context):
+    context.current_cluster_size = 4
+    expand(context)
