c0d99c9d6a8319d1fc887ec53b70e87c4477d84c
Heikki Linnakangas <hlinnakangas@pivotal.io>
2019-01-16 11:25:09 +0200

    Use zstandard instead of zlib to compress dispatched plans.
    Because it's faster and compresses better. Like with the old zlib code, if
    libzstd is not available, fall back to no compression. There was debate on
    whether we should fall back to zlib, if that's available, but maintaining
    alternative code is not free. Zstandard is so much faster than zlib, that
    anyone running a production system really should build with it. A
    developer's laptop is a different story, and falling back to no
    compression is fine for that.
    
    Discussion: https://groups.google.com/a/greenplum.org/d/msg/gpdb-dev/8w7vRuaqJ6c/uHpOJXwyEgAJ
    Reviewed-by: Ashwin Agrawal <aagrawal@pivotal.io>
    Reviewed-by: Daniel Gustafsson <dgustafsson@pivotal.io>

--- src/backend/cdb/cdbsrlz.c
+++ src/backend/cdb/cdbsrlz.c
@@ -15,15 +15,23 @@
 
 #include "postgres.h"
 
-#include <math.h>
-
 #include "cdb/cdbsrlz.h"
 #include "nodes/nodes.h"
 #include "utils/memaccounting.h"
-#include "utils/zlib_wrapper.h"
+#include "utils/memutils.h"
 
-static char *compress_string(const char *src, int uncompressed_size, int *size);
-static char *uncompress_string(const char *src, int size, int *uncompressed_len);
+#ifdef HAVE_LIBZSTD
+/* Zstandard library is provided */
+
+#include <zstd.h>
+
+static char *compress_string(const char *src, int uncompressed_size, int *compressed_size_p);
+static char *uncompress_string(const char *src, int size, int *uncompressed_size_p);
+
+/* zstandard compression level to use. */
+#define COMPRESS_LEVEL 3
+
+#endif			/* HAVE_LIBZSTD */
 
 /*
  * This is used by dispatcher to serialize Plan and Query Trees for
@@ -44,15 +52,19 @@ serializeNode(Node *node, int *size, int *uncompressed_size_out)
 		pszNode = nodeToBinaryStringFast(node, &uncompressed_size);
 		Assert(pszNode != NULL);
 
-		if (NULL != uncompressed_size_out)
-		{
-			*uncompressed_size_out = uncompressed_size;
-		}
+		/* If we have been compiled with libzstd, use it to compress it */
+#ifdef HAVE_LIBZSTD
 		sNode = compress_string(pszNode, uncompressed_size, size);
 		pfree(pszNode);
+#else
+		sNode = pszNode;
+		*size = uncompressed_size;
+#endif
 	}
 	END_MEMORY_ACCOUNT();
 
+	if (NULL != uncompressed_size_out)
+		*uncompressed_size_out = uncompressed_size;
 	return sNode;
 }
 
@@ -64,62 +76,65 @@ serializeNode(Node *node, int *size, int *uncompressed_size_out)
 Node *
 deserializeNode(const char *strNode, int size)
 {
-	char	   *sNode;
 	Node	   *node;
-	int			uncompressed_len;
 
 	Assert(strNode != NULL);
 
 	START_MEMORY_ACCOUNT(MemoryAccounting_CreateAccount(0, MEMORY_OWNER_TYPE_Deserializer));
 	{
+		/* If we have been compiled with libzstd, decompress */
+#ifdef HAVE_LIBZSTD
+		char	   *sNode;
+		int			uncompressed_len;
+
 		sNode = uncompress_string(strNode, size, &uncompressed_len);
-
 		Assert(sNode != NULL);
-
 		node = readNodeFromBinaryString(sNode, uncompressed_len);
-
 		pfree(sNode);
+#else
+		node = readNodeFromBinaryString(strNode, size);
+#endif			/* HAVE_LIBZSTD */
+
 	}
 	END_MEMORY_ACCOUNT();
 
 	return node;
 }
 
+#ifdef HAVE_LIBZSTD
 /*
- * Compress a (binary) string using zlib.
+ * Compress a (binary) string using libzstd
  *
  * returns the compressed data and the size of the compressed data.
  */
 static char *
 compress_string(const char *src, int uncompressed_size, int *size)
 {
-	int			level = 3;
-	unsigned long compressed_size;
-	int			status;
+	static ZSTD_CCtx  *cxt = NULL;      /* ZSTD compression context */
+	size_t		compressed_size;
+	size_t		dst_length_used;
+	char	   *result;
 
-	Bytef	   *result;
-
-	Assert(size != NULL);
-
-	if (src == NULL)
+	if (!cxt)
 	{
-		*size = 0;
-		return NULL;
+		cxt = ZSTD_createCCtx();
+		if (!cxt)
+			elog(ERROR, "out of memory");
 	}
 
-	compressed_size = gp_compressBound(uncompressed_size);	/* worst case */
+	compressed_size = ZSTD_compressBound(uncompressed_size);	/* worst case */
 
-	result = palloc(compressed_size + sizeof(int));
-	memcpy(result, &uncompressed_size, sizeof(int));	/* save the original
-														 * length */
+	result = palloc(compressed_size);
 
-	status = gp_compress2(result + sizeof(int), &compressed_size, (Bytef *) src, uncompressed_size, level);
-	if (status != Z_OK)
-		elog(ERROR, "Compression failed: %s (errno=%d) uncompressed len %d, compressed %d",
-			 zError(status), status, uncompressed_size, (int) compressed_size);
+	dst_length_used = ZSTD_compressCCtx(cxt,
+										result, compressed_size,
+										src, uncompressed_size,
+										COMPRESS_LEVEL);
+	if (ZSTD_isError(dst_length_used))
+		elog(ERROR, "Compression failed: %s uncompressed len %d",
+			 ZSTD_getErrorName(dst_length_used), uncompressed_size);
 
-	*size = compressed_size + sizeof(int);
-
+	*size = dst_length_used;
 	return (char *) result;
 }
 
@@ -127,28 +142,47 @@ compress_string(const char *src, int uncompressed_size, int *size)
  * Uncompress the binary string
  */
 static char *
-uncompress_string(const char *src, int size, int *uncompressed_len)
+uncompress_string(const char *src, int size, int *uncompressed_size_p)
 {
-	Bytef	   *result;
-	unsigned long resultlen;
-	int			status;
+	static ZSTD_DCtx  *cxt = NULL;      /* ZSTD decompression context */
+	char	   *result;
+	unsigned long long uncompressed_size;
+	int			dst_length_used;
 
-	*uncompressed_len = 0;
+	*uncompressed_size_p = 0;
 
-	if (src == NULL)
-		return NULL;
+	if (!cxt)
+	{
+		cxt = ZSTD_createDCtx();
+		if (!cxt)
+			elog(ERROR, "out of memory");
+	}
 
 	Assert(size >= sizeof(int));
 
-	memcpy(uncompressed_len, src, sizeof(int));
+	uncompressed_size = ZSTD_getFrameContentSize(src, size);
+	if (uncompressed_size == ZSTD_CONTENTSIZE_UNKNOWN)
+		elog(ERROR, "decompressed size not known");
+	if (uncompressed_size == ZSTD_CONTENTSIZE_ERROR)
+		elog(ERROR, "invalid compressed data");
 
-	resultlen = *uncompressed_len;
-	result = palloc(resultlen);
+	if (uncompressed_size > MaxAllocSize)
+		elog(ERROR, "decompressed plan tree too large (" UINT64_FORMAT " bytes)",
+			 (uint64) uncompressed_size);
 
-	status = gp_uncompress(result, &resultlen, (Bytef *) (src + sizeof(int)), size - sizeof(int));
-	if (status != Z_OK)
-		elog(ERROR, "Uncompress failed: %s (errno=%d compressed len %d, uncompressed %d)",
-			 zError(status), status, size, *uncompressed_len);
+	result = palloc(uncompressed_size);
+
+	dst_length_used = ZSTD_decompressDCtx(cxt,
+										  result, uncompressed_size,
+										  src, size);
+	if (ZSTD_isError(dst_length_used))
+	{
+		elog(ERROR, "%s", ZSTD_getErrorName(dst_length_used));
+	}
+	Assert(dst_length_used == uncompressed_size);
+
+	*uncompressed_size_p = dst_length_used;
 
 	return (char *) result;
 }
+#endif			/* HAVE_LIBZSTD */
