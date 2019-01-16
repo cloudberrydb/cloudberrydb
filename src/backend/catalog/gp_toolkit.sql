1045e4e7dba4bf9064d74640be634992c7a17820
Ning Yu <nyu@pivotal.io>
2019-01-16 10:53:14 +0800

    resgroup: add new flatten views on the status
    Resource group comes with a view gp_toolkit.gp_resgroup_status to get
    the running status of all the resource groups.  The cpu and memory
    usages as displayed in json format, they are hard to parse and
    understand by human.  To make it more user friendly we now provide two
    new views on the status, the cpu and memory usages are flatten into
    multiple columns.  They also group the status by segment or host so it
    is easier to find out the usages at different levels.
    
    Reviewed-by: Zhenghua Lyu <zlv@pivotal.io>

--- src/backend/catalog/gp_toolkit.sql
+++ src/backend/catalog/gp_toolkit.sql
@@ -1816,6 +1816,98 @@ CREATE VIEW gp_toolkit.gp_resgroup_status AS
 GRANT SELECT ON gp_toolkit.gp_resgroup_status TO public;
 
 --------------------------------------------------------------------------------
+-- @view:
+--              gp_toolkit.gp_resgroup_status_per_host
+--
+-- @doc:
+--              Resource group runtime status information grouped by host
+--
+--------------------------------------------------------------------------------
+
+CREATE VIEW gp_toolkit.gp_resgroup_status_per_host AS
+    WITH s AS (
+        SELECT
+            rsgname
+          , groupid
+          , (json_each(cpu_usage)).key::smallint AS segment_id
+          , (json_each(cpu_usage)).value AS cpu
+          , (json_each(memory_usage)).value AS memory
+        FROM gp_toolkit.gp_resgroup_status
+    )
+    SELECT
+        s.rsgname
+      , s.groupid
+      , c.hostname
+      , sum((s.cpu                       )::text::numeric) AS cpu
+      , sum((s.memory->'used'            )::text::integer) AS memory_used
+      , sum((s.memory->'available'       )::text::integer) AS memory_available
+      , sum((s.memory->'quota_used'      )::text::integer) AS memory_quota_used
+      , sum((s.memory->'quota_available' )::text::integer) AS memory_quota_available
+      , sum((s.memory->'quota_proposed'  )::text::integer) AS memory_quota_proposed
+      , sum((s.memory->'shared_used'     )::text::integer) AS memory_shared_used
+      , sum((s.memory->'shared_available')::text::integer) AS memory_shared_available
+      , sum((s.memory->'shared_granted'  )::text::integer) AS memory_shared_granted
+      , sum((s.memory->'shared_proposed' )::text::integer) AS memory_shared_proposed
+    FROM s
+    INNER JOIN pg_catalog.gp_segment_configuration AS c
+        ON s.segment_id = c.content
+        AND c.role = 'p'
+    GROUP BY
+        s.rsgname
+      , s.groupid
+      , c.hostname
+    ;
+
+GRANT SELECT ON gp_toolkit.gp_resgroup_status_per_host TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--              gp_toolkit.gp_resgroup_status_per_segment
+--
+-- @doc:
+--              Resource group runtime status information grouped by segment
+--
+--------------------------------------------------------------------------------
+
+CREATE VIEW gp_toolkit.gp_resgroup_status_per_segment AS
+    WITH s AS (
+        SELECT
+            rsgname
+          , groupid
+          , (json_each(cpu_usage)).key::smallint AS segment_id
+          , (json_each(cpu_usage)).value AS cpu
+          , (json_each(memory_usage)).value AS memory
+        FROM gp_toolkit.gp_resgroup_status
+    )
+    SELECT
+        s.rsgname
+      , s.groupid
+      , c.hostname
+      , s.segment_id
+      , sum((s.cpu                       )::text::numeric) AS cpu
+      , sum((s.memory->'used'            )::text::integer) AS memory_used
+      , sum((s.memory->'available'       )::text::integer) AS memory_available
+      , sum((s.memory->'quota_used'      )::text::integer) AS memory_quota_used
+      , sum((s.memory->'quota_available' )::text::integer) AS memory_quota_available
+      , sum((s.memory->'quota_proposed'  )::text::integer) AS memory_quota_proposed
+      , sum((s.memory->'shared_used'     )::text::integer) AS memory_shared_used
+      , sum((s.memory->'shared_available')::text::integer) AS memory_shared_available
+      , sum((s.memory->'shared_granted'  )::text::integer) AS memory_shared_granted
+      , sum((s.memory->'shared_proposed' )::text::integer) AS memory_shared_proposed
+    FROM s
+    INNER JOIN pg_catalog.gp_segment_configuration AS c
+        ON s.segment_id = c.content
+        AND c.role = 'p'
+    GROUP BY
+        s.rsgname
+      , s.groupid
+      , c.hostname
+      , s.segment_id
+    ;
+
+GRANT SELECT ON gp_toolkit.gp_resgroup_status_per_segment TO public;
+
+--------------------------------------------------------------------------------
 -- AO/CO diagnostics functions
 --------------------------------------------------------------------------------
 
