1486e528f39cd16a72fde8a89c84ee6aa6292cf7
Jacob Champion <pchampion@pivotal.io>
2019-02-01 11:58:59 -0800

    gpcheckcat: fix gp_distribution_policy.distkey comparisons
    Follow-up to 69ec6926. distkey is no longer a nullable column, so
    comparisons against NULL (to check for random distribution) need to be
    replaced with checks for an empty int2vector.
    
    Although this change causes the currently failing master tests to pass,
    I'm not sure if
    
        distkey = ''
    
    is really an idiomatic way to check for an empty vector. I expect a
    follow-up to this commit.

--- gpMgmt/test/behave/mgmt_utils/steps/data/gpcheckcat/create_invalid_constraint.sql
+++ gpMgmt/test/behave/mgmt_utils/steps/data/gpcheckcat/create_invalid_constraint.sql
@@ -1,3 +1,3 @@
 set allow_system_table_mods=true;
 create table foo(i int primary key);
-update gp_distribution_policy  set distkey=NULL where localoid='foo'::regclass::oid;
+update gp_distribution_policy  set distkey='' where localoid='foo'::regclass::oid;
