df6b44d5fe89cecf89501f218f8ac77db09a2a73
Adam Berlin <aberlin@pivotal.io>
2019-02-02 10:19:53 -0500

    Disable pg_isready test.

--- src/bin/scripts/t/080_pg_isready.pl
+++ src/bin/scripts/t/080_pg_isready.pl
@@ -1,13 +1,17 @@
 use strict;
 use warnings;
 use TestLib;
-use Test::More tests => 10;
+use Test::More tests => 9;
 
 program_help_ok('pg_isready');
 program_version_ok('pg_isready');
 program_options_handling_ok('pg_isready');
 
-command_fails(['pg_isready'], 'fails with no server running');
+#
+# Disable temporarily. This test expects the server to not currently
+# be running which is not the case on Concourse.
+#
+#command_fails(['pg_isready'], 'fails with no server running');
 
 my $tempdir = tempdir;
 start_test_server $tempdir;
