58b88aa4b0625db75c155d797b36a7bb2c61759c
Adam Berlin <aberlin@pivotal.io>
2019-02-04 16:45:17 -0500

    squash! Skip attempts to create target gp tablespace.
    handle force overwrite scenario for --target-gp-dbid

--- src/test/isolation2/helpers/gp_management_utils_helpers.sql
+++ src/test/isolation2/helpers/gp_management_utils_helpers.sql
@@ -6,15 +6,20 @@ create or replace language plpythonu;
 --   port: port of the gpdb segment to back up
 --   slotname: desired slot name to create and associate with backup
 --   datadir: destination data directory of the backup
+--   forceoverwrite: overwrite the destination directory if it exists already
+--
 --
 -- usage: `select pg_basebackup('somehost', 12345, 'some_slot_name', '/some/destination/data/directory')`
 --
-create or replace function pg_basebackup(host text, dbid int, port int, slotname text, datadir text) returns text as $$
+create or replace function pg_basebackup(host text, dbid int, port int, slotname text, datadir text, force_overwrite boolean) returns text as $$
     import subprocess
     cmd = 'pg_basebackup -h %s -p %d --xlog-method stream -R -D %s --target-gp-dbid %d' % (host, port, datadir, dbid)
 
     if slotname is not None:
         cmd += ' --slot %s' % (slotname)
+
+    if force_overwrite:
+        cmd += ' --force-overwrite'
 
     try:
         results = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace('.', '')
