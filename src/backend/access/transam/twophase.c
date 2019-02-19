1d4dfbb77ab75500d63318c1e69b318d9fa4f93a
Paul Guo <paulguo@gmail.com>
2019-02-19 14:18:41 +0800

    Replace twophase_XLogPageRead() with read_local_xlog_page() for generic xlog page read for two phase code.
    logical_read_local_xlog_page() was renamed as read_local_xlog_page() in
    postgres commit 422a55a68784fd00f4514834f3649140a9166fa5 for generic xlog page
    read. We should use that for two phase state xlog reading given that comes from
    upstream and also it seems that twophase_XLogPageRead() is buggy also (e.g.  it
    does not consider cross segment file). We backport
    422a55a68784fd00f4514834f3649140a9166fa5 also because it is used by another
    patch that we will backport also (see below 2).
    
    This patch removes two FIXMEs also:
    
    1. Previously XLogCloseReadRecord() was needed to re-initialize for
       XLogPageRead() (typically after startup ends), but now we of course
       do not need that, so removing the comment below.
    
        * GPDB_93_MERGE_FIXME: GPDB used to do XLogCloseReadRecord() and then read,
        * do we need to perform something similar with new interface.
    
    2.  The FIXME below is deleted when we replace the
        function twophase_XLogPageRead(), but the comment is reasonable
        because it is possible that PREPARE xlog exists in an older segment
        file with previous timeline id even during mirror promotion some
        previous xlogs are copied when creating the new segment file for
        the new timeline id. We backport postgres patch 49e4196 to address this.
    
       // GPDB_93_MERGE_FIXME: Is ThisTimeLineID correct here? Do we need to
       // fetch prepare records for past timelines, after promotion?

--- src/backend/access/transam/twophase.c
+++ src/backend/access/transam/twophase.c
@@ -1261,84 +1261,6 @@ StandbyTransactionIdIsPrepared(TransactionId xid)
 #endif
 }
 
-
-typedef struct XLogPageReadPrivate
-{
-	int			readFile;
-	XLogSegNo	readSegNo;
-} XLogPageReadPrivate;
-
-/*
- * This is simpler than XLogPageRead: no retry logic is needed, as the WAL should be
- * present in pg_xlog.
- */
-static int
-twophase_XLogPageRead(XLogReaderState *xlogreader, XLogRecPtr targetPagePtr, int reqLen,
-					  XLogRecPtr targetRecPtr, char *readBuf, TimeLineID *readTLI)
-{
-	XLogPageReadPrivate *private =
-	(XLogPageReadPrivate *) xlogreader->private_data;
-	uint32		targetPageOff;
-	XLogSegNo targetSegNo PG_USED_FOR_ASSERTS_ONLY;
-	char		path[MAXPGPATH];
-
-	XLByteToSeg(targetPagePtr, targetSegNo);
-	targetPageOff = targetPagePtr % XLogSegSize;
-
-	/*
-	 * See if we need to switch to a new segment because the requested record
-	 * is not in the currently open one.
-	 */
-	if (private->readFile >= 0 && !XLByteInSeg(targetPagePtr, private->readSegNo))
-	{
-		elogif(debug_xlog_record_read, LOG,
-			   "twophase xlog page read -- Requested record %X/%X does not exist in"
-			   "current read xlog file (readsegno " UINT64_FORMAT ")",
-			   (uint32) (targetRecPtr >> 32), (uint32) targetRecPtr,
-			   private->readSegNo);
-
-		close(private->readFile);
-		private->readFile = -1;
-	}
-
-	XLByteToSeg(targetPagePtr, private->readSegNo);
-
-	elogif(debug_xlog_record_read, LOG,
-		   "twophase xlog page read -- Requested record %X/%X has "
-		   "targetsegno " UINT64_FORMAT ", targetpageoff %u",
-		   (uint32) (targetRecPtr >> 32), (uint32) targetRecPtr,
-		   private->readSegNo, targetPageOff);
-
-	if (private->readFile == -1)
-	{
-		// GPDB_93_MERGE_FIXME: Is ThisTimeLineID correct here? Do we need to
-		// fetch prepare records for past timelines, after promotion?
-
-		XLogFilePath(path, ThisTimeLineID, private->readSegNo);
-
-		private->readFile = BasicOpenFile(path, O_RDONLY | PG_BINARY, 0);
-		if (private->readFile < 0)
-			ereport(ERROR,
-					(errcode_for_file_access(),
-					 errmsg("could not open file \"%s\": %m", path)));
-	}
-
-	/* Read the requested page */
-	if (lseek(private->readFile, (off_t) targetPageOff, SEEK_SET) < 0)
-		ereport(ERROR,
-				(errcode_for_file_access(),
-				 errmsg("could not seek in log segment %s to offset %u: %m",
-						path, targetPageOff)));
-
-	if (read(private->readFile, readBuf, XLOG_BLCKSZ) != XLOG_BLCKSZ)
-		ereport(ERROR,
-				(errcode_for_file_access(),
-				 errmsg("could not read from log segment %s, offset %u: %m",
-						path, targetPageOff)));
-
-	return reqLen;
-}
-
 /*
  * FinishPreparedTransaction: execute COMMIT PREPARED or ROLLBACK PREPARED
  */
@@ -1361,7 +1283,6 @@ FinishPreparedTransaction(const char *gid, bool isCommit, bool raiseErrorIfNotFo
 	SharedInvalidationMessage *invalmsgs;
 
 	XLogReaderState *xlogreader;
-	struct XLogPageReadPrivate private;
 	char	   *errormsg;
     XLogRecPtr   tfXLogRecPtr;
     XLogRecord  *tfRecord  = NULL;
@@ -1407,18 +1328,12 @@ FinishPreparedTransaction(const char *gid, bool isCommit, bool raiseErrorIfNotFo
 
     /* get the two phase information from the xlog */
 	/*
-	 * GPDB_93_MERGE_FIXME: GPDB used to do XLogCloseReadRecord() and then read,
-	 * do we need to perform something similar with new interface.
-	 *
 	 * GPDB_93_MERGE_FIXME: It's a bit inefficient to allocate a new XLogReader
 	 * object for every call. We could avoid opening+closing every WAL segment,
 	 * if we kept the XLogReader object around. Or reused xlog.c's openLogFile
 	 * file descriptor here.
 	 */
-
-	private.readFile = -1;
-	private.readSegNo = 0;
-	xlogreader = XLogReaderAllocate(&twophase_XLogPageRead, &private);
+	xlogreader = XLogReaderAllocate(&read_local_xlog_page, NULL);
 
 	tfRecord = XLogReadRecord(xlogreader, tfXLogRecPtr, &errormsg);
 	if (tfRecord == NULL)
@@ -1547,8 +1462,6 @@ FinishPreparedTransaction(const char *gid, bool isCommit, bool raiseErrorIfNotFo
 	SIMPLE_FAULT_INJECTOR(FinishPreparedAfterRecordCommitPrepared);
 
 	XLogReaderFree(xlogreader);
-	if (private.readFile != -1)
-		close(private.readFile);
 
 	RESUME_INTERRUPTS();
 
@@ -1675,7 +1588,6 @@ PrescanPreparedTransactions(TransactionId **xids_p, int *nxids_p)
 	int			nxids = 0;
 	int			allocsize = 0;
 	XLogReaderState *xlogreader;
-	struct XLogPageReadPrivate private;
 	char	   *errormsg;
 
 	if (crashRecoverPostCheckpointPreparedTransactions_map_ht != NULL)
@@ -1688,9 +1600,7 @@ PrescanPreparedTransactions(TransactionId **xids_p, int *nxids_p)
 			tfXLogRecPtr = entry->xlogrecptr;
 	}
 
-	private.readFile = -1;
-	private.readSegNo = 0;
-	xlogreader = XLogReaderAllocate(&twophase_XLogPageRead, &private);
+	xlogreader = XLogReaderAllocate(&read_local_xlog_page, NULL);
 
 	while (tfXLogRecPtr != InvalidXLogRecPtr)
 	{
@@ -1787,8 +1697,6 @@ PrescanPreparedTransactions(TransactionId **xids_p, int *nxids_p)
 	}
 
 	XLogReaderFree(xlogreader);
-	if (private.readFile != -1)
-		close(private.readFile);
 
 	if (xids_p)
 	{
@@ -1857,12 +1765,9 @@ RecoverPreparedTransactions(void)
 	HASH_SEQ_STATUS hsStatus;
 	bool		overwriteOK = false;
 	XLogReaderState *xlogreader;
-	struct XLogPageReadPrivate private;
 	char	   *errormsg;
 
-	private.readFile = -1;
-	private.readSegNo = 0;
-	xlogreader = XLogReaderAllocate(&twophase_XLogPageRead, &private);
+	xlogreader = XLogReaderAllocate(&read_local_xlog_page, NULL);
 
 	if (crashRecoverPostCheckpointPreparedTransactions_map_ht != NULL)
 	{
@@ -1974,8 +1879,6 @@ RecoverPreparedTransactions(void)
 	}  /* end while (xlogrecptr = (XLogRecPtr *)hash_seq_search(&hsStatus)) */
 
 	XLogReaderFree(xlogreader);
-	if (private.readFile != -1)
-		close(private.readFile);
 }
 
 /*
