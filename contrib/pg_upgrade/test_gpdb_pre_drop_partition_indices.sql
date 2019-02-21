26417f4346aa45f6bb223b8b134b1be7a0e3ccfe
Taylor Vesely <tvesely@pivotal.io>
2019-02-21 12:58:36 -0800

    pg_upgrade: Drop all indexes from partition tables
    This commit is part of Add Partitioned Indexes #7047.
    
    Until indexes can be upgraded drop all indexes. Drop from the root
    partition instead of on intermediate/leaf partitions, since they now
    cascade due to their INTERNAL_AUTO dependencies.
    
    Co-authored-by: Kalen Krempely <kkrempely@pivotal.io>

--- contrib/pg_upgrade/test_gpdb_pre_drop_partition_indices.sql
+++ contrib/pg_upgrade/test_gpdb_pre_drop_partition_indices.sql
@@ -7,24 +7,36 @@ DECLARE
 BEGIN
        FOR part_indexes IN
        WITH partitions AS (
-           SELECT DISTINCT n.nspname,
-                  c.relname
-           FROM pg_catalog.pg_partition p
-                JOIN pg_catalog.pg_class c ON (p.parrelid = c.oid)
-                JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
-           UNION
-           SELECT n.nspname,
-                  partitiontablename AS relname
-           FROM pg_catalog.pg_partitions p
-                JOIN pg_catalog.pg_class c ON (p.partitiontablename = c.relname)
-                JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
-       )
-       SELECT nspname,
-              relname,
-              indexname
-       FROM partitions
-            JOIN pg_catalog.pg_indexes ON (relname = tablename
-                                                                               AND nspname = schemaname)
+          SELECT DISTINCT n.nspname,
+                 c.relname
+          FROM pg_catalog.pg_partition p
+               JOIN pg_catalog.pg_class c ON (p.parrelid = c.oid)
+               JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
+          UNION
+
+          SELECT n.nspname,
+                 partitiontablename AS relname
+          FROM pg_catalog.pg_partitions p
+                 JOIN pg_catalog.pg_class c ON (p.partitiontablename = c.relname)
+                 JOIN pg_catalog.pg_namespace n ON (n.oid = c.relnamespace)
+        )
+        SELECT nspname,
+               relname,
+               indexname
+        FROM partitions
+               JOIN (SELECT n.nspname AS schemaname,
+                            c.relname AS tablename,
+                            i.relname AS indexname
+                     FROM pg_index x
+                            JOIN pg_class c ON c.oid = x.indrelid
+                            JOIN pg_class i ON i.oid = x.indexrelid
+                            LEFT JOIN pg_inherits inh ON inh.inhrelid = x.indexrelid
+                            LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+                            LEFT JOIN pg_tablespace t ON t.oid = i.reltablespace
+                     WHERE (c.relkind = ANY (ARRAY ['r'::"char", 'm'::"char"]))
+                       AND i.relkind = 'i'::"char"
+                       AND inh.inhrelid is Null) inx
+                    ON (relname = inx.tablename AND nspname = inx.schemaname)
        LOOP
                EXECUTE 'DROP INDEX IF EXISTS ' || quote_ident(part_indexes.nspname) || '.' || quote_ident(part_indexes.indexname);
        END LOOP;
