e8781bc2ba1419dccfcd2ff970ac44be33eb9887
Jacob Champion <pchampion@pivotal.io>
2019-02-21 16:45:17 -0800

    gpcheckcat: fix repair for randomly distributed tables with invalid constraints
    The two separate queries made as part of the distribution_policy checks
    overlapped when the table in question was randomly distributed. This led
    to two copies of the same DROP CONSTRAINT statement in the repair
    scripts, which failed.
    
    To fix, we can remove the overlapping WHERE clause from the second
    query, and only focus on column-distributed tables there.
    
    Co-authored-by: Jimmy Yih <jyih@pivotal.io>

--- gpMgmt/test/behave/mgmt_utils/steps/data/gpcheckcat/create_invalid_constraint.sql
+++ gpMgmt/test/behave/mgmt_utils/steps/data/gpcheckcat/create_invalid_constraint.sql
@@ -1,3 +1,9 @@
 set allow_system_table_mods=true;
+
+-- Create a table with a distribution key that's a superset of its primary key
 create table foo(i int primary key);
 update pg_constraint set conkey='{}' where conname = 'foo_pkey';
+
+-- Force a table with unique constraints to have a random distribution
+create table bar(i int unique) distributed by (i);
+update gp_distribution_policy set distkey='', distclass='' where localoid='bar'::regclass;
