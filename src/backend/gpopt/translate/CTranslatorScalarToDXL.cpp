bhuvnesh2703/gpdb

Support MCV based cardinality estimation for text columns in ORCA
Prior to this patch, the MCVs for text columns was not being passed to ORCA.
Hence the cardinality estimation for predicates involving text was inaccurate
and led to sub optimal plans being picked. This patch allows the MCVs to be
passed in to ORCA so that it can now estimate the cardinality using MCVs equal
and not equal operators for text columns.

Co-authored-by: Abhijit Subramanya <asubramanya@pivotal.io>
Co-authored-by: Bhuvnesh Chaudhary <bchaudhary@pivotal.io>

src/backend/gpopt/translate/CTranslatorScalarToDXL.cpp
@@ -2462,14 +2462,30 @@ CTranslatorScalarToDXL::ExtractLintValueFromDatum
 		}
 		else
 		{
-			hash = gpos::HashValue<BYTE>(bytes);
-			for (ULONG ul = 1; ul < length; ul++)
+			ULONG true_length = length;
+			BYTE *arg = bytes;
+			/*
+			 * For text related types, only hash the actual data and exclude the
+			 * header and padded blanks
+			 */
+			if (mdid->Equals(&CMDIdGPDB::m_mdid_bpchar))
 			{
-				hash = gpos::CombineHashes(hash, gpos::HashValue<BYTE>(&bytes[ul]));
+				arg = (BYTE *)VARDATA_ANY(bytes);
+				true_length = gpdb::BpCharLen((Datum) bytes);
+			}
+			else if (mdid->Equals(&CMDIdGPDB::m_mdid_varchar) || mdid->Equals(&CMDIdGPDB::m_mdid_text))
+			{
+				arg = (BYTE *)VARDATA_ANY(bytes);
+				true_length -= ((char *)arg - (char *)bytes);
+			}
+			hash = gpos::HashValue<BYTE>(arg);
+			for (ULONG ul = 1; ul < true_length; ul++)
+			{
+				hash = gpos::CombineHashes(hash, gpos::HashValue<BYTE>(&arg[ul]));
 			}
 		}
 
-		lint_value = (LINT) (hash / 4);
+		lint_value = (LINT) hash;
 	}
 
 	return lint_value;

cloudberrydb/cloudberrydb

src/backend/gpopt/translate/CTranslatorScalarToDXL.cpp
@@ -2462,14 +2462,17 @@ CTranslatorScalarToDXL::ExtractLintValueFromDatum
 		}
 		else
 		{
-			hash = gpos::HashValue<BYTE>(bytes);
-			for (ULONG ul = 1; ul < length; ul++)
+			if (mdid->Equals(&CMDIdGPDB::m_mdid_bpchar))
 			{
-				hash = gpos::CombineHashes(hash, gpos::HashValue<BYTE>(&bytes[ul]));
+				hash = gpdb::HashBpChar((Datum) bytes);
+			}
+			else
+			{
+				hash = gpdb::HashText((Datum) bytes);
 			}
 		}
 
-		lint_value = (LINT) (hash / 4);
+		lint_value = (LINT) hash;
 	}
 
 	return lint_value;