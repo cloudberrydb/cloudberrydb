11529a7ed1d88f141dbdba306c320b77ec0b0bad
David Yozie <dyozie@pivotal.io>
2019-03-11 13:47:35 -0700

    Changing version in path from /600 to /6-0 for consistency with other docs and to recognize that only the minor version digit is significant for the doc build (#7141)

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/600/common/gpdb-features.html'
-r301 '/index.html', '/600/common/gpdb-features.html'
-r301 '/600/index.html', '/600/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/600/common/gpdb-features.html'
+r301 '/', '/6-0/common/gpdb-features.html'
+r301 '/index.html', '/6-0/common/gpdb-features.html'
+r301 '/6-0/index.html', '/6-0/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/6-0/common/gpdb-features.html'
