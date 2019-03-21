2c5dd23be43444ace345b4b18cf0c15dd5be18ee
Mark Sliva <msliva@pivotal.io>
2019-03-21 13:09:57 -0600

    tap tests: re-enable pg_isready test
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- src/bin/scripts/t/080_pg_isready.pl
+++ src/bin/scripts/t/080_pg_isready.pl
@@ -1,17 +1,13 @@
 use strict;
 use warnings;
 use TestLib;
-use Test::More tests => 9;
+use Test::More tests => 10;
 
 program_help_ok('pg_isready');
 program_version_ok('pg_isready');
 program_options_handling_ok('pg_isready');
 
-#
-# Disable temporarily. This test expects the server to not currently
-# be running which is not the case on Concourse.
-#
-#command_fails(['pg_isready'], 'fails with no server running');
+command_fails(['pg_isready'], 'fails with no server running');
 
 my $tempdir = tempdir;
 start_test_server $tempdir;
