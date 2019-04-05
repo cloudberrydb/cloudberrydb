2bf458766dfbe02328ec0ef93cf291399488f09c
Daniel Gustafsson <dgustafsson@pivotal.io>
2019-04-05 13:33:25 +0200

    Refactor old bugbuster metadata_track suite
    The metadata_track suite was originally part of cdbfast, dating back
    around 10-11 years. It was later moved into bugbuster around six years
    ago, already then with doubts as to what it actually did. After the
    open sourcing we scrapped bugbuster, moving anything worthwhile (or
    just not analyzed for usefulness yet) into the normal regress schedule
    which is where metadata_track remained till now. This all according to
    memory and the old proprietary issue tracker.
    
    Looking at metadata_track, it's entirely duplicative only issuing
    lots of DDL already tested elsewhere without verifying the results,
    most likely since it was originally testing the metadata tracking
    of the operations. Since the latter part is no longer happening,
    move parts of the test into the existing pg_stat_last_operation
    test and remove the rest, as the remaining value of this quite slow
    test (spending ~10-12 minutes serially in the pipeline) is highly
    debatable.
    
    The existing pg_stat_last_operation was, and I quote, "underwhelming"
    so most of it is replaced herein. There is still work to be done in
    order to boost metadata tracking test coverage, but this is at least
    a start.
    
    Reviewed-by: Jimmy Yih

--- src/test/regress/sql/pg_stat_last_operation.sql
+++ src/test/regress/sql/pg_stat_last_operation.sql
@@ -1,20 +1,102 @@
-CREATE TABLE PG_STAT_LAST_OPERATION_TEST (foo int) DISTRIBUTED BY (foo);
+CREATE SCHEMA mdt_test;
+SET search_path = mdt_test;
 
-SELECT classname, objname, schemaname, actionname, subtype 
-FROM pg_stat_operations WHERE schemaname = 'public' 
-AND objname = 'pg_stat_last_operation_test'
-AND actionname = 'CREATE';
+-- CREATE VIEW
+CREATE VIEW
+	pg_stat_last_operation_testview AS
+SELECT
+	c.relname AS objname, staactionname AS actionname, stasubtype AS subtype
+FROM
+	pg_stat_last_operation lo
+		JOIN pg_catalog.pg_class c ON (
+			c.oid = lo.objid
+		)
+		JOIN pg_catalog.pg_namespace nsp ON (
+			c.relnamespace = nsp.oid AND nsp.nspname = 'mdt_test'
+		)
+ORDER BY
+	statime;  
 
-SELECT classname, objname, schemaname, actionname, subtype 
-FROM pg_stat_operations WHERE schemaname = 'public' 
-AND objname = 'pg_stat_last_operation_test'
-AND actionname = 'TRUNCATE';
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'CREATE';
+ 
+-- CREATE TABLE
+CREATE TABLE pg_stat_last_operation_test (foo int) DISTRIBUTED BY (foo);
 
-insert into PG_STAT_LAST_OPERATION_TEST select generate_series(1,100);
+CREATE TABLE mdt_test_part1 (
+  id int,
+  rank int,
+  year date,
+  gender char(1))
+DISTRIBUTED BY (id, gender, year)
+PARTITION BY list (gender)
+SUBPARTITION BY range (year)
+SUBPARTITION TEMPLATE (start (date '2001-01-01'))
+(VALUES ('M'), VALUES ('F'));
+ALTER TABLE mdt_test_part1 ADD DEFAULT PARTITION default_part;
 
-truncate PG_STAT_LAST_OPERATION_TEST;
+CREATE TABLE mdt_all_types (
+  a int,
+  col001 char DEFAULT 'z',
+  col002 numeric,
+  col003 boolean DEFAULT false,
+  col004 bit(3) DEFAULT '111',
+  col005 text DEFAULT 'pookie',
+  col006 integer[] DEFAULT '{5, 4, 3, 2, 1}',
+  col007 character varying(512) DEFAULT 'Now is the time',
+  col008 character varying DEFAULT 'Now is the time',
+  col009 character varying(512)[],
+  col010 numeric(8),
+  col011 int,
+  col012 double precision,
+  col013 bigint,
+  col014 char(8),
+  col015 bytea,
+  col016 timestamp with time zone,
+  col017 interval,
+  col018 cidr,
+  col019 inet,
+  col020 macaddr,
+  col021 serial,
+  col022 money,
+  col023 bigserial,
+  col024 timetz,
+  col025 circle,
+  col026 box,
+  col027 name,
+  col028 path,
+  col029 int2,
+  col031 bit varying(256),
+  col032 date,
+  col034 lseg,
+  col035 point,
+  col036 polygon,
+  col037 real,
+  col039 time,
+  col040 timestamp
+)
+WITH (appendonly=true);
 
-SELECT classname, objname, schemaname, actionname, subtype 
-FROM pg_stat_operations WHERE schemaname = 'public' 
-AND objname = 'pg_stat_last_operation_test'
-AND actionname = 'TRUNCATE';
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'CREATE';
+
+-- GRANT
+GRANT ALL ON mdt_all_types TO public;
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'PRIVILEGE';
+
+-- VACUUM
+VACUUM ANALYZE mdt_all_types ;
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname in ('VACUUM','ANALYZE');
+SELECT * FROM pg_stat_last_operation_testview WHERE objname like ('mdt_all_%');
+
+-- TRUNCATE
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'TRUNCATE';
+INSERT INTO pg_stat_last_operation_test SELECT generate_series(1, 5);
+TRUNCATE pg_stat_last_operation_test;
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'TRUNCATE';
+
+-- DROP TABLE
+SELECT * FROM pg_stat_last_operation_testview WHERE actionname = 'DROP';
+SELECT * FROM pg_stat_last_operation_testview WHERE objname like ('mdt_%');
+DROP TABLE mdt_all_types;
+SELECT * FROM pg_stat_last_operation_testview WHERE objname like ('mdt_%');
+DROP TABLE mdt_test_part1;
+SELECT * FROM pg_stat_last_operation_testview WHERE objname like ('mdt_%');
