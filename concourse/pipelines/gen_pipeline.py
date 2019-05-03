8af57a90378ae56f5da41a122b83cd12d4a397a4
David Sharp <dsharp@pivotal.io>
2019-05-03 15:10:22 -0700

    concourse: Remove ubuntu16 build
    Co-authored-by: David Sharp <dsharp@pivotal.io>
    Co-authored-by: Goutam Tadi <gtadi@pivotal.io>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -66,20 +66,15 @@ JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE = (
         'icw_gporca_centos6_gpos_memory',
         'walrep_2',
         'compile_gpdb_sles11',
-        'compile_gpdb_ubuntu16',
         'icw_gporca_sles11',
         'icw_gporca_sles12',
         'icw_planner_sles12',
-        'icw_planner_ubuntu16',
-        'icw_gporca_conan_ubuntu16',
-        'gpdb_packaging_ubuntu16',
         'resource_group_sles12',
         'madlib_build_gppkg',
         'MADlib_Test_planner_centos6',
         'MADlib_Test_orca_centos6',
         'MADlib_Test_planner_centos7',
         'MADlib_Test_orca_centos7',
-        'icw_extensions_gpcloud_ubuntu16'
     ] + RELEASE_VALIDATOR_JOB + JOBS_THAT_ARE_GATES
 )
 
@@ -256,7 +251,7 @@ def main():
         action='store',
         dest='os_types',
         default=['centos6'],
-        choices=['centos6', 'centos7', 'sles', 'win', 'ubuntu16'],
+        choices=['centos6', 'centos7', 'sles', 'win'],
         nargs='+',
         help='List of OS values to support'
     )
@@ -311,7 +306,7 @@ def main():
     args = parser.parse_args()
 
     if args.pipeline_type == 'prod':
-        args.os_types = ['centos6', 'centos7', 'sles', 'win', 'ubuntu16']
+        args.os_types = ['centos6', 'centos7', 'sles', 'win']
         args.test_sections = [
             'ICW',
             'Replication',
