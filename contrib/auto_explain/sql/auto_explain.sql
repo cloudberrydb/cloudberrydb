1348afc0054e3ddfa3d85d3590cfb38941ad79ac
Hao Wu <37101401+gfphoenix78@users.noreply.github.com>
2019-05-06 10:02:51 +0800

    Fix auto explain init file
    Fix auto_explain init file for #7195
    
    This patch includes
    
    1. fix init_file
    2. use float number for memory usage
    3. Update the last SQL whose running time may be less than 1ms
    4. Update test case: use newly created table other than pg_class
    5. Add answer file for orca & enable nestloop

--- contrib/auto_explain/sql/auto_explain.sql
+++ contrib/auto_explain/sql/auto_explain.sql
@@ -1,3 +1,10 @@
+CREATE SCHEMA auto_explain_test;
+CREATE TABLE auto_explain_test.t1(a int);
+INSERT INTO auto_explain_test.t1 VALUES(generate_series(0, 1000));
+CREATE TABLE auto_explain_test.t2(b int);
+INSERT INTO auto_explain_test.t2 VALUES(generate_series(0, 1000));
+
+SET enable_nestloop = ON;
 SET CLIENT_MIN_MESSAGES = LOG;
 LOAD 'auto_explain';
 SET auto_explain.log_analyze = TRUE;
@@ -9,11 +16,18 @@ SET auto_explain.log_timing = FALSE;
 SET auto_explain.log_verbose = FALSE;
 
 SELECT relname FROM pg_class WHERE relname='pg_class';
-SELECT count(*)>0 FROM pg_class, pg_index WHERE oid = indrelid AND indisunique;
+SELECT count(*) FROM auto_explain_test.t1, auto_explain_test.t2;
 
 SET auto_explain.log_min_duration = 1;
 SET auto_explain.log_triggers = FALSE;
 SET auto_explain.log_verbose = TRUE;
 
+-- this select should not dump execution plan
 SELECT relname FROM pg_class WHERE relname='pg_class';
-SELECT count(*)>0 FROM pg_class, pg_index WHERE oid = indrelid AND indisunique;
+-- this select should also dump plan, since it takes too much time to run
+SELECT count(*) FROM auto_explain_test.t1, auto_explain_test.t2;
+
+-- clean jobs
+DROP TABLE auto_explain_test.t1;
+DROP TABLE auto_explain_test.t2;
+DROP SCHEMA auto_explain_test;
