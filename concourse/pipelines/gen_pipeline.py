1197ca01b150118089bf8713ea7346dc5a2c16e3
Bradford D. Boyle <bboyle@pivotal.io>
2019-06-26 11:24:01 -0700

    Update generated pipeline w/ Concourse unit tests
    Co-authored-by: Bradford D. Boyle <bboyle@pivotal.io>
    Co-authored-by: Mark Sliva <msliva@pivotal.io>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -71,6 +71,7 @@ JOBS_THAT_SHOULD_NOT_BLOCK_RELEASE = (
         'combine_cli_coverage',
         'compile_gpdb_binary_swap_centos6',
         'compile_gpdb_clients_windows',
+        'concourse_unit_tests',
         'test_gpdb_clients_windows',
         'icw_gporca_centos6_gpos_memory',
         'walrep_2',
