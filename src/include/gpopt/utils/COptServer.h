7c962aa6b14b5554dde659931c7aedff59e28029
Hans Zeller <hzeller@pivotal.io>
2019-07-16 16:02:03 -0700

    Bump ORCA version from 3.55 to 3.57 (#8149)
    * remove reference to deleted header file
    * Remove more references to CSpinlock
    * Bump ORCA version to 3.56 for PR503
    * Bump ORCA version to 3.57 for PR510

--- src/include/gpopt/utils/COptServer.h
+++ src/include/gpopt/utils/COptServer.h
@@ -23,7 +23,6 @@
 #include "gpos/common/CSyncHashtableAccessByIter.h"
 #include "gpos/common/CSyncHashtableIter.h"
 #include "gpos/net/CSocket.h"
-#include "gpos/sync/CSpinlock.h"
 #include "gpos/task/CTask.h"
 
 // forward declarations
@@ -91,18 +90,6 @@ namespace gpoptudfs
 				{}
 
 			};
-
-			typedef CSyncHashtable<SConnectionDescriptor, ULONG_PTR, CSpinlockOS>
-				ConnectionHT;
-
-			typedef CSyncHashtableAccessByKey<SConnectionDescriptor, ULONG_PTR, CSpinlockOS>
-				ConnectionKeyAccessor;
-
-			typedef CSyncHashtableIter<SConnectionDescriptor, ULONG_PTR, CSpinlockOS>
-				ConnectionIter;
-
-			typedef CSyncHashtableAccessByIter<SConnectionDescriptor, ULONG_PTR, CSpinlockOS>
-				ConnectionIterAccessor;
 
 			// path where socket is initialized
 			const CHAR *m_socket_path;
