f722631e8e120f4c2f14dfb1a3330287f8acbbb6
Daniel Gustafsson <dgustafsson@pivotal.io>
2019-07-29 11:25:27 +0200

    Fix duplicate function prototype
    It seems that the duplicate prototype snuck in during the 8.4 merge,
    which should've been mostly a noop as I had synced pgcrypto with 8.4
    in 16d26ee99eb. I had however placed the prototype on the wrong line
    and so git merge was able to cleanly place the protype a few lines
    down, creating a duplicate.

--- contrib/pgcrypto/px.h
+++ contrib/pgcrypto/px.h
@@ -199,7 +199,6 @@ const char *px_strerror(int err);
 const char *px_resolve_alias(const PX_Alias *aliases, const char *name);
 
 void		px_set_debug_handler(void (*handler) (const char *));
-void		px_memset(void *ptr, int c, size_t len);
 
 void		px_memset(void *ptr, int c, size_t len);
 
