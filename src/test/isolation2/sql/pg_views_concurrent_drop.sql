dd44426dcd4da5fe0ba352980c0280119eb8ef85
Paul Guo <paulguo@gmail.com>
2019-08-30 10:46:29 +0800

    Fix flaky test pg_views_concurrent_drop. (#8526)
    It should proceed only after the 'forked' connection is really blocking at the
    lock. & means blocking but the test framework has no way to really know the sql is blocking at lock waiting so test case should take the responsibility. 
    
    Reviewed-by: Asim R P <apraveen@pivotal.io>

--- src/test/isolation2/sql/pg_views_concurrent_drop.sql
+++ src/test/isolation2/sql/pg_views_concurrent_drop.sql
@@ -1,6 +1,27 @@
-1:drop view if exists mpp25160 cascade;
-1:create view mpp25160 as select * from pg_class;
-1:select viewname from pg_views where viewname = 'mpp25160';
+-- Helper function
+CREATE or REPLACE FUNCTION lock_wait_until_ungranted()
+RETURNS bool AS
+$$
+declare
+retries int; /* in func */
+begin /* in func */
+  retries := 1200; /* in func */
+  loop /* in func */
+    if (select not granted from pg_locks where granted='f' and relation='concurrent_drop_view'::regclass) then /* in func */
+      return true; /* in func */
+    end if; /* in func */
+    if retries <= 0 then /* in func */
+      return false; /* in func */
+    end if; /* in func */
+    perform pg_sleep(0.1); /* in func */
+    retries := retries - 1; /* in func */
+  end loop; /* in func */
+end; /* in func */
+$$ language plpgsql;
+
+1:drop view if exists concurrent_drop_view cascade;
+1:create view concurrent_drop_view as select * from pg_class;
+1:select viewname from pg_views where viewname = 'concurrent_drop_view';
 
 -- One transaction drops the view, and before it commits, another
 -- transaction selects its definition from pg_views. The view is still
@@ -11,7 +32,8 @@
 -- but getting a spurious ERROR when all you do is query pg_views is
 -- not nice.)
 1:begin;
-1:drop view mpp25160;
-2&:select viewname, definition from pg_views where viewname = 'mpp25160';
+1:drop view concurrent_drop_view;
+2&:select viewname, definition from pg_views where viewname = 'concurrent_drop_view';
+3:select lock_wait_until_ungranted();
 1:commit;
 2<:
