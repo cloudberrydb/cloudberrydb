3ff02a8f9bd2a5631413f0a47796192b397c70a7
Ashwin Agrawal <aagrawal@pivotal.io>
2019-09-09 10:21:08 -0700

    Avoid extra argument to pg_regress for row and column file generation
    If "./pg_regress --init-file init_file uao_ddl/compresstype_row" is
    executed, it fails. As extra argument `--ao-dir=uao` needs to be
    specified to pg_regress, to convey, to convert these .source files to
    row and column .sql/.out files and not use regular standard logic on
    them. This approach has following shortfalls:
    
    - additional developer overhead to remember to add that option
    - without that option .sql/.out files are still generated but are not
      usable / incorrect
    - row and column test files directories will always need same prefix
    - the check for ao-dir prefix was checked for each and every file in
      input/output directories, which is unnecessary
    
    To improve the situation, modifying the logic in pg_regress. No more
    need extra argument to pg_regress, instead, presence of special file
    named "GENERATE_ROW_AND_COLUMN_FILES" conveys to pg_regress to apply
    special conversion rule on that directory. This way always correct
    conversion rule is applied and developer don't need to specify any
    extra option.

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -105,7 +105,6 @@ static char *user = NULL;
 static _stringlist *extraroles = NULL;
 static char *config_auth_datadir = NULL;
 static char *initfile = NULL;
-static char *aodir = NULL;
 static bool  ignore_plans = false;
 
 /* internal variables */
@@ -795,15 +794,6 @@ convert_sourcefiles_in(char *source_subdir, char *dest_dir, char *dest_subdir, c
 		bool		has_tokens = false;
 		struct stat fst;
 
-		if (aodir && strncmp(*name, aodir, strlen(aodir)) == 0 &&
-			(strlen(*name) < 8 || strcmp(*name + strlen(*name) - 7, ".source") != 0))
-		{
-			snprintf(srcfile, MAXPGPATH, "%s/%s",  indir, *name);
-			snprintf(destfile, MAXPGPATH, "%s/%s", dest_subdir, *name);
-			count += generate_uao_sourcefiles(srcfile, destfile, suffix, &repls);
-			continue;
-		}
-
 		snprintf(srcfile, MAXPGPATH, "%s/%s",  indir, *name);
 		if (stat(srcfile, &fst) < 0)
 		{
@@ -815,9 +805,17 @@ convert_sourcefiles_in(char *source_subdir, char *dest_dir, char *dest_subdir, c
 		/* recurse if it's a directory */
 		if (S_ISDIR(fst.st_mode))
 		{
+			char generate_uao_file[MAXPGPATH];
+			snprintf(generate_uao_file, MAXPGPATH, "%s/%s",  srcfile, "GENERATE_ROW_AND_COLUMN_FILES");
+
 			snprintf(srcfile, MAXPGPATH, "%s/%s", source_subdir, *name);
 			snprintf(destfile, MAXPGPATH, "%s/%s", dest_subdir, *name);
-			count += convert_sourcefiles_in(srcfile, dest_dir, destfile, suffix);
+
+			if (access(generate_uao_file, F_OK) != -1)
+				count += generate_uao_sourcefiles(srcfile, destfile, suffix, &repls);
+			else
+				count += convert_sourcefiles_in(srcfile, dest_dir, destfile, suffix);
+
 			continue;
 		}
 
@@ -2573,8 +2571,6 @@ help(void)
 	/* Please put GPDB specific options at the end */
 	printf(_("  --exclude-tests=TEST      command or space delimited tests to exclude from running\n"));
     printf(_(" --init-file=GPD_INIT_FILE  init file to be used for gpdiff\n"));
-	printf(_("  --ao-dir=DIR              directory name prefix containing generic\n"));
-	printf(_("                            UAO row and column tests\n"));
 	printf(_("  --ignore-plans            ignore any explain plan diffs\n"));
 	printf(_("  --print-failure-diffs     Print the diff file to standard out after a failure\n"));
 	printf(_("  --tablespace-dir=DIR      place tablespace files in DIR/testtablespace (default \"./testtablespace\")\n"));
@@ -2623,7 +2619,6 @@ regression_main(int argc, char *argv[], init_function ifunc, test_function tfunc
 		{"load-extension", required_argument, NULL, 22},
 		{"config-auth", required_argument, NULL, 24},
 		{"init-file", required_argument, NULL, 25},
-		{"ao-dir", required_argument, NULL, 26},
 		{"exclude-tests", required_argument, NULL, 27},
 		{"ignore-plans", no_argument, NULL, 28},
 		{"prehook", required_argument, NULL, 29},
@@ -2746,18 +2741,15 @@ regression_main(int argc, char *argv[], init_function ifunc, test_function tfunc
                 initfile = strdup(optarg);
                 break;
             case 26:
-                aodir = strdup(optarg);
-                break;
-            case 27:
                 split_to_stringlist(strdup(optarg), ", ", &exclude_tests);
                 break;
-			case 28:
+			case 27:
 				ignore_plans = true;
 				break;
-			case 29:
+			case 28:
 				prehook = strdup(optarg);
 				break;
-			case 30:
+			case 29:
 				print_failure_diffs_is_enabled = true;
 				break;
 			case 80:
