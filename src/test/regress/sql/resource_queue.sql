52d156cbe9ef3190976bf45ac729825815cf718f
Heikki Linnakangas <hlinnakangas@pivotal.io>
2019-10-25 08:55:31 +0300

    Move resource queue tests from 'catalog' to 'resource_queue' test file.
    There used to be more stuff in 'catalog' before, but commit 62449ea4f9
    removed it, leaving only these queries on resource queue catalog views.
    The comment in greenplum_schedule about running pg_get_constraintdef()
    concurrently with other tests was also obsoleted by commit 62449ea4f9,
    which removed the test that it applied to.
    
    Reviewed-by: Ashwin Agrawal <aagrawal@pivotal.io>

--- src/test/regress/sql/resource_queue.sql
+++ src/test/regress/sql/resource_queue.sql
@@ -196,6 +196,37 @@ DROP USER reg_u2;
 DROP RESOURCE QUEUE reg_activeq;
 DROP RESOURCE QUEUE reg_costq;
 
+-- catalog tests
+set optimizer_enable_master_only_queries = on;
+select count(*)/1000 from
+(select
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='priority') as "Priority",
+(select count(*) from pg_resqueue x,pg_roles y
+where x.oid=y.rolresqueue and a.rsqname=x.rsqname) as "RQAssignedUsers"
+from ( select distinct rsqname from pg_resqueue_attributes ) a)
+as foo;
+
+select count(*)/1000 from
+(select a.rsqname as "RQname",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='active_statements') as "ActiveStatment",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='max_cost') as "MaxCost",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='min_cost') as "MinCost",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='cost_overcommit') as "CostOvercommit",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='memory_limit') as "MemoryLimit",
+(select ressetting from pg_resqueue_attributes b
+where a.rsqname=b.rsqname and resname='priority') as "Priority",
+(select count(*) from pg_resqueue x,pg_roles y
+where x.oid=y.rolresqueue and a.rsqname=x.rsqname) as "RQAssignedUsers"
+from ( select distinct rsqname from pg_resqueue_attributes ) a)
+as foo;
+
+reset optimizer_enable_master_only_queries;
 
 -- Followup additional tests.
 -- MPP-7474
