48a962091bf029616ccff45ae37b44d280d29a4c
Heikki Linnakangas <hlinnakangas@pivotal.io>
2019-11-04 11:18:00 +0200

    Fix DNS lookups of IPv6 addresses in URLs of web external tables.
    The code to look up an IP address based on a domain name checked
    '!hostname', but 'hostname' is a local array, so the condition was never
    true. When compiling with -Waddress, GCC gave a warning about it:
    
    cdbutil.c: In function ‘getDnsCachedAddress’:
    cdbutil.c:1239:23: warning: the address of ‘hostinfo’ will always evaluate as ‘true’ [-Waddress]
       if (((!use_cache && !hostinfo) || (use_cache && e == NULL))
                           ^
    
    You don't see this normally, because we set -Wno-address in configure.in,
    but it sure looks wrong.
    
    I haven't been able to find a test case where this would cause a
    user-visible failure. I think you'd need a domain name that resolves to
    something that's not an IPv4 address, and use that domain in an external
    table's URL.

--- src/backend/cdb/cdbutil.c
+++ src/backend/cdb/cdbutil.c
@@ -1198,6 +1198,7 @@ getDnsCachedAddress(char *name, int port, int elevel, bool use_cache)
 		if (use_cache)
 			oldContext = MemoryContextSwitchTo(TopMemoryContext);
 
+		hostinfo[0] = '\0';
 		for (addr = addrs; addr; addr = addr->ai_next)
 		{
 #ifdef HAVE_UNIX_SOCKETS
@@ -1235,7 +1236,7 @@ getDnsCachedAddress(char *name, int port, int elevel, bool use_cache)
 		 * we'd only want to use the IPv6 address if there isn't an IPv4
 		 * address.  All we really need to do is test this.
 		 */
-		if (((!use_cache && !hostinfo) || (use_cache && e == NULL))
+		if (((!use_cache && !hostinfo[0]) || (use_cache && e == NULL))
 			&& addrs->ai_family == AF_INET6)
 		{
 			char		hostinfo[NI_MAXHOST];
