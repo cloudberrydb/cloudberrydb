429cf84a6ad7db594bd04c6620ec4587499380b8
dyozie <dyozie@pivotal.io>
2019-11-05 09:55:06 -0800

    Docs - updating build, headers in master to reflect next major release version

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/6-0/common/gpdb-features.html'
-r301 '/index.html', '/6-0/common/gpdb-features.html'
-r301 '/6-0/index.html', '/6-0/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/6-0/common/gpdb-features.html'
+r301 '/', '/7-0/common/gpdb-features.html'
+r301 '/index.html', '/7-0/common/gpdb-features.html'
+r301 '/7-0/index.html', '/7-0/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/7-0/common/gpdb-features.html'
