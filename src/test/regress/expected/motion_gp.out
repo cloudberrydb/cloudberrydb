cloudberrydb/cloudberrydb

commit 774613a83c7d7693c5442ce2134148f30fb56379

Revert "When serializing a tuple for Motion, don't decompress compressed datums."
This reverts commit 4c7f6cf7ec49eda70988e2552b523bdfab0ac850.

Thank you Ekta for finding this simple repro that demonstrates the
problem with this patch and Jesse for initial analysis:

   CREATE TABLE foo(a text, b text);
   INSERT INTO foo SELECT repeat('123456789', 100000)::text as a,
                          repeat('123456789', 10)::text as b;
   SELECT * FROM foo;

The motion receiver has no idea whether a datum it received is
compressed or not, because the varlena header is stripped off before
sending the data.  Heikki and I discussed two options to fix this:

1. Include varlena header when sending.  This incurs at the most 8-byte
overhead per variable length datum in a heap tuple.

2. Always send tuples as MemTuples.  This is more desirable because it
simplifies code, but also comes with performance cost.

Let's evaluate the two options based on performance and then commit the
best one.

src/backend/cdb/motion/tupser.c
@@ -16,7 +16,6 @@
 #include "postgres.h"
 
 #include "access/htup.h"
-#include "access/tuptoaster.h"
 #include "catalog/pg_type.h"
 #include "cdb/cdbmotion.h"
 #include "cdb/cdbsrlz.h"
@@ -656,17 +655,9 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 					 * avoid memory leakage: we want to force the detoast
 					 * allocation(s) to happen in our reset-able serialization
 					 * context.
-					 *
-					 * Only detoast, don't decompress. It saves network bandwidth
-					 * to let the receiver decompress it. Furthermore, it's possible
-					 * that the receiver doesn't need to decompress it at all, which
-					 * can be a very big win.
 					 */
 					oldCtxt = MemoryContextSwitchTo(s_tupSerMemCtxt);
-					if (VARATT_IS_EXTERNAL(origattr))
-						attr = PointerGetDatum(heap_tuple_fetch_attr((struct varlena *) DatumGetPointer(origattr)));
-					else
-						attr = origattr;
+					attr = PointerGetDatum(PG_DETOAST_DATUM_PACKED(origattr));
 					MemoryContextSwitchTo(oldCtxt);
 
 					sz = VARSIZE_ANY_EXHDR(attr);

commit a693c8896dd51410fda318e967b7fe637be20d1c

Remove Motion codepath to detoast HeapTuples, convert to MemTuple instead.
The Motion sender code has four different codepaths for serializing a
tuple from the input slot:

1. Fetch MemTuple from slot, copy it out as it is.

2. Fetch MemTuple from slot, re-format it into a new MemTuple by fetching
   and inlining any toasted datums. Copy out the re-formatted MemTuple.

3. Fetch HeapTuple from slot, copy it out as it is.

4. Fetch HeapTuple from slot, copy out each attribute separately, fetching
   and inlining any toasted datums.

In addition to the above, there are "direct" versions of codepaths 1 and 3,
used when the tuple fits in the caller-provided output buffer.

As discussed in https://github.com/greenplum-db/gpdb/issues/9253, the
fourth codepath is very inefficient, if the input tuple contains datums
that are compressed inline, but not toasted. We decompress such tuples
before serializing, and in the worst case, might need to recompress them
again in the receiver if it's written out to a table. I tried to fix that
in commit 4c7f6cf7ec, but it was broken and was reverted in commit
774613a83c.

This is a new attempt at fixing the issue. This commit removes codepath 4.
altogether, so that if the input tuple is a HeapTuple with any toasted
attributes, it is first converted to a MemTuple and codepath 2 is used
to serialize it. That way, we have less code to test, and materializing a
MemTuple is roughly as fast as the old code to write out the attributes
of a HeapTuple one by one, except that the MemTuple codepath avoids the
decompression of already-compressed datums.

While we're at it, add some tests for the various codepaths through
SerializeTuple().

To test the performance of the affected case, where the input tuple is
a HeapTuple with toasted datums, I used this:

---
CREATE temporary TABLE foo (a text, b text, c text, d text, e text, f text,
  g text, h text, i text, j text, k text, l text, m text, n text, o text,

src/backend/cdb/motion/tupser.c
@@ -399,15 +399,11 @@ CandidateForSerializeDirect(int16 targetRoute, struct directTransportBuffer *b)
 int
 SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTransportBuffer *b, TupleChunkList tcList, int16 targetRoute)
 {
-	int			i;
 	int			natts;
 	int			dataSize = TUPLE_CHUNK_HEADER_SIZE;
-	MemoryContext oldCtxt;
 	TupleDesc	tupdesc;
 	TupleChunkListItem tcItem = NULL;
-	GenericTuple gtuple = ExecFetchSlotGenericTuple(slot);
 
-	AssertArg(gtuple != NULL);
 	AssertArg(pSerInfo != NULL);
 	AssertArg(b != NULL);
 
@@ -429,14 +425,24 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 	tcList->serialized_data_length = 0;
 	tcList->max_chunk_length = Gp_max_tuple_chunk_size;
 
-	if (is_memtuple(gtuple)) /* memtuple case */
+	if (slot->PRIVATE_tts_heaptuple == NULL ||
+		(slot->PRIVATE_tts_heaptuple->t_data->t_infomask & HEAP_HASEXTERNAL) != 0)
 	{
-		MemTuple	tuple = (MemTuple) gtuple;
+		/*
+		 * Virtual or MemTuple slot, or a HeapTuple with toasted datums.
+		 * Send it as a MemTuple.
+		 */
+		MemTuple	tuple;
 		int			tupleSize;
 		int			paddedSize;
-		bool need_toast = memtuple_get_hasext(tuple);
 
-		if (need_toast)
+		if (slot->PRIVATE_tts_memtuple &&
+			!memtuple_get_hasext(slot->PRIVATE_tts_memtuple))
+		{
+			/* we can use the existing MemTuple as it is. */
+			tuple = slot->PRIVATE_tts_memtuple;
+		}
+		else
 		{
 			MemoryContext oldContext;
 			oldContext = MemoryContextSwitchTo(s_tupSerMemCtxt);
@@ -486,17 +492,25 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 
 		MemoryContextReset(s_tupSerMemCtxt);
 	}
-	else /* heaptuple case */
+	else
 	{
-		HeapTuple	tuple = (HeapTuple) gtuple;
+		/* HeapTuple that doesn't require detoasting */
+		HeapTuple	tuple = slot->PRIVATE_tts_heaptuple;
 		TupSerHeader tsh;
-
 		unsigned int datalen;
 		unsigned int nullslen;
 
 		HeapTupleHeader t_data = tuple->t_data;
 
-		unsigned char *pos;
+		datalen = tuple->t_len - t_data->t_hoff;
+		if (HeapTupleHasNulls(tuple))
+			nullslen = BITMAPLEN(HeapTupleHeaderGetNatts(t_data));
+		else
+			nullslen = 0;
+
+		tsh.tuplen = sizeof(TupSerHeader) + TYPEALIGN(TUPLE_CHUNK_ALIGN, nullslen) + datalen;
+		tsh.natts = HeapTupleHeaderGetNatts(t_data);
+		tsh.infomask = t_data->t_infomask;
 
 		if (CandidateForSerializeDirect(targetRoute, b))
 		{
@@ -504,20 +518,10 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 			 * Here we first try to in-line serialize the tuple directly into
 			 * buffer.
 			 */
-			datalen = tuple->t_len - t_data->t_hoff;
-			if (HeapTupleHasNulls(tuple))
-				nullslen = BITMAPLEN(HeapTupleHeaderGetNatts(t_data));
-			else
-				nullslen = 0;
-
-			tsh.tuplen = sizeof(TupSerHeader) + TYPEALIGN(TUPLE_CHUNK_ALIGN, nullslen) + TYPEALIGN(TUPLE_CHUNK_ALIGN, datalen);
-			tsh.natts = HeapTupleHeaderGetNatts(t_data);
-			tsh.infomask = t_data->t_infomask;
-
-
-			if (dataSize + tsh.tuplen <= b->prilen &&
-				(tsh.infomask & HEAP_HASEXTERNAL) == 0)
+			if (dataSize + tsh.tuplen <= b->prilen)
 			{
+				unsigned char *pos;
+
 				pos = b->pri + TUPLE_CHUNK_HEADER_SIZE;
 
 				memcpy(pos, (char *) &tsh, sizeof(TupSerHeader));
@@ -540,7 +544,6 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 
 				SetChunkType(b->pri, TC_WHOLE);
 				SetChunkDataSize(b->pri, dataSize - TUPLE_CHUNK_HEADER_SIZE);
-
 				return dataSize;
 			}
 		}
@@ -556,128 +559,16 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 
 		AssertState(s_tupSerMemCtxt != NULL);
 
-		datalen = tuple->t_len - t_data->t_hoff;
-		if (HeapTupleHasNulls(tuple))
-			nullslen = BITMAPLEN(HeapTupleHeaderGetNatts(t_data));
-		else
-			nullslen = 0;
-
-		tsh.tuplen = sizeof(TupSerHeader) + TYPEALIGN(TUPLE_CHUNK_ALIGN, nullslen) + datalen;
-		tsh.natts = HeapTupleHeaderGetNatts(t_data);
-		tsh.infomask = t_data->t_infomask;
-
 		addByteStringToChunkList(tcList, (char *) &tsh, sizeof(TupSerHeader), &pSerInfo->chunkCache);
 
-		/*
-		 * If we don't have any attributes which have been toasted, we can be
-		 * very very simple: just send the raw data.
-		 */
-		if ((tsh.infomask & HEAP_HASEXTERNAL) == 0)
+		if (nullslen)
 		{
-			if (nullslen)
-			{
-				addByteStringToChunkList(tcList, (char *) t_data->t_bits, nullslen, &pSerInfo->chunkCache);
-				addPadding(tcList, &pSerInfo->chunkCache, nullslen);
-			}
-
-			addByteStringToChunkList(tcList, (char *) t_data + t_data->t_hoff, datalen, &pSerInfo->chunkCache);
-			addPadding(tcList, &pSerInfo->chunkCache, datalen);
+			addByteStringToChunkList(tcList, (char *) t_data->t_bits, nullslen, &pSerInfo->chunkCache);
+			addPadding(tcList, &pSerInfo->chunkCache, nullslen);
 		}
-		else
-		{
-			/*
-			 * We have to be more careful when we have tuples that have been
-			 * toasted. Ideally we'd like to send the untoasted attributes in
-			 * as "raw" a format as possible but that makes rebuilding the
-			 * tuple harder .
-			 */
-			oldCtxt = MemoryContextSwitchTo(s_tupSerMemCtxt);
 
-			/* deconstruct the tuple (faster than a heap_getattr loop) */
-			heap_deform_tuple(tuple, tupdesc, pSerInfo->values, pSerInfo->nulls);
-
-			MemoryContextSwitchTo(oldCtxt);
-
-			/* Send the nulls character-array. */
-			addByteStringToChunkList(tcList, pSerInfo->nulls, natts, &pSerInfo->chunkCache);
-			addPadding(tcList, &pSerInfo->chunkCache, natts);
-
-			/*
-			 * send the attributes of this tuple: NOTE anything which
-			 * allocates temporary space (e.g. could result in a
-			 * PG_DETOAST_DATUM) should be executed with the memory context
-			 * set to s_tupSerMemCtxt
-			 */
-			for (i = 0; i < natts; ++i)
-			{
-				SerAttrInfo *attrInfo = pSerInfo->myinfo + i;
-				Datum		origattr = pSerInfo->values[i],
-							attr;
-
-				/* skip null attributes (already taken care of above) */
-				if (pSerInfo->nulls[i])
-					continue;
-
-				if (attrInfo->typlen == -1)
-				{
-					int32		sz;
-					char	   *data;
-
-					/*
-					 * If we have a toasted datum, forcibly detoast it here to
-					 * avoid memory leakage: we want to force the detoast
-					 * allocation(s) to happen in our reset-able serialization
-					 * context.
-					 */
-					oldCtxt = MemoryContextSwitchTo(s_tupSerMemCtxt);
-					attr = PointerGetDatum(PG_DETOAST_DATUM_PACKED(origattr));
-					MemoryContextSwitchTo(oldCtxt);
-
-					sz = VARSIZE_ANY_EXHDR(attr);
-					data = VARDATA_ANY(attr);
-
-					/* Send length first, then data */
-					addInt32ToChunkList(tcList, sz, &pSerInfo->chunkCache);
-					addByteStringToChunkList(tcList, data, sz, &pSerInfo->chunkCache);
-					addPadding(tcList, &pSerInfo->chunkCache, sz);
-				}
-				else if (attrInfo->typlen == -2)
-				{
-					int32		sz;
-					char	   *data;
-
-					/*
-					 * CString, we would send the string with the terminating
-					 * '\0'
-					 */
-					data = DatumGetCString(origattr);
-					sz = strlen(data) + 1;
-
-					/* Send length first, then data */
-					addInt32ToChunkList(tcList, sz, &pSerInfo->chunkCache);
-					addByteStringToChunkList(tcList, data, sz, &pSerInfo->chunkCache);
-					addPadding(tcList, &pSerInfo->chunkCache, sz);
-				}
-				else if (attrInfo->typbyval)
-				{
-					/*
-					 * We send a full-width Datum for all pass-by-value types,
-					 * regardless of the actual size.
-					 */
-					addByteStringToChunkList(tcList, (char *) &origattr, sizeof(Datum), &pSerInfo->chunkCache);
-					addPadding(tcList, &pSerInfo->chunkCache, sizeof(Datum));
-				}
-				else
-				{
-					addByteStringToChunkList(tcList, DatumGetPointer(origattr), attrInfo->typlen, &pSerInfo->chunkCache);
-					addPadding(tcList, &pSerInfo->chunkCache, attrInfo->typlen);
-
-					attr = origattr;
-				}
-			}
-
-			MemoryContextReset(s_tupSerMemCtxt);
-		}
+		addByteStringToChunkList(tcList, (char *) t_data + t_data->t_hoff, datalen, &pSerInfo->chunkCache);
+		addPadding(tcList, &pSerInfo->chunkCache, datalen);
 	}
 
 	/*
@@ -709,122 +600,6 @@ SerializeTuple(TupleTableSlot *slot, SerTupInfo *pSerInfo, struct directTranspor
 	 * performed "out-of-line" serialization
 	 */
 	return 0;
-}
-
-/*
- * Deserialize a HeapTuple's data from a byte-array.
- *
- * This code is based on the binary input handling functions in copy.c.
- */
-HeapTuple
-DeserializeTuple(SerTupInfo *pSerInfo, StringInfo serialTup)
-{
-	MemoryContext oldCtxt;
-	TupleDesc	tupdesc;
-	HeapTuple	htup;
-	int			natts;
-	SerAttrInfo *attrInfo;
-	int			i;
-
-	AssertArg(pSerInfo != NULL);
-	AssertArg(serialTup != NULL);
-
-	tupdesc = pSerInfo->tupdesc;
-	natts = tupdesc->natts;
-
-	/*
-	 * Flip to our tuple-serialization memory-context, to speed up memory
-	 * reclamation operations.
-	 */
-	AssertState(s_tupSerMemCtxt != NULL);
-	oldCtxt = MemoryContextSwitchTo(s_tupSerMemCtxt);
-
-	/* Receive nulls character-array. */
-	pq_copymsgbytes(serialTup, pSerInfo->nulls, natts);
-	skipPadding(serialTup);
-
-	/* Deserialize the non-NULL attributes of this tuple */
-	for (i = 0; i < natts; ++i)
-	{
-		attrInfo = pSerInfo->myinfo + i;
-
-		if (pSerInfo->nulls[i]) /* NULL field. */
-		{
-			pSerInfo->values[i] = (Datum) 0;
-			continue;
-		}
-
-		if (attrInfo->typlen == -1)
-		{
-			int32		sz;
-			struct varlena *p;
-
-			/* Read length first */
-			pq_copymsgbytes(serialTup, (char *) &sz, sizeof(int32));
-			if (sz < 0)
-				elog(ERROR, "invalid length received for a varlen Datum");
-
-			p = palloc(sz + VARHDRSZ);
-
-			pq_copymsgbytes(serialTup, VARDATA(p), sz);
-			SET_VARSIZE(p, sz + VARHDRSZ);
-
-			pSerInfo->values[i] = PointerGetDatum(p);
-		}
-		else if (attrInfo->typlen == -2)
-		{
-			int32		sz;
-			char	   *p;
-
-			/* CString, with terminating '\0' included */
-
-			/* Read length first */
-			pq_copymsgbytes(serialTup, (char *) &sz, sizeof(int32));
-			if (sz < 0)
-				elog(ERROR, "invalid length received for a CString");
-
-			p = palloc(sz);
-
-			/* Then data */
-			pq_copymsgbytes(serialTup, p, sz);
-
-			pSerInfo->values[i] = CStringGetDatum(p);
-		}
-		else if (attrInfo->typbyval)
-		{
-			/* Read a whole Datum */
-
-			pq_copymsgbytes(serialTup, (char *) &(pSerInfo->values[i]), sizeof(Datum));
-		}
-		else
-		{
-			/* fixed width, pass-by-ref */
-			char	   *p = palloc(attrInfo->typlen);
-
-			pq_copymsgbytes(serialTup, p, attrInfo->typlen);
-
-			pSerInfo->values[i] = PointerGetDatum(p);
-		}
-	}
-
-	/*
-	 * Construct the tuple from the Datums and nulls values.  NOTE:  Switch
-	 * out of our temporary context before we form the tuple!
-	 */
-	MemoryContextSwitchTo(oldCtxt);
-
-	htup = heap_form_tuple(tupdesc, pSerInfo->values, pSerInfo->nulls);
-
-	MemoryContextReset(s_tupSerMemCtxt);
-
-	/* Trouble if it didn't eat the whole buffer */
-	if (serialTup->cursor != serialTup->len)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_BINARY_REPRESENTATION),
-				 errmsg("incorrect binary data format")));
-
-	/* All done.  Return the result. */
-	return htup;
 }
 
 /*
@@ -1002,20 +777,9 @@ CvtChunksToTup(TupleChunkList tcList, SerTupInfo *pSerInfo, TupleRemapper *remap
 			pos += sizeof(TupSerHeader);
 
 			/*
-			 * if the tuple had toasted elements we have to deserialize the
-			 * old slow way.
+			 * Tuples with toasted elements should've been converted to MemTuples.
 			 */
-			if ((tshp->infomask & HEAP_HASEXTERNAL) != 0)
-			{
-				serData.cursor += sizeof(TupSerHeader);
-
-				tup = (GenericTuple) DeserializeTuple(pSerInfo, &serData);
-
-				/* Free up memory we used. */
-				if (serDataMustFree)
-					pfree(serData.data);
-				return tup;
-			}
+			Assert((tshp->infomask & HEAP_HASEXTERNAL) == 0);
 
 			/* reconstruct lengths of null bitmap and data part */
 			if (tshp->infomask & HEAP_HASNULL)

src/include/cdb/tupser.h
@@ -81,14 +81,11 @@ extern void SerializeRecordCacheIntoChunks(SerTupInfo *pSerInfo,
 										   TupleChunkList tcList,
 										   MotionConn *conn);
 
-/* Convert a HeapTuple into chunks directly in a set of transport buffers */
+/* Convert a tuple into chunks directly in a set of transport buffers */
 extern int SerializeTuple(TupleTableSlot *tuple, SerTupInfo *pSerInfo, struct directTransportBuffer *b, TupleChunkList tcList, int16 targetRoute);
 
-/* Deserialize a HeapTuple's data from a byte-array. */
-extern HeapTuple DeserializeTuple(SerTupInfo * pSerInfo, StringInfo serialTup);
-
 /* Convert a sequence of chunks containing serialized tuple data into a
- * HeapTuple.
+ * HeapTuple or MemTuple.
  */
 extern GenericTuple CvtChunksToTup(TupleChunkList tclist, SerTupInfo * pSerInfo, TupleRemapper *remapper);
 

src/test/regress/expected/motion_gp.out
@@ -1,0 +1,133 @@
+CREATE TABLE motiondata (
+  id int,
+  plain text,     -- inline, uncompressed
+  main text,      --inline, compressible
+  external text,  -- external, uncompressed
+  extended text); -- external, compressible
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'id' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+ALTER TABLE motiondata ALTER COLUMN plain SET STORAGE plain;
+ALTER TABLE motiondata ALTER COLUMN main SET STORAGE main;
+ALTER TABLE motiondata ALTER COLUMN external SET STORAGE external;
+ALTER TABLE motiondata ALTER COLUMN extended SET STORAGE extended;
+-- a simple small tuple.
+INSERT INTO motiondata (id, plain, main, external, extended) VALUES (1, 'foo', 'bar', 'baz', 'foobar');
+-- Large datum, inline uncompressed
+INSERT INTO motiondata (id, plain) VALUES (2, repeat('1234567890', 1000));
+-- Large datum, inline compressed
+INSERT INTO motiondata (id, main)  VALUES (3, repeat('1234567890', 1000));
+-- Large datum, inline compressed, but doesn't fit in a short varlen even
+-- after compression
+INSERT INTO motiondata (id, main)  VALUES (4, repeat('1234567890', 2000));
+-- Large datum, external uncompressed
+INSERT INTO motiondata (id, external) VALUES (5, repeat('1234567890', 1000));
+-- Large datum, external, compressed
+INSERT INTO motiondata (id, extended) VALUES (6, repeat('1234567890', 100000));
+-- Check that the sizes are as expected. The exact sizes are not important,
+-- but should be in the right ballpark.
+SELECT id,
+       pg_column_size(plain) AS plain_sz,
+       pg_column_size(main) AS main_sz,
+       pg_column_size(external) AS external_sz,
+       pg_column_size(extended) AS extended_sz
+FROM motiondata;
+ id | plain_sz | main_sz | external_sz | extended_sz 
+----+----------+---------+-------------+-------------
+  1 |        4 |       4 |           4 |           7
+  2 |    10004 |         |             |            
+  3 |          |     135 |             |            
+  4 |          |     251 |             |            
+  5 |          |         |       10000 |            
+  6 |          |         |             |       11463
+(6 rows)
+
+CREATE TABLE motiondata_ao WITH (appendonly=true) AS SELECT * from motiondata;
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column(s) named 'id' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+-- A helper function for printing an abbreviated version of a long datum. Otherwise, the output
+-- of selecting the large datums become unwieldly.
+create or replace function abbreviate(t text) returns text as $$
+begin
+  if length(t) <= 15 then
+    return length(t) || ': ' || t;
+  else
+    return length(t) || ': ' || substring(t from 1 for 5) || '...' || substring(t from length(t)-4 for 5);
+  end if;
+end;
+$$ language plpgsql;
+-- Runs query 'sql', and prints an abbreviated result set.
+--
+-- Note: It's important that we run the query as it is, and abbreviate the
+-- resulting values afterwards. If we put the abbreviate() function calls into
+-- the query itself, then they will be evaluated in the QEs, and the original
+-- large datums are not sent through the Motion at all. Since we're trying to
+-- test the Motion tuple serialization code, that would defeat the purpose.
+create or replace function abbreviate_result(sql text) returns setof motiondata
+as $$
+declare
+  rec motiondata%rowtype;
+begin
+  for rec in EXECUTE sql
+  loop
+     rec.plain = abbreviate(rec.plain);
+     rec.main = abbreviate(rec.main);
+     rec.external = abbreviate(rec.external);
+     rec.extended = abbreviate(rec.extended);
+     RETURN NEXT rec ;
+  end loop;
+end;
+$$ language plpgsql;
+-- This exercises the codepath where the HeapTuple is sent over the wire as is, if it
+-- doesn't contain toasted datums.
+select * from abbreviate_result($$
+  select id, plain, main, external, extended from motiondata
+$$) order by id;
+ id |        plain         |         main         |       external       |        extended        
+----+----------------------+----------------------+----------------------+------------------------
+  1 | 3: foo               | 3: bar               | 3: baz               | 6: foobar
+  2 | 10000: 12345...67890 |                      |                      | 
+  3 |                      | 10000: 12345...67890 |                      | 
+  4 |                      | 20000: 12345...67890 |                      | 
+  5 |                      |                      | 10000: 12345...67890 | 
+  6 |                      |                      |                      | 1000000: 12345...67890
+(6 rows)
+
+-- Because of the 'id + 0' expression, the Seq Scan node has to project,
+-- and passes a virtual slot to the Motion.
+select * from abbreviate_result($$
+  select id + 0 as id, plain, main, external, extended from motiondata
+$$) order by id;
+ id |        plain         |         main         |       external       |        extended        
+----+----------------------+----------------------+----------------------+------------------------
+  1 | 3: foo               | 3: bar               | 3: baz               | 6: foobar
+  2 | 10000: 12345...67890 |                      |                      | 
+  3 |                      | 10000: 12345...67890 |                      | 
+  4 |                      | 20000: 12345...67890 |                      | 
+  5 |                      |                      | 10000: 12345...67890 | 
+  6 |                      |                      |                      | 1000000: 12345...67890
+(6 rows)
+
+-- Here, the Motion gets an already-built MemTuple from the Seq Scan, because it's
+-- an AO table.
+select * from abbreviate_result($$
+  select id, plain, main, external, extended from motiondata_ao
+$$) order by id;
+ id |        plain         |         main         |       external       |        extended        
+----+----------------------+----------------------+----------------------+------------------------
+  1 | 3: foo               | 3: bar               | 3: baz               | 6: foobar
+  2 | 10000: 12345...67890 |                      |                      | 
+  3 |                      | 10000: 12345...67890 |                      | 
+  4 |                      | 20000: 12345...67890 |                      | 
+  5 |                      |                      | 10000: 12345...67890 | 
+  6 |                      |                      |                      | 1000000: 12345...67890
+(6 rows)
+
+-- Test with a table with zero columns. Motion tuple serialization has a special
+-- codepath for zero-attribute tuples.
+CREATE TABLE motion_noatts ();
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause, and no column type is suitable for a distribution key. Creating a NULL policy entry.
+INSERT INTO motion_noatts SELECT;
+SELECT * FROM motion_noatts;
+--
+(1 row)
+

src/test/regress/greenplum_schedule
@@ -55,7 +55,7 @@ test: gp_connections
 # bitmap_index triggers recovery, run it seperately
 test: bitmap_index
 test: gp_dump_query_oids analyze gp_owner_permission incremental_analyze
-test: indexjoin as_alias regex_gp gpparams with_clause transient_types gp_rules dispatch_encoding
+test: indexjoin as_alias regex_gp gpparams with_clause transient_types gp_rules dispatch_encoding motion_gp
 # dispatch should always run seperately from other cases.
 test: dispatch
 

src/test/regress/sql/motion_gp.sql
@@ -1,0 +1,100 @@
+CREATE TABLE motiondata (
+  id int,
+  plain text,     -- inline, uncompressed
+  main text,      --inline, compressible
+  external text,  -- external, uncompressed
+  extended text); -- external, compressible
+
+ALTER TABLE motiondata ALTER COLUMN plain SET STORAGE plain;
+ALTER TABLE motiondata ALTER COLUMN main SET STORAGE main;
+ALTER TABLE motiondata ALTER COLUMN external SET STORAGE external;
+ALTER TABLE motiondata ALTER COLUMN extended SET STORAGE extended;
+
+-- a simple small tuple.
+INSERT INTO motiondata (id, plain, main, external, extended) VALUES (1, 'foo', 'bar', 'baz', 'foobar');
+
+-- Large datum, inline uncompressed
+INSERT INTO motiondata (id, plain) VALUES (2, repeat('1234567890', 1000));
+
+-- Large datum, inline compressed
+INSERT INTO motiondata (id, main)  VALUES (3, repeat('1234567890', 1000));
+
+-- Large datum, inline compressed, but doesn't fit in a short varlen even
+-- after compression
+INSERT INTO motiondata (id, main)  VALUES (4, repeat('1234567890', 2000));
+
+-- Large datum, external uncompressed
+INSERT INTO motiondata (id, external) VALUES (5, repeat('1234567890', 1000));
+
+-- Large datum, external, compressed
+INSERT INTO motiondata (id, extended) VALUES (6, repeat('1234567890', 100000));
+
+-- Check that the sizes are as expected. The exact sizes are not important,
+-- but should be in the right ballpark.
+SELECT id,
+       pg_column_size(plain) AS plain_sz,
+       pg_column_size(main) AS main_sz,
+       pg_column_size(external) AS external_sz,
+       pg_column_size(extended) AS extended_sz
+FROM motiondata;
+
+CREATE TABLE motiondata_ao WITH (appendonly=true) AS SELECT * from motiondata;
+
+-- A helper function for printing an abbreviated version of a long datum. Otherwise, the output
+-- of selecting the large datums become unwieldly.
+create or replace function abbreviate(t text) returns text as $$
+begin
+  if length(t) <= 15 then
+    return length(t) || ': ' || t;
+  else
+    return length(t) || ': ' || substring(t from 1 for 5) || '...' || substring(t from length(t)-4 for 5);
+  end if;
+end;
+$$ language plpgsql;
+
+-- Runs query 'sql', and prints an abbreviated result set.
+--
+-- Note: It's important that we run the query as it is, and abbreviate the
+-- resulting values afterwards. If we put the abbreviate() function calls into
+-- the query itself, then they will be evaluated in the QEs, and the original
+-- large datums are not sent through the Motion at all. Since we're trying to
+-- test the Motion tuple serialization code, that would defeat the purpose.
+create or replace function abbreviate_result(sql text) returns setof motiondata
+as $$
+declare
+  rec motiondata%rowtype;
+begin
+  for rec in EXECUTE sql
+  loop
+     rec.plain = abbreviate(rec.plain);
+     rec.main = abbreviate(rec.main);
+     rec.external = abbreviate(rec.external);
+     rec.extended = abbreviate(rec.extended);
+     RETURN NEXT rec ;
+  end loop;
+end;
+$$ language plpgsql;
+
+-- This exercises the codepath where the HeapTuple is sent over the wire as is, if it
+-- doesn't contain toasted datums.
+select * from abbreviate_result($$
+  select id, plain, main, external, extended from motiondata
+$$) order by id;
+
+-- Because of the 'id + 0' expression, the Seq Scan node has to project,
+-- and passes a virtual slot to the Motion.
+select * from abbreviate_result($$
+  select id + 0 as id, plain, main, external, extended from motiondata
+$$) order by id;
+
+-- Here, the Motion gets an already-built MemTuple from the Seq Scan, because it's
+-- an AO table.
+select * from abbreviate_result($$
+  select id, plain, main, external, extended from motiondata_ao
+$$) order by id;
+
+-- Test with a table with zero columns. Motion tuple serialization has a special
+-- codepath for zero-attribute tuples.
+CREATE TABLE motion_noatts ();
+INSERT INTO motion_noatts SELECT;
+SELECT * FROM motion_noatts;