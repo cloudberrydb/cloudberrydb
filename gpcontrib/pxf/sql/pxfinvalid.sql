3b1ada53a0e3b1702e9a64debf1cd7cbb2a9c360
Oliver Albertini <oalbertini@pivotal.io>
2020-01-30 15:16:58 -0800

    Remove gpcontrib/pxf, rename {,gp}contrib/pxf_fdw
    Now that we are moving to FDW, we can remove the old external
    table-based PXF module. Since PXF FDW only currently supports Greenplum
    (not stand-alone Postgres) `pxf_fdw` should live under `gpcontrib`.
    
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- gpcontrib/pxf/sql/pxfinvalid.sql
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------------------
--- PXF invalid test
-------------------------------------------------------------------
-
-CREATE EXTERNAL TABLE pxf_invalid_test (a TEXT, b TEXT, c TEXT)
-LOCATION ('pxf://tmp/dummy1?FRAGMENTER=DemoFragmenter&ACCESSOR=&RESOLVER=DemoTextResolver')
-FORMAT 'TEXT' (DELIMITER ',');
--- gpcontrib/pxf/test/mock/pxfheaders_mock.c
+++
@@ -1,7 +0,0 @@
-/* mock implementation for pxfheaders.h */
-void
-build_http_headers(PxfInputData* input)
-{
-    check_expected(input);
-    mock();
-}
