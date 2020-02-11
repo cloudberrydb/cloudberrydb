2b62f99a386661ee81674d432d41e0b4c6b9dfe7
Chris Hajas <chajas@pivotal.io>
2020-02-11 14:34:20 -0800

    Remove unused files
    These are mdps, xmls and some random test files that aren't used and
    take up quite a bit of space.
    
    Additionally, this removes some scripts that aren't used.
    
    This reduces the directory size from 107MB to 84MB.
    
    Authored-by: Chris Hajas <chajas@pivotal.io>

--- server/src/unittest/gpopt/minidump/CICGTest.cpp
+++ server/src/unittest/gpopt/minidump/CICGTest.cpp
@@ -61,8 +61,6 @@ const CHAR *rgszFileNames[] =
 		"../data/dxl/minidump/ConstraintIntervalWithMultiColumnsNotIncludeNull.mdp",
 
 #ifndef GPOS_DEBUG
-		// TODO:  - Jul 14 2015; disabling it for debug build to reduce testing time
-		// "../data/dxl/minidump/HAWQ-TPCH09-NoTopBroadcast.mdp",
 		"../data/dxl/minidump/HAWQ-TPCH-Stat-Derivation.mdp",
 		"../data/dxl/minidump/HJN-Redistribute-One-Side.mdp",
 		"../data/dxl/minidump/TPCH-Q5.mdp",
