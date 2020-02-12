18d2b6fbbe91dab623a2716a92def5c26e3b0d13
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2020-02-12 17:21:16 -0600

    Update Makefiles to build ORCA using GNU make
    ORCA previously was built into a shared library using cmake. Now, we
    build ORCA into the postgres binary.

---
+++ src/backend/gporca/gporca.mk
@@ -0,0 +1,4 @@
+override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpos/include $(CPPFLAGS)
+override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpopt/include $(CPPFLAGS)
+override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libnaucrates/include $(CPPFLAGS)
+override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
