d51ecabe63f1e09435f458b091ee814a48fc4992
Jesse Zhang <sbjesse@gmail.com>
2020-04-14 15:48:34 -0700

    Use CXXFLAGS instead of CPPFLAGS for -std and -W* .
    Semantically, those are CXXFLAGS, not CPP flags (which typically deal
    with -D and -I stuff). Practically, this becomes a problem when we try
    to turn off certain warnings because CPPFLAGS come after CXXFLAGS and
    the order sensitivity of -Werror and -Wno-* flags won't mix well.

--- src/backend/gporca/gporca.mk
+++ src/backend/gporca/gporca.mk
@@ -4,5 +4,6 @@ override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libnaucrates/include $
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
 # Do not omit frame pointer. Even with RELEASE builds, it is used for
 # backtracing.
-override CPPFLAGS := -Werror -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CPPFLAGS)
-override CPPFLAGS := -std=gnu++98 $(CPPFLAGS)
+override CXXFLAGS := -Werror -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CXXFLAGS)
+# FIXME: this really should be done in autoconf
+override CXXFLAGS := -std=gnu++98 $(CXXFLAGS)
