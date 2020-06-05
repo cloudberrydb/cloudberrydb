cloudberrydb/cloudberrydb

commit bf36fb3b6926d8b8ad51c94f6b3e8c920331f35e

Squash me: address concerns in code review
Remember if the select call was interrupted.  Act on it after emitting
debug logs and checking cancel requests from dispatcher.

src/backend/cdb/motion/ic_tcp.c
@@ -1331,6 +1331,7 @@ SetupTCPInterconnect(EState *estate)
 		int			highsock = -1;
 		uint64		timeout_ms = 20 * 60 * 1000;
 		int			outgoing_fail_count = 0;
+		bool		select_errno;
 
 		iteration++;
 
@@ -1558,14 +1559,7 @@ SetupTCPInterconnect(EState *estate)
 
 		ML_CHECK_FOR_INTERRUPTS(interconnect_context->teardownActive);
 		n = select(highsock + 1, (fd_set *) &rset, (fd_set *) &wset, (fd_set *) &eset, &timeout);
-		if (n < 0)
-		{
-			if (errno == EINTR)
-				continue;
-			ereport(ERROR,
-					(errcode(ERRCODE_GP_INTERCONNECTION_ERROR),
-					 errmsg("interconnect error in select: %m")));
-		}
+		select_errno = errno;
 
 		ML_CHECK_FOR_INTERRUPTS(interconnect_context->teardownActive);
 		if (Gp_role == GP_ROLE_DISPATCH)
@@ -1583,7 +1577,6 @@ SetupTCPInterconnect(EState *estate)
 			{
 				int			elevel = (n == expectedTotalIncoming + expectedTotalOutgoing)
 				? DEBUG1 : LOG;
-				int			errnoSave = errno;
 
 				initStringInfo(&logbuf);
 				if (n > 0)
@@ -1599,8 +1592,18 @@ SetupTCPInterconnect(EState *estate)
 										elapsed_ms, logbuf.data)));
 				pfree(logbuf.data);
 				MemSet(&logbuf, 0, sizeof(logbuf));
-				errno = errnoSave;
 			}
+		}
+
+		/* An error other than EINTR is not acceptable */
+		if (n < 0)
+		{
+			if (select_errno == EINTR)
+				continue;
+			ereport(ERROR,
+					(errcode(ERRCODE_GP_INTERCONNECTION_ERROR),
+					 errmsg("interconnect error in select: %s",
+							strerror(select_errno))));
 		}
 
 		/*

commit a251127b175facc2ed1d6780ec8410ded75d2f52

Fix wrong data type introduced in bf36fb3b69

src/backend/cdb/motion/ic_tcp.c
@@ -1331,7 +1331,7 @@ SetupTCPInterconnect(EState *estate)
 		int			highsock = -1;
 		uint64		timeout_ms = 20 * 60 * 1000;
 		int			outgoing_fail_count = 0;
-		bool		select_errno;
+		int			select_errno;
 
 		iteration++;
 