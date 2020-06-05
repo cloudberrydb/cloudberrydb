1ee9998bb67a02a75ab17f4b2ac46d8fe2c02083
Hubert Zhang <hzhang@pivotal.io>
2020-06-05 08:57:10 +0800

    Fix flaky test in recoverseg_from_file
    1. after stop primary with content=1, we should check promotion
    status with 1U:
    2. after manually update dbid, we should trigger a fts probe and
    wait for the mirror promotion as well.
    
    Reviewed-by: Paul Guo <pguo@pivotal.io>

--- src/test/isolation2/sql/segwalrep/recoverseg_from_file.sql
+++ src/test/isolation2/sql/segwalrep/recoverseg_from_file.sql
@@ -34,21 +34,30 @@ where c.role='p' and c.content=1), 'stop');
 -- trigger failover
 select gp_request_fts_probe_scan();
 
+-- wait for content 1 (earlier mirror, now primary) to finish the promotion
+1U: select 1;
+-- Quit this utility mode session, as need to start fresh one below
+1Uq:
+
+-- make the dbid in gp_segment_configuration not continuous
+-- dbid=2 corresponds to content 0 and role p, change it to dbid=9
+set allow_system_table_mods to true;
+update gp_segment_configuration set dbid=9 where content=0 and role='p';
+
+-- trigger failover
+select gp_request_fts_probe_scan();
+
 -- wait for content 0 (earlier mirror, now primary) to finish the promotion
 0U: select 1;
 -- Quit this utility mode session, as need to start fresh one below
 0Uq:
-
--- make the dbid in gp_segment_configuration not continuous
-set allow_system_table_mods to true;
-update gp_segment_configuration set dbid=9 where dbid=2;
 
 -- generate recover config file
 select generate_recover_config_file(
 	(select datadir from gp_segment_configuration c where c.role='m' and c.content=1),
 	(select port from gp_segment_configuration c where c.role='m' and c.content=1)::text);
 
--- recover from config file
+-- recover from config file, only seg with content=1 will be recovered
 !\retcode gprecoverseg -a -i /tmp/recover_config_file;
 
 -- after gprecoverseg -i, the down segemnt should be up
@@ -62,13 +71,16 @@ select dbid from gp_segment_configuration where dbid=2;
 update gp_segment_configuration set dbid=2 where dbid=9;
 set allow_system_table_mods to false;
 
--- we manually change dbid from 2 to 9, which casue the
+-- we manually change dbid from 2 to 9, which causes the
 -- corresponding segment down as well, so recovery full
 -- at here
-!\retcode gprecoverseg -aF;
+!\retcode gprecoverseg -a;
 
 -- rebalance the cluster
 !\retcode gprecoverseg -ar;
+
+-- recheck gp_segment_configuration after rebalance
+SELECT dbid, role, preferred_role, content, mode, status FROM gp_segment_configuration order by dbid;
 
 -- remove the config file
 !\retcode rm /tmp/recover_config_file
