a3f34ae77937b2370682988a8d3c985a4665d019
Hao Wu <gfphoenix78@gmail.com>
2020-06-18 15:28:29 +0800

    Fix a flaky test for gdd/dist-deadlock-upsert (#10302)
    * Fix a flaky test for gdd/dist-deadlock-upsert
    
    When to run GDD probe is undermined, but it is import for the test
    gdd/dist-deadlock-upsert. If the GDD probe runs immediately after
    the 2 inter-dead-locked transactions, one of the transactions will
    be killed. The isolation2 framework consider the transaction being
    blocked if the transaction doesn't finished in 0.5 second. So, if
    the killed transaction is too early to be aborted, the test framework
    sees no dead lock.
    
    Analyzed-by: Gang Xiong <gxiong@pivotal.io>
    
    * rm sleep

--- src/test/isolation2/sql/gdd/dist-deadlock-upsert.sql
+++ src/test/isolation2/sql/gdd/dist-deadlock-upsert.sql
@@ -19,12 +19,18 @@ INSERT INTO t_upsert (id, val) SELECT i, i FROM generate_series(1, 100) i;
 
 20: INSERT INTO t_upsert VALUES (segid(1,1), segid(1,1)) on conflict (id, val) do update set val = 888;
 
+select gp_inject_fault('gdd_probe', 'suspend', dbid)
+  from gp_segment_configuration where content=-1 and role='p';
+select gp_wait_until_triggered_fault('gdd_probe', 1, dbid)
+  from gp_segment_configuration where content=-1 and role='p';
 -- seg 0: con20 ==> con10, xid lock
 20&: INSERT INTO t_upsert VALUES (segid(0,1), segid(0,1)) on conflict (id, val) do update set val = 666;
 
 -- seg 1: con10 ~~> con20, tuple lock
 10&: INSERT INTO t_upsert VALUES (segid(1,1), segid(1,1)) on conflict (id, val) do update set val = 777;
 
+select gp_inject_fault('gdd_probe', 'reset', dbid)
+  from gp_segment_configuration where content=-1 and role='p';
 -- con20 will be cancelled by gdd
 20<:
 20q:
