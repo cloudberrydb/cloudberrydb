0c9d63e1987a2f112a8f3497bd17b85b418f3194
Bradford D. Boyle <bradfordb@vmware.com>
2020-06-19 11:13:06 -0700

    Remove AIX logic from generate-greenplum-path.sh
    [#173046174]
    
    Authored-by: Bradford D. Boyle <bradfordb@vmware.com>
    (cherry picked from commit 28bcd4447551b3b9790e23d7865dbf403f79ef36)

--- gpMgmt/bin/generate-greenplum-path.sh
+++ gpMgmt/bin/generate-greenplum-path.sh
@@ -15,27 +15,6 @@ cat <<"EOF"
 PYTHONPATH="${GPHOME}/lib/python"
 PATH="${GPHOME}/bin:${PATH}"
 LD_LIBRARY_PATH="${GPHOME}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-EOF
-
-PLAT=`uname -s`
-if [ $? -ne 0 ] ; then
-    echo "Error executing uname -s"
-    exit 1
-fi
-
-# AIX uses yet another library path variable
-# Also, Python on AIX requires special copies of some libraries.  Hence, lib/pware.
-if [ "${PLAT}" = "AIX" ]; then
-cat <<"EOF"
-PYTHONPATH="${GPHOME}/ext/python/lib/python2.7:${PYTHONPATH}"
-LIBPATH="${GPHOME}/lib/pware:${GPHOME}/lib:${GPHOME}/ext/python/lib:/usr/lib/threads:${LIBPATH}"
-export LIBPATH
-GP_LIBPATH_FOR_PYTHON="${GPHOME}/lib/pware"
-export GP_LIBPATH_FOR_PYTHON
-EOF
-fi
-
-cat <<"EOF"
 
 if [ -e "${GPHOME}/etc/openssl.cnf" ]; then
 	OPENSSL_CONF="${GPHOME}/etc/openssl.cnf"
