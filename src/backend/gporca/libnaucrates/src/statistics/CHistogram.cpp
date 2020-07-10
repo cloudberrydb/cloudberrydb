ashuka24/gpdb

[Refactor] Pull out KHeap into CKHeap.h
Pull out the implementation for binary heap into its own templated h
file.

src/backend/gporca/libnaucrates/src/statistics/CHistogram.cpp
@@ -1698,7 +1698,7 @@ CHistogram::CombineBuckets
 
 	CBucketArray *result_buckets = GPOS_NEW(mp) CBucketArray(mp);
 	ULONG bucketsToCombine = buckets->Size() - desired_num_buckets;
-	KHeap<SBoundaryArray, SBoundary>* ratios = GPOS_NEW(mp) KHeap<SBoundaryArray, SBoundary> (mp, bucketsToCombine);
+	CKHeap<SBoundaryArray, SBoundary>* ratios = GPOS_NEW(mp) CKHeap<SBoundaryArray, SBoundary> (mp, bucketsToCombine);
 
 	for (ULONG ul = 0; ul < buckets->Size() - 1; ++ul)
 	{

cloudberrydb/cloudberrydb

src/backend/gporca/libnaucrates/src/statistics/CHistogram.cpp
@@ -1742,7 +1742,7 @@ CHistogram::CombineBuckets
 
 	CBucketArray *result_buckets = GPOS_NEW(mp) CBucketArray(mp);
 	ULONG bucketsToCombine = buckets->Size() - desired_num_buckets;
-	KHeap<SAdjBucketBoundaryArray, SAdjBucketBoundary>* boundary_factors = GPOS_NEW(mp) CKHeap<SAdjBucketBoundaryArray, SAdjBucketBoundary> (mp, bucketsToCombine);
+	CKHeap<SAdjBucketBoundaryArray, SAdjBucketBoundary>* boundary_factors = GPOS_NEW(mp) CKHeap<SAdjBucketBoundaryArray, SAdjBucketBoundary> (mp, bucketsToCombine);
 
 	// loop over the bucket boundaries and determine how much information
 	// we would lose by eliminating them