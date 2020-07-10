pivotal-ning-yu/gpdb

ic-proxy: implement the core logic
The interconnect proxy mode, a.k.a. ic-proxy, is a new interconnect
mode, all the backends communicate via a proxy bgworker, all the
backends on the same segment share the same proxy bgworker, so every two
segments only need one network connection between them, which reduces
the network flows as well the ports.

To enable the proxy mode we need to first configure the guc
gp_interconnect_proxy_addresses, for example:

    gpconfig \
      -c gp_interconnect_proxy_addresses \
      -v "'1:-1:10.0.0.1:2000,2:0:10.0.0.2:2001,3:1:10.0.0.3:2002'" \
      --skipvalidation

Then restart to take effect.

src/backend/cdb/motion/ic_proxy_key.h
@@ -1,0 +1,63 @@
+/*-------------------------------------------------------------------------
+ *
+ * ic_proxy_key.h
+ *	  TODO file description
+ *
+ *
+ * Copyright (c) 2020-Present Pivotal Software, Inc.
+ *
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef IC_PROXY_KEY_H
+#define IC_PROXY_KEY_H
+
+#include "postgres.h"
+
+typedef struct ICProxyKey ICProxyKey;
+
+/* we have to include it after the declaration of ICProxyKey */
+#include "ic_proxy_packet.h"
+
+/*
+ * XXX: sessionId and {local,remote}ContentId are only for debugging purpose,
+ * they are not actually part of the key.
+ *
+ * dbid and segindex are both defined as int32 in type GpId, however we define
+ * them as int16 to reduce the size of ICProxyKey, we also mark dbids as
+ * unsigned in hope that the compiler could help to catch the errors if we pass
+ * them in the wrong order.
+ */
+struct ICProxyKey
+{
+	int16		localContentId;
+	uint16		localDbid;
+	int32		localPid;
+
+	int16		remoteContentId;
+	uint16		remoteDbid;
+	int32		remotePid;
+
+	int32		sessionId;
+	uint32		commandId;
+	int16		sendSliceIndex;
+	int16		recvSliceIndex;
+};
+
+extern uint32 ic_proxy_key_hash(const ICProxyKey *key, Size keysize);
+extern bool ic_proxy_key_equal(const ICProxyKey *key1, const ICProxyKey *key2);
+extern int ic_proxy_key_equal_for_hash(const ICProxyKey *key1,
+									   const ICProxyKey *key2,
+									   Size keysize);
+extern void ic_proxy_key_from_pid(ICProxyKey *key,
+								  int32 sessionId, uint32 commandId,
+								  int16 sendSliceIndex, int16 recvSliceIndex,
+								  int16 localContentId, uint16 localDbid, int32 localPid,
+								  int16 remoteContentId, uint16 remoteDbid, int32 remotePid);
+extern void ic_proxy_key_from_p2c_pkt(ICProxyKey *key, const ICProxyPkt *pkt);
+extern void ic_proxy_key_from_c2p_pkt(ICProxyKey *key, const ICProxyPkt *pkt);
+extern void ic_proxy_key_reverse(ICProxyKey *key);
+extern const char *ic_proxy_key_to_str(const ICProxyKey *key);
+
+#endif   /* IC_PROXY_KEY_H */

cloudberrydb/cloudberrydb

src/backend/cdb/motion/ic_proxy_key.h
@@ -1,0 +1,62 @@
+/*-------------------------------------------------------------------------
+ *
+ * ic_proxy_key.h
+ *
+ *
+ * Copyright (c) 2020-Present Pivotal Software, Inc.
+ *
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef IC_PROXY_KEY_H
+#define IC_PROXY_KEY_H
+
+#include "postgres.h"
+
+typedef struct ICProxyKey ICProxyKey;
+
+/* we have to include it after the declaration of ICProxyKey */
+#include "ic_proxy_packet.h"
+
+/*
+ * XXX: sessionId and {local,remote}ContentId are only for debugging purpose,
+ * they are not actually part of the key.
+ *
+ * dbid and segindex are both defined as int32 in type GpId, however we define
+ * them as int16 to reduce the size of ICProxyKey, we also mark dbids as
+ * unsigned in hope that the compiler could help to catch the errors if we pass
+ * them in the wrong order.
+ */
+struct ICProxyKey
+{
+	int16		localContentId;
+	uint16		localDbid;
+	int32		localPid;
+
+	int16		remoteContentId;
+	uint16		remoteDbid;
+	int32		remotePid;
+
+	int32		sessionId;
+	uint32		commandId;
+	int16		sendSliceIndex;
+	int16		recvSliceIndex;
+};
+
+extern uint32 ic_proxy_key_hash(const ICProxyKey *key, Size keysize);
+extern bool ic_proxy_key_equal(const ICProxyKey *key1, const ICProxyKey *key2);
+extern int ic_proxy_key_equal_for_hash(const ICProxyKey *key1,
+									   const ICProxyKey *key2,
+									   Size keysize);
+extern void ic_proxy_key_init(ICProxyKey *key,
+							  int32 sessionId, uint32 commandId,
+							  int16 sendSliceIndex, int16 recvSliceIndex,
+							  int16 localContentId, uint16 localDbid, int32 localPid,
+							  int16 remoteContentId, uint16 remoteDbid, int32 remotePid);
+extern void ic_proxy_key_from_p2c_pkt(ICProxyKey *key, const ICProxyPkt *pkt);
+extern void ic_proxy_key_from_c2p_pkt(ICProxyKey *key, const ICProxyPkt *pkt);
+extern void ic_proxy_key_reverse(ICProxyKey *key);
+extern const char *ic_proxy_key_to_str(const ICProxyKey *key);
+
+#endif   /* IC_PROXY_KEY_H */