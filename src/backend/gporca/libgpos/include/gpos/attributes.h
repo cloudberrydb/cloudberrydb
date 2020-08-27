26ae898a151de55868138806657d2279666c7adb
Jesse Zhang <sbjesse@gmail.com>
2020-08-27 09:34:18 -0700

    Add GPOS_UNUSED attribute
    While we're at it, also add another attribute
    GPOS_ASSERTS_ONLY. This should help us eliminate a lot of
    clutter around the code that looks like this:
    
        BOOL result =
                m_cte_consumer_info->Insert(key, GPOS_NEW(m_mp) SCTEConsumerInfo(cte_plan));

--- src/backend/gporca/libgpos/include/gpos/attributes.h
+++ src/backend/gporca/libgpos/include/gpos/attributes.h
@@ -10,4 +10,16 @@
 
 #define GPOS_ATTRIBUTE_PRINTF(f, a) __attribute__((format(GPOS_FORMAT_ARCHETYPE, f, a)))
 
+#ifdef __GNUC__
+#define GPOS_UNUSED __attribute__((unused))
+#else
+#define GPOS_UNUSED
+#endif
+
+#ifndef GPOS_DEBUG
+#define GPOS_ASSERTS_ONLY GPOS_UNUSED
+#else
+#define GPOS_ASSERTS_ONLY
+#endif
+
 #endif // !GPOS_attributes_H
