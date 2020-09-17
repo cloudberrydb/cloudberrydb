3c673965015c20e19411740bf0490e9aef862b30
Jesse Zhang <sbjesse@gmail.com>
2020-09-17 11:18:54 -0700

    Insert new line to separate preprocessor directive
    Otherwise it seems clang-format recognizes this as a multi-line
    preprocessor directive, strange, but preventable.

--- src/backend/gporca/libgpos/include/gpos/common/CSyncPool.h
+++ src/backend/gporca/libgpos/include/gpos/common/CSyncPool.h
@@ -238,6 +238,7 @@ namespace gpos
     #ifdef GPOS_DEBUG
                             BOOL recycled =
     #endif // GPOS_DEBUG
+
                             // unset the reserve bit - must succeed
                             UnsetBit(&m_objs_reserved[elem_offset], bit_val);
 
