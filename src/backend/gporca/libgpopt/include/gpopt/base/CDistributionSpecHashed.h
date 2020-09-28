dgkimura/gpdb

Allow distribution spec match when hash spec uses gp_segment_id
This allows Orca to perform a GROUP BY over gp_segment_id without
requiring a redistribute motion.

```
EXPLAIN (COSTS OFF) SELECT count(*), gp_segment_id FROM motiondata
ROUP BY gp_segment_id;
                QUERY PLAN
------------------------------------------
 Gather Motion 3:1  (slice1; segments: 3)
   ->  GroupAggregate
         Group Key: gp_segment_id
         ->  Sort
               Sort Key: gp_segment_id
               ->  Seq Scan on motiondata
 Optimizer: Pivotal Optimizer (GPORCA)
(7 rows)
```

src/backend/gporca/libgpopt/include/gpopt/base/CDistributionSpecHashed.h
@@ -74,6 +74,8 @@ namespace gpopt
 			// exact match against given hashed distribution
 			BOOL FMatchHashedDistribution(const CDistributionSpecHashed *pdshashed) const;
 
+			BOOL FDistributionSpecHashedOnGpSegmentId() const;
+
 			// private copy ctor
 			CDistributionSpecHashed(const CDistributionSpecHashed &);
 			

cloudberrydb/cloudberrydb

src/backend/gporca/libgpopt/include/gpopt/base/CDistributionSpecHashed.h
@@ -72,6 +72,8 @@ private:
 	BOOL FMatchHashedDistribution(
 		const CDistributionSpecHashed *pdshashed) const;
 
+	BOOL FDistributionSpecHashedOnlyOnGpSegmentId() const;
+
 	// private copy ctor
 	CDistributionSpecHashed(const CDistributionSpecHashed &);
 