aee5f16d7896d312949ea0b59d7af2136817c0d5
Ashwin Agrawal <aashwin@vmware.com>
2020-10-22 13:55:27 -0700

    Add --without-python to resource group run_tests
    Seems resource group run_tests task didn't configure with Python
    before and seems image doesn't have PYTHON, as changing the configure
    default failed the job. Hence, explicitely adding --without-python to
    retain old functionality with changed default.

--- concourse/scripts/ic_gpdb_resgroup.bash
+++ concourse/scripts/ic_gpdb_resgroup.bash
@@ -62,7 +62,7 @@ run_resgroup_test() {
             --without-zlib --without-rt --without-libcurl \
             --without-libedit-preferred --without-docdir --without-readline \
             --disable-gpcloud --disable-gpfdist --disable-orca \
-            --disable-pxf ${CONFIGURE_FLAGS}
+            --disable-pxf --without-python ${CONFIGURE_FLAGS}
 
         make -C /home/gpadmin/gpdb_src/src/test/regress
         ssh sdw1 mkdir -p /home/gpadmin/gpdb_src/src/test/regress </dev/null
