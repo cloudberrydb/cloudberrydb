fd1f59c42afead1958ff5459bcfc3079a3e7f08e
Chen Mulong <chenmulong@gmail.com>
2021-01-11 13:49:36 +0800

    Fix CWE-23 for gpfdist (#11332)
    gpfdist disallowed `..` in the path before. However, the check was
    bypassed when the `.` is escaped.
    
    This commit addresses the issue with a stricter validation which won't
    allow escape character `\` appear in the path string. And also move the
    validation after URL percent decoding.
    
    On Windows, the `\` check is not done since the character works as the
    path separator on Windows.
    
    We don't use the absolute path comparison to do the validation since
    users may rely on the symbolic links in the gpfdist working directory.

--- src/bin/gpfdist/gpfdist.c
+++ src/bin/gpfdist/gpfdist.c
@@ -347,6 +347,7 @@ static void session_active_segs_dump(session_t* session);
 static int session_active_segs_isempty(session_t* session);
 static int request_validate(request_t *r);
 static int request_set_path(request_t *r, const char* d, char* p, char* pp, char* path);
+static int request_path_validate(request_t *r, const char* path);
 static int request_parse_gp_headers(request_t *r, int opt_g);
 static void free_session_cb(int fd, short event, void* arg);
 #ifdef GPFXDIST
@@ -2006,6 +2007,12 @@ static void do_read_request(int fd, short event, void* arg)
 	/* decode %xx to char */
 	percent_encoding_to_char(p, pp, path);
 
+	/* legit check for the path */
+	if (request_path_validate(r, path) != 0)
+	{
+		return;
+	}
+
 	/*
 	 * This is a debug hook. We'll get here By creating an external table with
 	 * name(a text) location('gpfdist://<host>:<port>/gpfdist/status').
@@ -3155,24 +3162,6 @@ done_processing_request:
 
 static int request_set_path(request_t *r, const char* d, char* p, char* pp, char* path)
 {
-
-	/*
-	 * disallow using a relative path in the request
-	 */
-	if (strstr(path, ".."))
-	{
-		gwarning(r, "reject invalid request from %s [%s %s] - request "
-						"is using a relative path",
-						r->peer,
-						r->in.req->argv[0],
-						r->in.req->argv[1]);
-
-		http_error(r, FDIST_BAD_REQUEST, "invalid request due to relative path");
-		request_end(r, 1, 0);
-
-		return -1;
-	}
-
 	r->path = 0;
 
 	/*
@@ -3208,6 +3197,51 @@ static int request_set_path(request_t *r, const char* d, char* p, char* pp, char
 	if (!r->path)
 	{
 		http_error(r, FDIST_BAD_REQUEST, "invalid request (unable to set path)");
+		request_end(r, 1, 0);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int request_path_validate(request_t *r, const char* path)
+{
+	const char* warn_msg = NULL;
+	const char* http_err_msg = NULL;
+
+#ifdef WIN32
+	if (strstr(path, ".."))
+#else
+	if (strstr(path, "\\"))
+	{
+		/*
+		 * '\' is the path separator under windows.
+		 * For *nix, escape char may cause some unexpected result with
+		 * the file API. e.g.: 'ls \.\.' equals to 'ls ..'.
+		 */
+		warn_msg = "contains escape character backslash '\\'";
+		http_err_msg = "invalid request, "
+			"escape character backslash '\\' is not allowed.";
+	}
+	else if (strstr(path, ".."))
+#endif
+	{
+		/*
+		 * disallow using a relative path in the request. CWE23
+		 */
+		warn_msg = "is using a relative path";
+		http_err_msg = "invalid request due to relative path";
+	}
+
+	if (warn_msg)
+	{
+		gwarning(r, "reject invalid request from %s [%s %s] - request %s",
+						r->peer,
+						r->in.req->argv[0],
+						r->in.req->argv[1],
+						warn_msg);
+
+		http_error(r, FDIST_BAD_REQUEST, http_err_msg);
 		request_end(r, 1, 0);
 		return -1;
 	}
