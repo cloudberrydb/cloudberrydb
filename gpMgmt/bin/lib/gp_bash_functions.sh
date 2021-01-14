d42662c02aeb07fd53b08b349618cb71e339e372
Brent Doil <bdoil@vmware.com>
2021-01-14 14:51:29 -0500

    gpinitsystem: DEFAULT_LOCALE_SETTING determined by output of locale -a
    Rather than hard coding DEFAULT_LOCALE_SETTING to en_US.utf8 on Linux,
    which is not available on all platforms, use locale -a output.
    
    Authored-by: Brent Doil <bdoil@vmware.com>

--- gpMgmt/bin/lib/gp_bash_functions.sh
+++ gpMgmt/bin/lib/gp_bash_functions.sh
@@ -1151,7 +1151,7 @@ case $OS_TYPE in
 		PG_METHOD="ident"
 		HOST_ARCH_TYPE="uname -i"
 		NOLINE_ECHO="$ECHO -e"
-		DEFAULT_LOCALE_SETTING=en_US.utf8
+		DEFAULT_LOCALE_SETTING=`locale -a | grep -i en_US.utf*8 | head -1`
 		PING6=`findCmdInPath ping6`
 		PING_TIME="-c 1"
 		;;
