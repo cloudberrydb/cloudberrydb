cdc656047533c4db9af6dfe0b68595cbb7352b98
Kalen Krempely <kkrempely@vmware.com>
2021-02-10 16:56:26 -0800

    ci: allow worker tags on multiple instances
    Allow Concourse step worker tags to be applied to multiple Concourse
    instances such as prod, dev, and cm.
    
    For more information see previous commit
    0dce0e8746035c9212129ff8529fe14f3559f07c

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -171,7 +171,8 @@ def create_pipeline(args):
         'os_types': args.os_types,
         'test_sections': args.test_sections,
         'pipeline_configuration': args.pipeline_configuration,
-        'test_trigger': test_trigger
+        'test_trigger': test_trigger,
+        'use_ICW_workers': args.use_ICW_workers
     }
 
     pipeline_yml = render_template(args.template_filename, context)
@@ -235,13 +236,15 @@ def header(args):
   OS Types ................. : %s
   Test sections ............ : %s
   test_trigger ............. : %s
+  use_ICW_workers .......... : %s
 ======================================================================
 ''' % (args.pipeline_target,
        args.output_filepath,
        args.template_filename,
        args.os_types,
        args.test_sections,
-       args.test_trigger_false
+       args.test_trigger_false,
+       args.use_ICW_workers
        )
 
 
@@ -358,6 +361,14 @@ def main():
         help='Developer userid to use for pipeline name and filename.'
     )
 
+    parser.add_argument(
+        '-U',
+        '--use_ICW_workers',
+        action='store_true',
+        default=False,
+        help='Set use_ICW_workers to "true".'
+    )
+
     args = parser.parse_args()
 
     validate_target(args.pipeline_target)
@@ -369,6 +380,11 @@ def main():
 
     if args.pipeline_target == 'prod':
         args.pipeline_configuration = 'prod'
+
+    # use_ICW_workers adds tags to the specified concourse definitions which
+    # correspond to dedicated concourse workers to increase performance.
+    if args.pipeline_target in ['prod', 'dev', 'cm']:
+        args.use_ICW_workers = True
 
     if args.pipeline_configuration == 'prod' or args.pipeline_configuration == 'full':
         args.os_types = ['centos6', 'centos7', 'ubuntu18.04', 'win']
