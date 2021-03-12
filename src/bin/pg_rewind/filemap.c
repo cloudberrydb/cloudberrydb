6f29a6fbfcda033388a1555d3bb11b6345a8c75e
Ashwin Agrawal <aashwin@vmware.com>
2021-03-12 11:14:05 -0800

    pg_rewind: avoid removing "log" files
    "log" is part of excluded directory list. The semantics of exclude
    file/directory list is to avoid copy from source to target. But also
    as side effect if those files are present in target, they are removed
    as well.
    
    This semantic is good for most of things but for "log" directory don't
    wish to get them removed. Hence, adding logic to add this exception.
    
    Reviewed-by: Asim R P <pasim@vmware.com>
    Reviewed-by: Paul Guo <paulguo@gmail.com>

--- src/bin/pg_rewind/filemap.c
+++ src/bin/pg_rewind/filemap.c
@@ -251,10 +251,11 @@ process_target_file(const char *path, file_type_t type, size_t size,
 	 * from the target data folder all paths which have been filtered out from
 	 * the source data folder when processing the source files.
 	 *
-	 * GPDB: GP_INTERNAL_AUTO_CONF_FILE_NAME is in the excluded file list.
-	 * It should not be copied but also should not be removed. In the future,
-	 * if there are more files that should not be copied but also should not be
-	 * removed, then a separate function for those files would be better.
+	 * GPDB: GP_INTERNAL_AUTO_CONF_FILE_NAME and "log" are in the excluded
+	 * file list.  These should not be copied but also should not be
+	 * removed. In the future, if there are more files or directories that
+	 * should not be copied but also should not be removed, then a separate
+	 * function for those would be better.
 	 */
 	{
 		const char *filename = last_dir_separator(path);
@@ -263,6 +264,8 @@ process_target_file(const char *path, file_type_t type, size_t size,
 		else
 			filename++;
 		if (strcmp(filename, GP_INTERNAL_AUTO_CONF_FILE_NAME) == 0)
+			return;
+		if (strstr(path, "log/") == path)
 			return;
 	}
 
