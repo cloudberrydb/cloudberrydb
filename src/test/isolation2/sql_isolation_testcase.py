greenplum-db/gpdb

Support "M" in isolation test framework.
In some tests, sometimes a primary is marked down by fts as mirror but it is
actually running as a primary, and we expect to run some commands on the node.
For the scenario, we add a "M" connection flag. This flags imples utility mode
connection also. An example is:
	0M: CHECKPOINT;

src/test/isolation2/sql_isolation_testcase.py
@@ -58,7 +58,7 @@ class SQLIsolationExecutor(object):
         # The re.S flag makes the "." in the regex match newlines.
         # When matched against a command in process_command(), all
         # lines in the command are matched and sent as SQL query.
-        self.command_pattern = re.compile(r"^(-?\d+|[*])([&\\<\\>USIq]*?)\:(.*)", re.S)
+        self.command_pattern = re.compile(r"^(-?\d+|[*])([&\\<\\>USIMq]*?)\:(.*)", re.S)
         if dbname:
             self.dbname = dbname
         else:
@@ -161,6 +161,16 @@ class SQLIsolationExecutor(object):
                 self.con = self.connectdb(given_dbname=self.dbname,
                                           given_host=hostname,
                                           given_port=port)
+            elif self.mode == "mirror":
+                # Connect to mirror even when it's role is recorded
+                # as mirror.  This is useful for scenarios where a
+                # primary is marked down but could actually accept
+                # connection. This implies utility connection.
+                (hostname, port) = self.get_hostname_port(name, 'm')
+                self.con = self.connectdb(given_dbname=self.dbname,
+                                          given_host=hostname,
+                                          given_port=port,
+                                          given_opt="-c gp_session_role=utility")
             else:
                 self.con = self.connectdb(self.dbname)
 
@@ -368,6 +378,8 @@ class SQLIsolationExecutor(object):
                 if len(flag) > 1:
                     flag = flag[1:]
                 con_mode = "standby"
+            elif flag and flag[0] == "M":
+                con_mode = "mirror"
             sql = m.groups()[2]
             sql = sql.lstrip()
             # If db_name is specifed , it should be of the following syntax:
@@ -452,6 +464,8 @@ class SQLIsolationExecutor(object):
                 raise Exception("No query should be given on quit")
             self.quit_process(output_file, process_name, con_mode, dbname=dbname)
         elif flag == "S":
+            self.get_process(output_file, process_name, con_mode, dbname=dbname).query(sql.strip())
+        elif flag == "M":
             self.get_process(output_file, process_name, con_mode, dbname=dbname).query(sql.strip())
         else:
             raise Exception("Invalid isolation flag")

cloudberrydb/cloudberrydb

(cherry picked from commit 6dad4b432bd60f98e0433db4fa88d5c4a67a134a)

src/test/isolation2/sql_isolation_testcase.py
@@ -48,7 +48,7 @@ class SQLIsolationExecutor(object):
         # The re.S flag makes the "." in the regex match newlines.
         # When matched against a command in process_command(), all
         # lines in the command are matched and sent as SQL query.
-        self.command_pattern = re.compile(r"^(-?\d+|[*])([&\\<\\>USq]*?)\:(.*)", re.S)
+        self.command_pattern = re.compile(r"^(-?\d+|[*])([&\\<\\>USMq]*?)\:(.*)", re.S)
         if dbname:
             self.dbname = dbname
         else:
@@ -151,6 +151,16 @@ class SQLIsolationExecutor(object):
                 self.con = self.connectdb(given_dbname=self.dbname,
                                           given_host=hostname,
                                           given_port=port)
+            elif self.mode == "mirror":
+                # Connect to mirror even when it's role is recorded
+                # as mirror.  This is useful for scenarios where a
+                # primary is marked down but could actually accept
+                # connection. This implies utility connection.
+                (hostname, port) = self.get_hostname_port(name, 'm')
+                self.con = self.connectdb(given_dbname=self.dbname,
+                                          given_host=hostname,
+                                          given_port=port,
+                                          given_opt="-c gp_role=utility")
             else:
                 self.con = self.connectdb(self.dbname)
 
@@ -372,6 +382,8 @@ class SQLIsolationExecutor(object):
                 if len(flag) > 1:
                     flag = flag[1:]
                 con_mode = "standby"
+            elif flag and flag[0] == "M":
+                con_mode = "mirror"
             sql = m.groups()[2]
             sql = sql.lstrip()
             # If db_name is specifed , it should be of the following syntax:
@@ -446,6 +458,8 @@ class SQLIsolationExecutor(object):
                 raise Exception("No query should be given on quit")
             self.quit_process(output_file, process_name, con_mode, dbname=dbname)
         elif flag == "S":
+            self.get_process(output_file, process_name, con_mode, dbname=dbname).query(sql.strip())
+        elif flag == "M":
             self.get_process(output_file, process_name, con_mode, dbname=dbname).query(sql.strip())
         else:
             raise Exception("Invalid isolation flag")