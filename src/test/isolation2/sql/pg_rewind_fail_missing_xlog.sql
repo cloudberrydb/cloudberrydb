greenplum-db/gpdb

Fix incremental recovery failure because the checkpoint redo wal file before divergence LSN is gone.
gprecoverseg calls pg_rewind to do incremental recovery. For unclean shutdown,
pg_rewind calls single mode postgres to ensure the clean shutdown, however the
single mode postgres would introduce two additional checkpoints, one is after
crash recovery, and another is for shutdown. Current gp6 xlog removing/recyling
logic ensures that the previous checkpoint.redo is kept when doing checkpoint, now
that two additional checkpoints are added the checkpoint wal file before the
divergence LSN might be recycled/removed and thus pg_rewind fails sometimes due
to missing wal files. This issue happens also when the primary is not actually
running but just was marked down by fts. The coordinator promotes the mirror,
and the continuing checkpoint on the primary could remove the needed wal files
for pg_rewind.

In real scenario this could be reproduced by the below script,

  psql -c 'create table t(a int);'
  psql -c 'insert into t select * from generate_series(1,10000000);'
  psql -c 'insert into t select * from generate_series(1,10000000);'
  psql -c 'insert into t select * from generate_series(1,10000000);'

  kill -9 `ps -ef |grep 6002|grep gpAux|awk '{print $2}'`
  psql -c "select gp_request_fts_probe_scan()" postgres

  gprecoverseg -av
  echo $?

For this case, the key point to reproduce this issue is there is an ongoing
checkpoint after bulk inserting, then if the primary postmaster is kill-9-ed,
the checkpointer flushes the replication slots in CheckPointReplicationSlots()
already but waits in CheckPointBuffers() for some time since bgwriter is gone.
The checkpoint might finish or not (checkpoint xlog write and xlog file
recycling/removal) before gprecoverseg, but either scenario could lead to the
issue (since this checkpoint xlog is after divergence LSN so it is uselss -
pg_rewind needs to find the common ancestor).

This issue happens on master and upstream also. Fixing

src/test/isolation2/sql/pg_rewind_fail_missing_xlog.sql
@@ -1,0 +1,114 @@
+-- Test the bug that if checkpoint.redo before the oldest replication slot LSN
+-- is removed/recylced in checkpointer, gprecoverseg (based on pg_rewind) would
+-- would fail.
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+include: helpers/server_helpers.sql;
+
+SHOW gp_keep_all_xlog;
+CREATE TABLE tst_missing_tbl (a int);
+INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- make the test faster.
+!\retcode gpconfig -c wal_keep_segments -v 2;
+!\retcode gpstop -ari;
+
+-- Test 1: primary was marked down by the master but acetually it keeps running
+-- and previously, checkpoints could recycle/remove the checkpoint.redo wal
+-- file before the oldest replication slot LSN and thus make pg_rewind fail due
+-- to missing xlog file.
+
+-- Run a checkpoint so that the below sqls won't cause a checkpoint
+-- until an explicit checkpoint command is issued by the test.
+-- checkpoint_timeout is by default 300 but the below test should be able to
+-- finish in 300 seconds.
+1: CHECKPOINT;
+
+0U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+0U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+0U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+-- Should be not needed mostly but let's 100% ensure since pg_switch_xlog()
+-- won't switch if it has been on the boundary (seldom though).
+0U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- Mark down the primary with content 0 via fts fault injection.
+1: SELECT gp_inject_fault_infinite('fts_handle_message', 'error', dbid) FROM gp_segment_configuration WHERE content = 0 AND role = 'p';
+
+-- Trigger failover and double check.
+1: SELECT gp_request_fts_probe_scan();
+1: SELECT role, preferred_role from gp_segment_configuration where content = 0;
+
+-- Run two more checkpoints. Previously this causes the checkpoint.redo wal
+-- file before the oldest replication slot LSN is recycled/removed.
+0M: CHECKPOINT;
+0M: CHECKPOINT;
+
+-- Write something (promote adds a 'End Of Recovery' xlog that causes the
+-- divergence between primary and mirror, but I add a write here so that we
+-- know that a wal divergence is explicitly triggered and 100% completed.  Also
+-- sanity check the tuple distribution (assumption of the test).
+2: INSERT INTO tst_missing_tbl values(2),(1),(5);
+2: SELECT gp_segment_id, count(*) from tst_missing_tbl group by gp_segment_id;
+
+-- Ensure that pg_rewind succeeds. Previously it could fail since the divergence
+-- LSN wal file is missing.
+!\retcode gprecoverseg -av;
+-- In case it fails it should not affect subsequent testing.
+!\retcode gprecoverseg -aF;
+2: SELECT wait_until_all_segments_synchronized();
+
+-- Test 2
+-- primary is abnormally shutdown, but pg_rewind would call single mode
+-- postgres to ensure it clean shutdown and that causes two checkpoints.
+
+-- See previous comment for why.
+3: CHECKPOINT;
+
+1U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+1U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+1U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+-- Should be not needed mostly but let's 100% ensure since pg_switch_xlog()
+-- won't switch if it is on the boundary already (seldom though).
+1U: SELECT pg_switch_xlog is not null FROM pg_switch_xlog();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- Hang at checkpointer before writing checkpoint xlog.
+3: SELECT gp_inject_fault('checkpoint_after_redo_calculated', 'suspend', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 1;
+1U&: CHECKPOINT;
+3: SELECT gp_wait_until_triggered_fault('checkpoint_after_redo_calculated', 1, dbid) FROM gp_segment_configuration WHERE role='p' AND content = 1;
+
+-- Stop the primary immediately and promote the mirror.
+3: SELECT pg_ctl(datadir, 'stop', 'immediate') FROM gp_segment_configuration WHERE role='p' AND content = 1;
+3: SELECT gp_request_fts_probe_scan();
+3: SELECT role, preferred_role from gp_segment_configuration where content = 1;
+
+4: INSERT INTO tst_missing_tbl values(2),(1),(5);
+4: SELECT gp_segment_id, count(*) from tst_missing_tbl group by gp_segment_id;
+
+-- CHECKPOINT should fail now.
+1U<:
+1Uq:
+
+-- Ensure that pg_rewind succeeds. For unclean shutdown, there are two
+-- checkpoints are introduced in pg_rewind when running single-mode postgres
+-- (one is the checkpoint after crash recovery and another is the shutdown
+-- checkpoint) and previously the checkpoints clean up the wal files that
+-- include the previous checkpoint (before divergence LSN) for pg_rewind and
+-- thus makes gprecoverseg (pg_rewind) fail.
+!\retcode gprecoverseg -av;
+-- In case it fails it should not affect subsequent testing.
+!\retcode gprecoverseg -aF;
+4: SELECT wait_until_all_segments_synchronized();
+
+-- Cleanup
+5: DROP TABLE tst_missing_tbl;
+!\retcode gprecoverseg -ar;
+5: SELECT wait_until_all_segments_synchronized();
+!\retcode gpconfig -r wal_keep_segments;
+!\retcode gpstop -ari;

cloudberrydb/cloudberrydb

It's also possible in another scenario that the "failed" primary continues
running thus checkpoints might remove some needed wal for pg_rewind so let's
fix this by keeping the checkpoint.redo before the oldest replication slot LSN.
This issue was first fixed on gp6 since we thought that the fix on gp6 and
master should be different but later we found the fix are similar on gp6 and
master.
(cherry picked from commit 49dc7ca484e67765a6796f73fe4a5e99b4263176)

src/test/isolation2/sql/pg_rewind_fail_missing_xlog.sql
@@ -1,0 +1,111 @@
+-- Test the bug that if checkpoint.redo before the oldest replication slot LSN
+-- is removed/recylced in checkpointer, gprecoverseg (based on pg_rewind) would
+-- would fail.
+
+CREATE TABLE tst_missing_tbl (a int);
+INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- make the test faster.
+!\retcode gpconfig -c wal_keep_segments -v 2;
+!\retcode gpstop -ari;
+
+-- Test 1: primary was marked down by the master but acetually it keeps running
+-- and previously, checkpoints could recycle/remove the checkpoint.redo wal
+-- file before the oldest replication slot LSN and thus make pg_rewind fail due
+-- to missing xlog file.
+
+-- Run a checkpoint so that the below sqls won't cause a checkpoint
+-- until an explicit checkpoint command is issued by the test.
+-- checkpoint_timeout is by default 300 but the below test should be able to
+-- finish in 300 seconds.
+1: CHECKPOINT;
+
+0U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+0U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+0U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+-- Should be not needed mostly but let's 100% ensure since pg_switch_wal()
+-- won't switch if it has been on the boundary (seldom though).
+0U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+1: INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- Mark down the primary with content 0 via fts fault injection.
+1: SELECT gp_inject_fault_infinite('fts_handle_message', 'error', dbid) FROM gp_segment_configuration WHERE content = 0 AND role = 'p';
+
+-- Trigger failover and double check.
+1: SELECT gp_request_fts_probe_scan();
+1: SELECT role, preferred_role from gp_segment_configuration where content = 0;
+
+-- Run two more checkpoints. Previously this causes the checkpoint.redo wal
+-- file before the oldest replication slot LSN is recycled/removed.
+0M: CHECKPOINT;
+0M: CHECKPOINT;
+
+-- Write something (promote adds a 'End Of Recovery' xlog that causes the
+-- divergence between primary and mirror, but I add a write here so that we
+-- know that a wal divergence is explicitly triggered and 100% completed.  Also
+-- sanity check the tuple distribution (assumption of the test).
+2: INSERT INTO tst_missing_tbl values(2),(1),(5);
+2: SELECT gp_segment_id, count(*) from tst_missing_tbl group by gp_segment_id;
+
+-- Ensure that pg_rewind succeeds. Previously it could fail since the divergence
+-- LSN wal file is missing.
+!\retcode gprecoverseg -av;
+-- In case it fails it should not affect subsequent testing.
+!\retcode gprecoverseg -aF;
+2: SELECT wait_until_all_segments_synchronized();
+
+-- Test 2
+-- primary is abnormally shutdown, but pg_rewind would call single mode
+-- postgres to ensure it clean shutdown and that causes two checkpoints.
+
+-- See previous comment for why.
+3: CHECKPOINT;
+
+1U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+1U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+1U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+-- Should be not needed mostly but let's 100% ensure since pg_switch_wal()
+-- won't switch if it is on the boundary already (seldom though).
+1U: SELECT pg_switch_wal is not null FROM pg_switch_wal();
+3: INSERT INTO tst_missing_tbl values(2),(1),(5);
+
+-- Hang at checkpointer before writing checkpoint xlog.
+3: SELECT gp_inject_fault('checkpoint_after_redo_calculated', 'suspend', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 1;
+1U&: CHECKPOINT;
+3: SELECT gp_wait_until_triggered_fault('checkpoint_after_redo_calculated', 1, dbid) FROM gp_segment_configuration WHERE role='p' AND content = 1;
+
+-- Stop the primary immediately and promote the mirror.
+3: SELECT pg_ctl(datadir, 'stop', 'immediate') FROM gp_segment_configuration WHERE role='p' AND content = 1;
+3: SELECT gp_request_fts_probe_scan();
+3: SELECT role, preferred_role from gp_segment_configuration where content = 1;
+
+4: INSERT INTO tst_missing_tbl values(2),(1),(5);
+4: SELECT gp_segment_id, count(*) from tst_missing_tbl group by gp_segment_id;
+
+-- CHECKPOINT should fail now.
+1U<:
+1Uq:
+
+-- Ensure that pg_rewind succeeds. For unclean shutdown, there are two
+-- checkpoints are introduced in pg_rewind when running single-mode postgres
+-- (one is the checkpoint after crash recovery and another is the shutdown
+-- checkpoint) and previously the checkpoints clean up the wal files that
+-- include the previous checkpoint (before divergence LSN) for pg_rewind and
+-- thus makes gprecoverseg (pg_rewind) fail.
+!\retcode gprecoverseg -av;
+-- In case it fails it should not affect subsequent testing.
+!\retcode gprecoverseg -aF;
+4: SELECT wait_until_all_segments_synchronized();
+
+-- Cleanup
+5: DROP TABLE tst_missing_tbl;
+!\retcode gprecoverseg -ar;
+5: SELECT wait_until_all_segments_synchronized();
+!\retcode gpconfig -r wal_keep_segments;
+!\retcode gpstop -ari;