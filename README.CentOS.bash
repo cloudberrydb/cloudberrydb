d7adf32a650b10563116ed25c1e11c7766ad3425
Adam Lee <adlee@vmware.com>
2021-09-08 13:29:43 +0000

    Add sysctl and limits configurations to README.*.bash
    Add to READMEs for newbies.

--- README.CentOS.bash
+++ README.CentOS.bash
@@ -32,3 +32,33 @@ sudo yum install -y \
 
 sudo pip3 install conan
 sudo pip3 install -r python-dependencies.txt
+
+sudo tee -a /etc/sysctl.conf << EOF
+kernel.shmmax = 5000000000000
+kernel.shmmni = 32768
+kernel.shmall = 40000000000
+
+kernel.msgmnb = 1048576
+kernel.msgmax = 1048576
+kernel.msgmni = 32768
+
+net.core.netdev_max_backlog = 80000
+net.core.rmem_default = 2097152
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+
+vm.overcommit_memory = 2
+vm.overcommit_ratio = 95
+EOF
+
+sudo sysctl -p
+
+sudo mkdir -p /etc/security/limits.d
+sudo tee -a /etc/security/limits.d/90-greenplum.conf << EOF
+* soft nofile 1048576
+* hard nofile 1048576
+* soft nproc 1048576
+* hard nproc 1048576
+EOF
+
+ulimit -n 65536 65536
--- README.Ubuntu.bash
+++ README.Ubuntu.bash
@@ -1,42 +1,72 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y \
-  bison \
-  ccache \
-  cmake \
-  curl \
-  flex \
-  git-core \
-  gcc \
-  g++ \
-  inetutils-ping \
-  krb5-kdc \
-  krb5-admin-server \
-  libapr1-dev \
-  libbz2-dev \
-  libcurl4-gnutls-dev \
-  libevent-dev \
-  libkrb5-dev \
-  libpam-dev \
-  libperl-dev \
-  libreadline-dev \
-  libssl-dev \
-  libxerces-c-dev \
-  libxml2-dev \
-  libyaml-dev \
-  libzstd-dev \
-  locales \
-  net-tools \
-  ninja-build \
-  openssh-client \
-  openssh-server \
-  openssl \
-  python3-dev \
-  python3-pip \
-  python3-psutil \
-  python3-pygresql \
-  python3-yaml \
-  zlib1g-dev
+sudo apt-get update
+sudo apt-get install -y \
+	bison \
+	ccache \
+	cmake \
+	curl \
+	flex \
+	git-core \
+	gcc \
+	g++ \
+	inetutils-ping \
+	krb5-kdc \
+	krb5-admin-server \
+	libapr1-dev \
+	libbz2-dev \
+	libcurl4-gnutls-dev \
+	libevent-dev \
+	libkrb5-dev \
+	libpam-dev \
+	libperl-dev \
+	libreadline-dev \
+	libssl-dev \
+	libxerces-c-dev \
+	libxml2-dev \
+	libyaml-dev \
+	libzstd-dev \
+	locales \
+	net-tools \
+	ninja-build \
+	openssh-client \
+	openssh-server \
+	openssl \
+	python3-dev \
+	python3-pip \
+	python3-psutil \
+	python3-pygresql \
+	python3-yaml \
+	zlib1g-dev
 
 pip3 install conan
+
+sudo tee -a /etc/sysctl.conf << EOF
+kernel.shmmax = 5000000000000
+kernel.shmmni = 32768
+kernel.shmall = 40000000000
+
+kernel.msgmnb = 1048576
+kernel.msgmax = 1048576
+kernel.msgmni = 32768
+
+net.core.netdev_max_backlog = 80000
+net.core.rmem_default = 2097152
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+
+vm.overcommit_memory = 2
+vm.overcommit_ratio = 95
+EOF
+
+sudo sysctl -p
+
+sudo mkdir -p /etc/security/limits.d
+sudo tee -a /etc/security/limits.d/90-greenplum.conf << EOF
+* soft nofile 1048576
+* hard nofile 1048576
+* soft nproc 1048576
+* hard nproc 1048576
+EOF
+
+ulimit -n 65536 65536
