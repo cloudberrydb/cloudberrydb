7b7841caaec6618bfdac06003c9084e4a0b3d1b8
Xing GUO <higuoxing@gmail.com>
2021-09-16 10:55:38 +0800

    Add kernel.sem to sysctl.conf. (#12590)
    This patch helps add kernel.sem to sysctl.conf, since gpstart/gpinitsystem
    may fail with "could not create semaphores: No space left on device".

--- README.Ubuntu.bash
+++ README.Ubuntu.bash
@@ -45,7 +45,7 @@ sudo tee -a /etc/sysctl.conf << EOF
 kernel.shmmax = 5000000000000
 kernel.shmmni = 32768
 kernel.shmall = 40000000000
-
+kernel.sem = 1000 32768000 1000 32768
 kernel.msgmnb = 1048576
 kernel.msgmax = 1048576
 kernel.msgmni = 32768
