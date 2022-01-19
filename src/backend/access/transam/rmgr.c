4b622452d75276007755b4ffaf0a9a1588fb9448
Jeff Davis <jdavis@postgresql.org>
2022-01-19 14:58:04 -0800

    Make logical decoding a part of the rmgr.
    Add a new rmgr method, rm_decode, and use that rather than a switch
    statement.
    
    In preparation for rmgr extensibility.
    
    Reviewed-by: Julien Rouhaud
    Discussion: https://postgr.es/m/ed1fb2e22d15d3563ae0eb610f7b61bb15999c0a.camel%40j-davis.com
    Discussion: https://postgr.es/m/20220118095332.6xtlcjoyxobv6cbk@jrouhaud

--- src/backend/access/transam/rmgr.c
+++ src/backend/access/transam/rmgr.c
@@ -24,6 +24,7 @@
 #include "commands/dbcommands_xlog.h"
 #include "commands/sequence.h"
 #include "commands/tablespace.h"
+#include "replication/decode.h"
 #include "replication/message.h"
 #include "replication/origin.h"
 #include "storage/standby.h"
@@ -35,8 +36,8 @@
 
 
 /* must be kept in sync with RmgrData definition in xlog_internal.h */
-#define PG_RMGR(symname,name,redo,desc,identify,startup,cleanup,mask) \
-	{ name, redo, desc, identify, startup, cleanup, mask },
+#define PG_RMGR(symname,name,redo,desc,identify,startup,cleanup,mask,decode) \
+	{ name, redo, desc, identify, startup, cleanup, mask, decode },
 
 const RmgrData RmgrTable[RM_MAX_ID + 1] = {
 #include "access/rmgrlist.h"
