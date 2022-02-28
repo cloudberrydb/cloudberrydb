13260ec04c2eae07ecb6611aa4ce9836828799bb
Xing Guo <higuoxing@gmail.com>
2022-02-28 15:08:30 +0800

    Set statement_mem to a larger value in tests to make pipelines happy. (#13144)
    We observed greenplum built without cassert couldn't pass tests. Because
    when the gpdb is configured with --enable-cassert, the minimum
    statement_mem it accepts is 50kB while if it's configured without
    --enable-cassert, the minimum statement_mem it accepts is 1000kB (See:
    backend/utils/misc/guc_gp.c). This patch is trying to make pipelines
    happy.

--- src/test/regress/sql/statement_mem_for_windowagg.sql
+++ src/test/regress/sql/statement_mem_for_windowagg.sql
@@ -1,12 +1,12 @@
 CREATE TABLE dummy_table(x int, y int) DISTRIBUTED BY (y);
-INSERT INTO dummy_table SELECT generate_series(0, 10000), 0;
-INSERT INTO dummy_table SELECT generate_series(0, 10000), 3;
-INSERT INTO dummy_table SELECT generate_series(0, 10000), 10;
+INSERT INTO dummy_table SELECT generate_series(0, 20000), 0;
+INSERT INTO dummy_table SELECT generate_series(0, 20000), 3;
+INSERT INTO dummy_table SELECT generate_series(0, 20000), 10;
 
 -- 1. Test that if we set statement_mem to a larger value, the tuplestore
 -- for caching the tuples in partition used in WindowAgg is able to be fitted
 -- in memory.
-SET statement_mem TO '2048kB';
+SET statement_mem TO '4096kB';
 
 SELECT gp_inject_fault('winagg_after_spool_tuples', 'skip', dbid)
   FROM gp_segment_configuration WHERE role='p' AND content>=0;
@@ -30,7 +30,7 @@ SELECT gp_inject_fault('winagg_after_spool_tuples', 'reset', dbid)
 
 -- 3. Test that if we set statement_mem to a larger value, the tuplesort
 -- operation in DISTINCT-qualified WindowAgg is able to be fitted in memory.
-SET statement_mem TO '1024kB';
+SET statement_mem TO '4096kB';
 
 SELECT gp_inject_fault_infinite('distinct_winagg_perform_sort', 'skip', dbid)
   FROM gp_segment_configuration WHERE role='p' AND content>=0;
@@ -42,7 +42,7 @@ SELECT gp_inject_fault_infinite('distinct_winagg_perform_sort', 'reset', dbid)
 
 -- 4. Test that if we set statement_mem to a smaller value, the tuplesort
 -- operation in DISTINCT-qualified WindowAgg will be spilled to disk.
-SET statement_mem TO '128kB';
+SET statement_mem TO '1024kB';
 
 SELECT gp_inject_fault_infinite('distinct_winagg_perform_sort', 'skip', dbid)
   FROM gp_segment_configuration WHERE role='p' AND content>=0;
