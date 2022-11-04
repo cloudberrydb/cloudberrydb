ade15156a05922a0fe673a927079313bdff580c9
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-11-04 15:19:24 -0700

    UPDATE on AO/CO tables with unique indexes
    This commit formally adds support for UPDATEs on AO/CO tables with
    unique indexes.
    
    Technically, UPDATEs were supported by the prior commits for DELETE and
    INSERT. This commit includes tests to formally support UPDATE and
    addresses the unsupported edge case:
    
    UPDATE foo SET i = 1 WHERE i = 1;
    
    Here, within the UPDATE command itself, there is a need to see what
    tuples are deleted in the delete half of the command, in uniqueness
    checks done by the insert half of the command. This is crucial to avoid
    spurious conflicts in similar situations as above, where the source and
    target keys are equal in the UPDATE invocation.
    
    To that end, we reuse the visimap used in the delete half of the command
    in the uniqueness check performed during the insert half.

--- src/include/cdb/cdbaocsam.h
+++ src/include/cdb/cdbaocsam.h
@@ -258,7 +258,30 @@ typedef struct AOCSFetchDescData
 
 typedef AOCSFetchDescData *AOCSFetchDesc;
 
-typedef struct AOCSUpdateDescData *AOCSUpdateDesc;
+/*
+ * AOCSDeleteDescData is used for delete data from AOCS relations.
+ * It serves an equivalent purpose as AppendOnlyScanDescData
+ * (relscan.h) only that the later is used for scanning append-only
+ * relations.
+ */
+typedef struct AOCSDeleteDescData
+{
+	/*
+	 * Relation to delete from
+	 */
+	Relation	aod_rel;
+
+	/*
+	 * visibility map
+	 */
+	AppendOnlyVisimap visibilityMap;
+
+	/*
+	 * Visimap delete support structure. Used to handle out-of-order deletes
+	 */
+	AppendOnlyVisimapDelete visiMapDelete;
+
+}			AOCSDeleteDescData;
 typedef struct AOCSDeleteDescData *AOCSDeleteDesc;
 
 typedef struct AOCSUniqueCheckDescData
