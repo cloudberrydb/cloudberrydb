e8e1df77e40334fe076dc643846d2dd8577b6eff
Andrey Sokolov <a.sokolov@arenadata.io>
2023-02-15 03:44:06 +0300

    Fix value in the cpu_usage column returned from the pg_resgroup_get_status() function (#14959)
    In the case of insert-select, the cpu_usage field contains information about
    master only. This leads to an error at the moment of analyzing the table, as the
    contents of the field do not correspond to json format.
    The error was fixed by setting the proexeclocation property of the function
    to i (PROEXECLOCATION_INITPLAN).
    
    https://github.com/greenplum-db/gpdb/issues/14154

--- src/test/isolation2/sql/resgroup/resgroup_functions.sql
+++ src/test/isolation2/sql/resgroup/resgroup_functions.sql
@@ -4,8 +4,11 @@ FROM pg_resgroup_get_status(NULL::oid) s(groupid, num_running, num_queueing, num
 -- end_ignore
 CREATE TEMP TABLE resgroup_function_test(LIKE gp_toolkit.gp_resgroup_status);
 
-INSERT INTO resgroup_function_test(groupid, num_running, num_queueing, num_queued, num_executed)
-SELECT s.groupid, s.num_running, s.num_queueing, s.num_queued, s.num_executed
+INSERT INTO resgroup_function_test(groupid, num_running, num_queueing, num_queued, num_executed, cpu_usage)
+SELECT s.groupid, s.num_running, s.num_queueing, s.num_queued, s.num_executed, cpu_usage
 FROM pg_resgroup_get_status(NULL::oid) s(groupid, num_running, num_queueing, num_queued, num_executed, total_queue_duration, cpu_usage) LIMIT 1;
 
 SELECT count(num_executed)>0 FROM resgroup_function_test WHERE num_executed IS NOT NULL;
+
+-- Check that the contents of the cpu_usage field are valid JSON
+ANALYZE resgroup_function_test;
