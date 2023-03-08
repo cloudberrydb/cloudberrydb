4f21f6f3198bf070081fa892c632aeca509cfdae
Huansong Fu <fuhuansong@gmail.com>
2023-03-08 07:17:31 -0800

    Add views/functions to check missing and orphaned data files
    1. Add views to get "existing" relation files in the database,
      including the default, global and user tablespaces. Note that
      this won't expose files outside of the data directory as we
      only use pg_ls_dir to get the file list, which won't have
      reach to any files outside the data directories (including
      the user tablespace directory).
    
    2. Add views to get "expected" relation files in the database,
      using the knowledge from the catalog.
    
    3. Using 1 and 2, construct views to get the missing files (i.e.
      files that are expected but not existed) and orphaned files (i.e.
      files that are there unexpectedly).
    
    4. Create views to run the above views in MPP. Also, we
      support checking extended data files for AO/CO tables
    
    5. Add regress tests.
    
    To use:
    ```
      -- checking non-extended files
      select * from gp_toolkit.gp_check_missing_files;
      select * from gp_toolkit.gp_check_orphaned_files;
    
      -- checking all data files including the extended data files
      -- (e.g. 12345.1, 99999.2). These do not count supporting files
      -- such as .fsm .vm etc. And currently we only support checking
      -- extended data files for AO/CO tables, not heap.
      select * from gp_toolkit.gp_check_missing_files_ext;
      select * from gp_toolkit.gp_check_orphaned_files_ext;
    ```
    
    Note:
    * As mentioned, currently support checking all the non-extended data
      files and the extended data files of AO/CO tables. The main reason
      to separate these two is performance: constructing expected file
      list for AO/CO segments runs dynamic SQL on each aoseg/aocsseg table
      and could be slow. So only do that if really required.
    * For heap tables, currently have no way to get the expected number
      of datafiles for a certain table: we cannot use pg_relation_size
      because that is in turn dependent on the number of datafiels itself.
      So always skip its extended files for now.

--- src/backend/catalog/gp_toolkit.sql
+++ src/backend/catalog/gp_toolkit.sql
@@ -2153,6 +2153,366 @@ ORDER BY segid;
 GRANT SELECT ON gp_toolkit.gp_workfile_mgr_used_diskspace TO public;
 
 --------------------------------------------------------------------------------
+-- @function:
+--        gp_toolkit.__get_ao_segno_list
+--
+-- @in:
+--
+-- @out:
+--        oid - relation oid
+--        int - segment number
+--
+-- @doc:
+--        UDF to retrieve AO segment file numbers for each ao_row table
+--
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION gp_toolkit.__get_ao_segno_list()
+RETURNS TABLE (relid oid, segno int) AS
+$$
+DECLARE
+  table_name text;
+  rec record;
+  cur refcursor;
+  row record;
+BEGIN
+  -- iterate over the aoseg relations
+  FOR rec IN SELECT sc.relname segrel, tc.oid tableoid 
+             FROM pg_appendonly a 
+             JOIN pg_class tc ON a.relid = tc.oid 
+             JOIN pg_am am ON tc.relam = am.oid 
+             JOIN pg_class sc ON a.segrelid = sc.oid 
+             WHERE amname = 'ao_row' 
+  LOOP
+    table_name := rec.segrel;
+    -- Fetch and return each row from the aoseg table
+    BEGIN
+      OPEN cur FOR EXECUTE format('SELECT segno FROM pg_aoseg.%I', table_name);
+      SELECT rec.tableoid INTO relid;
+      LOOP
+        FETCH cur INTO row;
+        EXIT WHEN NOT FOUND;
+        segno := row.segno;
+        IF segno <> 0 THEN -- there's no '.0' file, it means the file w/o extension
+          RETURN NEXT;
+        END IF;
+      END LOOP;
+      CLOSE cur;
+    EXCEPTION
+      -- If failed to open the aoseg table (e.g. the table itself is missing), continue
+      WHEN OTHERS THEN
+      RAISE WARNING 'Failed to read %: %', table_name, SQLERRM;
+    END;
+  END LOOP;
+  RETURN;
+END;
+$$
+LANGUAGE plpgsql;
+
+GRANT EXECUTE ON FUNCTION gp_toolkit.__get_ao_segno_list() TO public;
+
+--------------------------------------------------------------------------------
+-- @function:
+--        gp_toolkit.__get_aoco_segno_list
+--
+-- @in:
+--
+-- @out:
+--        oid - relation oid
+--        int - segment number
+--
+-- @doc:
+--        UDF to retrieve AOCO segment file numbers for each ao_column table
+--
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION gp_toolkit.__get_aoco_segno_list()
+RETURNS TABLE (relid oid, segno int) AS
+$$
+DECLARE
+  table_name text;
+  rec record;
+  cur refcursor;
+  row record;
+BEGIN
+  -- iterate over the aocoseg relations
+  FOR rec IN SELECT sc.relname segrel, tc.oid tableoid
+             FROM pg_appendonly a
+             JOIN pg_class tc ON a.relid = tc.oid
+             JOIN pg_am am ON tc.relam = am.oid
+             JOIN pg_class sc ON a.segrelid = sc.oid
+             WHERE amname = 'ao_column'
+  LOOP
+    table_name := rec.segrel;
+    -- Fetch and return each extended segno corresponding to filenum and segno in the aocoseg table
+    BEGIN
+      OPEN cur FOR EXECUTE format('SELECT ((a.filenum - 1) * 128 + s.segno) as segno '
+                                  'FROM (SELECT * FROM pg_attribute_encoding '
+                                  'WHERE attrelid = %s) a CROSS JOIN pg_aoseg.%I s', 
+                                   rec.tableoid, table_name);
+      SELECT rec.tableoid INTO relid;
+      LOOP
+        FETCH cur INTO row;
+        EXIT WHEN NOT FOUND;
+        segno := row.segno;
+        IF segno <> 0 THEN -- there's no '.0' file, it means the file w/o extension
+          RETURN NEXT;
+        END IF;
+      END LOOP;
+      CLOSE cur;
+    EXCEPTION
+      -- If failed to open the aocoseg table (e.g. the table itself is missing), continue
+      WHEN OTHERS THEN
+      RAISE WARNING 'Failed to read %: %', table_name, SQLERRM;
+    END;
+  END LOOP;
+  RETURN;
+END;
+$$
+LANGUAGE plpgsql;
+
+GRANT EXECUTE ON FUNCTION gp_toolkit.__get_aoco_segno_list() TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__get_exist_files
+--
+-- @doc:
+--        Retrieve a list of all existing data files in the default
+--        and user tablespaces.
+--
+--------------------------------------------------------------------------------
+-- return the list of existing files in the database
+CREATE OR REPLACE VIEW gp_toolkit.__get_exist_files AS
+-- 1. List of files in the default tablespace
+SELECT 0 AS tablespace, filename 
+FROM pg_ls_dir('base/' || (
+  SELECT d.oid::text
+  FROM pg_database d
+  WHERE d.datname = current_database()
+))
+AS filename
+UNION
+-- 2. List of files in the global tablespace
+SELECT 1664 AS tablespace, filename
+FROM pg_ls_dir('global/') 
+AS filename
+UNION
+-- 3. List of files in user-defined tablespaces
+SELECT ts.oid AS tablespace,
+       pg_ls_dir('pg_tblspc/' || ts.oid::text || '/' || get_tablespace_version_directory_name() || '/' || 
+         (SELECT d.oid::text FROM pg_database d WHERE d.datname = current_database()), true/*missing_ok*/,false/*include_dot*/) AS filename
+FROM pg_tablespace ts
+WHERE ts.oid > 1664; 
+
+GRANT SELECT ON gp_toolkit.__get_exist_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        __gp_toolkit.__get_expect_files
+--
+-- @doc:
+--        Retrieve a list of expected data files in the database,
+--        using the knowledge from catalogs. This does not include
+--        any extended data files.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__get_expect_files AS
+SELECT s.reltablespace AS tablespace, s.relname, a.amname AS AM,
+       (CASE WHEN s.relfilenode != 0 THEN s.relfilenode ELSE pg_relation_filenode(s.oid) END)::text AS filename
+FROM pg_class s
+LEFT JOIN pg_am a ON s.relam = a.oid;
+
+GRANT SELECT ON gp_toolkit.__get_expect_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__get_expect_files_ext
+--
+-- @doc:
+--        Retrieve a list of expected data files in the database,
+--        using the knowledge from catalogs. This includes all
+--        the extended data files for AO/CO tables.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__get_expect_files_ext AS
+SELECT s.reltablespace AS tablespace, s.relname, a.amname AS AM,
+       (CASE WHEN s.relfilenode != 0 THEN s.relfilenode ELSE pg_relation_filenode(s.oid) END)::text AS filename
+FROM pg_class s LEFT JOIN pg_am a ON s.relam = a.oid
+UNION
+-- AO extended files
+SELECT c.reltablespace AS tablespace, c.relname, a.amname AS AM,
+       format(c.relfilenode::text || '.' || s.segno::text) AS filename
+FROM gp_toolkit.__get_ao_segno_list() s
+JOIN pg_class c ON s.relid = c.oid
+LEFT JOIN pg_am a ON c.relam = a.oid
+UNION
+-- CO extended files
+SELECT c.reltablespace AS tablespace, c.relname, a.amname AS AM,
+       format(c.relfilenode::text || '.' || s.segno::text) AS filename
+FROM gp_toolkit.__get_aoco_segno_list() s
+JOIN pg_class c ON s.relid = c.oid
+LEFT JOIN pg_am a ON c.relam = a.oid;
+
+GRANT SELECT ON gp_toolkit.__get_expect_files_ext TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__check_orphaned_files
+--
+-- @doc:
+--        Check orphaned data files on default and user tablespaces,
+--        not including extended files.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__check_orphaned_files AS
+SELECT f1.tablespace, f1.filename
+from gp_toolkit.__get_exist_files f1
+LEFT JOIN gp_toolkit.__get_expect_files f2
+ON f1.tablespace = f2.tablespace AND f1.filename = f2.filename
+WHERE f2.tablespace IS NULL
+  AND f1.filename SIMILAR TO '[0-9]+';
+
+GRANT SELECT ON gp_toolkit.__check_orphaned_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__check_orphaned_files_ext
+--
+-- @doc:
+--        Check orphaned data files on default and user tablespaces,
+--        including extended files.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__check_orphaned_files_ext AS
+SELECT f1.tablespace, f1.filename
+FROM gp_toolkit.__get_exist_files f1
+LEFT JOIN gp_toolkit.__get_expect_files_ext f2
+ON f1.tablespace = f2.tablespace AND f1.filename = f2.filename
+WHERE f2.tablespace IS NULL
+  AND f1.filename SIMILAR TO '[0-9]+(\.[0-9]+)?'
+  AND NOT EXISTS (
+    -- XXX: not supporting heap for now, do not count them
+    SELECT 1 FROM pg_class c 
+    JOIN pg_am a 
+    ON c.relam = a.oid 
+    WHERE c.relfilenode::text = split_part(f1.filename, '.', 1) 
+        AND a.amname = 'heap'
+  );
+
+GRANT SELECT ON gp_toolkit.__check_orphaned_files_ext TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__check_missing_files
+--
+-- @doc:
+--        Check missing data files on default and user tablespaces,
+--        not including extended files.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__check_missing_files AS
+SELECT f1.tablespace, f1.relname, f1.filename
+from gp_toolkit.__get_expect_files f1
+LEFT JOIN gp_toolkit.__get_exist_files f2
+ON f1.tablespace = f2.tablespace AND f1.filename = f2.filename
+WHERE f2.tablespace IS NULL
+  AND f1.filename SIMILAR TO '[0-9]+';
+
+GRANT SELECT ON gp_toolkit.__check_missing_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.__check_missing_files_ext
+--
+-- @doc:
+--        Check missing data files on default and user tablespaces,
+--        including extended files.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.__check_missing_files_ext AS
+SELECT f1.tablespace, f1.relname, f1.filename
+FROM gp_toolkit.__get_expect_files_ext f1
+LEFT JOIN gp_toolkit.__get_exist_files f2
+ON f1.tablespace = f2.tablespace AND f1.filename = f2.filename
+WHERE f2.tablespace IS NULL
+  AND f1.filename SIMILAR TO '[0-9]+(\.[0-9]+)?';
+
+GRANT SELECT ON gp_toolkit.__check_missing_files_ext TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.gp_check_orphaned_files
+--
+-- @doc:
+--        User-facing view of gp_toolkit.__check_orphaned_files. 
+--        Gather results from coordinator and all segments.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.gp_check_orphaned_files AS 
+SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
+FROM gp_dist_random('gp_toolkit.__check_orphaned_files')
+UNION ALL 
+SELECT -1 AS gp_segment_id, *
+FROM gp_toolkit.__check_orphaned_files;
+
+GRANT SELECT ON gp_toolkit.gp_check_orphaned_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.gp_check_orphaned_files_ext
+--
+-- @doc:
+--        User-facing view of gp_toolkit.__check_orphaned_files_ext.
+--        Gather results from coordinator and all segments.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.gp_check_orphaned_files_ext AS 
+SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
+FROM gp_dist_random('gp_toolkit.__check_orphaned_files_ext')
+UNION ALL 
+SELECT -1 AS gp_segment_id, *
+FROM gp_toolkit.__check_orphaned_files; -- not checking ext on coordinator
+
+GRANT SELECT ON gp_toolkit.gp_check_orphaned_files_ext TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.gp_check_missing_files
+--
+-- @doc:
+--        User-facing view of gp_toolkit.__check_missing_files. 
+--        Gather results from coordinator and all segments.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.gp_check_missing_files AS 
+SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
+FROM gp_dist_random('gp_toolkit.__check_missing_files')
+UNION ALL 
+SELECT -1 AS gp_segment_id, *
+FROM gp_toolkit.__check_missing_files;
+
+GRANT SELECT ON gp_toolkit.gp_check_missing_files TO public;
+
+--------------------------------------------------------------------------------
+-- @view:
+--        gp_toolkit.gp_check_missing_files_ext
+--
+-- @doc:
+--        User-facing view of gp_toolkit.__check_missing_files_ext.
+--        Gather results from coordinator and all segments.
+--
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW gp_toolkit.gp_check_missing_files_ext AS 
+SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
+FROM gp_dist_random('gp_toolkit.__check_missing_files_ext')
+UNION ALL 
+SELECT -1 AS gp_segment_id, *
+FROM gp_toolkit.__check_missing_files; -- not checking ext on coordinator
+
+GRANT SELECT ON gp_toolkit.gp_check_missing_files_ext TO public;
+
+--------------------------------------------------------------------------------
 
 -- Finalize install
 COMMIT;
