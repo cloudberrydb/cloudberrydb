498557868514825a1c090b29c8bbd305412fe1a9
Zhenglong Li <lzhenglong@vmware.com>
2023-06-15 10:13:57 +0800

    fix cgroup v1 cpu_quota_us can not be larger than parent (#15699)
    In Cgroup v1, we can not set cpu_quota_us larger than parent's, it'll throw an
    error of "Invalid Argument":
    
    ```
    smart@postgres:/sys/fs/cgroup/cpu/gpdb/6441$ echo 370000 > cpu.cfs_quota_us
    bash: echo: write error: Invalid argument
    ```
    
    But this could happen in Cgroup v2, so the interface between them is not exactly
    the same, this PR is trying to fix it.

--- src/test/isolation2/sql/resgroup/resgroup_syntax.sql
+++ src/test/isolation2/sql/resgroup/resgroup_syntax.sql
@@ -255,3 +255,17 @@ SELECT groupname,cpu_max_percent,cpuset FROM gp_toolkit.gp_resgroup_config WHERE
 ALTER RESOURCE GROUP rg_test_group SET cpu_max_percent 10;
 SELECT groupname,cpu_max_percent,cpuset FROM gp_toolkit.gp_resgroup_config WHERE groupname='rg_test_group';
 DROP RESOURCE GROUP rg_test_group;
+
+-- test set cpu_hard_quota_limit to high value when gp_resource_group_cpu_limit is low
+-- start_ignore
+!\retcode gpconfig -c gp_resource_group_cpu_limit -v 0.5;
+!\retcode gpstop -ari;
+-- end_ignore
+0: CREATE RESOURCE GROUP rg_test_group WITH (cpu_hard_quota_limit=10);
+0: ALTER RESOURCE GROUP rg_test_group SET cpu_hard_quota_limit 100;
+0: DROP RESOURCE GROUP rg_test_group;
+-- start_ignore
+!\retcode gpconfig -c gp_resource_group_cpu_limit -v 1; 
+!\retcode gpstop -ari;
+-- end_ignore
+
