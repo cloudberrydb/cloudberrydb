e7b30cae610725c95ee8ddc20fe0bb6fb20b2d91
GongXun <gongxunaini@icloud.com>
2023-09-11 14:47:59 +0800

    Feature: add dml hook
    1. Two hook functions , ext_dml_init_hook and ext_dml_finish_hook have been added. These functions perform some resource initialization and cleanup at the start and end of data modification operations (such as modifyTable, CopyFrom, CreateAs, Matview, etc.)

--- src/backend/executor/execPartition.c
+++ src/backend/executor/execPartition.c
@@ -864,6 +864,8 @@ ExecInitPartitionInfo(ModifyTableState *mtstate, EState *estate,
 		appendonly_dml_init(leaf_part_rri->ri_RelationDesc, mtstate->operation);
 	else if (RelationIsAoCols(leaf_part_rri->ri_RelationDesc))
 		aoco_dml_init(leaf_part_rri->ri_RelationDesc, mtstate->operation);
+	else if (ext_dml_init_hook)
+		ext_dml_init_hook(leaf_part_rri->ri_RelationDesc, mtstate->operation);
 
 	MemoryContextSwitchTo(oldcxt);
 
@@ -1180,6 +1182,8 @@ ExecCleanupTupleRouting(ModifyTableState *mtstate,
 			appendonly_dml_finish(resultRelInfo->ri_RelationDesc, mtstate->operation);
 		if (RelationIsAoCols(resultRelInfo->ri_RelationDesc))
 			aoco_dml_finish(resultRelInfo->ri_RelationDesc, mtstate->operation);
+		else if (ext_dml_finish_hook)
+			ext_dml_finish_hook(resultRelInfo->ri_RelationDesc, mtstate->operation);
 
 		ExecCloseIndices(resultRelInfo);
 		table_close(resultRelInfo->ri_RelationDesc, NoLock);
