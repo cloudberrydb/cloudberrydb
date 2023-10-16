12f5581d9808b6308a54931c3790f6ca710b8b13
wuhao <wuhao@hashdata.cn>
2023-10-16 15:30:39 +0800

    Refactor the table AM: scan_begin_extractcolumns to contain execution context
    Refactor the signature of scan_begin_extractcolumns to contain execution
    context, so that the scan node is able to filter data by evaluating
    expressions in low level. Low-level filter may have less cost than extracting
    all columns in a row and filtered out by the upper execution node.

--- src/include/cdb/cdbappendonlyam.h
+++ src/include/cdb/cdbappendonlyam.h
@@ -432,6 +432,12 @@ extern TableScanDesc appendonly_beginscan(Relation relation,
 										  int nkeys, struct ScanKeyData *key,
 										  ParallelTableScanDesc pscan,
 										  uint32 flags);
+extern TableScanDesc appendonly_beginscan_extractcolumns(Relation rel,
+														 Snapshot snapshot,
+														 int nkeys, struct ScanKeyData *key,
+														 ParallelTableScanDesc parallel_scan,
+														 PlanState *ps,
+														 uint32 flags);
 extern void appendonly_rescan(TableScanDesc scan, ScanKey key,
 								bool set_params, bool allow_strat,
 								bool allow_sync, bool allow_pagemode);
