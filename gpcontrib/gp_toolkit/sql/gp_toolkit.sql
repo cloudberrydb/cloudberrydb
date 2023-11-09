55dc314d5686071ccfdb9233a042f9465f404c95
Huansong Fu <fuhuansong@gmail.com>
2023-11-08 21:13:51 -0800

    Fix a flakiness in gpcontrib/gp_toolkit test
    The test was flaky with a diff like:
    
    ```
    --- /tmp/build/e18b2f02/gpdb_src/gpcontrib/gp_toolkit/expected/gp_toolkit.out	2023-11-07 20:06:36.276652825 +0000
    +++ /tmp/build/e18b2f02/gpdb_src/gpcontrib/gp_toolkit/results/gp_toolkit.out	2023-11-07 20:06:36.304655494 +0000
    @@ -399,9 +399,11 @@
     -- However it will show there is a difference if segment guc is different
     -- primary_conninfo can be different due to failovers in isolation2 tests
     select * from gp_toolkit.gp_param_settings_seg_value_diffs where psdname != 'primary_conninfo';
    - psdname | psdvalue | psdcount
    ----------+----------+----------
    -(0 rows)
    +      psdname      |           psdvalue            | psdcount
    +-------------------+-------------------------------+----------
    + primary_slot_name |                               |        2
    + primary_slot_name | internal_wal_replication_slot |        1
    +(2 rows)
    
     -- gp_locks_on_relation
    ```
    
    The reason is that primary_slot_name just like primary_conninfo, can be
    different between primary segments when some primary has previously been a
    mirror so these mirror-specific GUCs persist. We might want to clean them up
    properly in future but currently just ignore the GUCs in test.

--- gpcontrib/gp_toolkit/sql/gp_toolkit.sql
+++ gpcontrib/gp_toolkit/sql/gp_toolkit.sql
@@ -244,8 +244,8 @@ select paramsegment,paramname from gp_toolkit.gp_param_settings() where paramnam
 
 -- Expected to have no results
 -- However it will show there is a difference if segment guc is different
--- primary_conninfo can be different due to failovers in isolation2 tests
-select * from gp_toolkit.gp_param_settings_seg_value_diffs where psdname != 'primary_conninfo';
+-- primary_slot_name/primary_conninfo can be different due to failovers.
+select * from gp_toolkit.gp_param_settings_seg_value_diffs where psdname != 'primary_conninfo' and psdname != 'primary_slot_name';
 
 
 -- gp_locks_on_relation
