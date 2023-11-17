a47aaa0cf68e9e02663a114b84dd4c514b04bdf5
Ray-Eldath <ray.eldath@outlook.com>
2023-11-17 18:23:08 +0800

    Add pg_upgrade and gpAdminLogs log to CI failure artifact
    We don't have log of pg_upgrade in our CI artifact because it sits
    another directory. This commit fix this. Also, gpAdminLogs are now
    added.

--- concourse/scripts/common.bash
+++ concourse/scripts/common.bash
@@ -124,7 +124,7 @@ function make_cluster() {
     export STATEMENT_MEM=250MB
 
     pushd gpdb_src/gpAux/gpdemo
-    su gpadmin -c "source $INSTALL_DIR/greenplum_path.sh; LANG=en_US.utf8 make create-demo-cluster"
+    su gpadmin -c "source $INSTALL_DIR/greenplum_path.sh; LANG=en_US.utf8 make create-demo-cluster; LANG=en_US.utf8 make probe"
 
     if [[ "$MAKE_TEST_COMMAND" =~ gp_interconnect_type=proxy ]]; then
         # generate the addresses for proxy mode
