c1d94b4ab94aece78e60a52cfb82a0c2351e84f0
Jianghua.yjh <yjhjstz@gmail.com>
2024-01-31 15:47:18 +0800

    Don't inherit parent's reloptions if child partition's AM differs
    This commit addresses an issue arising when creating a partitioned
    table with its child partitions using the classic partitioning syntax
    in one command. Semantically, if both the parent and the child table
    had specified reloptions (using the WITH clause), the child's
    reloptions would override the parent's.
    
    However, in Greenplum 7, with the integration of the table AM API, the
    internal parsing of "appendonly" and "orientation" as reloptions has
    been phased out. Instead, the syntax "USING ao_{row,column}" has been
    standardized, effectively making "WITH (appendonly=true,
    orientation=xxx)" an alias.
    
    Thus, if a child partition is created using the classic syntax, and it
    uses a different table access method than its parent but doesn't
    specify any "real" reloptions, it's better for the child not to inherit
    the parent's reloptions. This is because the parent's reloptions may
    not be compatible with the child's access method.
    
    This commit ensures that a child partition doesn't inherit the
    parent's reloptions when their table access methods are different.
    
    Fixes #14913
    
    Reported-by: Andrew Repp <reppa@vmware.com>
    Reviewed-by: Huansong Fu <fuhuansong@gmail.com>
    
    Co-authored-by: Lei (Alexandra) Wang <alexandra.wanglei@gmail.com>

--- src/backend/parser/parse_partition_gp.c
+++ src/backend/parser/parse_partition_gp.c
@@ -15,6 +15,7 @@
 #include "postgres.h"
 
 #include "access/table.h"
+#include "access/tableam.h"
 #include "catalog/partition.h"
 #include "catalog/pg_collation.h"
 #include "catalog/gp_partition_template.h"
@@ -1607,10 +1608,15 @@ generatePartitions(Oid parentrelid, GpPartitionDefinition *gpPartSpec,
 			/* if WITH has "tablename" then it will be used as name for partition */
 			partcomp.tablename = extract_tablename_from_options(&elem->options);
 
-			if (elem->options == NIL)
-				elem->options = parentoptions ? copyObject(parentoptions) : NIL;
 			if (elem->accessMethod == NULL)
 				elem->accessMethod = parentaccessmethod ? pstrdup(parentaccessmethod) : NULL;
+
+			/* if no options are specified AND child has same access method as parent, use parent options */
+			if (elem->options == NIL &&
+				(!elem->accessMethod ||
+				(parentaccessmethod && strcmp(elem->accessMethod, parentaccessmethod) == 0) ||
+				(!parentaccessmethod && strcmp(elem->accessMethod, default_table_access_method) == 0)))
+				elem->options = parentoptions ? copyObject(parentoptions) : NIL;
 
 			if (elem->accessMethod && strcmp(elem->accessMethod, "ao_column") == 0)
 				elem->colencs = merge_partition_encoding(pstate, elem->colencs, penc_cls);
