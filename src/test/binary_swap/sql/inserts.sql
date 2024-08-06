1ce51c5ea15e23c7bccafebd55968a0982d17e22
Xue-Bin Cong <146326625+congxuebin@users.noreply.github.com>
2024-08-06 23:09:48 +0800

    Adjust test cases for the cloudberrydb binary swap test (#537)
    Change Summary:
    
    **Change 1: introduction of the "system_group" in Binary Swap test
    Previously, cloudberrydb introduced a new control group named
    "system_group" to manage the resource usage of the postmaster
    process and all other auxiliary processes.
    
    ```
    select rsgname, parent from pg_resgroup
      where rsgname not like 'rg_dump_test%'
      order by oid;
        rsgname    | parent
    ---------------+--------
     default_group |      0
     admin_group   |      0
     system_group  |      0
    (3 rows)
    ```
    
    The "system_group" were added to the following three resource group test cases:
    resgroup_current_1_queue
    resgroup_other_2_queue
    resgroup_current_3_queue
    
    **Change 2: addressing missing DDL for "insert" test case under Binary Swap
    test suite
    
    ```
    create table if not exists test_table_heap_with_toast (id int, col1 int, col2 text) with (appendonly=false);
    create table if not exists test_table_ao_with_toast (id int, col1 int, col2 text) with (appendonly=true, orientation=row);
    create table if not exists test_table_co_with_toast (id int, col1 int, col2 text) with (appendonly=true, orientation=column);
    ```
    
    **Change 3: refinement of Binary Swap test suite scheduling**
    
    The "insert" and second occurrence of "pg_dumpall_current" were deemed
    unnecessary in the "schedule3" because no subsequent pg_dump comparison.
    Thus the redundant "pg_dumpall_current" has been removed and test case "insert"
    has been moved to "schedule1"

--- src/test/binary_swap/sql/inserts.sql
+++ src/test/binary_swap/sql/inserts.sql
@@ -2,6 +2,12 @@
 
 -- Insert into tables generated from src/test/regress
 -- Following tables from freeze_aux_tables:
+
+-- start_ignore
+create table if not exists test_table_heap_with_toast (id int, col1 int, col2 text) with (appendonly=false);
+create table if not exists test_table_ao_with_toast (id int, col1 int, col2 text) with (appendonly=true, orientation=row);
+create table if not exists test_table_co_with_toast (id int, col1 int, col2 text) with (appendonly=true, orientation=column);
+-- end_ignore
 INSERT INTO test_table_heap_with_toast SELECT i, i*2, i*5 FROM generate_series(1, 20)i;
 INSERT INTO test_table_ao_with_toast SELECT * FROM test_table_heap_with_toast;
 INSERT INTO test_table_co_with_toast SELECT * FROM test_table_heap_with_toast;
