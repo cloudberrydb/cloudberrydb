b72c1f7a4e444e652ae528c2f818d5696866dc8c
yang jianghua <yjhjstz@gmail.com>
2024-08-21 16:13:56 +0800

    Enable fsync=on by default in gpAux/gpdemo/gpdemo-defaults.sh
    Previously, fsync was set to off by default unless NUM_PRIMARY_MIRROR_PAIRS was zero or fsync was explicitly set to on.
    This change ensures that fsync is always on, improving data durability and consistency by default in the demo setup.

--- gpAux/gpdemo/gpdemo-defaults.sh
+++ gpAux/gpdemo/gpdemo-defaults.sh
@@ -16,7 +16,7 @@ if [ "${NUM_PRIMARY_MIRROR_PAIRS}" -eq "0" ]; then
   BLDWRAP_POSTGRES_CONF_ADDONS="fsync=on ${BLDWRAP_POSTGRES_CONF_ADDONS}"  # always enable fsync if singlenode
 fi
 if [[ ! ${BLDWRAP_POSTGRES_CONF_ADDONS} == *"fsync=on"* ]]; then
-  BLDWRAP_POSTGRES_CONF_ADDONS="fsync=off ${BLDWRAP_POSTGRES_CONF_ADDONS}" # only append fsync=off if it doesn't contains fsync=on
+  BLDWRAP_POSTGRES_CONF_ADDONS="fsync=on ${BLDWRAP_POSTGRES_CONF_ADDONS}" # always fsync=on
 else
   BLDWRAP_POSTGRES_CONF_ADDONS="${BLDWRAP_POSTGRES_CONF_ADDONS}"
 fi
