8b720e4bc5468f43d2dad8f0987aba5051864f72
Xiaoran Wang <fanfuxiaoran@gmail.com>
2024-09-18 15:07:31 +0800

    Fix ExplainNode forgetting to set sname for node 'T_TupleSplit' (#629)
    When node is 'T_TupleSplit' , the sname for it forgotten to set, then the explain command will fail.
    
    (cherry picked from commit c335f01b8d197161a33007adda92cc9c25e286c9)

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -1670,7 +1670,7 @@ ExplainNode(PlanState *planstate, List *ancestors,
 			pname = sname = "Sort";
 			break;
 		case T_TupleSplit:
-			pname = "TupleSplit";
+			pname = sname = "TupleSplit";
 			break;
 		case T_IncrementalSort:
 			pname = sname = "Incremental Sort";
