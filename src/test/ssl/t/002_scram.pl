27764957f9e4748ffe5b18917a4824304547e76f
zhangwenchao <656540940@qq.com>
2024-09-23 15:40:53 +0800

    Disable ssl tap test default.
    As we discover there are some errors in tap tests with some
    openssl version, we decide to disable the ssl tap test default
    temporariry which will be fixed in the future.
    
    Authored-by: Zhang Wenchao zwcpostgres@gmail.com

--- src/test/ssl/t/002_scram.pl
+++ src/test/ssl/t/002_scram.pl
@@ -20,6 +20,11 @@ if ($ENV{with_ssl} ne 'openssl')
 {
 	plan skip_all => 'OpenSSL not supported by this build';
 }
+elsif (!$ENV{PG_TEST_EXTRA} || $ENV{PG_TEST_EXTRA} !~ /\bssl\b/)
+{
+	plan skip_all =>
+	  'Potentially unsafe test SSL not enabled in PG_TEST_EXTRA';
+}
 
 # This is the hostname used to connect to the server.
 my $SERVERHOSTADDR = '127.0.0.1';
