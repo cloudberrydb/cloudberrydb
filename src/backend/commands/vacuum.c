greenplum-db/gpdb

Bring back pg_appendonly to relcache
We used to have the pg_appendonly row for the AO/CO table stored
in its relcache entry, but that somehow got lost during the PG12
merge (it is still in 6X). Now bringing it back, and utilize it
wherever possible.

This commit basically a partial cherrypick of ef92bcf151f99c9152a10c8750eee0d4dde39bc1.
But due to a lot of changes in the code base and also we don't
need other changes in that commit, we are not cherrypicking it.

src/backend/commands/vacuum.c
@@ -1031,9 +1031,10 @@ expand_vacuum_rel(VacuumRelation *vrel, int options)
 				classForm = (Form_pg_class) GETSTRUCT(tuple);
 				if (IsAccessMethodAO(classForm->relam))
 				{
+					Relation aorel = table_open(classForm->oid, AccessShareLock);
 					oldcontext = MemoryContextSwitchTo(vac_context);
 
-					GetAppendOnlyEntryAuxOids(classForm->oid, NULL,
+					GetAppendOnlyEntryAuxOids(aorel,
 											  &aoseg_relid,
 											  &aoblkdir_relid, NULL,
 											  &aovisimap_relid, NULL);
@@ -1047,6 +1048,7 @@ expand_vacuum_rel(VacuumRelation *vrel, int options)
 						ao_aux_vacrels = lappend(ao_aux_vacrels, makeVacuumRelation(NULL, aovisimap_relid, part_vrel->va_cols));
 
 					MemoryContextSwitchTo(oldcontext);
+					table_close(aorel, AccessShareLock);
 				}
 				else
 				{
@@ -2300,7 +2302,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 		 * AO_AUX_ONLY option is specified
 		 */
 		Assert(!(params->options & VACOPT_AO_AUX_ONLY));
-		GetAppendOnlyEntryAuxOids(RelationGetRelid(onerel), NULL,
+		GetAppendOnlyEntryAuxOids(onerel,
 								  &aoseg_relid,
 								  &aoblkdir_relid, NULL,
 								  &aovisimap_relid, NULL);

cloudberrydb/cloudberrydb

src/backend/commands/vacuum.c
@@ -1138,9 +1138,10 @@ expand_vacuum_rel(VacuumRelation *vrel, int options)
 				classForm = (Form_pg_class) GETSTRUCT(tuple);
 				if (IsAccessMethodAO(classForm->relam))
 				{
+					Relation aorel = table_open(classForm->oid, AccessShareLock);
 					oldcontext = MemoryContextSwitchTo(vac_context);
 
-					GetAppendOnlyEntryAuxOids(classForm->oid, NULL,
+					GetAppendOnlyEntryAuxOids(aorel,
 											  &aoseg_relid,
 											  &aoblkdir_relid, NULL,
 											  &aovisimap_relid, NULL);
@@ -1154,6 +1155,7 @@ expand_vacuum_rel(VacuumRelation *vrel, int options)
 						ao_aux_vacrels = lappend(ao_aux_vacrels, makeVacuumRelation(NULL, aovisimap_relid, part_vrel->va_cols));
 
 					MemoryContextSwitchTo(oldcontext);
+					table_close(aorel, AccessShareLock);
 				}
 				else
 				{
@@ -2510,7 +2512,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 		 * AO_AUX_ONLY option is specified
 		 */
 		Assert(!(params->options & VACOPT_AO_AUX_ONLY));
-		GetAppendOnlyEntryAuxOids(RelationGetRelid(rel), NULL,
+		GetAppendOnlyEntryAuxOids(rel,
 								  &aoseg_relid,
 								  &aoblkdir_relid, NULL,
 								  &aovisimap_relid, NULL);