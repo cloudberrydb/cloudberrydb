c0448fe0b44e26db2ede9b74dae1691168172213
Vladimir Rachkin <vova@kpnn.ru>
2024-10-24 16:37:20 +0300

    Add min to CdbExplain_DepStatAcc

--- src/backend/commands/explain_gp.c
+++ src/backend/commands/explain_gp.c
@@ -941,7 +941,7 @@ cdbexplain_collectStatsFromNode(PlanState *planstate, CdbExplain_SendStatCtx *ct
  */
 typedef struct CdbExplain_DepStatAcc
 {
-	/* vmax, vsum, vcnt, segmax */
+	/* vmax, vmin, vsum, vcnt, segmax, segmin */
 	CdbExplain_Agg agg;
 	/* max's received StatHdr */
 	CdbExplain_StatHdr *rshmax;
@@ -949,6 +949,12 @@ typedef struct CdbExplain_DepStatAcc
 	CdbExplain_StatInst *rsimax;
 	/* max's inst in NodeSummary */
 	CdbExplain_StatInst *nsimax;
+	/* min's received StatHdr */
+	CdbExplain_StatHdr *rshmin;
+	/* min's received inst in StatHdr */
+	CdbExplain_StatInst *rsimin;
+	/* min's inst in NodeSummary */
+	CdbExplain_StatInst *nsimin;
 	/* max run-time of all the segments */
 	double		max_total;
 	/* start time of the first iteration for node with maximum runtime */
@@ -962,6 +968,9 @@ cdbexplain_depStatAcc_init0(CdbExplain_DepStatAcc *acc)
 	acc->rshmax = NULL;
 	acc->rsimax = NULL;
 	acc->nsimax = NULL;
+	acc->rshmin = NULL;
+	acc->rsimin = NULL;
+	acc->nsimin = NULL;
 	acc->max_total = 0;
 	INSTR_TIME_SET_ZERO(acc->firststart_of_max_total);
 }								/* cdbexplain_depStatAcc_init0 */
@@ -978,6 +987,11 @@ cdbexplain_depStatAcc_upd(CdbExplain_DepStatAcc *acc,
 		acc->rshmax = rsh;
 		acc->rsimax = rsi;
 		acc->nsimax = nsi;
+	}
+	if (rsh->segindex == acc->agg.imin) {
+		acc->rshmin = rsh;
+		acc->rsimin = rsi;
+		acc->nsimin = nsi;
 	}
 	if (acc->max_total < nsi->total)
 	{
